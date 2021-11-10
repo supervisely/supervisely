4aa0e08f3f85360a1b0b0dfa98a17b9c1b127863
Max Kolomeychenko <maxim.kolomeychenko@gmail.com>
2021-11-10 14:24:32 +0200

    6.2.0 - annotation format for point cloud episodes

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(fname):
 # already have PyTorch installed.
 setup(
     name="supervisely",
-    version="6.1.104",
+    version="6.2.0",
     packages=find_packages(include=['supervisely_lib', 'supervisely_lib.*']),
     description="Supervisely Python SDK.",
     long_description=read("README.md"),
