4dc8100b70460a65a52fbaeea26ca2f872eca9df
kashkarov <kashkarov@dedicorp.ru>
2021-11-11 17:29:38 +0300

    #457 Fill frame param in pointcloud info

--- supervisely_lib/api/pointcloud/pointcloud_episode_api.py
+++ supervisely_lib/api/pointcloud/pointcloud_episode_api.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+from supervisely_lib.api.module_api import ApiField
 from supervisely_lib.api.pointcloud.pointcloud_api import PointcloudApi
 from supervisely_lib.api.pointcloud.pointcloud_episode_annotation_api import PointcloudEpisodeAnnotationAPI
 
@@ -8,19 +9,15 @@ class PointcloudEpisodeApi(PointcloudApi):
         super().__init__(api)
         self.annotation = PointcloudEpisodeAnnotationAPI(api)
 
+    def _convert_json_info(self, info: dict, skip_missing=True):
+        res = super()._convert_json_info(info, skip_missing=skip_missing)
+        if res.meta is not None:
+            return res._replace(frame=res.meta[ApiField.FRAME])
+        else:
+            raise RuntimeError('Error with point cloud meta or API version. Please, contact support')
+
     def get_frame_name_map(self, dataset_id):
         pointclouds = self.get_list(dataset_id)
-        try:
-            frame_name_map = {x.meta['frame']: x.name for x in pointclouds}
-            test_value = list(frame_name_map.keys())[0]
-            if test_value is not None and type(test_value) is int:
-                return frame_name_map
-            else:
-                raise ValueError(f'Incorrect value of meta. Got: {type(test_value)}:{test_value}, Expected: int')
-        except AttributeError:
-            raise AttributeError("Pointcloud has no meta field")
-        except TypeError:
-            raise TypeError('Meta is empty. Please upload pointclouds with Meta params')
-        except KeyError:
-            raise KeyError('No frame provided in meta. Please upload point cloud with passing "frame" to meta')
+        return {x.frame: x.name for x in pointclouds}
+
 
