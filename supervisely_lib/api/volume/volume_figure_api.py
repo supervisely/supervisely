a9f0d4395670bd98216a1dd26bc6f50f4332f54a
Borislav <borislav.kashkarov@gmail.com>
2021-11-18 12:57:03 +0300

    update upload meta

--- supervisely_lib/api/volume/volume_figure_api.py
+++ supervisely_lib/api/volume/volume_figure_api.py
@@ -1,18 +1,13 @@
 # coding: utf-8
 
 from supervisely_lib.api.module_api import ApiField
-from supervisely_lib.video_annotation.key_id_map import KeyIdMap
-from supervisely_lib.api.entity_annotation.figure_api import FigureApi
+from supervisely_lib.api.video.video_figure_api import VideoFigureApi
+import supervisely_lib.volume_annotation.constants as const
 
-
-class VolumeFigureApi(FigureApi):
-    def append_bulk(self, volume_id, figures, key_id_map: KeyIdMap):
-        keys = []
-        figures_json = []
-        for figure in figures:
-            keys.append(figure.key())
-            fig_json = figure.to_json(key_id_map)
-            fig_json[ApiField.META] = figure.meta
-            figures_json.append(fig_json)
-
-        self._append_bulk(volume_id, figures_json, keys, key_id_map)
+class VolumeFigureApi(VideoFigureApi):
+    def create(self, volume_id, object_id, slice_index, normal, geometry_json, geometry_type):
+        return super().create(volume_id,
+                              object_id,
+                              {ApiField.META: {const.INDEX: slice_index, const.NORMAL: normal}},
+                              geometry_json,
+                              geometry_type)
