fa4c7f184a4a0aff1caf832cf8fdfda98f7ab0f8
Borislav <borislav.kashkarov@gmail.com>
2021-11-18 16:27:41 +0300

    as video

--- supervisely_lib/api/volume/volume_annotation_api.py
+++ supervisely_lib/api/volume/volume_annotation_api.py
@@ -1,10 +1,11 @@
 # coding: utf-8
 
+import supervisely_lib.volume_annotation.constants as const
 from supervisely_lib.api.module_api import ApiField
 from supervisely_lib.video_annotation.key_id_map import KeyIdMap
 from supervisely_lib.volume_annotation.volume_annotation import VolumeAnnotation
 from supervisely_lib.api.entity_annotation.entity_annotation_api import EntityAnnotationAPI
-
+from supervisely_lib.io.json import dump_json_file
 
 class VolumeAnnotationApi(EntityAnnotationAPI):
     _method_download_bulk = 'volumes.annotations.bulk.info'
@@ -20,7 +21,14 @@ class VolumeAnnotationApi(EntityAnnotationAPI):
         return self.download_bulk(dataset_id, [entity_id]).pop()
 
     def append(self, volume_id, ann: VolumeAnnotation, key_id_map: KeyIdMap = None):
+        if key_id_map is None:
+            key_id_map = KeyIdMap()
+
         info = self._api.volume.get_info_by_id(volume_id)
-        self._append(self._api.volume.tag, self._api.volume.object, self._api.volume.figure,
-                     info.project_id, info.dataset_id, volume_id,
-                     ann.tags, ann.objects, ann.figures, key_id_map)
+
+        self._api.volume.tag.append_to_entity(volume_id, info.project_id, ann.tags, key_id_map)
+        self._api.volume.object.append_bulk(volume_id, ann.objects, key_id_map)
+
+        self._api.volume.figure.append_bulk(volume_id, ann.axial.figures, const.AXIAL, key_id_map)
+        self._api.volume.figure.append_bulk(volume_id, ann.coronal.figures, const.CORONAL, key_id_map)
+        self._api.volume.figure.append_bulk(volume_id, ann.sagittal.figures, const.SAGITTAL, key_id_map)
--- supervisely_lib/api/volume/volume_figure_api.py
+++ supervisely_lib/api/volume/volume_figure_api.py
@@ -1,8 +1,9 @@
 # coding: utf-8
-
+from supervisely_lib.video_annotation.key_id_map import KeyIdMap
 from supervisely_lib.api.module_api import ApiField
 from supervisely_lib.api.video.video_figure_api import VideoFigureApi
 import supervisely_lib.volume_annotation.constants as const
+
 
 class VolumeFigureApi(VideoFigureApi):
     def create(self, volume_id, object_id, slice_index, normal, geometry_json, geometry_type):
@@ -11,3 +12,17 @@ class VolumeFigureApi(VideoFigureApi):
                               {ApiField.META: {const.INDEX: slice_index, const.NORMAL: normal}},
                               geometry_json,
                               geometry_type)
+
+    def append_bulk(self, volume_id, figures, plane_name, key_id_map: KeyIdMap):
+        keys = []
+        figures_json = []
+        normal = const.PLANE_NORMALS[plane_name]
+        for figure in figures:
+            keys.append(figure.key())
+            fig_json = figure.to_json(key_id_map, save_meta=True)
+
+            slice_index = fig_json[ApiField.META][ApiField.FRAME]
+            fig_json[ApiField.META] = {const.INDEX: slice_index, const.NORMAL: normal}
+            figures_json.append(fig_json)
+
+        self._append_bulk(volume_id, figures_json, keys, key_id_map)
