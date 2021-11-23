bb8265a761ce85a4bec911e9915dcbe332806e7f
Max Kolomeychenko <maxim.kolomeychenko@gmail.com>
2021-11-23 14:30:15 +0200

    remove copypaste for volume object

--- supervisely_lib/volume_annotation/volume_object.py
+++ supervisely_lib/volume_annotation/volume_object.py
@@ -10,40 +10,4 @@ from supervisely_lib.geometry.constants import LABELER_LOGIN, UPDATED_AT, CREATE
 
 
 class VolumeObject(VideoObject):
-    @classmethod
-    def from_json(cls, data, project_meta: ProjectMeta, key_id_map: KeyIdMap = None):
-        obj_class_name = data[LabelJsonFields.OBJ_CLASS_NAME]
-        obj_class = project_meta.get_obj_class(obj_class_name)
-        if obj_class is None:
-            raise RuntimeError(f'Failed to deserialize a object from JSON: class name {obj_class_name!r} '
-                               f'was not found in the given project meta.')
-
-        object_id = data.get(ID, None)
-
-        existing_key = None
-        if object_id is not None and key_id_map is not None:
-            existing_key = key_id_map.get_object_key(object_id)
-        json_key = uuid.UUID(data[KEY]) if KEY in data else None
-        if (existing_key is not None) and (json_key is not None) and (existing_key != json_key):
-            raise RuntimeError("Object id = {!r}: existing_key {!r} != json_key {!r}"
-                               .format(object_id, existing_key, json_key))
-
-        if existing_key is not None:
-            key = existing_key
-        elif json_key is not None:
-            key = json_key
-        else:
-            key = uuid.uuid4()
-
-        if key_id_map is not None and existing_key is None:
-            key_id_map.add_object(key, object_id)
-
-        class_id = data.get(CLASS_ID, None)
-        labeler_login = data.get(LABELER_LOGIN, None)
-        updated_at = data.get(UPDATED_AT, None)
-        created_at = data.get(CREATED_AT, None)
-
-        return cls(obj_class=obj_class,
-                   key=key,
-                   tags=VideoTagCollection.from_json(data[LabelJsonFields.TAGS], project_meta.tag_metas),
-                   class_id=class_id, labeler_login=labeler_login, updated_at=updated_at, created_at=created_at)
+    pass
