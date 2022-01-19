79fa455da99671145f73872e13976e5045bfdd48
cxnt <pavelbartsits@gmail.com>
2022-01-19 17:46:58 +0300

    material theme configuration

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,17 +14,20 @@
 import os
 import sys
 
-#sys.path.insert(0, os.path.abspath('./repo'))
-sys.path.insert(0, os.path.abspath('../../supervisely_lib'))
-#sys.path.insert(0, '/app')
+PATH_HERE = os.path.abspath(os.path.dirname(__file__))
+PATH_ROOT = os.path.join(PATH_HERE, "..", "..")
+sys.path.insert(0, os.path.abspath(PATH_ROOT))
+# sys.path.insert(0, os.path.abspath('./repo'))
+# sys.path.insert(0, os.path.abspath('../../supervisely_lib'))
+# sys.path.insert(0, '/app')
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'Supervisely SDK for Python'
+# project = 'Supervisely SDK for Python'
+project = 'Supervisely'
 copyright = '2021, Supervisely Team'
 author = 'Supervisely Team'
-
 
 # -- General configuration ---------------------------------------------------
 
@@ -32,9 +35,12 @@ author = 'Supervisely Team'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.viewcode',
+    'sphinx.ext.intersphinx',
     'sphinx_copybutton',
     'm2r2',
     'nbsphinx'
@@ -51,19 +57,66 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '**.ipynb_checkpoints']
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = '_static/images/logo-white-custom-row.png'
+html_logo = '_static/images/sly-top-logo.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-#html_favicon = '_static/favicon.ico'
+html_favicon = '_static/images/favicon.png'  # need fix later
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#html_theme = 'sphinx_rtd_theme'
 html_theme = 'sphinx_material'
+html_theme_options = {
+
+    # Set the name of the project to appear in the navigation.
+    'nav_title': 'Supervisely',
+
+    # Specify a base_url used to generate sitemap.xml. If not
+    # specified, then no sitemap will be built.
+    # 'base_url': 'https://github.com/supervisely/supervisely',
+
+    # Set the color and the accent color
+    'theme_color': 'ffffff',
+    'color_primary': 'blue',
+    'color_accent': 'light-blue',
+
+    # Set the repo location to get a badge with stats
+    'repo_url': 'https://github.com/supervisely/supervisely',
+    'repo_name': 'Supervisely',
+
+    # Visible levels of the global TOC; -1 means unlimited
+    'globaltoc_depth': 3,
+    # If False, expand all TOC entries
+    'globaltoc_collapse': True,
+    # If True, show hidden TOC entries
+    'globaltoc_includehidden': True,
+
+    'html_prettify': False,
+    'html_minify': True,
+    'css_minify': True,
+
+    "nav_links": [
+        {"href": "index", "internal": True, "title": "Material"},
+        {
+            "href": "https://supervise.ly/",
+            "internal": False,
+            "title": "Supervisely Platform",
+        },
+    ],
+
+    "version_dropdown": True,
+    "version_json": "_static/versions.json",
+    "version_info": {
+        "Release": "https://bashtage.github.io/sphinx-material/",
+        "Development": "https://bashtage.github.io/sphinx-material/devel/",
+        "Release (rel)": "/sphinx-material/",
+        "Development (rel)": "/sphinx-material/devel/",
+    },
+    "table_classes": ["plain"],
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
