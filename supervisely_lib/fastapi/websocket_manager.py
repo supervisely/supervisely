73623b709db0307490e12a59322c757376184ee5
Max Kolomeychenko <maxim.kolomeychenko@gmail.com>
2022-01-24 09:38:25 +0200

    WebsocketManager broadcast

---
+++ supervisely_lib/fastapi/websocket_manager.py
@@ -0,0 +1,24 @@
+# https://fastapi.tiangolo.com/advanced/websockets/#handling-disconnections-and-multiple-clients
+# https://github.com/tiangolo/fastapi/issues/2639
+from typing import List
+from fastapi import WebSocket
+
+
+class WebsocketManager:
+    def __init__(self):
+        self.active_connections: List[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, state: dict): 
+        for connection in self.active_connections:
+            await connection.send_json(state)
+
