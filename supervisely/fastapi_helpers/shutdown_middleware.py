d5047314cdf5700b26c4d969cfeed875c7d2323d
Max Kolomeychenko <maxim.kolomeychenko@gmail.com>
2022-01-25 15:34:43 +0200

    shutdown middleware done

--- supervisely/fastapi_helpers/shutdown_middleware.py
+++ supervisely/fastapi_helpers/shutdown_middleware.py
@@ -1,27 +1,34 @@
 import os
 import signal
 import psutil
-import time
 from starlette.types import ASGIApp, Receive, Scope, Send
-from fastapi import Response, Request
+from fastapi import Response
 
 
 class ShutdownMiddleware:
     def __init__(
-        self, app: ASGIApp
+        self, 
+        app: ASGIApp,
+        path: str = '/sly-app-shutdown',
     ) -> None:
         self.app = app
+        self.path = path
 
     async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
         if scope["type"] == "http":
-            if hasattr(scope["app"].state, 'STOPPED') and scope["app"].state.STOPPED:
+            if scope["path"] == self.path and not hasattr(scope["app"].state, 'STOPPED'):
+                scope["app"].state.STOPPED = True
+                await Response()(scope, receive, send) 
+                return await _shutdown_fastapi()
+            elif hasattr(scope["app"].state, 'STOPPED') and scope["app"].state.STOPPED:
                 response = Response(content="Server is being shut down", status_code=403)
                 return await response(scope, receive, send) 
         await self.app(scope, receive, send)
 
 
-def shutdown_fastapi(request: Request):
-    request.app.state.STOPPED = True
-    # time.sleep(10)
+async def _shutdown_fastapi():
+    # for debug
+    # import asyncio
+    # await asyncio.sleep(10) 
     current_process = psutil.Process(os.getpid())
     current_process.send_signal(signal.SIGINT) # emit ctrl + c
