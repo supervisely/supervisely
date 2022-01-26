1677c99581940195534824088971fe2df69af7c4
Max Kolomeychenko <maxim.kolomeychenko@gmail.com>
2022-01-26 15:17:09 +0200

    ShutdownMiddleware

--- supervisely/fastapi_helpers/shutdown_middleware.py
+++ supervisely/fastapi_helpers/shutdown_middleware.py
@@ -1,9 +1,10 @@
 import os
-import types
 import signal
 import psutil
+
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 from starlette.types import ASGIApp, Receive, Scope, Send
-from fastapi import Response
 
 
 class ShutdownMiddleware:
@@ -14,27 +15,20 @@ class ShutdownMiddleware:
     ) -> None:
         self.app = app
         self.path = path
-        self._server = app.app.dependency_overrides_provider
-        self._server.shutdown = types.MethodType( graceful_shutdown, self._server )
-        # https://stackoverflow.com/questions/972/adding-a-method-to-an-existing-object-instance
-        # setattr(FastAPI, 'shutdown', graceful_shutdown)
 
     async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
         if scope["type"] != "http":
             return await self.app(scope, receive, send)
         if scope["path"] == self.path and not hasattr(scope["app"].state, 'STOPPED'):
-            scope["app"].state.STOPPED = True
-            await Response()(scope, receive, send) 
-            return self.app.graceful_shutdown()
+            await JSONResponse(content="app will be shutdown")(scope, receive, send) 
+            return shutdown(scope["app"])
         elif hasattr(scope["app"].state, 'STOPPED') and scope["app"].state.STOPPED:
-            response = Response(content="Server is being shut down", status_code=403)
+            response = JSONResponse(content="Server is being shut down", status_code=403)
             return await response(scope, receive, send) 
         await self.app(scope, receive, send)
 
 
-def graceful_shutdown(self):
-    # for debug
-    # import asyncio
-    # await asyncio.sleep(10)
+def shutdown(app: FastAPI):
+    setattr(app.state, "STOPPED", True)
     current_process = psutil.Process(os.getpid())
     current_process.send_signal(signal.SIGINT) # emit ctrl + c
--- supervisely/fastapi_helpers/state_middleware.py
+++ supervisely/fastapi_helpers/state_middleware.py
@@ -8,23 +8,23 @@ class StateMiddleware:
     def __init__(
         self, 
         app: ASGIApp,
-        state: dict,
         path = "/sly-app-state"
     ) -> None:
         self.app = app
-        self.state = state
         self.path = path
+        
 
     async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
-        if scope["type"] == "http": 
-            if scope["path"] == self.path:
-                response = JSONResponse(content=self.state)
-                return await response(scope, receive, send) 
-            else:
-                request = Request(scope, receive, send)
-                #TODO: if json body
-                request_json = await request.json()
-                if "state" in request_json:
-                    self.state.clear()
-                    self.state.update(request_json["state"])
-        await self.app(scope, receive, send)
+        if scope["type"] != "http":
+            return await self.app(scope, receive, send)
+        if scope["path"] == self.path:
+            response = JSONResponse(content=self.state)
+            return await response(scope, receive, send) 
+        else:
+            request = Request(scope, receive, send)
+            #TODO: if json body
+            request_json = await request.json()
+            if "state" in request_json:
+                self.state.clear()
+                self.state.update(request_json["state"])
+            return await self.app(scope, receive, send)
