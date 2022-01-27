486111f5bfa050166296dd0ed44f01e278bf7602
Max Kolomeychenko <maxim.kolomeychenko@gmail.com>
2022-01-27 17:04:23 +0200

    app_content done, not tested

--- supervisely/fastapi_helpers/app_content.py
+++ supervisely/fastapi_helpers/app_content.py
@@ -11,17 +11,19 @@ from supervisely.fastapi_helpers.websocket import WebsocketManager
 class Field(str, enum.Enum):
     STATE = 'state'
     DATA = 'data'
+    CONTEXT = 'context'
 
 
 class PatchableJson(dict):
     _app = None
-
+    _field = None
+    
     def __init__(self, app: FastAPI, field: Field, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        if self._app is None:
-            self._app = app
+        if PatchableJson._app is None:
+            PatchableJson._app = app
+        PatchableJson._field = field
         self._ws = WebsocketManager(app)
-        self._field = field
         self._last = dict(self)
         self._lock = asyncio.Lock()
 
@@ -38,6 +40,12 @@ class PatchableJson(dict):
         await self._apply_patch(patch)
         await self._ws.broadcast({self._field: patch})
 
+    @classmethod
+    async def from_request(cls, request: Request):
+        content = await request.json()
+        d = content.get(cls._field, {})
+        return cls(cls._app, d)
+
 
 class LastStateJson(PatchableJson, metaclass=Singleton):
     def __init__(self, app: FastAPI, *args, **kwargs):
@@ -52,16 +60,23 @@ class StateJson(PatchableJson):
         super()._apply_patch(patch)
         LastStateJson()._apply_patch(patch)
 
+
+class DataJson(PatchableJson, metaclass=Singleton):
+    def __init__(self, app: FastAPI, *args, **kwargs):
+        super().__init__(app, Field.DATA, *args, **kwargs)
+    
+    @classmethod
+    async def from_request(cls, request: Request):
+        raise RuntimeError(f"""Request from Supervisely App never contains \"{cls._field}\" field. Every request from app contains by default current state and context""")
+
+
+class ContextJson(PatchableJson):
+    def __init__(self, app: FastAPI, *args, **kwargs):
+        super().__init__(app, Field.CONTEXT, *args, **kwargs)
+
     @classmethod
     async def from_request(cls, request: Request):
         content = await request.json()
         state = content.get(Field.STATE, {})
         return cls(cls._app, state)
-
-
-class DataJson(PatchableJson, metaclass=Singleton):
-    def __init__(self, app: FastAPI, *args, **kwargs):
-        super().__init__(app, Field.DATA, *args, **kwargs)
-
-
 
