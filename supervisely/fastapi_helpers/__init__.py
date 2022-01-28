d7098396ef89bda0853f3568348d79d88bb24332
Max Kolomeychenko <maxim.kolomeychenko@gmail.com>
2022-01-28 15:50:20 +0300

    socket middleware - done

--- supervisely/fastapi_helpers/__init__.py
+++ supervisely/fastapi_helpers/__init__.py
@@ -1,4 +1,5 @@
 from supervisely.fastapi_helpers.websocket import WebsocketManager
+from supervisely.fastapi_helpers.websocket_middleware import WebsocketMiddleware
 from supervisely.fastapi_helpers.shutdown_middleware import ShutdownMiddleware, shutdown
 from supervisely.fastapi_helpers.state_middleware import StateMiddleware
 from supervisely.fastapi_helpers.data_middleware import DataMiddleware
--- supervisely/fastapi_helpers/websocket.py
+++ supervisely/fastapi_helpers/websocket.py
@@ -1,15 +1,21 @@
 from typing import List
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from pydantic import NoneIsAllowedError
 from supervisely.fastapi_helpers.singleton import Singleton
 
 
 class WebsocketManager(metaclass=Singleton):
-    def __init__(self, app: FastAPI, path="/sly-app-ws"):
-        self.app = app
+    def __init__(self, path="/sly-app-ws"):
+        self.app = None
         self.path = path
         self.active_connections: List[WebSocket] = []
-        app.add_api_websocket_route(path=self.path, endpoint=self.endpoint)
-        
+    
+    def set_app(self, app: FastAPI):
+        if self.app is not None:
+            return
+        self.app = app
+        self.app.add_api_websocket_route(path=self.path, endpoint=self.endpoint)
+
     async def connect(self, websocket: WebSocket):
         await websocket.accept()
         self.active_connections.append(websocket)
@@ -18,6 +24,8 @@ class WebsocketManager(metaclass=Singleton):
         self.active_connections.remove(websocket)
 
     async def broadcast(self, d: dict): 
+        if self.app is None:
+            raise ValueError("WebSocket is not initialized, use Websocket middleware for that")
         for connection in self.active_connections:
             await connection.send_json(d)
 
