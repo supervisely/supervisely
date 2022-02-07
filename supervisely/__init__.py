e0d6c22082c76aa8f8ada6497f5f35836b0073b0
Max Kolomeychenko <maxim.kolomeychenko@gmail.com>
2022-02-07 13:44:09 +0200

    move app-engine v1

--- supervisely/__init__.py
+++ supervisely/__init__.py
@@ -97,9 +97,9 @@ from supervisely.project.pointcloud_episode_project import PointcloudEpisodeData
 
 from supervisely.pyscripts_utils import utils as ps
 from supervisely.io import docker_utils
-import supervisely.app as app
-from supervisely.app.app_service import AppService
-import supervisely.app.widgets
+import supervisely.app
+# from supervisely.app.v1.app_service import AppService
+# import supervisely.app.v
 
 from supervisely.decorators.profile import timeit
 from supervisely.decorators.profile import update_fields
