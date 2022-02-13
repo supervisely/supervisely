0b0861554351090cf3060539ea0204ff5d8c1be6
Daniel Yarockii <33942379+qanelph@users.noreply.github.com>
2022-02-13 11:53:36 +0300

    patchable-json-last-state-fix (#98)
    * patchable-json-last-state-fix
    
    * Update content.py
    
    * Update content.py

--- supervisely/app/content.py
+++ supervisely/app/content.py
@@ -1,3 +1,4 @@
+import copy
 import os
 import enum
 import json
@@ -37,7 +38,7 @@ class _PatchableJson(dict):
     def __init__(self, field: Field, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._ws = WebsocketManager()
-        self._last = dict(self)
+        self._last = copy.deepcopy(dict(self))
         self._lock = asyncio.Lock()
         self._field = field.value
 
@@ -53,7 +54,8 @@ class _PatchableJson(dict):
     async def _apply_patch(self, patch):
         async with self._lock:
             patch.apply(self._last, in_place=True)
-
+            self._last = copy.deepcopy(self._last)
+            
     async def synchronize_changes(self):
         patch = self._get_patch()
         await self._apply_patch(patch)
@@ -83,7 +85,7 @@ class LastStateJson(_PatchableJson, metaclass=Singleton):
             async with last_state._lock:
                 last_state.clear()
                 last_state.update(d)
-                last_state._last = dict(last_state)
+                last_state._last = copy.deepcopy(dict(last_state))
         return last_state
 
     async def replace(self, d: dict):
@@ -92,7 +94,7 @@ class LastStateJson(_PatchableJson, metaclass=Singleton):
             async with self._lock:
                 self.clear()
                 self.update(d)
-                self._last = dict(self)
+                self._last = copy.deepcopy(dict(self))
 
 
 class ContextJson(_PatchableJson):
