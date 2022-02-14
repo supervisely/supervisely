571783c2440b848c241a95cdfe5a6e27db002ed8
Max Kolomeychenko <maxim.kolomeychenko@gmail.com>
2022-02-14 11:35:26 +0200

    radio table done

--- supervisely/app/widgets/radio_table/radio_table.py
+++ supervisely/app/widgets/radio_table/radio_table.py
@@ -1,6 +1,7 @@
 from typing import List, Dict
 from pathlib import Path
 import os
+from black import format_cell
 import markupsafe
 from jinja2 import Environment
 from supervisely.app.jinja2 import create_env
@@ -12,22 +13,45 @@ from supervisely.app.jinja2 import create_env
 class RadioTable:
     def __init__(
         self,
-        state_field: str,
-        data_field: str,
-        radio_key: str,
+        widget_id: str,
         columns: List[str],
-        rows: List[Dict],
-        subtitles: dict = {},
+        rows: List[List[str]],
+        subtitles: dict = {},  # col_name -> subtitle
+        column_formatters: dict = {},
     ):
-        self.state_field = state_field
-        self.data_field = data_field
+        self.widget_id = widget_id
+        self.columns = columns
         self.rows = rows
-        self._columns = None
-        self._subtitles = subtitles
+        self.subtitles = subtitles
+        self.column_formatters = column_formatters
 
-        # for row in content:
-        #     for k in row.keys():
-        #         if
+        self._header = []
+        for col in self.columns:
+            self._header.append({"title": col, "subtitle": self.subtitles.get(col)})
+
+        self._frows = []
+        for idx, row in enumerate(self.rows):
+            if len(row) != len(self.columns):
+                raise ValueError(
+                    f"Row #{idx} length is {len(row)} != number of columns ({len(self.columns)})"
+                )
+            frow = []
+            for col, val in zip(self.columns, row):
+                frow.append(self.format_value(col, val))
+            self._frows.append(frow)
+
+    def format_value(self, column_name: str, value):
+        fn = self.column_formatters.get(column_name, self.default_formatter)
+        return fn(value)
+
+    def default_formatter(self, value):
+        if value is None:
+            return "-"
+        return value
+
+    def init(self, data: dict, state: dict):
+        data[self.widget_id] = {"header": self._header, "rows": self._frows}
+        state[self.widget_id] = {"selectedRow": 0}
 
     def to_html(self):
         current_dir = Path(__file__).parent.absolute()
