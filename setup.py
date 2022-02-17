3ad702993f61e7dbdb349ccb75808b8494a0b831
Max Kolomeychenko <maxim.kolomeychenko@gmail.com>
2022-02-17 11:02:01 +0200

    fix setup

--- setup.py
+++ setup.py
@@ -21,8 +21,8 @@ setup(
     long_description_content_type='text/markdown',
     url="https://github.com/supervisely/supervisely",
     package_data={
-        "supervisely.app.widgets": ["**.html", "**.css"], 
-        "supervisely.video": ["**.sh"]
+        "supervisely": ["app/widgets/*.html", "app/widgets/*.css"], 
+        "supervisely": ["video/*.sh"]
     },
     install_requires=[
         "numpy>=1.19",
