1118b20c08766689404fd481f824f45fe48cf0c8
Max Kolomeychenko <maxim.kolomeychenko@gmail.com>
2022-02-20 17:28:14 +0300

    done_label

--- supervisely/app/widgets/__init__.py
+++ supervisely/app/widgets/__init__.py
@@ -1,3 +1,4 @@
 from supervisely.app.widgets.widget import Widget
 from supervisely.app.widgets.radio_table.radio_table import RadioTable
 from supervisely.app.widgets.notification_box.notification_box import NotificationBox
+from supervisely.app.widgets.done_label.done_label import DoneLabel
