7439ed8aa091f4d786c1638dc19c423a387cd565
Max Kolomeychenko <maxim.kolomeychenko@gmail.com>
2022-02-21 15:35:46 +0300

    wip

--- supervisely/app/fastapi/subapp.py
+++ supervisely/app/fastapi/subapp.py
@@ -44,12 +44,14 @@ def create() -> FastAPI:
         server_address = os.environ.get(SERVER_ADDRESS)
         if server_address is not None:
             server_address = Api.normalize_server_address(server_address)
-            
-        response = JSONResponse(content={
-            TASK_ID: os.environ.get(TASK_ID),
-            SERVER_ADDRESS: server_address,
-            API_TOKEN: os.environ.get(API_TOKEN),            
-        })
+
+        response = JSONResponse(
+            content={
+                TASK_ID: os.environ.get(TASK_ID),
+                SERVER_ADDRESS: server_address,
+                API_TOKEN: os.environ.get(API_TOKEN)
+            }
+        )
         return response
 
     @app.post("/shutdown")
--- supervisely/app/widgets/restart_step/restart_step.py
+++ supervisely/app/widgets/restart_step/restart_step.py
@@ -12,23 +12,23 @@ class RestartStep(Widget):
         widget_id: str = None,
     ):
         self.steps = steps
-        self.router = APIRouter()
-        self.method_name = method_name
+        # self.router = APIRouter()
+        # self.method_name = method_name
         super().__init__(widget_id=widget_id, file_path=__file__)
 
-        @self.router.post(self.method_name)
-        async def restart(
-            request: Request, state: StateJson = Depends(StateJson.from_request)
-        ):
-            data = DataJson()
-            for restart_fn in self.steps:
-                restart_fn(data, state)
-            state["restart_step"] = None
-            await state.synchronize_changes()
-            await data.synchronize_changes()
+        # @self.router.post(self.method_name)
+        # async def restart(
+        #     request: Request, state: StateJson = Depends(StateJson.from_request)
+        # ):
+        #     data = DataJson()
+        #     for restart_fn in self.steps:
+        #         restart_fn(data, state)
+        #     state["restart_step"] = None
+        #     await state.synchronize_changes()
+        #     await data.synchronize_changes()
 
     def init_data(self):
         return {}  # {"steps": {name: endpoint for (name, endpoint) in self.steps}}
 
     def init_state(self):
-        return {"restart_step": None}
+        return {"step": None}
