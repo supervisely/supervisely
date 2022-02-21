ebe38ca590b40891eedaff76c6e52f9ae5d3ac62
Max Kolomeychenko <maxim.kolomeychenko@gmail.com>
2022-02-21 09:57:06 +0300

    wip

--- supervisely/app/widgets/__init__.py
+++ supervisely/app/widgets/__init__.py
@@ -2,3 +2,4 @@ from supervisely.app.widgets.widget import Widget
 from supervisely.app.widgets.radio_table.radio_table import RadioTable
 from supervisely.app.widgets.notification_box.notification_box import NotificationBox
 from supervisely.app.widgets.done_label.done_label import DoneLabel
+from supervisely.app.widgets.restart_dialog.restart_dialog import RestartDialog
--- supervisely/app/widgets/restart_dialog/restart_dialog.py
+++ supervisely/app/widgets/restart_dialog/restart_dialog.py
@@ -1,18 +1,30 @@
 from typing import List
+from fastapi import APIRouter, Request, Depends
 from supervisely.app.widgets import Widget
+from supervisely.app.content import StateJson
 
 
 class RestartDialog(Widget):
     def __init__(
         self,
-        steps: List[tuple(str, str)],
+        steps: List,  # (str, callable)
+        method_name: str = "/restart",
         widget_id: str = None,
     ):
         self.steps = steps
+        self.router = APIRouter()
+        self.method_name = method_name
         super().__init__(widget_id=widget_id, file_path=__file__)
 
+        @self.router.post(self.method_name)
+        def retart(
+            request: Request, state: StateJson = Depends(StateJson.from_request)
+        ):
+            print(123)
+            pass
+
     def init_data(self):
-        return {"steps": {name: endpoint for (name, endpoint) in self.steps}}
+        return {}  # {"steps": {name: endpoint for (name, endpoint) in self.steps}}
 
     def init_state(self):
         return {"restartName": None}
