873b61ec5f4fd74ed1ec1f839ac97f65e2863451
Pavel Bartsits <48913536+cxnt@users.noreply.github.com>
2022-02-26 15:25:02 +0300

    add new decorator crop_input_before_inference

---
+++ supervisely/decorators/inference.py
@@ -0,0 +1,40 @@
+import os
+import supervisely as sly
+
+
+def crop_input_before_inference(func):
+    def wrapper_inference(image_path, context, state, app_logger):
+        assert isinstance(image_path, str)
+        image = sly.image.read(image_path)
+        if "rectangle_crop" not in state.keys() or state["rectangle_crop"] is None:
+            ann_json = func(image_path, context, state, app_logger)
+            return ann_json
+
+        # crop image
+        image_base_dir = os.path.dirname(image_path)
+        image_name, image_ext = os.path.splitext(os.path.basename(image_path))
+
+        selected_figure_bbox = state["rectangle_crop"]
+        sly_rect = sly.Rectangle.from_json(selected_figure_bbox)
+
+        image_crop = sly.image.crop(image, sly_rect)
+        image_crop_path = os.path.join(image_base_dir, sly.rand_str(10) + "_" + image_name + "_crop" + image_ext)
+        sly.image.write(image_crop_path, image_crop)
+
+        # get inference from crop
+        ann_json = func(image_crop_path, context, state, app_logger)
+
+        # scale ann to original image
+        original_height, original_width = image.shape[:2]
+        ann_json["size"]["height"], ann_json["size"]["width"] = original_height, original_width
+
+        for object in ann_json["objects"]:
+            object_ext_points = object["points"]["exterior"]
+            object_ext_points[0][0] += sly_rect.left
+            object_ext_points[0][1] += sly_rect.top
+            object_ext_points[1][0] += sly_rect.left
+            object_ext_points[1][1] += sly_rect.top
+
+        sly.fs.silent_remove(image_crop_path)
+        return ann_json
+    return wrapper_inference
