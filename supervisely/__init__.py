5498c3d9868d41b20f0bc958efef7d2825e2518d
Max Kolomeychenko <maxim.kolomeychenko@gmail.com>
2022-03-11 12:39:45 +0000

    fix app importd

--- supervisely/__init__.py
+++ supervisely/__init__.py
@@ -98,7 +98,7 @@ from supervisely.project.pointcloud_episode_project import PointcloudEpisodeData
 from supervisely.pyscripts_utils import utils as ps
 from supervisely.io import docker_utils
 # import supervisely.app as app
-from supervisely.app.v1.app_service import AppService
+# from supervisely.app.v1.app_service import AppService
 
 from supervisely.decorators.profile import timeit
 from supervisely.decorators.profile import update_fields
