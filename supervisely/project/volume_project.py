e5b1126cf702273e30c095e762fcab732cb89a3d
Maxim Kolomeychenko <maxim@supervise.ly>
2022-05-02 14:23:46 +0300

    start download

--- supervisely/project/volume_project.py
+++ supervisely/project/volume_project.py
@@ -1,201 +1,163 @@
-# # coding: utf-8
+# coding: utf-8
 
-# from collections import namedtuple
-# import os
+from collections import namedtuple
+import os
 
-# from supervisely.io.fs import touch, mkdir
-# from supervisely.io.json import load_json_file
-# from supervisely.project.project_meta import ProjectMeta
-# from supervisely.task.progress import Progress
-# from supervisely._utils import batched
-# from supervisely.video_annotation.key_id_map import KeyIdMap
+from supervisely.io.fs import file_exists, touch
+from supervisely.io.json import dump_json_file, load_json_file
+from supervisely.project.project_meta import ProjectMeta
+from supervisely.task.progress import Progress
+from supervisely._utils import batched
+from supervisely.video_annotation.key_id_map import KeyIdMap
 
-# from supervisely.api.module_api import ApiField
-# from supervisely.collection.key_indexed_collection import KeyIndexedCollection
-# from supervisely.project.project import OpenMode
-# from supervisely.project.video_project import VideoProject, VideoDataset
-# from supervisely.project.project_type import ProjectType
+from supervisely.api.module_api import ApiField
+from supervisely.collection.key_indexed_collection import KeyIndexedCollection
+from supervisely.volume import volume as sly_volume
 
-# from supervisely.volume import volume
+from supervisely.project.project import Dataset, Project, OpenMode
+from supervisely.project.video_project import VideoDataset, VideoProject
+from supervisely.project.project import read_single_project as read_project_wrapper
+from supervisely.project.project_type import ProjectType
+from supervisely.volume_annotation.volume_annotation import VolumeAnnotation
 
-# # from sdk_part.volume import volume as sly_volume
-# # from sdk_part.volume_annotation.volume_annotation import VolumeAnnotation
-# # from sdk_part.volume_annotation.constants import VOLUME_NAME
-# # from sdk_part.volume.volume import validate_format
-
-# VolumeItemPaths = namedtuple("VolumeItemPaths", ["volume_path", "ann_path"])
+VolumeItemPaths = namedtuple("VolumeItemPaths", ["volume_path", "ann_path"])
 
 
-# # class VolumeDataset(VideoDataset):
-# #     item_dir_name = "volume"
-# #     related_files_dir_name = "interpolation"
-# #     annotation_class = VolumeAnnotation
+class VolumeDataset(VideoDataset):
+    item_dir_name = "volume"
+    annotation_class = VolumeAnnotation
+    item_module = sly_volume
+    paths_tuple = VolumeItemPaths
+
+    def _get_empty_annotaion(self, item_name):
+        path = item_name
+        _, volume_meta = sly_volume.read_nrrd_serie_volume(path)
+        return self.annotation_class(volume_meta)
 
 
-# #     @staticmethod
-# #     def _has_valid_ext(path: str) -> bool:
-# #         return sly_volume.has_valid_ext(path)
+class VolumeProject(VideoProject):
+    dataset_class = VolumeDataset
 
-# #     def _get_empty_annotaion(self, item_name):
-# #         return self.annotation_class()
-
-# #     @staticmethod
-# #     def _validate_added_item_or_die(item_path):
-# #         try:
-# #             sly_volume.validate_format(item_path)
-# #         except (
-# #             sly_volume.UnsupportedVolumeFormat,
-# #             sly_volume.VolumeReadException,
-# #         ) as e:
-# #             os.remove(item_path)
-# #             raise e
-
-# #     def get_related_files_path(self, item_name):
-# #         rimg_dir = os.path.join(self.directory, self.related_files_dir_name, item_name)
-# #         return rimg_dir
-
-# #     def get_interpolation_path(self, item_name, figure):
-# #         rel_dir = self.get_related_files_path(item_name)
-# #         return os.path.join(rel_dir, figure.key().hex + ".stl")
-
-# #     # def set_interpolation(self, volume_name, objects, interpolations_bytes):
-# #     #     for obj, interpolation_bytes in zip(objects, interpolations_bytes):
-# #     #         if not interpolation_bytes:
-# #     #             continue
-# #     #         dst_interpolation_path = self.get_interpolation_path(volume_name, obj)
-# #     #         mkdir(os.path.dirname(dst_interpolation_path))
-# #     #         with open(dst_interpolation_path, 'wb') as f:
-# #     #             f.write(interpolation_bytes)
-
-# #     def get_item_paths(self, item_name) -> VolumeItemPaths:
-# #         volume_path = self.get_img_path(item_name)
-# #         validate_format(volume_path)
-# #         return VolumeItemPaths(
-# #             volume_path=self.get_img_path(item_name),
-# #             ann_path=self.get_ann_path(item_name),
-# #         )
+    class DatasetDict(KeyIndexedCollection):
+        item_type = VolumeDataset
 
 
-# # class VolumeProject(VideoProject):
-# #     dataset_class = VolumeDataset
+def download_volume_project(
+    api,
+    project_id,
+    dest_dir,
+    dataset_ids=None,
+    download_volumes=True,
+    log_progress=False,
+):
+    """
+    Download project with given id in destination directory
+    :param api: Api class object
+    :param project_id: int
+    :param dest_dir: str
+    :param dataset_ids: list of integers
+    :param download_videos: bool
+    :param log_progress: bool
+    """
+    LOG_BATCH_SIZE = 1
 
-# #     class DatasetDict(KeyIndexedCollection):
-# #         item_type = VolumeDataset
+    key_id_map = KeyIdMap()
 
+    project_fs = VideoProject(dest_dir, OpenMode.CREATE)
 
-# # def download_volume_project(
-# #     api,
-# #     project_id,
-# #     dest_dir,
-# #     dataset_ids=None,
-# #     download_volumes=True,
-# #     batch_size=10,
-# #     log_progress=False,
-# # ):
-# #     key_id_map = KeyIdMap()
+    meta = ProjectMeta.from_json(api.project.get_meta(project_id))
+    project_fs.set_meta(meta)
 
-# #     project_fs = VolumeProject(dest_dir, OpenMode.CREATE)
-# #     meta = ProjectMeta.from_json(api.project.get_meta(project_id))
-# #     project_fs.set_meta(meta)
+    datasets_infos = []
+    if dataset_ids is not None:
+        for ds_id in dataset_ids:
+            datasets_infos.append(api.dataset.get_info_by_id(ds_id))
+    else:
+        datasets_infos = api.dataset.get_list(project_id)
 
-# #     datasets_infos = []
-# #     if dataset_ids is not None:
-# #         for ds_id in dataset_ids:
-# #             datasets_infos.append(api.dataset.get_info_by_id(ds_id))
-# #     else:
-# #         datasets_infos = api.dataset.get_list(project_id)
+    for dataset in datasets_infos:
+        dataset_fs = project_fs.create_dataset(dataset.name)
+        videos = api.video.get_list(dataset.id)
 
-# #     for dataset in datasets_infos:
-# #         dataset_fs = project_fs.create_dataset(dataset.name)
-# #         volumes = api.volume.get_list(dataset.id)
+        ds_progress = None
+        if log_progress:
+            ds_progress = Progress(
+                "Downloading dataset: {!r}".format(dataset.name), total_cnt=len(videos)
+            )
+        for batch in batched(videos, batch_size=LOG_BATCH_SIZE):
+            video_ids = [video_info.id for video_info in batch]
+            video_names = [video_info.name for video_info in batch]
 
-# #         ds_progress = None
-# #         if log_progress:
-# #             ds_progress = Progress(
-# #                 "Downloading dataset: {!r}".format(dataset.name), total_cnt=len(volumes)
-# #             )
-# #         for batch in batched(volumes, batch_size=batch_size):
-# #             volume_ids = [volume_info.id for volume_info in batch]
-# #             volume_names = [volume_info.name for volume_info in batch]
-# #             ann_jsons = api.volume.annotation.download_bulk(dataset.id, volume_ids)
+            ann_jsons = api.video.annotation.download_bulk(dataset.id, video_ids)
 
-# #             for volume_id, volume_name, ann_json in zip(
-# #                 volume_ids, volume_names, ann_jsons
-# #             ):
-# #                 if volume_name != ann_json[VOLUME_NAME]:
-# #                     raise RuntimeError(
-# #                         "Error in api.volume.annotation.download_batch: broken order"
-# #                     )
+            for video_id, video_name, ann_json in zip(
+                video_ids, video_names, ann_jsons
+            ):
+                if video_name != ann_json[ApiField.VIDEO_NAME]:
+                    raise RuntimeError(
+                        "Error in api.video.annotation.download_batch: broken order"
+                    )
 
-# #                 volume_annotation = VolumeAnnotation.from_json(
-# #                     ann_json, project_fs.meta, key_id_map
-# #                 )
-# #                 volume_file_path = dataset_fs.generate_item_path(volume_name)
-# #                 if download_volumes is True:
-# #                     api.volume.download_path(volume_id, volume_file_path)
-# #                 else:
-# #                     touch(volume_file_path)
+                video_file_path = dataset_fs.generate_item_path(video_name)
+                if download_videos is True:
+                    api.video.download_path(video_id, video_file_path)
+                else:
+                    touch(video_file_path)
 
-# #                 dataset_fs.add_item_file(
-# #                     volume_name,
-# #                     volume_file_path,
-# #                     ann=volume_annotation,
-# #                     _validate_item=False,
-# #                 )
+                dataset_fs.add_item_file(
+                    video_name,
+                    video_file_path,
+                    ann=VideoAnnotation.from_json(
+                        ann_json, project_fs.meta, key_id_map
+                    ),
+                    _validate_item=False,
+                )
 
-# #                 mesh_ids = []
-# #                 mesh_paths = []
-# #                 for sf in volume_annotation.spatial_figures:
-# #                     figure_key = sf.key()
-# #                     figure_id = key_id_map.get_figure_id(figure_key)
-# #                     mesh_ids.append(figure_id)
-# #                     figure_path = dataset_fs.get_interpolation_path(volume_name, sf)
-# #                     mesh_paths.append(figure_path)
+            ds_progress.iters_done_report(len(batch))
 
-# #                 api.volume.figure.download_geometries_paths(mesh_ids, mesh_paths)
-# #                 # vol_interp = api.volume.object.get_interpolation(volume_id,
-# #                 #                                                  volume_annotation.objects,
-# #                 #                                                  key_id_map)
-# #                 # dataset_fs.set_interpolation(volume_name, volume_annotation.objects, vol_interp)
-
-# #             if log_progress:
-# #                 ds_progress.iters_done_report(len(batch))
-
-# #     project_fs.set_key_id_map(key_id_map)
+    project_fs.set_key_id_map(key_id_map)
 
 
-# # def upload_volume_project(
-# #     directory, api, workspace_id, project_name=None, progress_cb=None
-# # ):
-# #     project_fs = VolumeProject(directory, mode=OpenMode.READ)
-# #     if project_name is None:
-# #         project_name = project_fs.name
+def upload_video_project(dir, api, workspace_id, project_name=None, log_progress=True):
+    project_fs = VideoProject.read_single(dir)
+    if project_name is None:
+        project_name = project_fs.name
 
-# #     if api.project.exists(workspace_id, project_name):
-# #         project_name = api.project.get_free_name(workspace_id, project_name)
+    if api.project.exists(workspace_id, project_name):
+        project_name = api.project.get_free_name(workspace_id, project_name)
 
-# #     project = api.project.create(workspace_id, project_name, ProjectType.VOLUMES)
-# #     api.project.update_meta(project.id, project_fs.meta.to_json())
+    project = api.project.create(workspace_id, project_name, ProjectType.VIDEOS)
+    api.project.update_meta(project.id, project_fs.meta.to_json())
 
-# #     uploaded_objects = KeyIdMap()
-# #     for dataset_fs in project_fs:
-# #         dataset = api.dataset.create(
-# #             project.id, dataset_fs.name, change_name_if_conflict=True
-# #         )
+    for dataset_fs in project_fs.datasets:
+        dataset = api.dataset.create(project.id, dataset_fs.name)
 
-# #         anns, item_names, volume_paths, volume_metas = [], [], [], []
-# #         for item_name in dataset_fs:
-# #             volume_path, ann_path = dataset_fs.get_item_paths(item_name)
-# #             ann = VolumeAnnotation.from_json(load_json_file(ann_path), project_fs.meta)
-# #             anns.append(ann)
-# #             item_names.append(item_name)
-# #             volume_paths.append(volume_path)
-# #             volume_metas.append(ann.volume_meta)
+        names, item_paths, ann_paths = [], [], []
+        for item_name in dataset_fs:
+            img_path, ann_path = dataset_fs.get_item_paths(item_name)
+            names.append(item_name)
+            item_paths.append(img_path)
+            ann_paths.append(ann_path)
 
-# #         volumes = api.volume.upload_paths(
-# #             dataset.id, item_names, volume_paths, volume_metas, progress_cb=progress_cb
-# #         )
+        progress_cb = None
+        if log_progress:
+            ds_progress = Progress(
+                "Uploading videos to dataset {!r}".format(dataset.name),
+                total_cnt=len(item_paths),
+            )
+            progress_cb = ds_progress.iters_done_report
 
-# #         for i, ann in enumerate(anns):
-# #             api.volume.annotation.append(volumes[i].id, ann, uploaded_objects)
-# #     return project
+        item_infos = api.video.upload_paths(dataset.id, names, item_paths, progress_cb)
+        item_ids = [item_info.id for item_info in item_infos]
+        if log_progress:
+            ds_progress = Progress(
+                "Uploading annotations to dataset {!r}".format(dataset.name),
+                total_cnt=len(item_paths),
+            )
+            progress_cb = ds_progress.iters_done_report
+
+        api.video.annotation.upload_paths(
+            item_ids, ann_paths, project_fs.meta, progress_cb
+        )
+
+    return project.id, project.name
