d280096828fa5de3176129d63d4df30e7adcd883
Daniil Yarockii <yaroc.d@gmail.com>
2022-06-30 12:16:29 +0300

    run_sync updated

--- supervisely/app/fastapi/utils.py
+++ supervisely/app/fastapi/utils.py
@@ -6,9 +6,5 @@ from supervisely import logger
 
 def run_sync(coroutine):
     with concurrent.futures.ThreadPoolExecutor(max_workers=1) as executor:
-        try:
-            result = executor.submit(lambda coroutine_to_exec: asyncio.run(coroutine_to_exec), coroutine).result()
-        except Exception as ex:
-            logger.warning(f"run_sync failed: {ex}")
-
+        result = executor.submit(lambda coroutine_to_exec: asyncio.run(coroutine_to_exec), coroutine).result()
     return result
