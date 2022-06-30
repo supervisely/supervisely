ea3a056dfbf64887b703f573efdcfa86422c256b
Daniil Yarockii <yaroc.d@gmail.com>
2022-06-30 14:59:37 +0300

    - repr added

--- supervisely/app/fastapi/utils.py
+++ supervisely/app/fastapi/utils.py
@@ -7,6 +7,7 @@ def run_sync(coroutine):
         with concurrent.futures.ThreadPoolExecutor(max_workers=1) as executor:
             result = executor.submit(lambda coroutine_to_exec: asyncio.run(coroutine_to_exec), coroutine).result()
         return result
-    except RuntimeError:
+    except RuntimeError as ex:
+        print(repr(ex))
         return None
 
