2c08c755245d219c5b93f5d8ef5a2dfbc8f9209e
Maxim Kolomeychenko <maxim@supervise.ly>
2022-08-08 20:00:20 +0300

    fix project url

--- supervisely/app/widgets/project_thumbnail/project_thumbnail.py
+++ supervisely/app/widgets/project_thumbnail/project_thumbnail.py
@@ -1,11 +1,13 @@
 from supervisely.app.widgets import Widget
 from supervisely.api.project_api import ProjectInfo
+from supervisely.project.project import Project
 
 
 class ProjectThumbnail(Widget):
     def __init__(self, info: ProjectInfo):
         self._info = info
         self._description = f"{self._info.items_count} {self._info.type}"
+        self._url = Project.get_url(self._info.id)
         super().__init__(file_path=__file__)
 
     def get_json_data(self):
@@ -13,6 +15,7 @@ class ProjectThumbnail(Widget):
             "id": self._info.id,
             "name": self._info.name,
             "description": self._description,
+            "url": self._url,
             "image_preview_url": self._info.image_preview_url,
         }
 
