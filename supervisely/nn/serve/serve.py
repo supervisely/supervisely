c15a703f9527b5dd2679c8287648bd417447657b
Maxim Kolomeychenko <maxim@supervise.ly>
2022-08-16 19:48:03 +0300

    wip

--- supervisely/nn/serve/serve.py
+++ supervisely/nn/serve/serve.py
@@ -1,3 +1,9 @@
+from supervisely._utils import is_production, is_development
+
+
 class Serve:
-    def __init__(self, path):
-        pass
+    def __init__(self, dir: str):
+        if is_production():
+            raise NotImplementedError("TBD - download directory")
+        elif is_development():
+            pass
