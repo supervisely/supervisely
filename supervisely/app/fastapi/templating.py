fa4b246fcf3d162ec6ea2f084d269a2f9205c8dd
Maxim Kolomeychenko <maxim@supervise.ly>
2022-08-18 16:42:57 +0300

    upgrade JS for appsv2 + loading

--- supervisely/app/fastapi/templating.py
+++ supervisely/app/fastapi/templating.py
@@ -7,8 +7,8 @@ from starlette.templating import _TemplateResponse as _TemplateResponse
 from starlette.background import BackgroundTask
 from supervisely.app.singleton import Singleton
 
-js_bundle_version = "2.1.8"
-js_frontend_version = "0.0.21"
+js_bundle_version = "2.1.9"
+js_frontend_version = "0.0.22"
 
 
 class Jinja2Templates(_fastapi_Jinja2Templates, metaclass=Singleton):
