63ce9ac2e969af343d63be0201e0e1ed0d5eafa5
Umar Sadekov <sadekov.u.r@gmail.com>
2022-08-20 15:57:39 +0300

    Update templating.py

--- supervisely/app/fastapi/templating.py
+++ supervisely/app/fastapi/templating.py
@@ -7,8 +7,8 @@ from starlette.templating import _TemplateResponse as _TemplateResponse
 from starlette.background import BackgroundTask
 from supervisely.app.singleton import Singleton
 
-js_bundle_version = "2.1.9"
-js_frontend_version = "0.0.23"
+js_bundle_version = "2.1.10"
+js_frontend_version = "0.0.24"
 
 
 class Jinja2Templates(_fastapi_Jinja2Templates, metaclass=Singleton):
