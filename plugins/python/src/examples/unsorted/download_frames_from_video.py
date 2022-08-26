553967b85a6ef1efafd0b4fc97ced62ea11beb6e
cxnt <pavelbartsits@gmail.com>
2022-08-26 15:45:37 +0300

    ids -> indexes

--- plugins/python/src/examples/unsorted/download_frames_from_video.py
+++ plugins/python/src/examples/unsorted/download_frames_from_video.py
@@ -8,12 +8,13 @@ api = sly.Api.from_env()
 video_id = 2859430
 video_info = api.video.get_info_by_id(video_id)
 
-all_frame_ids = list(range(video_info.frames_count))
-specific_frame_ids = [0, 1, 2, 50, 15, 3, 120]
+all_frame_indexes = list(range(video_info.frames_count))
+specific_frame_indexes = [0, 1, 2, 50, 15, 3, 120]
 
 # download frames as numpy ndarrays
-frames = api.video.frame.download_nps(video_id=video_id, ids=specific_frame_ids)
+frames = api.video.frame.download_nps(video_id=video_id, frame_indexes=specific_frame_indexes)
 
 # download frames and save them to given paths
-save_paths = [f"/home/admin/projects/video_project/frames/{idx}.png" for idx in specific_frame_ids]
-api.video.frame.download_paths(video_id=video_id, ids=specific_frame_ids, paths=save_paths)
+save_dir = "/home/admin/projects/video_project/frames/"
+save_paths = [f"{save_dir}{idx}.png" for idx in specific_frame_indexes]
+api.video.frame.download_paths(video_id=video_id, frame_indexes=specific_frame_indexes, paths=save_paths)
--- supervisely/api/video/video_frame_api.py
+++ supervisely/api/video/video_frame_api.py
@@ -74,18 +74,18 @@ class VideoFrameAPI(ModuleApi):
             image_np = api.video.frame.download_np(video_id, frame_idx)
         """
         response = self._download(video_id, frame_index)
-        img = sly_image.read_bytes(response.content)
-        return img
+        frame = sly_image.read_bytes(response.content)
+        return frame
 
-    def download_nps(self, video_id: int, ids: List[int], progress_cb: Optional[Callable] = None,
+    def download_nps(self, video_id: int, frame_indexes: List[int], progress_cb: Optional[Callable] = None,
                      keep_alpha: Optional[bool] = False) -> List[np.ndarray]:
         """
         Download frames with given indexes from given video ID in numpy format(RGB).
 
         :param video_id: Video ID in Supervisely.
         :type video_id: int
-        :param ids: Indexes of frames to download.
-        :type ids: List[int]
+        :param frame_indexes: Indexes of frames to download.
+        :type frame_indexes: List[int]
         :param progress_cb: Function for tracking download progress.
         :type progress_cb: Progress, optional
         :return: None
@@ -101,19 +101,19 @@ class VideoFrameAPI(ModuleApi):
             api = sly.Api.from_env()
 
             video_id = 198703211
-            frame_ids = [1,2,3,4,5,10,11,12,13,14,15]
-            api.video.frame.download_nps(video_id, frame_ids)
+            frame_indexes = [1,2,3,4,5,10,11,12,13,14,15]
+            api.video.frame.download_nps(video_id, frame_indexes)
         """
         return [
-            sly_image.read_bytes(img_bytes, keep_alpha)
-            for img_bytes in self.download_bytes(
-                video_id=video_id, ids=ids, progress_cb=progress_cb
+            sly_image.read_bytes(frame_bytes, keep_alpha)
+            for frame_bytes in self.download_bytes(
+                video_id=video_id, frame_indexes=frame_indexes, progress_cb=progress_cb
             )
         ]
 
     def download_path(self, video_id: int, frame_index: int, path: str) -> None:
         """
-        Downloads Image on the given path for frame with given index from given Video ID.
+        Downloads frame on the given path for frame with given index from given Video ID.
 
         :param video_id: Video ID in Supervisely.
         :type video_id: int
@@ -135,7 +135,7 @@ class VideoFrameAPI(ModuleApi):
 
             video_id = 198703211
             frame_idx = 5
-            save_path = '/home/admin/Downloads/img/result.png'
+            save_path = '/home/admin/Downloads/frames/result.png'
             api.video.frame.download_path(video_id, frame_idx, save_path)
         """
         response = self._download(video_id, frame_index)
@@ -144,15 +144,15 @@ class VideoFrameAPI(ModuleApi):
             for chunk in response.iter_content(chunk_size=1024 * 1024):
                 fd.write(chunk)
 
-    def download_paths(self, video_id: int, ids: List[int], paths: List[str],
+    def download_paths(self, video_id: int, frame_indexes: List[int], paths: List[str],
                        progress_cb: Optional[Callable] = None) -> None:
         """
-        Downloads frames to given paths for frames with given ids from given Video ID.
+        Downloads frames to given paths for frames with given indexes from given Video ID.
 
         :param video_id: Video ID in Supervisely.
         :type video_id: int
-        :param ids: Indexes of frames to download.
-        :type ids: List[int]
+        :param frame_indexes: Indexes of frames to download.
+        :type frame_indexes: List[int]
         :param paths: Local save paths for frames.
         :type paths: List[str]
         :param progress_cb: Function for tracking download progress.
@@ -170,32 +170,32 @@ class VideoFrameAPI(ModuleApi):
             api = sly.Api.from_env()
 
             video_id = 198703211
-            frame_ids = [1,2,3,4,5,10,11,12,13,14,15]
-            save_paths = [f"/home/admin/projects/video_project/frames/{idx}.png" for idx in frame_ids]
-            api.video.frame.download_paths(video_id, frame_ids, save_paths)
+            frame_indexes = [1,2,3,4,5,10,11,12,13,14,15]
+            save_paths = [f"/home/admin/projects/video_project/frames/{idx}.png" for idx in frame_indexes]
+            api.video.frame.download_paths(video_id, frame_indexes, save_paths)
         """
-        if len(ids) == 0:
+        if len(frame_indexes) == 0:
             return
-        if len(ids) != len(paths):
+        if len(frame_indexes) != len(paths):
             raise ValueError(
-                'Can not match "ids" and "paths" lists, len(ids) != len(paths)'
+                'Can not match "indexes" and "paths" lists, len(frame_indexes) != len(paths)'
             )
 
-        id_to_path = {id: path for id, path in zip(ids, paths)}
-        for frame_id, resp_part in self._download_batch(video_id, ids, progress_cb):
-            with open(id_to_path[frame_id], "wb") as w:
+        idx_to_path = {idx: path for idx, path in zip(frame_indexes, paths)}
+        for frame_id, resp_part in self._download_batch(video_id, frame_indexes, progress_cb):
+            with open(idx_to_path[frame_id], "wb") as w:
                 w.write(resp_part.content)
 
     def download_bytes(
-            self, video_id: int, ids: List[int], progress_cb: Optional[Callable] = None
+            self, video_id: int, frame_indexes: List[int], progress_cb: Optional[Callable] = None
     ) -> List[bytes]:
         """
-        Download Images with given IDs from Dataset in Binary format.
+        Download frames with given indexes from Dataset in Binary format.
 
         :param video_id: Video ID in Supervisely.
         :type video_id: int
-        :param ids: List of Frames IDs in Supervisely.
-        :type ids: List[int]
+        :param frame_indexes: List of video frames indexes in Supervisely.
+        :type frame_indexes: List[int]
         :param progress_cb: Function for tracking download progress.
         :type progress_cb: Progress, optional
         :return: List of Images in binary format
@@ -211,15 +211,15 @@ class VideoFrameAPI(ModuleApi):
             api = sly.Api.from_env()
 
             video_id = 213542
-            frame_ids = [1,2,3,4,5,10,11,12,13,14,15]
-            frames_bytes = api.video.frame.download_bytes(video_id=video_id, ids=frame_ids)
+            frame_indexes = [1,2,3,4,5,10,11,12,13,14,15]
+            frames_bytes = api.video.frame.download_bytes(video_id=video_id, frame_indexes=frame_indexes)
             print(frames_bytes)
             # Output: [b'\xff\xd8\xff\xe0\x00\x10JFIF\x00\x01\x01\...]
         """
-        if len(ids) == 0:
+        if len(frame_indexes) == 0:
             return []
 
-        id_to_img = {}
-        for img_id, resp_part in self._download_batch(video_id, ids, progress_cb):
-            id_to_img[img_id] = resp_part.content
-        return [id_to_img[id] for id in ids]
+        idx_to_frame = {}
+        for frame_idx, resp_part in self._download_batch(video_id, frame_indexes, progress_cb):
+            idx_to_frame[frame_idx] = resp_part.content
+        return [idx_to_frame[idx] for idx in frame_indexes]
