e551fac70806d2c14ff786061f304765f96f407c
Maxim Kolomeychenko <maxim@supervise.ly>
2022-09-02 11:58:24 +0300

    select - no HTML

--- supervisely/app/widgets/select/select.py
+++ supervisely/app/widgets/select/select.py
@@ -1,76 +1,75 @@
+from __future__ import annotations
 from supervisely.app import DataJson
 from supervisely.app.widgets import Widget
-from typing import List
+from typing import List, Dict
+
 try:
     from typing import Literal
 except ImportError:
     from typing_extensions import Literal
 
 
-class SelectItem:
-    def __init__(self, value, label: str = None, group: str = None) -> None:
-        self.value = value
-        self.label = label
-        if label is None:
-            self.label = str(self.value)
-        self.group = group
-
-
 class Select(Widget):
+    class Item:
+        def __init__(self, value, label: str = None) -> None:
+            self.value = value
+            self.label = label
+            if label is None:
+                self.label = str(self.value)
+
+        def to_json(self) -> Dict:
+            return {"label": self.label, "value": self.value}
+
+    class Group:
+        def __init__(self, label, items: List[Select.Item] = None) -> None:
+            self.label = label
+            self.items = items
+
+        def to_json(self) -> Dict:
+            return {
+                "label": self.label,
+                "options": [item.to_json() for item in self.items],
+            }
+
     def __init__(
         self,
-        item: List[SelectItem],
+        items: List[Select.Item] = None,
+        groups: List[Select.Group] = None,
         filterable: bool = False,
-        placeholder: str = None,
+        placeholder: str = "select",
         widget_id: str = None,
     ):
-        self._text = None
-        self._status = None
-        self._icon = None
-        self._icon_color = None
-        self._text_color = None
+        if items is None and groups is None:
+            raise ValueError("One of the arguments has to be defined: items or groups")
+
+        if items is not None and groups is not None:
+            raise ValueError(
+                "Only one of the arguments has to be defined: items or groups"
+            )
+
+        self._items = items
+        self._groups = groups
+        self._filterable = filterable
+        self._placeholder = placeholder
+
         super().__init__(widget_id=widget_id, file_path=__file__)
-        self.set(text, status)
+
+    def _get_first_value(self) -> Select.Item:
+        if self._items is not None:
+            return self._items[0]
+        if self._groups is not None:
+            return self._groups[0][0]
 
     def get_json_data(self):
-        return {
-            "status": self._status,
-            "text": self._text,
-            "text_color": self._text_color,
-            "icon": self._icon,
-            "icon_color": self._icon_color,
+        res = {
+            "filterable": self._filterable,
+            "placeholder": self._placeholder,
         }
+        if self._items is not None:
+            res["items"] = [item.to_json() for item in self._items]
+        if self._groups is not None:
+            res["groups"] = [group.to_json() for group in self._groups]
+        return res
 
     def get_json_state(self):
-        return None
-
-    @property
-    def text(self):
-        return self._text
-
-    @text.setter
-    def text(self, value):
-        self._text = value
-        self.update_data()
-        DataJson().send_changes()
-
-    @property
-    def status(self):
-        return self._status
-
-    @status.setter
-    def status(self, value: Literal["text", "info", "success", "warning", "error"]):
-        if value not in type_to_icon:
-            raise ValueError(f'Unknown status "{value}"')
-        self._status = value
-        self._icon = type_to_icon[self._status]
-        self._icon_color = type_to_icon_color[self._status]
-        self._text_color = type_to_text_color[self._status]
-        self.update_data()
-        DataJson().send_changes()
-
-    def set(
-        self, text: str, status: Literal["text", "info", "success", "warning", "error"]
-    ):
-        self.text = text
-        self.status = status
+        return {"value": self._get_first_value().value}
