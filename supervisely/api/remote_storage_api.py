e83d28a69bbd9b38fb43c3a1059643f05e5c4c23
Pavel Bartsits <48913536+cxnt@users.noreply.github.com>
2022-09-05 14:34:40 +0300

    add limit and docstring to remote storage api list method (#196)

--- supervisely/api/remote_storage_api.py
+++ supervisely/api/remote_storage_api.py
@@ -21,10 +21,10 @@ class Provider(StrEnum):
     @staticmethod
     def validate_path(path):
         if (
-            not path.startswith(str(Provider.S3))
-            and not path.startswith(str(Provider.GOOGLE))
-            and not path.startswith(str(Provider.AZURE))
-            and not path.startswith(str(Provider.FS))
+                not path.startswith(str(Provider.S3))
+                and not path.startswith(str(Provider.GOOGLE))
+                and not path.startswith(str(Provider.AZURE))
+                and not path.startswith(str(Provider.FS))
         ):
             raise ValueError(
                 "Incorrect cloud path, learn more here: https://docs.supervise.ly/enterprise-edition/advanced-tuning/s3#links-plugin-cloud-providers-support"
@@ -38,8 +38,26 @@ class RemoteStorageApi(ModuleApiBase):
         """_convert_json_info"""
         return info
 
-    def list(self, path, recursive=True, files=True, folders=True):
-        """list"""
+    def list(self, path: str, recursive: bool = True, files: bool = True, folders: bool = True, limit: int = 10000, start_after: str = "") -> dict:
+        """
+        List files and directories for given remote path.
+
+        :param path: Remote path with items that you want to list.
+        :type path: str
+        :param recursive: List remote path revursively.
+        :type recursive: bool
+        :param files: List files in the given path.
+        :type files: bool
+        :param folders: List folders in the given path.
+        :type folders: bool
+        :param limit: Limit of files to list. 10000 is the maximum limit.
+        :type limit: int
+        :param start_after: Start listing path after given file name.
+        :type start_after: str
+        :returns: List of files in the given remote path
+        :rtype: dict
+
+        """
         Provider.validate_path(path)
         path = path.rstrip("/") + "/"
         resp = self._api.get(
@@ -49,6 +67,8 @@ class RemoteStorageApi(ModuleApiBase):
                 "recursive": recursive,
                 "files": files,
                 "folders": folders,
+                "limit": limit,
+                "startAfter": start_after
             },
         )
         return resp.json()
