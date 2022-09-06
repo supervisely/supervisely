eb0aeceb5767b08a4ff908c5c17ee25b0e20284f
Maxim Kolomeychenko <maxim@supervise.ly>
2022-09-06 19:16:07 +0300

    basic menu done

--- supervisely/app/widgets/menu/menu.py
+++ supervisely/app/widgets/menu/menu.py
@@ -5,6 +5,24 @@ from supervisely.app import DataJson, StateJson
 from supervisely.app.widgets import Widget
 
 
+"""
+
+g1_items = [
+    sly.app.widgets.Menu.Item(title="m1", content=r),
+    sly.app.widgets.Menu.Item(title="m2", content=l),
+]
+g2_items = [
+    sly.app.widgets.Menu.Item(title="m3"),
+    sly.app.widgets.Menu.Item(title="m4"),
+]
+g1 = sly.app.widgets.Menu.Group("g1", g1_items)
+g2 = sly.app.widgets.Menu.Group("g2", g2_items)
+# menu = sly.app.widgets.Menu(groups=[g1, g2])
+menu = sly.app.widgets.Menu(items=g1_items)
+
+"""
+
+
 class Menu(Widget):
     class Item:
         def __init__(
@@ -42,8 +60,20 @@ class Menu(Widget):
         width_percent: int = 25,
         widget_id: str = None,
     ):
+        if items is None and groups is None:
+            raise ValueError("One of the arguments has to be defined: items or groups")
 
+        if items is not None and groups is not None:
+            raise ValueError(
+                "Only one of the arguments has to be defined: items or groups"
+            )
+
+        self._items = items
+        if self._items is None:
+            self._items = []
         self._groups = groups
+        if self._groups is None:
+            self._groups = []
         self._width_percent = width_percent
         self._options = {"sidebarWidth": self._width_percent}
         StateJson()["app_body_padding"] = "0px"
@@ -51,7 +81,17 @@ class Menu(Widget):
         super().__init__(widget_id=widget_id, file_path=__file__)
 
     def get_json_data(self):
-        return {"groups": [g.to_json() for g in self._groups]}
+        res = {"items": [], "groups": []}
+        if self._items is not None:
+            res["items"] = [item.to_json() for item in self._items]
+        if self._groups is not None:
+            res["groups"] = [g.to_json() for g in self._groups]
+        return res
 
     def get_json_state(self):
-        return {"menuIndex": self._groups[0].items[0].index}
+        index = None
+        if len(self._items) > 0:
+            index = self._items[0].index
+        if len(self._groups) > 0:
+            index = self._groups[0].items[0].index
+        return {"menuIndex": index}
