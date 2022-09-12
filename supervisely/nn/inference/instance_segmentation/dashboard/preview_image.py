35465b5f8269acd0765e57d647a4a0135b4d9dce
Maxim Kolomeychenko <maxim@supervise.ly>
2022-09-12 11:54:18 +0300

    flexbox with tests in dashboards

--- supervisely/nn/inference/instance_segmentation/dashboard/preview_image.py
+++ supervisely/nn/inference/instance_segmentation/dashboard/preview_image.py
@@ -1,9 +1,17 @@
-from supervisely.app.widgets import Container, OneOf
+from supervisely.app.widgets import Container, OneOf, Text, Flexbox
 import supervisely.nn.inference.instance_segmentation.dashboard.preview as preview
 
 
 oneof_block = OneOf(preview.image_source)
 
+t1 = Text("1")
+t2 = Text("2")
+t3 = Text("3")
+t4 = Text("4")
+t5 = Text("5")
+
+flex_container = Flexbox([t1, t2, t3, t4, t5])
+
 preview_image_layout = Container(
-    [preview.image_source_field, oneof_block],
+    [preview.image_source_field, oneof_block, flex_container],
 )
