03dad06c34ea5b24d942f13f2a921b54167c3e10
Maxim Kolomeychenko <maxim@supervise.ly>
2022-09-16 10:26:24 +0300

    add gap to flexbox

--- supervisely/app/widgets/flexbox/flexbox.py
+++ supervisely/app/widgets/flexbox/flexbox.py
@@ -6,10 +6,12 @@ class Flexbox(Widget):
     # https://www.w3schools.com/css/css3_flexbox.asp
     def __init__(
         self,
-        widgets: List[Widget],  # or RadioGroup in future
+        widgets: List[Widget],
+        gap: int = 10,
         widget_id: str = None,
     ):
         self._widgets = widgets
+        self._gap = gap
         super().__init__(widget_id=widget_id, file_path=__file__)
 
     def get_json_data(self) -> Dict:
