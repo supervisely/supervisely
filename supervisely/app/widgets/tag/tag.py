eebfa0d66ec2caa1e0168c99e8fca6c4584c0808
cxnt <pavelbartsits@gmail.com>
2022-09-16 14:46:24 +0300

    update tag, add type validation value -> text

--- supervisely/app/widgets/tag/tag.py
+++ supervisely/app/widgets/tag/tag.py
@@ -6,48 +6,60 @@ try:
 except ImportError:
     from typing_extensions import Literal
 
+SUPPORTED_TAG_WIDGET_TYPES = ["primary", "gray", "success", "warning", "danger"]
+
 
 class Tag(Widget):
     def __init__(
             self,
-            value: str = "",
+            text: str = "",
             type: Literal["primary", "gray", "success", "warning", "danger"] = None,
             hit: bool = False,
             widget_id: str = None
     ):
-        self._value = value
+        self._text = text
+        self._validate_type(type)
         self._type = type
         self._hit = hit
-        self._widget_id = None
 
         super().__init__(widget_id=widget_id, file_path=__file__)
 
+    def _validate_type(self, value):
+        if value is None:
+            return
+        if value not in SUPPORTED_TAG_WIDGET_TYPES:
+            raise ValueError(
+                "type = {!r} is unknown, should be one of {}".format(
+                    value, SUPPORTED_TAG_WIDGET_TYPES
+                )
+            )
+
     def get_json_data(self):
         return {
+            "text": self._text,
             "type": self._type,
-            "hit": self._hit,
+            "hit": self._hit
         }
 
     def get_json_state(self):
-        return {"value": self._value}
+        return {}
 
-    def set_value(self, value: str):
-        StateJson()[self.widget_id]["value"] = value
-        StateJson().send_changes()
+    def set_text(self, value: str):
+        DataJson()[self.widget_id]["text"] = value
+        DataJson().send_changes()
 
-    def get_value(self):
-        return StateJson()[self.widget_id]["value"]
+    def get_text(self):
+        return StateJson()[self.widget_id]["text"]
 
-    def set_type(self, value: str):
-        if value not in ["primary", "gray", "success", "warning", "danger"]:
-            return
+    def set_type(self, value: Literal["primary", "gray", "success", "warning", "danger"]):
+        self._validate_type(value)
         DataJson()[self.widget_id]["value"] = value
         DataJson().send_changes()
 
     def get_type(self):
         return DataJson()[self.widget_id]["value"]
 
-    def is_highlighted(self):
+    def is_border_highlighted(self):
         return DataJson()[self.widget_id]["hit"]
 
     def enable_border_highlighting(self):
