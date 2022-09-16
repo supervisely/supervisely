be52ace944c7c275af749275c5fae8b6e025dd4c
Maxim Kolomeychenko <maxim@supervise.ly>
2022-09-16 17:31:46 +0300

    flexbox center_content

--- supervisely/app/widgets/flexbox/flexbox.py
+++ supervisely/app/widgets/flexbox/flexbox.py
@@ -2,20 +2,28 @@ from supervisely.app.widgets import Widget
 from typing import Dict, List
 
 
+"""
+
+"""
+
+
 class Flexbox(Widget):
     # https://www.w3schools.com/css/css3_flexbox.asp
     def __init__(
         self,
         widgets: List[Widget],
         gap: int = 10,
+        center_content: bool = False,
         widget_id: str = None,
     ):
         self._widgets = widgets
         self._gap = gap
+        self._center_content = center_content
         super().__init__(widget_id=widget_id, file_path=__file__)
 
     def get_json_data(self) -> Dict:
-        return {}
+        res = {"center": self._center_content}
+        return res
 
     def get_json_state(self) -> Dict:
         return {}
