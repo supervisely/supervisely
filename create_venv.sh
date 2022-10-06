70fde104923aca4787b1411138122b4576436852
Maxim Kolomeychenko <maxim@supervise.ly>
2022-10-06 08:16:41 +0300

    add tests for debugging

---
+++ create_venv.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# learn more in documentation
+# Official python docs: https://docs.python.org/3/library/venv.html
+# Superviely developer portal: https://developer.supervise.ly/getting-started/installation#venv
+
+if [ -d ".venv" ]; then
+    echo "VENV already exists, will be removed"
+    rm -rf .venv
+fi
+
+echo "VENV will be created" && \
+python3 -m venv .venv && \
+source .venv/bin/activate && \
+
+echo "Install requirements..." && \
+pip3 install . && \
+echo "Requirements have been successfully installed" && \
+deactivate
