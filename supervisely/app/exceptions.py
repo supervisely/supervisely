0c63132b43b98b6902000d546ee1267ee14f7728
Maxim Kolomeychenko <maxim@supervise.ly>
2022-11-15 18:10:24 +0000

    done

--- supervisely/app/exceptions.py
+++ supervisely/app/exceptions.py
@@ -1,7 +1,14 @@
-class DialogWindowError(Exception):
-    def __init__(self, title, description):
+try:
+    from typing import Literal
+except ImportError:
+    from typing_extensions import Literal
+
+
+class DialogWindowBase(Exception):
+    def __init__(self, title, description, status):
         self.title = title
         self.description = description
+        self.status = status
         super().__init__(self.get_message())
 
     def get_message(self):
@@ -11,5 +18,30 @@ class DialogWindowError(Exception):
         return self.get_message()
 
 
+class DialogWindowError(DialogWindowBase):
+    def __init__(self, title, description):
+        super().__init__(title, description, "error")
+
+
+class DialogWindowWarning(DialogWindowBase):
+    def __init__(self, title, description):
+        super().__init__(title, description, "warning")
+
+
+# for compatibility
 class DialogWindowMessage(DialogWindowError):
-    pass
+    def __init__(self, title, description):
+        super().__init__(title, description, "info")
+
+
+def show_dialog(
+    title, description, status: Literal["info", "success", "warning", "error"] = "info"
+):
+    from supervisely.app import StateJson, DataJson
+
+    StateJson()["slyAppShowDialog"] = True
+    DataJson()["slyAppDialogTitle"] = title
+    DataJson()["slyAppDialogMessage"] = description
+    DataJson()["slyAppDialogStatus"] = status
+    DataJson().send_changes()
+    StateJson().send_changes()
