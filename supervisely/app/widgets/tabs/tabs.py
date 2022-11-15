93bb40a440756849950533db01e5aa9c34ef10bc
Nikita <arzamaznik@gmail.com>
2022-11-15 16:08:17 +0000

    tabs widget added

--- supervisely/app/widgets/tabs/tabs.py
+++ supervisely/app/widgets/tabs/tabs.py
@@ -1,8 +1,9 @@
 from __future__ import annotations
 
-from typing import List, Union
+from typing import List, Union, Optional
 from supervisely.app import DataJson, StateJson
 from supervisely.app.widgets import Widget, Text
+import uuid
 
 try:
     from typing import Literal
@@ -11,53 +12,42 @@ except ImportError:
 
 
 class Tabs(Widget):
-    class Routes:
-        VALUE_CHANGED = "value_changed"
-
     class TabPane:
-        def __init__(
-                self,
-                label: str = "",
-                name: str = "",
-                content: str = None
-        ) -> Tabs.TabPane:
-            self._label = label
-            self._name = name  # identifier corresponding to the active tab
-            self._content = content
-
-        def to_json(self):
-            return {
-                "label": self._label,
-                "name": self._name,
-                "content": self._content
-            }
+        def __init__(self, label: Optional[str] = "", content: Optional[Widget] = None):
+            self.label = label
+            self.name = label  # identifier corresponding to the active tab
+            self.content = content
 
     def __init__(
-            self,
-            items: List[TabPane] = [],
-            value: str = None,
-            type: Literal["card", "border-card"] = None,
-            widget_id=None
+        self,
+        labels: List[str],
+        contents: List[Widget],
+        type: Literal["card", "border-card"] = "border-card",
+        widget_id=None,
     ):
-        self._items = items
-        self._value = value
+        if len(labels) != len(contents):
+            raise ValueError(
+                "items_labels length must be equal to items_content length in Tabs widget."
+            )
+        if len(set(labels)) != len(labels):
+            raise ValueError("All of tab labels should be unique.")
+        self._items = []
+        for label, widget in zip(labels, contents):
+            self._items.append(Tabs.TabPane(label=label, content=widget))
+        self._value = labels[0]
         self._type = type
-        self._value_changed = False
         super().__init__(widget_id=widget_id, file_path=__file__)
 
     def get_json_data(self):
-        return {
-            "items": self._items,
-            "value": self._value,
-            "type": self._type
-        }
+        return {"type": self._type}
 
     def get_json_state(self):
-        return {}
+        return {"value": self._value}
 
     def set_active_tab(self, value):
-        DataJson()[self.widget_id]["value"] = value
-        DataJson().send_changes()
+        self._value = value
+        StateJson()[self.widget_id]["value"] = self._value
+        StateJson().send_changes()
 
     def get_active_tab(self):
-        return DataJson()[self.widget_id]["value"]
+        return StateJson()[self.widget_id]["value"]
