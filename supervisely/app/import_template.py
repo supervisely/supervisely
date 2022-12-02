dfe37cf88d5a47fa872c3bec47ff0d18d430d55c
cxnt <pavelbartsits@gmail.com>
2022-12-02 14:50:12 +0400

    add context class

--- supervisely/app/import_template.py
+++ supervisely/app/import_template.py
@@ -6,6 +6,7 @@ from supervisely.app import get_data_dir
 from os.path import join, basename
 from pathlib import Path
 
+from typing import NamedTuple
 
 try:
     from typing import Literal
@@ -15,19 +16,28 @@ except ImportError:
 
 
 class Import:
-    def process(
-        self, workspace_id: int, project_id: int, dataset_id: int, path: str, is_directory: bool
-    ) -> Optional[Union[int, None]]:
+    class Context(NamedTuple):
+        task_id=None
+        team_id=None
+        workspace_id=None
+        project_id=None
+        dataset_id=None
+        path=None
+        is_directory=True
+    
+    def process(self, context: Context) -> Optional[Union[int, None]]:
         raise NotImplementedError() # implement your own method when inherit
 
     def run(self):
         api = Api.from_env()
-        task_id = None
+        context = Import.Context
+        
+        context.task_id = None
         if is_production():
-            task_id = env.task_id()
+            context.task_id = env.task_id()
 
-        team_id = env.team_id()
-        workspace_id = env.workspace_id()
+        context.team_id = env.team_id()
+        context.workspace_id = env.workspace_id()
           
         file = env.file(raise_not_found=False)
         folder = env.folder(raise_not_found=False)
@@ -41,40 +51,39 @@ class Import:
                 "One of the environment variables has to be defined for the import app: FILE or FOLDER"
             )
 
-        is_directory = True
-        path = folder
+        context.path = folder
         if file is not None:
-            path = file
-            is_directory = False
+            context.path = file
+            context.is_directory = False
             
-        project_id = env.project_id(raise_not_found=False)
-        dataset_id = env.dataset_id(raise_not_found=False)
+        context.project_id = env.project_id(raise_not_found=False)
+        context.dataset_id = env.dataset_id(raise_not_found=False)
         
         # get or create project with the same name as input file and empty dataset in it
-        if project_id is None:
-            project_name = Path(path).stem
-            project = api.project.create(workspace_id=workspace_id, name=project_name, change_name_if_conflict=True)
+        if context.project_id is None:
+            project_name = Path(context.path).stem
+            project = api.project.create(workspace_id=context.workspace_id, name=project_name, change_name_if_conflict=True)
         else:
             project = api.project.get_info_by_id(id=project_id)
         print(f"Working project: id={project.id}, name={project.name}")
             
-        if dataset_id is None: 
+        if context.dataset_id is None: 
             dataset = api.dataset.create(project_id=project.id, name="dataset", change_name_if_conflict=True)
         else:
-            dataset = api.dataset.get_info_by_id(id=dataset_id)
+            dataset = api.dataset.get_info_by_id(id=context.dataset_id)
         print(f"Working dataset: id={dataset.id}, name={dataset.name}")
 
         if is_production():
             local_save_path = join(get_data_dir(), basename(path))
-            if is_directory:
+            if context.is_directory:
                 raise NotImplementedError()
                 # api.file.download_directory(team_id=team_id, remote_path=path, local_save_path=local_save_path)
             else:
-                api.file.download(team_id=team_id, remote_path=path, local_save_path=local_save_path)
+                api.file.download(team_id=context.team_id, remote_path=path, local_save_path=local_save_path)
             path = local_save_path
             
-        project_id = self.process(workspace_id=workspace_id, project_id=project.id, dataset_id=dataset.id, path=path, is_directory=is_directory)
+        project_id = self.process(context=context)
         if type(project_id) is int and is_production():
             info = api.project.get_info_by_id(project_id)
-            api.task.set_output_project(task_id=task_id, project_id=info.id, project_name=info.name)
+            api.task.set_output_project(task_id=context.task_id, project_id=info.id, project_name=info.name)
             print(f"Result project: id={info.id}, name={info.name}")
