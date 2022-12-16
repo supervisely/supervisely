b4339b218631619c1764a99ec46581a8e6dc1a1a
almaz_macbook <almazgimaev@gmail.com>
2022-12-16 15:08:17 +0300

    fix: Image methods. refactor template and styles.

--- supervisely/app/widgets/image/image.py
+++ supervisely/app/widgets/image/image.py
@@ -3,59 +3,24 @@ from supervisely.app.widgets import Widget
 
 
 class Image(Widget):
-
-    def __init__(
-        self,
-        widget_id: str = None,
-    ):
-
-        self._image_url = ""
-        self._title = ""
-        self._loading = False
-
+    def __init__(self, widget_id: str = None):
+        self._url = ""
         super().__init__(widget_id=widget_id, file_path=__file__)
 
     def get_json_data(self):
-        return {
-            "content": {
-                "image_url": self._image_url,
-                "title": self._title
-            },
-            "loading": self._loading,
-        }
+        return {"content": {"url": self._url}}
 
     def get_json_state(self):
-        return {}
+        return None
 
-
-    def set_image(
-        self,
-        image_url: str,
-        title: str = "",
-    ):
-
-        self._image_url = image_url
-        self._title = title
-
+    def set(self, url: str):
+        self._url = url
         self._update()
 
     def clean_up(self):
-        self._data = ""
-        self._title = ""
+        self._url = ""
         self._update()
 
-
     def _update(self):
-        DataJson()[self.widget_id]["content"]["image_url"] = self._image_url
-        DataJson()[self.widget_id]["content"]["title"] = self._title
-        DataJson().send_changes()
-
-    @property
-    def loading(self):
-        return self._loading
-
-    @loading.setter
-    def loading(self, value: bool):
-        self._loading = value
-        DataJson()[self.widget_id]["loading"] = self._loading
+        DataJson()[self.widget_id]["content"]["url"] = self._url
         DataJson().send_changes()
