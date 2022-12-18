f35ecdddc3277f69926d93a839f1e95839bcb53c
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-18 04:57:41 +0000

    Adding tarfile member sanitization to extractall()

--- supervisely/api/neural_network_api.py
+++ supervisely/api/neural_network_api.py
@@ -81,7 +81,26 @@ class NeuralNetworkApi(CloneableModuleApi, RemoveableModuleApi):
         self.download_to_tar(workspace_id, name, model_tar, progress_cb)
         model_dir = os.path.join(directory, name)
         with tarfile.open(model_tar) as archive:
-            archive.extractall(model_dir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(archive, model_dir)
         silent_remove(model_tar)
         return model_dir
 
