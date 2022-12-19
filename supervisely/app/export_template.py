8e9509ba296ff61ec6a0f58ce2a3242f23469b08
cxnt <pavelbartsits@gmail.com>
2022-12-20 00:18:15 +0400

    class for export template

--- supervisely/app/export_template.py
+++ supervisely/app/export_template.py
@@ -3,8 +3,9 @@ from supervisely._utils import is_production
 import supervisely.io.env as env
 from supervisely.api.api import Api
 from supervisely.app import get_data_dir
-from os.path import join, basename
-from pathlib import Path
+from os.path import join, isdir, isfile, basename
+from supervisely.io.fs import get_file_name_with_ext
+from supervisely import Progress
 
 from supervisely.project.project import download_project
 from supervisely.project.video_project import download_video_project
@@ -12,7 +13,7 @@ from supervisely.project.volume_project import download_volume_project
 from supervisely.project.pointcloud_project import download_pointcloud_project
 from supervisely.project.pointcloud_episode_project import download_pointcloud_episode_project
 from supervisely.project.project_type import ProjectType
-from supervisely.project.project import Project, Dataset, OpenMode
+from supervisely.project.project import Project, OpenMode
 
 try:
     from typing import Literal
@@ -20,67 +21,15 @@ except ImportError:
     # for compatibility with python 3.7
     from typing_extensions import Literal
 
+ARCHIVE_EXT = ["zip", "tar", "rar", "tar.gz", "7z"]
+
 
 class Export:
-    class Context:
-        def __init__(
-            self,
-            team_id: int,
-            workspace_id: int,
-            project_id: int,
-            dataset_id: int,
-            project: Project,
-            dataset: Dataset,
-        ):
-            self._team_id = team_id
-            if self._team_id is None:
-                raise ValueError(f"Team ID is not specified: {self._team_id}")
-            if type(self._team_id) is not int:
-                raise ValueError(f"Team ID must be 'int': {self._team_id}")
 
-            self._workspace_id = workspace_id
-            if self._workspace_id is None:
-                raise ValueError(f"Workspace ID is not specified: {self._workspace_id}")
-            if type(self._workspace_id) is not int:
-                raise ValueError(f"Workspace ID must be 'int': {self._workspace_id}")
+    # context
+    #     project_info, dataset_info, items_infos, anns_infos, work_dir
 
-            self._project_id = project_id
-            if self._project_id is None:
-                raise ValueError(f"Project ID is not specified: {self._project_id}")
-            if type(self._project_id) is not int:
-                raise ValueError(f"Project ID must be 'int': {self._project_id}")
-
-            self._dataset_id = dataset_id
-            if self._dataset_id is None:
-                raise ValueError(f"Dataset ID is not specified: {self._dataset_id}")
-            if type(self._dataset_id) is not int:
-                raise ValueError(f"Dataset ID must be 'int': {self._dataset_id}")
-
-        def __str__(self):
-            return (
-                f"Team ID: {self._team_id}\n"
-                f"Workspace ID: {self._workspace_id}\n"
-                f"Project ID: {self._project_id}\n"
-                f"Dataset ID: {self._dataset_id}\n"
-            )
-
-        @property
-        def team_id(self) -> int:
-            return self._team_id
-
-        @property
-        def workspace_id(self) -> int:
-            return self._workspace_id
-
-        @property
-        def project_id(self) -> int:
-            return self._project_id
-
-        @property
-        def dataset_id(self) -> int:
-            return self._dataset_id
-
-    def process(self, project: Project) -> str:
+    def process(self, project: Project, work_dir: str) -> str:
         raise NotImplementedError()  # implement your own method when inherit
 
     def prepare(
@@ -157,6 +106,7 @@ class Export:
             print(f"Exporting Dataset: id={dataset.id}, name={dataset.name}")
 
         local_project_path = join(get_data_dir(), project.name)
+        work_dir = join(get_data_dir(), "work_dir")
 
         local_project = self.prepare(
             api=api,
@@ -167,8 +117,32 @@ class Export:
             log_progress=True,
         )
 
-        path_to_upload = self.process(project=local_project)
-        if type(project_id) is int and is_production():
-            info = api.project.get_info_by_id(project_id)
-            api.task.set_output_project(task_id=task_id, project_id=info.id, project_name=info.name)
-            print(f"Result project: id={info.id}, name={info.name}")
+        local_path = self.process(project=local_project, work_dir=work_dir)
+
+        if type(local_path) is str:
+            if isfile():
+                progress = Progress(message="Uploading file", total_cnt=1)
+                remote_path = join("template-export-app", get_file_name_with_ext(local_path))
+                file_info = api.file.upload(
+                    team_id=team_id, src=local_path, dst=remote_path, progress_cb=progress
+                )
+                api.task.set_output_archive(
+                    task_id=task_id, file_id=file_info.id, file_name=file_info.name
+                )
+                print(f"Remote file: id={file_info.id}, name={file_info.name}")
+            elif isdir():
+                progress = Progress(message="Uploading file", total_cnt=1)
+                remote_path = join("template-export-app", basename(local_path))
+                file_info = api.file.upload_directory(
+                    team_id=team_id,
+                    local_dir=local_path,
+                    remote_dir=remote_path,
+                    change_name_if_conflict=True,
+                    progress_size_cb=progress,
+                )
+                api.task.set_output_directory(
+                    task_id=task_id, file_id=file_info.id, directory_path=file_info.name
+                )
+                print(f"Remote directory: id={file_info.id}, name={file_info.name}")
+        else:
+            raise ValueError("Path must be a 'string'")
