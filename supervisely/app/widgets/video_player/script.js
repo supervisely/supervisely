597fd3f3731119615f904b0524aad0a272d39bac
almaz_macbook <almazgimaev@gmail.com>
2022-12-30 13:16:08 +0300

    fix play pause methods in script

--- supervisely/app/widgets/video_player/script.js
+++ supervisely/app/widgets/video_player/script.js
@@ -1,5 +1,5 @@
 Vue.component("html-video", {
-  props: ["time_to_set", "url", "mime_type", "playing"],
+  props: ["time_to_set", "url", "mime_type", "is_playing"],
   template: `
 <div>
     <video ref="video"
@@ -7,6 +7,8 @@ Vue.component("html-video", {
         height="auto"
         controls
         @timeupdate="$emit('timeupdate', $refs['video'].currentTime)"
+        @play="$emit('update:is_playing', true)"
+        @pause="$emit('update:is_playing', false)"
     >
         <source ref="video-data" :src="url" :type="mime_type">
     </video>
@@ -32,33 +34,39 @@ Vue.component("html-video", {
       },
       immediate: true,
     },
-    playing: {
-      handler() {
-        this.play_pause();
+    is_playing: {
+      handler(value) {
+        this.play_pause(value);
       },
-      immediate: true,
     },
+  },
+  mounted() {
+    if (this.is_playing) {
+      this.$emit("update:is_playing", false);
+    }
   },
   methods: {
     update_video_src() {
-      if (!this.url || !this.mime_type || !this.$refs["video"]) {
+      const video = this.$refs["video"];
+      const source = this.$refs["video-data"];
+
+      if (!this.url || !this.mime_type || !video) {
         return;
       }
-      this.$refs["video"].pause();
-
-      this.$refs["video-data"].setAttribute("src", this.url);
-      this.$refs["video-data"].setAttribute("type", this.mime_type);
-
-      this.$refs["video"].load();
+      video.pause();
+      source.setAttribute("src", this.url);
+      source.setAttribute("type", this.mime_type);
+      video.load();
     },
     play_pause() {
-      if (!this.$refs["video"]) {
+      const video = this.$refs["video"];
+      if (!video) {
         return;
       }
-      if (this.$refs["video"] && this.playing === false) {
-        this.$refs["video"].pause();
+      if (this.is_playing) {
+        video.play();
       } else {
-        this.$refs["video"].play();
+        video.pause();
       }
     },
   },
--- supervisely/app/widgets/video_player/video_player.py
+++ supervisely/app/widgets/video_player/video_player.py
@@ -6,11 +6,15 @@ from supervisely.app.widgets_context import JinjaWidgets
 
 class VideoPlayer(Widget):
     def __init__(self, url: str = None, mime_type: str = "video/mp4", widget_id: str = None):
+
         self._api = Api()
+
         self._url = url
         self._mime_type = mime_type
+
         self._current_timestamp = 0
-        self._playing = False
+        self._is_playing = False
+
         super().__init__(widget_id=widget_id, file_path=__file__)
 
         script_path = "./sly/css/app/widgets/video_player/script.js"
@@ -26,7 +30,7 @@ class VideoPlayer(Widget):
         return {
             "currentTime": 0,
             "timeToSet": 0,
-            "playing": False
+            "is_playing": False
         }
 
     @property
@@ -46,6 +50,22 @@ class VideoPlayer(Widget):
         StateJson()[self.widget_id]["currentTime"] = 0
         StateJson().send_changes()
 
+    def play(self):
+        is_playing = StateJson()[self.widget_id]["is_playing"]
+        if is_playing:
+            return
+        self._is_playing = True
+        StateJson()[self.widget_id]["is_playing"] = True
+        StateJson().send_changes()
+
+    def pause(self):
+        is_playing = StateJson()[self.widget_id]["is_playing"]
+        if not is_playing:
+            return
+        self._is_playing = False
+        StateJson()[self.widget_id]["is_playing"] = False
+        StateJson().send_changes()
+
     def get_current_timestamp(self):
         self._current_timestamp = round(StateJson()[self.widget_id]["currentTime"])
         return self._current_timestamp
@@ -55,17 +75,3 @@ class VideoPlayer(Widget):
         StateJson()[self.widget_id]["timeToSet"] = value
         StateJson().send_changes()
         return self._current_timestamp
-
-    def play(self):
-        if self._playing == True:
-            return
-        self._playing = True
-        StateJson()[self.widget_id]["playing"] = True
-        StateJson().send_changes()
-
-    def pause(self):
-        if self._playing == False:
-            return
-        self._playing = False
-        StateJson()[self.widget_id]["playing"] = False
-        StateJson().send_changes()
