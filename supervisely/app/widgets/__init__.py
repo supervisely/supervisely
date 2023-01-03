056556041c23766f8c6f1d4e10171ea71fbed25c
Nikolai Petukhov <petukhov.niko@gmail.com>
2023-01-03 16:01:52 -0300

    added RadioGroup to app.widgets

--- supervisely/app/widgets/__init__.py
+++ supervisely/app/widgets/__init__.py
@@ -1,3 +1,4 @@
+from supervisely.app.widgets.widget import ConditionalWidget, ConditionalItem
 from supervisely.app.widgets.widget import Widget, generate_id
 from supervisely.app.widgets.radio_table.radio_table import RadioTable
 from supervisely.app.widgets.notification_box.notification_box import NotificationBox
