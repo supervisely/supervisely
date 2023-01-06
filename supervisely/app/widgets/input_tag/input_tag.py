1bd681a6c4e72d8302caba6388425aaf02ea923d
Nikolai Petukhov <petukhov.niko@gmail.com>
2023-01-06 17:50:15 -0300

    reworked widget properties

--- supervisely/app/widgets/input_tag/input_tag.py
+++ supervisely/app/widgets/input_tag/input_tag.py
@@ -39,6 +39,9 @@ class InputTag(Widget):
                 content=Field(content=RadioGroup(items=items), title=tag_meta.name)
             )
 
+    def get_tag_meta(self):
+        return self._tag_meta
+
     def activate(self):
         tag_component = self._component
         tag_component.check()
@@ -47,59 +50,45 @@ class InputTag(Widget):
         tag_component = self._component
         tag_component.uncheck()
 
-    def set(self, value: Union[bool, int, str, None] = None, tag: Tag = None):
-        """
-        Sets value for the tag. If argument "tag" is present, then value is ignored.
-        Pass None as value to deactivate tag
-        """
-        if not tag is None:
-            value = tag.value
-            if tag.meta.value_type == str(TagValueType.NONE):
-                value = True
-        if value is None:
-            self.deactivate()
-            return
-        if self._tag_meta.value_type == str(TagValueType.NONE):
-            if value == True:
-                self.activate()
-            elif value == False:
-                self.deactivate()
-            else:
-                raise ValueError(f'Tag value "{value}" is invalid')
-        else:
-            self._set_tag_value(value)
-            self.activate()
+    def is_active(self):
+        return self._component.is_checked()
 
-    def get_value(self):
-        tag_component = self._component
-        if not tag_component.is_checked():
-            return None
-        content = tag_component._content._content
-        if type(content) is Empty:
-            return True
-        if type(content) is InputNumber:
-            return content.get_value()
-        if type(content) is Input:
-            return content.get_value()
-        if type(content) is RadioGroup:
-            return content.get_value()
+    @property
+    def value(self):
+        return self._get_value()
+
+    @value.setter
+    def value(self, value):
+        self._set_value(value)
+
+    def is_valid_value(self, value):
+        return self._tag_meta.is_valid_value(value)
+    
+    def set(self, tag: Tag):
+        self._set_value(tag.value)
+        self.activate()
 
     def get_tag(self):
-        tag_value = self.get_value()
-        if tag_value is None:
+        if not self.is_active():
             return None
-        if tag_value is True:
-            tag_value = None
+        tag_value = self._get_value()
         return Tag(self._tag_meta, tag_value)
 
-    def _set_tag_value(self, value):
-        if not self._tag_meta.is_valid_value(value):
+    def _get_value(self):
+        tag_component = self._component
+        content = tag_component._content._content
+        if type(content) is Empty:
+            return None
+        else:
+            return content.get_value()
+
+    def _set_value(self, value):
+        if not self.is_valid_value(value):
             raise ValueError(f'Tag value "{value}" is invalid')
         tag_component = self._component
         content = tag_component._content._content
         if type(content) is InputNumber:
             content.value = value
-            content.update_state()
         if type(content) is Input:
             content.set_value(value)
         if type(content) is RadioGroup:
