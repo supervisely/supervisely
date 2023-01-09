02ed82c89e6de72b79746efca1153bf58eea28c1
cxnt <pavelbartsits@gmail.com>
2023-01-09 15:36:00 +0400

    fix thumbnails

--- supervisely/app/widgets/dataset_thumbnail/dataset_thumbnail.py
+++ supervisely/app/widgets/dataset_thumbnail/dataset_thumbnail.py
@@ -14,44 +14,38 @@ class DatasetThumbnail(Widget):
         show_project_name: bool = True,
         widget_id: str = None,
     ):
-        self._project_info = None
-        self._dataset_info = None
-        self._show_project_name = show_project_name
-
-        self._description = None
-        self._url = None
-        self._set_info(project_info, dataset_info)
+        self._project_info: ProjectInfo = None
+        self._dataset_info: DatasetInfo = None
+        self._id: int = None
+        self._name: str = None
+        self._description: str = None
+        self._url: str = None
+        self._image_preview_url: str = None
+        self._show_project_name: bool = show_project_name
+        self._project_name: str = None
+        self._project_url: str = None
+        self._set_info(project_info, dataset_info, show_project_name)
 
         super().__init__(widget_id=widget_id, file_path=__file__)
 
     def get_json_data(self):
-        if self._project_info is None or self._dataset_info is None:
-            return {
-                "id": None,
-                "name": None,
-                "description": None,
-                "url": None,
-                "image_preview_url": None,
-                "show_project_name": self._show_project_name,
-                "project_name": None,
-                "project_url": None,
-            }
-
         return {
-            "id": self._dataset_info.id,
-            "name": self._dataset_info.name,
+            "id": self._id,
+            "name": self._name,
             "description": self._description,
             "url": self._url,
-            "image_preview_url": self._dataset_info.image_preview_url,
+            "image_preview_url": self._image_preview_url,
             "show_project_name": self._show_project_name,
-            "project_name": self._project_info.name,
-            "project_url": Project.get_url(self._project_info.id),
+            "project_name": self._project_name,
+            "project_url": self._project_url,
         }
 
     def get_json_state(self):
         return None
 
-    def _set_info(self, project_info: ProjectInfo, dataset_info: DatasetInfo):
+    def _set_info(
+        self, project_info: ProjectInfo, dataset_info: DatasetInfo, show_project_name: bool
+    ):
         if project_info is None:
             return
         if dataset_info is None:
@@ -59,13 +53,18 @@ class DatasetThumbnail(Widget):
 
         self._project_info = project_info
         self._dataset_info = dataset_info
-
+        self._id = dataset_info.id
+        self._name = dataset_info.name
         self._description = f"{self._dataset_info.items_count} {self._project_info.type} in dataset"
-        self._url = Dataset.get_url(
-            project_id=self._project_info.id, dataset_id=self._dataset_info.id
-        )
+        self._url = Dataset.get_url(project_id=project_info.id, dataset_id=dataset_info.id)
+        self._image_preview_url = dataset_info.image_preview_url
+        self._show_project_name = show_project_name
+        self._project_name = project_info.name
+        self._project_url = Project.get_url(project_info.id)
 
-    def set(self, project_info: ProjectInfo, dataset_info: DatasetInfo):
-        self._set_info(project_info, dataset_info)
+    def set(
+        self, project_info: ProjectInfo, dataset_info: DatasetInfo, show_project_name: bool = True
+    ):
+        self._set_info(project_info, dataset_info, show_project_name)
         self.update_data()
         DataJson().send_changes()
--- supervisely/app/widgets/project_thumbnail/project_thumbnail.py
+++ supervisely/app/widgets/project_thumbnail/project_thumbnail.py
@@ -7,28 +7,22 @@ from supervisely.project.project import Project
 class ProjectThumbnail(Widget):
     def __init__(self, info: ProjectInfo = None, widget_id: str = None):
         self._info: ProjectInfo = None
+        self._id: int = None
+        self._name: str = None
         self._description: str = None
         self._url: str = None
+        self._image_preview_url: str = None
         self._set_info(info)
 
         super().__init__(widget_id=widget_id, file_path=__file__)
 
     def get_json_data(self):
-        if self._info is None:
-            return {
-                "id": None,
-                "name": None,
-                "description": None,
-                "url": None,
-                "image_preview_url": None,
-            }
-
         return {
-            "id": self._info.id,
-            "name": self._info.name,
+            "id": self._id,
+            "name": self._name,
             "description": self._description,
             "url": self._url,
-            "image_preview_url": self._info.image_preview_url,
+            "image_preview_url": self._image_preview_url,
         }
 
     def get_json_state(self):
@@ -38,8 +32,11 @@ class ProjectThumbnail(Widget):
         if info is None:
             return
         self._info = info
-        self._description = f"{self._info.items_count} {self._info.type} in project"
-        self._url = Project.get_url(self._info.id)
+        self._id = info.id
+        self._name = info.name
+        self._description = f"{info.items_count} {info.type} in project"
+        self._url = Project.get_url(info.id)
+        self._image_preview_url = info.image_preview_url
 
     def set(self, info: ProjectInfo):
         self._set_info(info)
