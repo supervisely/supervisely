9bb9c41c0cef7d0664106db4d952f4381bd747e7
Maxim Kolomeychenko <maxim@supervise.ly>
2023-01-10 11:09:11 +0000

    processing video frames TODO

--- supervisely/nn/inference/video_inference.py
+++ supervisely/nn/inference/video_inference.py
@@ -10,7 +10,9 @@ from tqdm import tqdm
 
 
 class InferenceVideoInterface:
-    def __init__(self, api, start_frame_index, frames_count, frames_direction, video_info, imgs_dir):
+    def __init__(
+        self, api, start_frame_index, frames_count, frames_direction, video_info, imgs_dir
+    ):
         self.api = api
 
         self.video_info = video_info
@@ -27,7 +29,9 @@ class InferenceVideoInterface:
 
         self._add_frames_indexes()
 
-        self._frames_path = os.path.join(imgs_dir, f'video_inference_{video_info.id}_{time.time_ns()}', 'frames')
+        self._frames_path = os.path.join(
+            imgs_dir, f"video_inference_{video_info.id}_{time.time_ns()}", "frames"
+        )
         self._imgs_dir = imgs_dir
 
         self._local_video_path = None
@@ -38,17 +42,28 @@ class InferenceVideoInterface:
         total_frames = self.video_info.frames_count
         cur_index = self.start_frame_index
 
-        if self.frames_direction == 'forward':
-            end_point = cur_index + self.frames_count if cur_index + self.frames_count < total_frames else total_frames
-            self._frames_indexes = [curr_frame_index for curr_frame_index in range(cur_index, end_point, 1)]
+        if self.frames_direction == "forward":
+            end_point = (
+                cur_index + self.frames_count
+                if cur_index + self.frames_count < total_frames
+                else total_frames
+            )
+            self._frames_indexes = [
+                curr_frame_index for curr_frame_index in range(cur_index, end_point, 1)
+            ]
         else:
             end_point = cur_index - self.frames_count if cur_index - self.frames_count > -1 else -1
-            self._frames_indexes = [curr_frame_index for curr_frame_index in range(cur_index, end_point, -1)]
+            self._frames_indexes = [
+                curr_frame_index for curr_frame_index in range(cur_index, end_point, -1)
+            ]
             self._frames_indexes = []
 
     def _download_video_by_frames(self):
-        for index, frame_index in tqdm(enumerate(self._frames_indexes), desc='Downloading frames',
-                                       total=len(self._frames_indexes)):
+        for index, frame_index in tqdm(
+            enumerate(self._frames_indexes),
+            desc="Downloading frames",
+            total=len(self._frames_indexes),
+        ):
             frame_path = os.path.join(f"{self._frames_path}", f"frame{index:06d}.png")
             self.images_paths.append(frame_path)
 
@@ -56,8 +71,9 @@ class InferenceVideoInterface:
                 continue
 
             img_rgb = self.api.video.frame.download_np(self.video_info.id, frame_index)
-            cv2.imwrite(os.path.join(f"{self._frames_path}", f"frame{index:06d}.png"),
-                        img_rgb)  # save frame as PNG file
+            cv2.imwrite(
+                os.path.join(f"{self._frames_path}", f"frame{index:06d}.png"), img_rgb
+            )  # save frame as PNG file
 
     def _download_entire_video(self):
         def videos_to_frames(video_path, frames_range=None):
@@ -69,11 +85,12 @@ class InferenceVideoInterface:
                 # we are looking for
                 rotate_code = None
                 try:
-                    if int(meta_dict['streams'][0]['tags']['rotate']) == 90:
+                    # @TODO: process -90/-180/-270 (issue #1695)
+                    if int(meta_dict["streams"][0]["tags"]["rotate"]) == 90:
                         rotate_code = cv2.ROTATE_90_CLOCKWISE
-                    elif int(meta_dict['streams'][0]['tags']['rotate']) == 180:
+                    elif int(meta_dict["streams"][0]["tags"]["rotate"]) == 180:
                         rotate_code = cv2.ROTATE_180
-                    elif int(meta_dict['streams'][0]['tags']['rotate']) == 270:
+                    elif int(meta_dict["streams"][0]["tags"]["rotate"]) == 270:
                         rotate_code = cv2.ROTATE_90_COUNTERCLOCKWISE
                 except Exception as ex:
                     pass
@@ -106,18 +123,23 @@ class InferenceVideoInterface:
 
             fps = vidcap.get(cv2.CAP_PROP_FPS)
 
-            return {'frames_path': self._frames_path, 'fps': fps, 'video_path': video_path}
+            return {"frames_path": self._frames_path, "fps": fps, "video_path": video_path}
 
-        self._local_video_path = os.path.join(self._imgs_dir, f'{time.time_ns()}_{self.video_info.name}')
+        self._local_video_path = os.path.join(
+            self._imgs_dir, f"{time.time_ns()}_{self.video_info.name}"
+        )
         self.api.video.download_path(self.video_info.id, self._local_video_path)
-        return videos_to_frames(self._local_video_path, [self.start_frame_index, self.start_frame_index + self.frames_count - 1])
+        return videos_to_frames(
+            self._local_video_path,
+            [self.start_frame_index, self.start_frame_index + self.frames_count - 1],
+        )
 
     def download_frames(self):
         if self.frames_count > (self.video_info.frames_count * 0.3):
-            sly.logger.debug('Downloading entire video')
+            sly.logger.debug("Downloading entire video")
             self._download_entire_video()
         else:
-            sly.logger.debug('Downloading video frame by frame')
+            sly.logger.debug("Downloading video frame by frame")
             self._download_video_by_frames()
 
     def __del__(self):
