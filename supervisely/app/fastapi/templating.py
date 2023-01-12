aef686287b8429525d55f3dbfce3558691192ba0
almaz_macbook <almazgimaev@gmail.com>
2023-01-12 17:29:01 +0300

    ugraded js bundle version

--- supervisely/app/fastapi/templating.py
+++ supervisely/app/fastapi/templating.py
@@ -9,10 +9,10 @@ from supervisely.app.singleton import Singleton
 from supervisely.app.widgets_context import JinjaWidgets
 
 # https://github.com/supervisely/js-bundle
-js_bundle_version = "2.1.30"
+js_bundle_version = "2.1.32"
 
 # https://github.com/supervisely-ecosystem/supervisely-app-frontend-js
-js_frontend_version = "0.0.35"
+js_frontend_version = "0.0.36"
 
 
 class Jinja2Templates(_fastapi_Jinja2Templates, metaclass=Singleton):
