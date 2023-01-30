7587ebe89a33ca5f64f9db96876c98e89e6639b8
Nikolai Petukhov <petukhov.niko@gmail.com>
2023-01-29 22:56:19 -0300

    added selector and get_stat() method

--- supervisely/app/widgets/match_tags_or_classes/match_tags_or_classes.py
+++ supervisely/app/widgets/match_tags_or_classes/match_tags_or_classes.py
@@ -1,5 +1,5 @@
 from supervisely.app.widgets import Widget
-from supervisely.app import DataJson
+from supervisely.app import DataJson, StateJson
 from supervisely import (
     TagMetaCollection,
     ObjClassCollection,
@@ -15,20 +15,22 @@ class MatchTagMetasOrClasses(Widget):
         self,
         left_collection: Union[TagMetaCollection, ObjClassCollection],
         right_collection: Union[TagMetaCollection, ObjClassCollection],
+        selectable: bool = False,
         widget_id: str = None,
     ):
         self._left_collection = left_collection
         self._right_collection = right_collection
+        self._selectable = selectable
 
         self._table = self._get_table()
 
         super().__init__(widget_id=widget_id, file_path=__file__)
 
     def get_json_data(self):
-        return {"table": self._table}
+        return {"table": self._table, "selectable": self._selectable}
 
     def get_json_state(self):
-        return {}
+        return {"selected": []}
     
     def set(
         self,
@@ -42,11 +44,36 @@ class MatchTagMetasOrClasses(Widget):
         self._table = self._get_table()
         DataJson()[self.widget_id]["table"] = self._table
         DataJson().send_changes()
+        StateJson()[self.widget_id] = self.get_json_state()
+        StateJson().send_changes()
 
-    def _get_table(
-        self,
-        diff_msg="Automatic conversion to rectangle format",
-    ):
+    def get_stat(self):
+        stat = {
+            "match": [],
+            "only_left": [],
+            "only_right": [],
+            "different_shape": [],
+            "different_value_type": [],
+            "different_one_of_options": [],
+        }
+        for row in self._table:
+            message = row.get("infoMessage")
+            name = row["name1"] if "name1" in row.keys() else row["name2"]
+            if message == "Match":
+                stat["match"].append(name)
+            elif message == "Not found in right Project":
+                stat["only_left"].append(name)
+            elif message == "Not found in left Project":
+                stat["only_right"].append(name)
+            elif message == "Different shape":
+                stat["different_shape"].append(name)
+            elif message == "Different value type":
+                stat["different_value_type"].append(name)
+            elif message == "Type OneOf: conflict of possible values":
+                stat["different_one_of_options"].append(name)
+        return stat
+
+    def _get_table(self):
         items1 = {item.name: 1 for item in self._left_collection}
         items2 = {item.name: 1 for item in self._right_collection}
         names = items1.keys() | items2.keys()
@@ -78,8 +105,6 @@ class MatchTagMetasOrClasses(Widget):
             if meta2 is not None:
                 set_info(compare, 2, meta2)
 
-            compare["infoMessage"] = "Match"
-            compare["infoColor"] = "green"
             if name in mutual:
                 flag = True
                 if (
@@ -87,15 +112,16 @@ class MatchTagMetasOrClasses(Widget):
                     and meta1.geometry_type != meta2.geometry_type
                 ):
                     flag = False
+                    diff_msg = "Different shape"
                 if type(meta1) is TagMeta:
                     meta1: TagMeta
                     meta2: TagMeta
                     if meta1.value_type != meta2.value_type:
                         flag = False
-                    if meta1.value_type == TagValueType.ONEOF_STRING:
-                        if set(meta1.possible_values) != set(meta2.possible_values):
-                            diff_msg = "Type OneOf: conflict of possible values"
+                        diff_msg = "Different value type"
+                    if meta1.value_type == TagValueType.ONEOF_STRING and set(meta1.possible_values) != set(meta2.possible_values):
                         flag = False
+                        diff_msg = "Type OneOf: conflict of possible values"
 
                 if flag is False:
                     compare["infoMessage"] = diff_msg
@@ -103,18 +129,20 @@ class MatchTagMetasOrClasses(Widget):
                     compare["infoIcon"] = (["zmdi zmdi-flag"],)
                     differ.append(compare)
                 else:
+                    compare["infoMessage"] = "Match"
+                    compare["infoColor"] = "green"
                     compare["infoIcon"] = (["zmdi zmdi-check"],)
                     match.append(compare)
             else:
                 if name in diff1:
-                    compare["infoMessage"] = "Not found in PRED Project"
+                    compare["infoMessage"] = "Not found in right Project"
                     compare["infoIcon"] = [
                         "zmdi zmdi-alert-circle-o",
                         "zmdi zmdi-long-arrow-right",
                     ]
                     compare["iconPosition"] = "right"
                 else:
-                    compare["infoMessage"] = "Not found in GT Project"
+                    compare["infoMessage"] = "Not found in left Project"
                     compare["infoIcon"] = [
                         "zmdi zmdi-long-arrow-left",
                         "zmdi zmdi-alert-circle-o",
@@ -132,3 +160,6 @@ class MatchTagMetasOrClasses(Widget):
         table.extend(missed)
 
         return table
+
+    def get_selected(self):
+        return StateJson()[self.widget_id]["selected"]
