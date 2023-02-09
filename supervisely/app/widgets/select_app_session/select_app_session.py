82d069c94d4f82e5ac002494eae26d2b5b06c07d
MaxTeselkin <tesmaxxxx@gmail.com>
2023-02-09 21:58:47 +0300

    update select_app_session

--- supervisely/app/widgets/select_app_session/select_app_session.py
+++ supervisely/app/widgets/select_app_session/select_app_session.py
@@ -2,6 +2,7 @@ try:
     from typing import Literal
 except ImportError:
     from typing_extensions import Literal
+from typing import List
 import supervisely as sly
 from supervisely.app import StateJson
 from supervisely.app.widgets import Widget
@@ -10,32 +11,34 @@ from supervisely.app.widgets import Widget
 class SelectAppSession(Widget):
     def __init__(
         self,
-        team_id: int = None,
-        allowed_session_tags: list = ["deployed_nn"],
+        team_id: int,
+        tags: List[str],
         show_label: bool = False,
         size: Literal["large", "small", "mini"] = "mini",
         widget_id: str = None,
+        operation: str = "or",
     ):
         self._session_id = None
         self._team_id = team_id
-        self._allowed_session_tags = allowed_session_tags
+        self._tags = tags
         self._show_label = show_label
         self._size = size
+        self._operation = operation
 
-        if self._team_id is None:
-            self._team_id = sly.env.team_id()
+        if len(tags) < 1:
+            raise ValueError("Parameter tags must be a list of strings, but got empty list")
+
         super().__init__(widget_id=widget_id, file_path=__file__)
 
     def get_json_data(self):
         data = {}
         data["teamId"] = self._team_id
         data["ssOptions"] = {
-            "sessionTags": self._allowed_session_tags,
+            "sessionTags": self._tags,
             "showLabel": self._show_label,
             "size": self._size,
+            "sessionTagsOperation": self._operation,
         }
-        if len(self._allowed_session_tags) > 1:
-            data["ssOptions"]["sessionTagsOperation"] = "or"
         return data
 
     def get_json_state(self):
