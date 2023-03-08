653c4126a8a6ea147cdb691fda71d609561d8053
almaz_macbook <almazgimaev@gmail.com>
2023-03-08 14:41:11 +0300

    fix widget file and template

--- supervisely/app/widgets/file_storage_upload/file_storage_upload.py
+++ supervisely/app/widgets/file_storage_upload/file_storage_upload.py
@@ -13,23 +13,20 @@ class FileStorageUpload(Widget):
         self._api = Api()
         self._team_id = team_id
         self._folder_name = None
+        self._files = []
         if folder_name is not None:
-            self._folder_name = self._api.file.get_free_dir_name(self._team_id, dir_path=folder_name)
+            self._folder_name = self._api.file.get_free_dir_name(self._team_id, folder_name)
         else:
             path = "/import-from-widget/"
-            self._folder_name = self._api.file.get_free_dir_name(self._team_id, dir_path=path)
+            self._folder_name = self._api.file.get_free_dir_name(self._team_id, path)
 
         super().__init__(widget_id=widget_id, file_path=__file__)
 
     def get_json_data(self):
-        return {
-            "teamId": self._team_id,
-            "folderName": self._folder_name,
-        }
+        return {"folderName": self._folder_name}
 
     def get_json_state(self):
-        return None
-
+        return {"files": self._files}
 
     def set_folder_name(self, value):
         self._folder_name = self._api.file.get_free_dir_name(self._team_id, dir_path=value)
