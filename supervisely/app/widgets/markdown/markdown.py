e3fe7979210f9b3b679a4ba9f7661aa8b023d974
peixed351 <peixed351@gmail.com>
2023-03-10 15:49:34 +0300

    widget_markdown

---
+++ supervisely/app/widgets/markdown/markdown.py
@@ -0,0 +1,28 @@
+from supervisely.app import DataJson, StateJson
+from supervisely.app.widgets import Widget
+from typing import Dict
+
+
+class Markdown(Widget):
+    def __init__(self, md: str = "", height: int = 300, widget_id: str = None):
+        self._md = md
+        self._height = f"{height}px"
+
+        super().__init__(widget_id=widget_id, file_path=__file__)
+
+    def get_json_data(self) -> Dict:
+        return {"md": self._md, "options": {"height": self._height}}
+
+    def get_json_state(self) -> Dict:
+        return {}
+
+    def set_value(self, value: str):
+        self._md = value
+        DataJson()[self.widget_id]["md"] = value
+        DataJson().send_changes()
+
+    def get_value(self):
+        return DataJson()[self.widget_id]["md"]
+
+    def get_height(self):
+        return DataJson()[self.widget_id]["options"]["height"]
