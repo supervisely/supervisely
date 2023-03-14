66df95e725dd4460d7161669ee3acabd21a85128
MaxTeselkin <tesmaxxxx@gmail.com>
2023-03-14 04:46:51 +0300

    added salient object segmentation support

--- supervisely/nn/inference/__init__.py
+++ supervisely/nn/inference/__init__.py
@@ -8,4 +8,5 @@ from supervisely.nn.inference.semantic_segmentation.semantic_segmentation import
     SemanticSegmentation,
 )
 from supervisely.nn.inference.pose_estimation.pose_estimation import PoseEstimation
+from supervisely.nn.inference.salient_object_segmentation import SalientObjectSegmentation
 from supervisely.nn.inference.session import Session, SessionJSON
