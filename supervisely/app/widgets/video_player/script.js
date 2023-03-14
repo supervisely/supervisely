5edc84b62245b2d5e99d92328f28dc59b67be0fa
cxnt <pavelbartsits@gmail.com>
2023-03-14 17:04:10 +0400

    fix VideoPlayer widget rewind

--- supervisely/app/widgets/video_player/script.js
+++ supervisely/app/widgets/video_player/script.js
@@ -1,5 +1,5 @@
 Vue.component("html-video", {
-  props: ["time_to_set", "url", "mime_type", "is_playing", "mask_path"],
+  props: ["timeToSet", "url", "mimeType", "isPlaying", "maskPath"],
   template: `
 <div style="position: relative;">
     <video ref="video"
@@ -7,12 +7,12 @@ Vue.component("html-video", {
         height="auto"
         controls
         @timeupdate="$emit('timeupdate', $refs['video'].currentTime)"
-        @play="$emit('update:is_playing', true)"
-        @pause="$emit('update:is_playing', false)"
+        @play="$emit('update:is-playing', true)"
+        @pause="$emit('update:is-playing', false)"
     >
-        <source ref="video-data" :src="url" :type="mime_type">
+        <source ref="video-data" :src="url" :type="mimeType">
     </video>
-    <div v-if="mask_path"
+    <div v-if="maskPath"
         ref="mask"
         style="
             opacity: 0.4;
@@ -28,59 +28,59 @@ Vue.component("html-video", {
   computed: {
     overlayStyle() {
       return {
-        backgroundImage: `url("${this.mask_path}")`,
+        backgroundImage: `url("${this.maskPath}")`,
       };
     },
   },
   watch: {
-    time_to_set(time) {
+    timeToSet(time) {
       if (Number.isFinite(time)) {
         this.$refs["video"].currentTime = time;
-        this.$emit("update:time_to_set", null);
+        this.$emit("update:time-to-set", null);
       }
     },
     url: {
       handler() {
-        this.update_video_src();
+        this.updateVideoSrc();
       },
       immediate: true,
     },
-    mime_type: {
+    mimeType: {
       handler() {
-        this.update_video_src();
+        this.updateVideoSrc();
       },
       immediate: true,
     },
-    is_playing: {
+    isPlaying: {
       handler(value) {
-        this.play_pause(value);
+        this.playPause(value);
       },
     },
   },
   mounted() {
-    if (this.is_playing) {
-      this.$emit("update:is_playing", false);
+    if (this.isPlaying) {
+      this.$emit("update:is-playing", false);
     }
   },
   methods: {
-    update_video_src() {
+    updateVideoSrc() {
       const video = this.$refs["video"];
       const source = this.$refs["video-data"];
 
-      if (!this.url || !this.mime_type || !video) {
+      if (!this.url || !this.mimeType || !video) {
         return;
       }
       video.pause();
       source.setAttribute("src", this.url);
-      source.setAttribute("type", this.mime_type);
+      source.setAttribute("type", this.mimeType);
       video.load();
     },
-    play_pause() {
+    playPause() {
       const video = this.$refs["video"];
       if (!video) {
         return;
       }
-      this.is_playing ? video.play() : video.pause();
+      this.isPlaying ? video.play() : video.pause();
     },
   },
 });
