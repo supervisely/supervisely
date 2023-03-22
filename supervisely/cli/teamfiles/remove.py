7009b2faf9d23025afee3bc68df72aaa2d597425
Ars Vladimirov <grokhael@gmail.com>
2023-03-23 00:48:25 +0600

    add remove_file to sdk

--- supervisely/cli/teamfiles/remove.py
+++ supervisely/cli/teamfiles/remove.py
@@ -11,13 +11,7 @@ def remove_file(team_id, path):
     api = sly.Api.from_env()
 
     try:
-        file_info = api.file.get_info_by_path(team_id, path)
-
-        if file_info is None:
-            print(f'Not a file. Maybe you entered directory? (Path: {path})')
-            return False
-        
-        api.file.remove(team_id, path)
+        api.file.remove_file(team_id, path)
         return True
     
     except:
@@ -29,13 +23,9 @@ def remove_dir(team_id, path):
     api = sly.Api.from_env()
     
     try:
-        if api.file.dir_exists(team_id, path):
-            api.file.remove(team_id, path)
-            return True
-        
-        print(f"Directory '{path}' not exists.")
-        return False
+        api.file.remove_dir(team_id, path)
+        return True
     
-    except Exception as e:
+    except:
         traceback.print_exc()
         return False
