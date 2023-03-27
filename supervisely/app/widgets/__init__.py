b2b5e795dc99532f92b7c3695ffeddd862bd132e
GoldenAnpu <sych.sergius@gmail.com>
2023-03-27 18:06:48 +0200

    resolve conflict in pr #486 next try

--- supervisely/app/widgets/__init__.py
+++ supervisely/app/widgets/__init__.py
@@ -76,7 +76,7 @@ from supervisely.app.widgets.augmentations.augmentations import Augmentations, A
 
 from supervisely.app.widgets.tabs_dynamic.tabs_dynamic import TabsDynamic
 from supervisely.app.widgets.stepper.stepper import Stepper
-from supervisely.app.widgets.datetime_picker.datetime_picker import DateTimePicker
 from supervisely.app.widgets.slider.slider import Slider
 from supervisely.app.widgets.file_storage_upload.file_storage_upload import FileStorageUpload
 from supervisely.app.widgets.image_region_selector.image_region_selector import ImageRegionSelector
+from supervisely.app.widgets.datetime_picker.datetime_picker import DateTimePicker
