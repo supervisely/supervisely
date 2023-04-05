bb85956a6027cb862814234b4da6906c459705e8
peixed351 <peixed351@gmail.com>
2023-04-05 18:34:31 +0300

    add dev prod

--- tests/progress_tqdm.py
+++ tests/progress_tqdm.py
@@ -1,21 +1,31 @@
 import supervisely as sly
 from dotenv import load_dotenv
 import os
+from time import sleep
 from tqdm import tqdm
 
 load_dotenv(os.path.expanduser("~/supervisely.env"))
 api = sly.Api()
+os.environ["ENV"] = "production"
 
 
 class MyTqdm(sly.Progress, tqdm):
-    def __init__(self, message, total_cnt):
+    def __init__(self, *args, **kwargs):
 
-        self.message = message
-        self.total = total_cnt
-        super().__init__(message=message, total_cnt=total_cnt)
+        self.temp_current = 0
+
+        super().__init__(*args, **kwargs)
 
     def write(self, count):
-        super().iters_done_report(count)
+        if sly.is_development():
+            self.temp_current += count
+            if self.temp_current != self.total:
+                pass
+            else:
+                for _ in tqdm(range(self.total)):
+                    sleep(0.01)
+        else:
+            super().iters_done_report(count)
 
 
 data_len = 120
@@ -25,16 +35,8 @@ data_list = [i for i in range(data_len)]
 def test_progress(x, progress):
 
     batch_size = 30
-    if type(progress) == sly.Progress:
-        for i in sly.batched(seq=x, batch_size=batch_size):
-            progress.iters_done_report(batch_size)
-    else:
-        for i in sly.batched(seq=x, batch_size=batch_size):
-            progress.write(batch_size)
-
-
-progress = sly.Progress("Uploading data:", total_cnt=data_len)
-test_progress(data_list, progress)
+    for i in sly.batched(seq=x, batch_size=batch_size):
+        progress.write(batch_size)
 
 
 progress_tqdm = MyTqdm(message="Uploading data:", total_cnt=data_len)
