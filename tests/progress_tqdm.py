00f0d14338e86d339d850dd42522be354406a5a5
peixed351 <peixed351@gmail.com>
2023-04-07 16:24:10 +0300

    in progress

--- tests/progress_tqdm.py
+++ tests/progress_tqdm.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import supervisely as sly
 from dotenv import load_dotenv
-import os
+import os, math
 from time import sleep
 from tqdm import tqdm
 from typing import Optional
@@ -34,26 +34,26 @@ class MyTqdm(tqdm, sly.Progress):
         self.need_info_log = need_info_log
         self.min_report_percent = min_report_percent
 
-        if sly.is_production():
-            self.progress = sly.Progress(
-                message=self.message,
-                total_cnt=self.total,
-                ext_logger=self.ext_logger,
-                is_size=self.is_size,
-                need_info_log=self.need_info_log,
-                min_report_percent=self.min_report_percent,
-            )
+        self.current = 0
+        self.is_total_unknown = total_cnt == 0
+
+        self.total_label = ""
+        self.current_label = ""
+        self.reported_cnt = 0
+        self.logger = logger if ext_logger is None else ext_logger
+        self.report_every = max(1, math.ceil(total_cnt / 100 * min_report_percent))
+        self.need_info_log = need_info_log
 
         if sly.is_development():
             super().__init__(total=total_cnt, desc=message, *args, **kwargs)
         else:
-            super().__init__(disable=True)
+            super().__init__(disable=True, total=total_cnt)
 
     def update(self, count):
         if sly.is_development():
             super().update(count)
         else:
-            self.progress.iters_done_report(count)
+            super().iters_done_report(count)
 
 
 data_len = 145
@@ -67,17 +67,17 @@ def test_progress(x, progress):
         progress.update(len(batch))
 
 
-progress_tqdm = MyTqdm(message="Uploading data", total_cnt=data_len)
-test_progress(data_list, progress_tqdm)
-progress_tqdm.close()
+# progress_tqdm = MyTqdm(message="Uploading data", total_cnt=data_len)
+# test_progress(data_list, progress_tqdm)
+# progress_tqdm.close()
 
 
-# size = api.file.get_directory_size(439, "/test.tar")
-# progress_tqdm_file = MyTqdm("File downloaded: ", total_cnt=size, is_size=True)
-# api.file.download(
-#     team_id=439,
-#     remote_path="/test.tar",
-#     local_save_path="/home/alex/Downloads/test.tar",
-#     progress_cb=progress_tqdm_file.update,
-# )
-# progress_tqdm_file.close()
+size = api.file.get_directory_size(439, "/test.tar")
+progress_tqdm_file = MyTqdm("File downloaded: ", total_cnt=size, is_size=True)
+api.file.download(
+    team_id=439,
+    remote_path="/test.tar",
+    local_save_path="/home/alex/Downloads/test.tar",
+    progress_cb=progress_tqdm_file.update,
+)
+progress_tqdm_file.close()
