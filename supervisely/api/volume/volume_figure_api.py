e6933863e1846ea9b5af72b6170a1755ab54a8d1
almaz_macbook <almazgimaev@gmail.com>
2023-04-10 16:17:05 +0300

    backward compatibility in create figure method

--- supervisely/api/volume/volume_figure_api.py
+++ supervisely/api/volume/volume_figure_api.py
@@ -31,8 +31,12 @@ class VolumeFigureApi(FigureApi):
             volume_id,
             object_id,
             {
-                constants.SLICE_INDEX: slice_index,
-                constants.NORMAL: Plane.get_normal(plane_name),
+                ApiField.META: {
+                    constants.SLICE_INDEX: slice_index,
+                    constants.NORMAL: Plane.get_normal(plane_name),
+                },
+                constants.SLICE_INDEX: slice_index,  # for backward compatibility
+                constants.NORMAL: Plane.get_normal(plane_name),  # for backward compatibility
             },
             geometry_json,
             geometry_type,
@@ -52,26 +56,20 @@ class VolumeFigureApi(FigureApi):
 
     def _download_geometries_batch(self, ids):
         for batch_ids in batched(ids):
-            response = self._api.post(
-                "figures.bulk.download.geometry", {ApiField.IDS: batch_ids}
-            )
+            response = self._api.post("figures.bulk.download.geometry", {ApiField.IDS: batch_ids})
             decoder = MultipartDecoder.from_response(response)
             for part in decoder.parts:
                 content_utf8 = part.headers[b"Content-Disposition"].decode("utf-8")
                 # Find name="1245" preceded by a whitespace, semicolon or beginning of line.
                 # The regex has 2 capture group: one for the prefix and one for the actual name value.
-                figure_id = int(
-                    re.findall(r'(^|[\s;])name="(\d*)"', content_utf8)[0][1]
-                )
+                figure_id = int(re.findall(r'(^|[\s;])name="(\d*)"', content_utf8)[0][1])
                 yield figure_id, part
 
     def download_stl_meshes(self, ids, paths):
         if len(ids) == 0:
             return
         if len(ids) != len(paths):
-            raise RuntimeError(
-                'Can not match "ids" and "paths" lists, len(ids) != len(paths)'
-            )
+            raise RuntimeError('Can not match "ids" and "paths" lists, len(ids) != len(paths)')
 
         id_to_path = {id: path for id, path in zip(ids, paths)}
         for img_id, resp_part in self._download_geometries_batch(ids):
@@ -79,9 +77,7 @@ class VolumeFigureApi(FigureApi):
             with open(id_to_path[img_id], "wb") as w:
                 w.write(resp_part.content)
 
-    def interpolate(
-        self, volume_id, spatial_figure: VolumeFigure, key_id_map: KeyIdMap
-    ):
+    def interpolate(self, volume_id, spatial_figure: VolumeFigure, key_id_map: KeyIdMap):
         if type(spatial_figure._geometry) != ClosedSurfaceMesh:
             raise TypeError(
                 "Interpolation can be created only for figures with geometry ClosedSurfaceMesh"
