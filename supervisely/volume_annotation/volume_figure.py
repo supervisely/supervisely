a1ae2de7dd77789b88559fdfefd5beabb25910e1
almaz_macbook <almazgimaev@gmail.com>
2023-04-12 13:56:25 +0300

    add docstring

--- supervisely/volume_annotation/volume_figure.py
+++ supervisely/volume_annotation/volume_figure.py
@@ -23,6 +23,62 @@ from supervisely.volume_annotation.volume_object_collection import (
 
 
 class VolumeFigure(VideoFigure):
+    """
+    VolumeFigure object for :class:`VolumeAnnotation<supervisely.volume_annotation.volume_annotation.VolumeAnnotation>`. :class:`VolumeFigure<VolumeFigure>` object is immutable.
+
+    :param volume_object: VolumeObject object.
+    :type volume_object: VolumeObject
+    :param geometry: Label :class:`geometry<supervisely.geometry.geometry.Geometry>`.
+    :type geometry: Geometry
+    :param plane_name: Name of the volume plane.
+    :type plane_name: str
+    :param slice_index: Index of slice to which VolumeFigure belongs.
+    :type slice_index: int
+    :param key: KeyIdMap object.
+    :type key: KeyIdMap, optional
+    :param class_id: ID of :class:`VolumeObject<VolumeObject>` to which VolumeFigure belongs.
+    :type class_id: int, optional
+    :param labeler_login: Login of the user who created VolumeFigure.
+    :type labeler_login: str, optional
+    :param updated_at: Date and Time when VolumeFigure was modified last. Date Format: Year:Month:Day:Hour:Minute:Seconds. Example: '2021-01-22T19:37:50.158Z'.
+    :type updated_at: str, optional
+    :param created_at: Date and Time when VolumeFigure was created. Date Format is the same as in "updated_at" parameter.
+    :type created_at: str, optional
+    :Usage example:
+
+     .. code-block:: python
+
+        import supervisely as sly
+
+        obj_class_heart = sly.ObjClass('heart', sly.Rectangle)
+        volume_obj_heart = sly.VolumeObject(obj_class_heart)
+        slice_index = 7
+        plane_name = "axial"
+        geometry = sly.Rectangle(0, 0, 100, 100)
+        volume_figure_heart = sly.VolumeFigure(volume_obj_heart, geometry, slice_index)
+        volume_figure_heart_json = volume_figure_heart.to_json()
+        print(volume_figure_heart_json)
+        # Output: {
+        #     "geometry": {
+        #         "points": {
+        #         "exterior": [
+        #             [0, 0],
+        #             [100, 100]
+        #         ],
+        #         "interior": []
+        #         }
+        #     },
+        #     "geometryType": "rectangle",
+        #     "key": "158e6cf4f4ac4c639fc6994aad127c16",
+        #     "meta": {
+        #         "normal": { "x": 0, "y": 0, "z": 1 },
+        #         "planeName": "axial",
+        #         "sliceIndex": 7
+        #     },
+        #     "objectKey": "bf63ffe342e949899d3ddcb6b0f73f54"
+        # }
+    """
+
     def __init__(
         self,
         volume_object,
@@ -53,6 +109,8 @@ class VolumeFigure(VideoFigure):
 
     @property
     def volume_object(self) -> VolumeObject:
+        """Get a parent VolumeObject object of volume figure."""
+        
         return self._video_object
 
     @property
@@ -63,6 +121,8 @@ class VolumeFigure(VideoFigure):
 
     @property
     def parent_object(self) -> VolumeObject:
+        """Get a parent VolumeObject object of volume figure."""
+
         return self.volume_object
 
     @property
@@ -71,14 +131,20 @@ class VolumeFigure(VideoFigure):
 
     @property
     def slice_index(self):
+        """Get a slice index of volume figure."""
+
         return self._slice_index
 
     @property
     def plane_name(self):
+        """Get a plane name of volume figure."""
+
         return self._plane_name
 
     @property
     def normal(self):
+        """Get a normal vector associated with a plane name."""
+
         from supervisely.volume_annotation.plane import Plane
 
         return Plane.get_normal(self.plane_name)
@@ -101,6 +167,35 @@ class VolumeFigure(VideoFigure):
         super()._validate_geometry()
 
     def validate_bounds(self, img_size, _auto_correct=False):
+        """
+        Checks if given image with given size contains a figure.
+
+        :param img_size: Size of the image (height, width).
+        :type img_size: Tuple[int, int]
+        :param _auto_correct: Correct the geometry of a shape if it is out of bounds or not.
+        :type _auto_correct: bool, optional
+        :raises: :class:`OutOfImageBoundsException<supervisely.video_annotation.video_figure.OutOfImageBoundsException>`, if figure is out of image bounds
+        :return: None
+        :rtype: :class:`NoneType`
+
+        :Usage Example:
+
+         .. code-block:: python
+
+            import supervisely as sly
+
+            obj_class_heart = sly.ObjClass('heart', sly.Rectangle)
+            volume_obj_heart = sly.VolumeObject(obj_class_heart)
+            slice_index = 7
+            plane_name = "axial"
+            geometry = sly.Rectangle(0, 0, 100, 100)
+            volume_figure_heart = sly.VolumeFigure(volume_obj_heart, geometry, plane_name, slice_index)
+
+            im_size = (50, 200)
+            volume_figure_heart.validate_bounds(im_size)
+            # raise OutOfImageBoundsException("Figure is out of image bounds")
+        """
+
         if type(self._geometry) == ClosedSurfaceMesh:
             return
         super().validate_bounds(img_size, _auto_correct)
@@ -117,6 +212,73 @@ class VolumeFigure(VideoFigure):
         updated_at=None,
         created_at=None,
     ):
+        """
+        Makes a copy of VolumeFigure with new fields, if fields are given, otherwise it will use fields of the original VolumeFigure.
+
+        :param volume_object: VolumeObject object.
+        :type volume_object: VolumeObject, optional
+        :param geometry: Label :class:`geometry<supervisely.geometry.geometry.Geometry>`.
+        :type geometry: Geometry, optional
+        :param plane_name: Name of the volume plane.
+        :type plane_name: str, optional
+        :param slice_index: Index of slice to which VolumeFigure belongs.
+        :type slice_index: int, optional
+        :param key: KeyIdMap object.
+        :type key: KeyIdMap, optional
+        :param class_id: ID of :class:`ObjClass<supervisely.annotation.obj_class.ObjClass>` to which VolumeFigure belongs.
+        :type class_id: int, optional
+        :param labeler_login: Login of the user who created VolumeFigure.
+        :type labeler_login: str, optional
+        :param updated_at: Date and Time when VolumeFigure was modified last. Date Format: Year:Month:Day:Hour:Minute:Seconds. Example: '2021-01-22T19:37:50.158Z'.
+        :type updated_at: str, optional
+        :param created_at: Date and Time when VolumeFigure was created. Date Format is the same as in "updated_at" parameter.
+        :type created_at: str, optional
+        :return: VolumeFigure object
+        :rtype: :class:`VolumeFigure`
+
+        :Usage example:
+
+         .. code-block:: python
+
+            import supervisely as sly
+
+            obj_class_heart = sly.ObjClass('heart', sly.Rectangle)
+            volume_obj_heart = sly.VolumeObject(obj_class_heart)
+            slice_index = 7
+            plane_name = "axial"
+            geometry = sly.Rectangle(0, 0, 100, 100)
+            volume_figure_heart = sly.VolumeFigure(volume_obj_heart, geometry, plane_name, slice_index)
+
+            obj_class_lang = sly.ObjClass('lang', sly.Rectangle)
+            volume_obj_lang = sly.VolumeObject(obj_class_lang)
+            slice_index_lang = 15
+            geometry_lang = sly.Rectangle(0, 0, 500, 600)
+
+            # Remember that VolumeFigure object is immutable, and we need to assign new instance of VolumeFigure to a new variable
+            volume_figure_lang = volume_figure_heart.clone(volume_object=volume_obj_lang, geometry=geometry_lang, slice_index=slice_index_lang)
+            print(volume_figure_lang.to_json())
+            # Output: {
+            #     "geometry": {
+            #         "points": {
+            #         "exterior": [
+            #             [0, 0],
+            #             [600, 500]
+            #         ],
+            #         "interior": []
+            #         }
+            #     },
+            #     "geometryType": "rectangle",
+            #     "key": "2974165267224bf6b677e17ca2304b04",
+            #     "meta": {
+            #         "normal": { "x": 0, "y": 0, "z": 1 },
+            #         "planeName": "axial",
+            #         "sliceIndex": 15
+            #     },
+            #     "objectKey": "dafe3adaacad474ba5163ecebcc57cd0"
+            # }
+
+        """
+
         return self.__class__(
             volume_object=take_with_default(volume_object, self.parent_object),
             geometry=take_with_default(geometry, self.geometry),
@@ -130,6 +292,10 @@ class VolumeFigure(VideoFigure):
         )
 
     def get_meta(self):
+        """
+        Get a dictionary with metadata associated with volume figure.
+        """
+
         return {
             constants.SLICE_INDEX: self.slice_index,
             constants.PLANE_NAME: self.plane_name,
@@ -145,6 +311,38 @@ class VolumeFigure(VideoFigure):
         slice_index,
         key_id_map: KeyIdMap = None,
     ):
+        """
+        Convert a json dict to VolumeFigure. Read more about `Supervisely format <https://docs.supervise.ly/data-organization/00_ann_format_navi>`_.
+
+        :param data: Dict in json format.
+        :type data: dict
+        :param objects: VolumeObjectCollection object.
+        :type objects: VolumeObjectCollection
+        :param plane_name: Name of the volume plane.
+        :type plane_name: str
+        :param slice_index: Index of slice to which VolumeFigure belongs.
+        :type slice_index: int
+        :param key_id_map: KeyIdMap object.
+        :type key_id_map: KeyIdMap, optional
+        :raises: :class:`RuntimeError`, if volume object ID and volume object key are None, if volume object key and key_id_map are None, if volume object with given id not found in key_id_map
+        :return: VolumeFigure object
+        :rtype: :class:`VolumeFigure`
+
+        :Usage example:
+
+         .. code-block:: python
+
+            import supervisely as sly
+
+            # Create VolumeFigure from json we use data from example to_json(see above)
+            new_volume_figure = sly.VolumeFigure.from_json(
+                data=volume_figure_json,
+                objects=sly.VolumeObjectCollection([volume_obj_heart]),
+                plane_name="axial",
+                slice_index=7
+            )
+        """
+
         # @#TODO: copypaste from video figure, add base class and refactor copypaste later
         # _video_figure = super().from_json(data, objects, slice_index, key_id_map)
 
@@ -206,6 +404,50 @@ class VolumeFigure(VideoFigure):
         )
 
     def to_json(self, key_id_map=None, save_meta=True):
+        """
+        Convert the VolumeFigure to a json dict. Read more about `Supervisely format <https://docs.supervise.ly/data-organization/00_ann_format_navi>`_.
+
+        :param key_id_map: KeyIdMap object.
+        :type key_id_map: KeyIdMap, optional
+        :param save_meta: Save frame index or not.
+        :type save_meta: bool, optional
+        :return: Json format as a dict
+        :rtype: :class:`dict`
+
+        :Usage example:
+
+         .. code-block:: python
+
+            import supervisely as sly
+
+            obj_class_heart = sly.ObjClass('heart', sly.Rectangle)
+            volume_obj_heart = sly.VolumeObject(obj_class_heart)
+            fr_index = 7
+            geometry = sly.Rectangle(0, 0, 100, 100)
+            volume_figure_heart = sly.VolumeFigure(volume_obj_heart, geometry, fr_index)
+            volume_figure_json = volume_figure_heart.to_json(save_meta=True)
+            print(volume_figure_json)
+            # Output: {
+            #     "geometry": {
+            #         "points": {
+            #         "exterior": [
+            #             [0, 0],
+            #             [100, 100]
+            #         ],
+            #         "interior": []
+            #         }
+            #     },
+            #     "geometryType": "rectangle",
+            #     "key": "158e6cf4f4ac4c639fc6994aad127c16",
+            #     "meta": {
+            #         "normal": { "x": 0, "y": 0, "z": 1 },
+            #         "planeName": "axial",
+            #         "sliceIndex": 7
+            #     },
+            #     "objectKey": "bf63ffe342e949899d3ddcb6b0f73f54"
+            # }
+        """
+
         json_data = super().to_json(key_id_map, save_meta)
         if type(self._geometry) == ClosedSurfaceMesh:
             json_data.pop(ApiField.GEOMETRY)
