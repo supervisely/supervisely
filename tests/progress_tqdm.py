1191b13c726f28658b54d3cb571112a97b952f50
Ars Vladimirov <grokhael@gmail.com>
2023-04-17 16:34:19 +0600

    check instance workability

--- tests/progress_tqdm.py
+++ tests/progress_tqdm.py
@@ -8,10 +8,15 @@ from tqdm import tqdm
 from typing import Optional
 from supervisely.sly_logger import logger
 
-
-load_dotenv(os.path.expanduser("~/supervisely.env"))
+if sly.is_development():
+    load_dotenv(os.path.expanduser("~/supervisely.env"))
+    load_dotenv("local.env")
 api = sly.Api()
 # os.environ["ENV"] = "production"
+
+TF_PATH = "/cardio.tar"
+LOC_PATH = "prog_test.tar"
+TEAM_ID = sly.env.team_id()
 
 
 class MyTqdm(tqdm, sly.Progress):
@@ -40,13 +45,23 @@ class MyTqdm(tqdm, sly.Progress):
         self.current_label = ""
         self.reported_cnt = 0
         self.logger = logger if ext_logger is None else ext_logger
+
         self.report_every = max(1, math.ceil(total_cnt / 100 * min_report_percent))
-        self.need_info_log = need_info_log
 
         if sly.is_development():
-            super().__init__(total=total_cnt, desc=message, *args, **kwargs)
+            super().__init__(
+                total=total_cnt,
+                desc=message,
+                *args,
+                **kwargs,
+            )
         else:
-            super().__init__(disable=True, total=total_cnt)
+            super().__init__(
+                disable=True,
+                total=total_cnt,
+                *args,
+                **kwargs,
+            )
 
     def update(self, count):
         if sly.is_development():
@@ -55,21 +70,38 @@ class MyTqdm(tqdm, sly.Progress):
             super().iters_done_report(count)
 
 
-data_len = 145
-data_list = list(range(data_len))
+# data_len = 145
+# data_list = list(range(data_len))
 
 
-def test_progress(x, progress):
-    batch_size = 30
-    for batch in sly.batched(seq=x, batch_size=batch_size):
-        progress.update(len(batch))
+# def test_progress(x, progress):
+#     batch_size = 30
+#     for batch in sly.batched(seq=x, batch_size=batch_size):
+#         progress.update(len(batch))
 
 
-progress_tqdm = MyTqdm(message="Uploading data", total_cnt=data_len)
-test_progress(data_list, progress_tqdm)
-progress_tqdm.close()
+p = MyTqdm(
+    message="Downloading data with MyTqdm",
+    total_cnt=api.file.get_directory_size(TEAM_ID, TF_PATH),
+    unit="B",
+    unit_scale=True,
+)
+
+# p = tqdm(
+#     desc="Downloading data with original tqdm",
+#     total=api.file.get_directory_size(TEAM_ID, TF_PATH),
+#     unit="B",
+#     unit_scale=True,
+# )
 
 
+# progress_tqdm
+# test_progress(data_list, progress_tqdm)
+# progress_tqdm.close()
+
+api.file.download(TEAM_ID, TF_PATH, LOC_PATH, progress_cb=p.update)
+
+print(os.listdir(os.getcwd()))
 # size = api.file.get_directory_size(439, "/test.tar")
 # progress_tqdm_file = MyTqdm("File downloaded: ", total_cnt=size, is_size=True)
 # api.file.download(
