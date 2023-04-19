8f01da6e1ba035fa8afd4cf3ed084207051279b8
peixed351 <peixed351@gmail.com>
2023-04-19 13:59:04 +0300

    widget-copytoclipboard

--- supervisely/app/widgets/copy_to_clipboard/copy_to_clipboard.py
+++ supervisely/app/widgets/copy_to_clipboard/copy_to_clipboard.py
@@ -11,15 +11,17 @@ except ImportError:
 class CopyToClipboard(Widget):
     def __init__(
         self,
-        content: str = "",
+        content: Optional[str] = "",
+        # content: Widget = None,
         max_lines: Optional[int] = 200,
-        language_mode: Optional[Literal["json", "html", "plain_text", "yaml", "python"]] = "json",
+        language_mode: Optional[
+            Literal["json", "html", "plain_text", "yaml", "python"]
+        ] = "ace/mode/json",
         readonly: Optional[bool] = True,
         show_line_numbers: Optional[bool] = False,
         highlight_active_line: Optional[bool] = False,
         widget_id: str = None,
     ):
-
         self._content = content
         self._max_lines = max_lines
         self._language_mode = language_mode
@@ -30,15 +32,57 @@ class CopyToClipboard(Widget):
         super().__init__(widget_id=widget_id, file_path=__file__)
 
     def get_json_data(self) -> Dict:
-        return {
-            "options": {
-                "mode": self._language_mode,
-                "showGutter": self._show_line_numbers,
-                "readOnly": self._readonly,
-                "maxLines": self._max_lines,
-                "highlightActiveLine": self._highlight_active_line,
-            },
-        }
+        return {}
 
     def get_json_state(self) -> Dict:
         return {"content": self._content}
+
+    # def get_json_data(self) -> Dict:
+    #     return {
+    #         "options": {
+    #             "mode": self._language_mode,
+    #             "showGutter": self._show_line_numbers,
+    #             "readOnly": self._readonly,
+    #             "maxLines": self._max_lines,
+    #             "highlightActiveLine": self._highlight_active_line,
+    #         },
+    #     }
+
+    # def get_json_state(self) -> Dict:
+    #     return {"content": self._content}
+
+    # def get_content(self) -> str:
+    #     return StateJson()[self.widget_id]["content"]
+
+    # def set_content(
+    #     self,
+    #     content: Optional[str] = "",
+    #     language_mode: Optional[Literal["json", "html", "plain_text", "yaml", "python"]] = None,
+    # ) -> None:
+    #     self._language_mode = language_mode
+    #     StateJson()[self.widget_id]["content"] = content
+    #     StateJson().send_changes()
+    #     if language_mode is not None:
+    #         self._language_mode = f"ace/mode/{language_mode}"
+    #         DataJson()[self.widget_id]["options"]["mode"] = self._language_mode
+    #         DataJson().send_changes()
+
+    # @property
+    # def readonly(self) -> bool:
+    #     return self._readonly
+
+    # @readonly.setter
+    # def readonly(self, value: bool):
+    #     self._readonly = value
+    #     DataJson()[self.widget_id]["options"]["readOnly"] = self._readonly
+    #     DataJson().send_changes()
+
+    # def show_line_numbers(self):
+    #     self._show_line_numbers = True
+    #     DataJson()[self.widget_id]["options"]["showGutter"] = self._show_line_numbers
+    #     DataJson().send_changes()
+
+    # def hide_line_numbers(self):
+    #     self._show_line_numbers = False
+    #     DataJson()[self.widget_id]["options"]["showGutter"] = self._show_line_numbers
+    #     DataJson().send_changes()
