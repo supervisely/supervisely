4b36a3fce37776d8db28cb30951e07bd758ced10
peixed351 <peixed351@gmail.com>
2023-04-21 17:04:08 +0300

    in progress

--- supervisely/app/widgets/compare_images/compare_images.py
+++ supervisely/app/widgets/compare_images/compare_images.py
@@ -1,113 +1,22 @@
 from supervisely.annotation.annotation import Annotation
 from supervisely.app import DataJson
-from supervisely.app.widgets import GridGallery
+from supervisely.app.widgets import Widget
 
 
-class CompareImages(GridGallery):
+class CompareImages(Widget):
     def __init__(
         self,
-        annotations_opacity: float = 0.5,
-        show_opacity_slider: bool = True,
-        enable_zoom: bool = False,
-        resize_on_zoom: bool = False,
-        fill_rectangle: bool = True,
-        border_width: int = 3,
+        widget_left: Widget = "Left image is not selected",
+        widget_right: Widget = "Right image is not selected",
         widget_id: str = None,
     ):
-        super().__init__(
-            columns_number=2,
-            annotations_opacity=annotations_opacity,
-            show_opacity_slider=show_opacity_slider,
-            enable_zoom=enable_zoom,
-            resize_on_zoom=resize_on_zoom,
-            fill_rectangle=fill_rectangle,
-            border_width=border_width,
-            widget_id=widget_id,
-        )
+        self._left = widget_left
+        self._right = widget_right
 
-    def _add_image(
-        self,
-        title,
-        image_url,
-        position_index: int,
-        ann: Annotation = None,
-        zoom_to: int = None,
-        zoom_factor: float = 1.2,
-        title_url=None,
-    ):
-        exist_data = None
-        for curr_data in self._data:
-            if curr_data["column_index"] == position_index:
-                exist_data = curr_data
+        super().__init__(widget_id=widget_id, file_path=__file__)
 
-        self.clean_up()
-        super().append(
-            image_url=image_url,
-            annotation=ann,
-            title=title,
-            column_index=position_index ^ 1,
-            zoom_to=zoom_to,
-            zoom_factor=zoom_factor,
-            title_url=title_url,
-        )
-        if exist_data is not None:
-            super().append(
-                image_url=exist_data["image_url"],
-                annotation=exist_data["annotation"],
-                title=exist_data["title"],
-                column_index=position_index,
-                zoom_to=exist_data["zoom_to"],
-                zoom_factor=exist_data["zoom_factor"],
-                title_url=exist_data["title_url"],
-            )
-        DataJson().send_changes()
+    def get_json_data(self):
+        return {}
 
-    def set_left(
-        self,
-        title,
-        image_url,
-        ann: Annotation = None,
-        zoom_to: int = None,
-        zoom_factor: float = 1.2,
-        title_url=None,
-    ):
-        self._add_image(
-            title=title,
-            position_index=1,
-            image_url=image_url,
-            ann=ann,
-            zoom_to=zoom_to,
-            zoom_factor=zoom_factor,
-            title_url=title_url,
-        )
-
-    def set_right(
-        self,
-        title,
-        image_url,
-        ann: Annotation = None,
-        zoom_to: int = None,
-        zoom_factor: float = 1.2,
-        title_url=None,
-    ):
-        self._add_image(
-            title=title,
-            position_index=0,
-            image_url=image_url,
-            ann=ann,
-            zoom_to=zoom_to,
-            zoom_factor=zoom_factor,
-            title_url=title_url,
-        )
-
-    def append(
-        self,
-        image_url: str,
-        annotation: Annotation = None,
-        title: str = "",
-        column_index: int = None,
-        zoom_to: int = None,
-        zoom_factor: float = 1.2,
-        title_url=None,
-    ):
-        raise ValueError("Use only 'set_left' or 'set_right' methods to add data")
+    def get_json_state(self):
+        return {}
