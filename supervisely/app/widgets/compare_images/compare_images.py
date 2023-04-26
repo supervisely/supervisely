596b31110e76834098efc3b68788811199d25130
peixed351 <peixed351@gmail.com>
2023-04-26 18:15:51 +0300

    in progress

--- supervisely/app/widgets/compare_images/compare_images.py
+++ supervisely/app/widgets/compare_images/compare_images.py
@@ -1,45 +1,77 @@
 from supervisely.app import DataJson, StateJson
-from supervisely.app.widgets import Widget, LabeledImage, Image, Text
+from supervisely.app.widgets import Widget, LabeledImage, Image, Text, Card
 from typing import Optional
 
 
 class CompareImages(Widget):
     def __init__(
         self,
-        left: Optional[Image or LabeledImage] = None,
-        right: Optional[Image or LabeledImage] = None,
+        left: Card = None,
+        right: Card = None,
         widget_id: str = None,
     ):
         self._left = left
         self._right = right
 
-        self._check_input_items()
+        # self._check_input_items()
 
         super().__init__(widget_id=widget_id, file_path=__file__)
 
     def _check_input_items(self):
-        if type(self._left) not in [Image, LabeledImage] and self._left is not None:
-            raise TypeError(
-                f"Left widget has {type(self._left)} type, Image, LabeledImage only are possible"
-            )
+        if type(self._left) is not Card:
+            if self._left is not None:
+                raise TypeError(f"Left widget has {type(self._left)} type, Card only is possible")
+        else:
+            self.left_content = self._left._content
+            if (
+                type(self.left_content) not in [Image, LabeledImage]
+                and self.left_content is not None
+            ):
+                raise TypeError(
+                    f"Left widget card content has {type(self.left_content)} type, Image, LabeledImage only are possible"
+                )
 
-        if type(self._right) not in [Image, LabeledImage] and self._right is not None:
+        if type(self._right) is not Card:
+            if self._right is not None:
+                raise TypeError(f"Right widget has {type(self._right)} type, Card only is possible")
+
+        else:
+            self.right_content = self._right._content
+            if (
+                type(self.right_content) not in [Image, LabeledImage]
+                and self.right_content is not None
+            ):
+                raise TypeError(
+                    f"Right widget card content has {type(self.right_content)} type, Image, LabeledImage only are possible"
+                )
+
+        if self._left is None and self._right is None:
+            raise TypeError("Both left and right widgets are not set")
+
+        self._correct_input_items()
+
+    def _correct_input_items(self):
+        if self._left is None:
+            if type(self.right_content) == LabeledImage:
+                self.left_content = LabeledImage()
+            else:
+                self.left_content = Image()
+            self._left = Card(content=self.left_content)
+
+        if self._right is None:
+            if type(self.left_content) == LabeledImage:
+                self.right_content = LabeledImage()
+            else:
+                self.right_content = Image()
+            self._right = Card(content=self.right_content)
+
+        if type(self.left_content) != type(self.right_content):
             raise TypeError(
-                f"Right widget has {type(self._right)} type, Image, LabeledImage only are possible"
+                f"You try to compare different content types: {type(self.left_content)} with {type(self.right_content)}, check your input data"
             )
 
     def get_json_data(self):
-        if self._left is None:
-            self._left_data = None
-        else:
-            self._left_data = self._left.get_json_data()
-
-        if self._right is None:
-            self._right_data = None
-        else:
-            self._right_data = self._right.get_json_data()
-
-        return {"left": self._left_data, "right": self._right_data}
+        return {}
 
     def get_json_state(self):
         {}
@@ -50,20 +82,8 @@ class CompareImages(Widget):
     def get_right(self):
         return self._right
 
-    def set_left(
-        self,
-        item: Optional[Image or LabeledImage],
-    ):
-        self._left = item
-        self._check_input_items()
-        DataJson()[self.widget_id]["left"] = self._left.get_json_data()
-        DataJson().send_changes()
+    def set_left(self, *args, **kwargs):
+        self.left_content.set(*args, **kwargs)
 
-    def set_right(
-        self,
-        item: Optional[Image or LabeledImage],
-    ):
-        self._right = item
-        self._check_input_items()
-        DataJson()[self.widget_id]["right"] = self._right.get_json_data()
-        DataJson().send_changes()
+    def set_right(self, *args, **kwargs):
+        self.right_content.set(*args, **kwargs)
