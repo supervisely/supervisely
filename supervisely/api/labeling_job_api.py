0bee1169745684f0fe69d0f45c8d4459c3ee1464
Almaz <79905215+almazgimaev@users.noreply.github.com>
2023-04-28 12:55:14 +0300

    LabelingJobApi - change parent class, add remove api method and field names (#536)
    * add _remove_api_method_name
    
    * change parent class name

--- supervisely/api/labeling_job_api.py
+++ supervisely/api/labeling_job_api.py
@@ -16,7 +16,7 @@ from supervisely.collection.str_enum import StrEnum
 from supervisely.api.module_api import (
     ApiField,
     ModuleApi,
-    RemoveableModuleApi,
+    RemoveableBulkModuleApi,
     ModuleWithStatus,
     WaitingTimeExceeded,
 )
@@ -61,7 +61,7 @@ class LabelingJobInfo(NamedTuple):
     entities: list
 
 
-class LabelingJobApi(RemoveableModuleApi, ModuleWithStatus):
+class LabelingJobApi(RemoveableBulkModuleApi, ModuleWithStatus):
     """
     API for working with Labeling Jobs. :class:`LabelingJobApi<LabelingJobApi>` object is immutable.
 
@@ -256,6 +256,16 @@ class LabelingJobApi(RemoveableModuleApi, ModuleWithStatus):
 
             res = self.InfoType(*field_values)
             return LabelingJobInfo(**res._asdict())
+
+    def _remove_batch_api_method_name(self):
+        """Api remove method name."""
+
+        return "jobs.bulk.remove"
+    
+    def _remove_batch_field_name(self):
+        """Returns onstant string that represents API field name."""
+
+        return ApiField.IDS
 
     def create(
         self,
