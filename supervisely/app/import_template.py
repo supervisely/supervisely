aceba99d58e0ea97799155419cb1d22aa3dae25b
cxnt <pavelbartsits@gmail.com>
2023-05-08 17:03:41 +0400

    update import template from import-template-v2 branch

--- supervisely/app/import_template.py
+++ supervisely/app/import_template.py
@@ -4,13 +4,15 @@ import supervisely.io.env as env
 from supervisely.api.api import Api
 from supervisely.app import get_data_dir
 from os.path import join, basename
-from pathlib import Path
+from os import getenv
+from distutils.util import strtobool
+from supervisely.sly_logger import logger
+from supervisely.io.fs import dir_exists, file_exists, remove_dir, silent_remove
 
-try:
-    from typing import Literal
-except ImportError:
-    # for compatibility with python 3.7
-    from typing_extensions import Literal
+
+# @TODO: check agent import
+# @TODO: remove source checkbox working for UI and non UI imports
+# @TODO:
 
 
 class Import:
@@ -23,6 +25,7 @@ class Import:
             dataset_id: int,
             path: str,
             is_directory: bool = True,
+            is_on_agent: bool = False,
             is_path_required: bool = True,
         ):
             self._team_id = team_id
@@ -46,6 +49,11 @@ class Import:
                 raise ValueError(f"Dataset ID must be 'int': {self._dataset_id}")
 
             self._is_path_required = is_path_required
+            if type(self._is_path_required) is not bool:
+                raise ValueError(
+                    f"Flag 'is_path_required' must be 'bool': {self._is_path_required}"
+                )
+
             self._path = path
             if self._is_path_required is True and self._path is None:
                 raise ValueError(f"Remote path is not specified: {self._path}")
@@ -54,7 +62,11 @@ class Import:
 
             self._is_directory = is_directory
             if self._is_path_required is True and type(self._is_directory) is not bool:
-                raise ValueError(f"Remote path must be 'bool': {self._is_directory}")
+                raise ValueError(f"Flag 'is_directory' must be 'bool': {self._is_directory}")
+
+            self._is_on_agent = is_on_agent
+            if self._is_path_required is True and type(self._is_on_agent) is not bool:
+                raise ValueError(f"Flag 'is_on_agent' must be 'bool': {self._is_on_agent}")
 
         def __str__(self):
             return (
@@ -63,7 +75,8 @@ class Import:
                 f"Project ID: {self._project_id}\n"
                 f"Dataset ID: {self._dataset_id}\n"
                 f"Path: {self._path}\n"
-                f"Is directory: {self._is_directory}"
+                f"Is directory: {self._is_directory}\n"
+                f"Is on agent: {self._is_on_agent}"
             )
 
         @property
@@ -90,8 +103,13 @@ class Import:
         def is_directory(self) -> bool:
             return self._is_directory
 
+        @property
+        def is_on_agent(self) -> bool:
+            return self._is_on_agent
+
     def process(self, context: Context) -> Optional[Union[int, None]]:
-        raise NotImplementedError()  # implement your own method when inherit
+        # implement your own method for import
+        raise NotImplementedError()
 
     def is_path_required(self) -> bool:
         return True
@@ -124,26 +142,40 @@ class Import:
             path = file
             is_directory = False
 
+        remote_path = path
+        is_on_agent = api.file.is_on_agent(path)
+
+        if not self.is_path_required() and path is None:
+            is_directory = False
+
         project_id = env.project_id(raise_not_found=False)
         dataset_id = env.dataset_id(raise_not_found=False)
 
         if project_id is not None:
             # lets validate that project exists
             project = api.project.get_info_by_id(id=project_id)
-            print(f"Importing to existing Project: id={project.id}, name={project.name}")
+            if project is None:
+                raise ValueError(f"Project with ID: '{project_id}' is not found or either archived")
+            logger.info(f"Importing to existing Project: id={project.id}, name={project.name}")
         if dataset_id is not None:
             # lets validate that dataset exists
             dataset = api.dataset.get_info_by_id(id=dataset_id)
-            print(f"Importing to existing Dataset: id={dataset.id}, name={dataset.name}")
+            if dataset is None:
+                raise ValueError(f"Project with ID: '{dataset_id}' is not found or either archived")
+            logger.info(f"Importing to existing Dataset: id={dataset.id}, name={dataset.name}")
 
         if is_production():
             if path is not None:
                 local_save_path = join(get_data_dir(), basename(path.rstrip("/")))
+                if dir_exists(local_save_path):
+                    remove_dir(local_save_path)
                 if is_directory:
                     api.file.download_directory(
                         team_id=team_id, remote_path=path, local_save_path=local_save_path
                     )
                 else:
+                    if file_exists(local_save_path):
+                        silent_remove(local_save_path)
                     api.file.download(
                         team_id=team_id, remote_path=path, local_save_path=local_save_path
                     )
@@ -156,6 +188,7 @@ class Import:
             dataset_id=dataset_id,
             path=path,
             is_directory=is_directory,
+            is_on_agent=is_on_agent,
             is_path_required=self.is_path_required(),
         )
 
@@ -163,4 +196,9 @@ class Import:
         if type(project_id) is int and is_production():
             info = api.project.get_info_by_id(project_id)
             api.task.set_output_project(task_id=task_id, project_id=info.id, project_name=info.name)
-            print(f"Result project: id={info.id}, name={info.name}")
+            remove_source = bool(strtobool(getenv("modal.state.removeSource", None)))
+            if remove_source and not is_on_agent:
+                api.file.remove(team_id=context.team_id, path=remote_path)
+                remove_dir(context.path)
+                logger.info(msg=f"Source directory: '{remote_path}' was successfully removed.")
+            logger.info(f"Result project: id={info.id}, name={info.name}")
