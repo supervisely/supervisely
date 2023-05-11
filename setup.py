15ab7eca7dfb5fb9d93638412a731e4854116650
Maxim Kolomeychenko <maxim@supervise.ly>
2023-05-11 15:25:47 +0100

    requests-toolbelt remove upper limit

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ INSTALL_REQUIRES = [
     "protobuf>=3.14.0, <=3.20.3",
     "python-json-logger>=0.1.11, <3.0.0",
     "requests>=2.27.1, <3.0.0",
-    "requests-toolbelt>=0.9.1, <1.0.0",
+    "requests-toolbelt>=0.9.1",  # , <1.0.0
     "Shapely>=1.7.1, <2.0.0",
     "bidict>=0.21.2, <1.0.0",
     "varname>=0.8.1, <1.0.0",
