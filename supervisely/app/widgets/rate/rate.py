49bf00b6aecc737a6680a4bfa3387a7fad3aa523
almaz_macbook <almazgimaev@gmail.com>
2023-05-24 11:47:45 +0300

    update rate widget and template

--- supervisely/app/widgets/rate/rate.py
+++ supervisely/app/widgets/rate/rate.py
@@ -1,6 +1,6 @@
 from supervisely.app import DataJson, StateJson
 from supervisely.app.widgets import Widget
-from typing import List
+from typing import List, Union
 
 
 class Rate(Widget):
@@ -9,66 +9,91 @@ class Rate(Widget):
 
     def __init__(
         self,
+        value: Union[int, float] = None,
         max: int = 5,
-        colors: List = ["#F7BA2A", "#F7BA2A", "#F7BA2A"],
         disabled: bool = False,
         allow_half: bool = False,
         texts: List[str] = [],
         show_text: bool = False,
         text_color: str = "#1F2D3D",
+        text_template: str = "",
+        colors: List = ["#F7BA2A", "#F7BA2A", "#F7BA2A"],
         void_color: str = "#C6D1DE",
         disabled_void_color: str = "#EFF2F7",
         widget_id: str = None,
     ):
+        self._value = value
         self._max = max
-        self._colors = colors
         self._disabled = disabled
         self._allow_half = allow_half
+
+        # text properties
         self._texts = texts
         self._show_text = show_text
         self._text_color = text_color
+        self._text_template = text_template  # available if self._disabled is True
+
+        # color properties
+        self._colors = colors
         self._void_color = void_color
         self._disabled_void_color = disabled_void_color
+
+
         self._changes_handled = False
-        self._value = None
+
         super().__init__(widget_id=widget_id, file_path=__file__)
 
     def get_json_data(self):
         return {
             "max": self._max,
-            "colors": self._colors,
             "disabled": self._disabled,
-            "allow_half": self._allow_half,
+            "allowHalf": self._allow_half,
             "texts": self._texts,
-            "show_text": self._show_text,
-            "text_color": self._text_color,
-            "void_color": self._void_color,
-            "disabled_void_color": self._disabled_void_color,
+            "showText": self._show_text,
+            "textColor": self._text_color,
+            "textTemplate": self._text_template,
+            "colors": self._colors,
+            "voidColor": self._void_color,
+            "disabledVoidColor": self._disabled_void_color,
         }
 
     def get_json_state(self):
         return {"value": self._value}
 
-    def get_value(self):
-        return StateJson()[self.widget_id]["value"]
+    @property
+    def is_disabled(self):
+        self._disabled = DataJson()[self.widget_id]["disabled"]
+        return self._disabled is True
 
-    def set_current_value(self, value: int):
+    def get_value(self):
+        self._value = StateJson()[self.widget_id]["value"]
+        return self._value
+
+    def set_value(self, value: Union[int, float]):
+        if not isinstance(value, (int, float)):
+            raise TypeError("Value type has to be one of int or float.")
         self._value = value
         StateJson()[self.widget_id]["value"] = self._value
         StateJson().send_changes()
 
     def get_max_value(self):
-        return DataJson()[self.widget_id]["max"]
+        self._max = DataJson()[self.widget_id]["max"]
+        return self._max
 
     def set_max_value(self, value: int):
+        if not isinstance(value, int):
+            raise TypeError("Max value type has to be a integer.")
         self._max = value
         DataJson()[self.widget_id]["max"] = self._max
         DataJson().send_changes()
 
     def get_colors(self):
-        return DataJson()[self.widget_id]["colors"]
+        self._colors = DataJson()[self.widget_id]["colors"]
+        return self._colors
 
     def set_colors(self, value: List[str]):
+        if not isinstance(value, list):
+            raise TypeError("Argument value type has to be a list.")
         self._colors = value
         DataJson()[self.widget_id]["colors"] = self._colors
         DataJson().send_changes()
@@ -83,68 +108,66 @@ class Rate(Widget):
         DataJson()[self.widget_id]["disabled"] = self._disabled
         DataJson().send_changes()
 
-    def get_disabled(self):
-        return DataJson()[self.widget_id]["disabled"]
-
-    def enable_allow_half(self):
+    def allow_half_precision(self):
         self._allow_half = True
         DataJson()[self.widget_id]["allow_half"] = self._allow_half
         DataJson().send_changes()
 
-    def disable_allow_half(self):
+    def disallow_half_precision(self):
         self._allow_half = False
         DataJson()[self.widget_id]["allow_half"] = self._allow_half
         DataJson().send_changes()
 
-    def get_allow_half(self):
-        return DataJson()[self.widget_id]["allow_half"]
-
     def get_texts(self):
-        return DataJson()[self.widget_id]["texts"]
+        self._texts = DataJson()[self.widget_id]["texts"]
+        return self._texts
 
     def set_texts(self, value: List[str]):
+        if not isinstance(value, list):
+            raise TypeError("Argument value type has to be a list.")
         self._texts = value
         DataJson()[self.widget_id]["texts"] = self._texts
         DataJson().send_changes()
 
-    def add_texts(self, value: List[str]):
-        self._texts.extend(value)
-        DataJson()[self.widget_id]["texts"] = self._texts
-        DataJson().send_changes()
-
-    def unable_show_text(self):
+    def show_text(self):
         self._show_text = True
         DataJson()[self.widget_id]["show_text"] = self._show_text
         DataJson().send_changes()
 
-    def disable_show_text(self):
+    def hide_text(self):
         self._show_text = False
         DataJson()[self.widget_id]["show_text"] = self._show_text
         DataJson().send_changes()
 
-    def get_show_text(self):
-        return DataJson()[self.widget_id]["show_text"]
-
     def get_text_color(self):
-        return DataJson()[self.widget_id]["text_color"]
+        self._text_color = DataJson()[self.widget_id]["text_color"]
+        return self._text_color
 
     def set_text_color(self, value: str):
+        if not isinstance(value, str):
+            raise TypeError("Argument value type has to be a str.")
         self._text_color = value
         DataJson()[self.widget_id]["text_color"] = self._text_color
         DataJson().send_changes()
 
     def get_void_color(self):
-        return DataJson()[self.widget_id]["void_color"]
+        self._void_color = DataJson()[self.widget_id]["void_color"]
+        return self._void_color
 
     def set_void_color(self, value: str):
+        if not isinstance(value, str):
+            raise TypeError("Argument value type has to be a str.")
         self._void_color = value
         DataJson()[self.widget_id]["void_color"] = self._void_color
         DataJson().send_changes()
 
     def get_disabled_void_color(self):
-        return DataJson()[self.widget_id]["disabled_void_color"]
+        self._disabled_void_color = DataJson()[self.widget_id]["disabled_void_color"]
+        return self._disabled_void_color
 
     def set_disabled_void_color(self, value: str):
+        if not isinstance(value, str):
+            raise TypeError("Argument value type has to be a str.")
         self._disabled_void_color = value
         DataJson()[self.widget_id]["disabled_void_color"] = self._disabled_void_color
         DataJson().send_changes()
@@ -156,8 +179,7 @@ class Rate(Widget):
 
         @server.post(route_path)
         def _click():
-            res = self.get_value()
-            self._value = res
-            func(res)
+            self.get_value()
+            func(self._value)
 
         return _click
