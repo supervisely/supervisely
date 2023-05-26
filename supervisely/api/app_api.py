81351fef2011fef87d55a396965920e2fc9a2eb1
Ars Vladimirov <grokhael@gmail.com>
2023-05-26 21:40:11 +0600

    check type

--- supervisely/api/app_api.py
+++ supervisely/api/app_api.py
@@ -588,7 +588,8 @@ class AppApi(TaskApi):
             module_id=module_id,
             redirect_requests=redirect_requests,
         )
-        result = [result]
+        if type(result) is not list:
+            result = [result]
         if len(result) != 1:
             raise ValueError(f"{len(result)} tasks started instead of one")
         return SessionInfo.from_json(result[0])
