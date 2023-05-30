92611143b97ff3f27f5bcaebad20f5e4be2c71e8
Ars Vladimirov <grokhael@gmail.com>
2023-05-30 19:47:47 +0600

    refactoring CopyToClipBoard

--- supervisely/app/widgets/copy_to_clipboard/copy_to_clipboard.py
+++ supervisely/app/widgets/copy_to_clipboard/copy_to_clipboard.py
@@ -8,7 +8,7 @@ class CopyToClipboard(Widget):
     def __init__(
         self,
         content: Union[Editor, Text, TextArea, Input, str] = "",
-        widget_id: str = None,
+        widget_id: Optional[str] = None,
     ):
         self._content = content
         self._content_text = None
@@ -18,28 +18,25 @@ class CopyToClipboard(Widget):
         super().__init__(widget_id=widget_id, file_path=__file__)
 
     def _init_content(self, content):
-        self._res_data = {}
-        self._res_state = {}
-        self._editor_or_input = False
-        self._text = False
-        self._only_string = False
+        self._json_data = {}
+        self._json_state = {}
+
         if isinstance(content, Editor):
             self._editor_or_input = True
             self._content_text = content.get_text()
-            self._res_state = {"content": self._content_text, "curr_widget_text": "text"}
-
+            self._json_state = {"content": self._content_text, "curr_widget_text": "text"}
         elif isinstance(content, Text):
             self._text = True
             self._content_text = content.text
-            self._res_data = {"content": self._content_text, "curr_widget_text": "text"}
+            self._json_data = {"content": self._content_text, "curr_widget_text": "text"}
         elif isinstance(content, TextArea):
             self._text = True
             self._content_text = content.get_value()
-            self._res_data = {"content": self._content_text, "curr_widget_text": "value"}
+            self._json_data = {"content": self._content_text, "curr_widget_text": "value"}
         elif isinstance(content, Input):
             self._editor_or_input = True
             self._content_text = content.get_value()
-            self._res_state = {
+            self._json_state = {
                 "content": self._content_text,
                 "value": self._content_text,
                 "curr_widget_text": "value",
@@ -47,21 +44,21 @@ class CopyToClipboard(Widget):
         elif isinstance(content, str):
             self._only_string = True
             self._content_text = content
-            self._res_state = {"content": self._content_text}
+            self._json_state = {"content": self._content_text}
         else:
             raise TypeError(
                 f"Supported types: str, Editor, Text, TextArea, Input. Your type: {type(content).__name__}"
             )
 
     def get_json_data(self) -> Dict:
-        return self._res_data
+        return self._json_data
 
     def get_json_state(self) -> Dict:
-        return self._res_state
+        return self._json_state
 
-    def get_content(self):
+    def get_content(self) -> Union[Editor, Text, TextArea, Input, str]:
         return self._content
 
     @property
-    def text(self):
+    def text(self) -> str:
         return self._content_text
