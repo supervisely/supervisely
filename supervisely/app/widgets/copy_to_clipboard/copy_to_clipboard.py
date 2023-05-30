6e653850ecfdbe2f30b4a9b54883813c467f8caf
Ars Vladimirov <grokhael@gmail.com>
2023-05-30 18:28:52 +0600

    add isinstance

--- supervisely/app/widgets/copy_to_clipboard/copy_to_clipboard.py
+++ supervisely/app/widgets/copy_to_clipboard/copy_to_clipboard.py
@@ -23,19 +23,20 @@ class CopyToClipboard(Widget):
         self._editor_or_input = False
         self._text = False
         self._only_string = False
-        if type(content) is Editor:
+        if isinstance(content, Editor):
             self._editor_or_input = True
             self._content_text = content.get_text()
             self._res_state = {"content": self._content_text, "curr_widget_text": "text"}
-        elif type(content) is Text:
+
+        elif isinstance(content, Text):
             self._text = True
             self._content_text = content.text
             self._res_data = {"content": self._content_text, "curr_widget_text": "text"}
-        elif type(content) is TextArea:
+        elif isinstance(content, TextArea):
             self._text = True
             self._content_text = content.get_value()
             self._res_data = {"content": self._content_text, "curr_widget_text": "value"}
-        elif type(content) is Input:
+        elif isinstance(content, Input):
             self._editor_or_input = True
             self._content_text = content.get_value()
             self._res_state = {
@@ -43,7 +44,7 @@ class CopyToClipboard(Widget):
                 "value": self._content_text,
                 "curr_widget_text": "value",
             }
-        elif type(content) is str:
+        elif isinstance(content, str):
             self._only_string = True
             self._content_text = content
             self._res_state = {"content": self._content_text}
