cf230c40741f7d01b5636f5b973337ae6d7b31bf
GoldenAnpu <seryios.sych@gmail.com>
2023-06-20 13:34:08 +0200

    change info to debug messages

--- supervisely/api/volume/volume_api.py
+++ supervisely/api/volume/volume_api.py
@@ -629,13 +629,13 @@ class VolumeApi(RemoveableBulkModuleApi):
         logger.info("Start volume compression before upload...")
         start = timer()
         volume_bytes = volume.encode(np_data, meta)
-        logger.info(f"Volume has been compressed in {timer() - start} seconds")
+        logger.debug(f"Volume has been compressed in {timer() - start} seconds")
 
-        logger.info(f"Start uploading bytes of 3d volume ...")
+        logger.debug(f"Start uploading bytes of 3d volume ...")
         start = timer()
         volume_hash = get_bytes_hash(volume_bytes)
         self._api.image._upload_data_bulk(lambda v: v, [(volume_bytes, volume_hash)])
-        logger.info(f"3d Volume bytes has been sucessfully uploaded in {timer() - start} seconds")
+        logger.debug(f"3d Volume bytes has been sucessfully uploaded in {timer() - start} seconds")
         volume_info = self.upload_hash(dataset_id, name, volume_hash, meta)
         if progress_cb is not None:
             progress_cb(1)  # upload volume
