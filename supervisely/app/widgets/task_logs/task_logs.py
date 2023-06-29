6eb226de38f6d7d4fb46c2196f6efb1369fe7901
almaz <almazgimaev@gmail.com>
2023-06-29 21:17:32 +0300

    remove unnecessary options

--- supervisely/app/widgets/task_logs/task_logs.py
+++ supervisely/app/widgets/task_logs/task_logs.py
@@ -9,14 +9,10 @@ class TaskLogs(Widget):
     def __init__(
         self,
         task_id: int = None,
-        multiple: bool = False,
-        filterable: bool = True,
         widget_id: str = None,
     ):
         self._task_id = task_id
         self._is_development = is_development()
-        self._multiple = multiple
-        self._filterable = filterable
 
         self._task_logs_stop = Button("Set task id", button_size="mini", plain=True)
 
@@ -51,10 +47,7 @@ class TaskLogs(Widget):
         super().__init__(widget_id=widget_id, file_path=__file__)
 
     def get_json_data(self) -> Dict:
-        return {
-            "taskId": self._task_id,
-            "options": {"multiple": self._multiple, "filterable": self._filterable},
-        }
+        return {"taskId": self._task_id}
 
     def get_json_state(self) -> Dict:
         return {}
