0f6714521c78b0ff3d70230141cb07b6d4c624c4
almaz <almazgimaev@gmail.com>
2023-06-29 21:46:22 +0300

    remove controls

--- supervisely/app/widgets/task_logs/task_logs.py
+++ supervisely/app/widgets/task_logs/task_logs.py
@@ -14,35 +14,35 @@ class TaskLogs(Widget):
         self._task_id = task_id
         self._is_development = is_development()
 
-        self._task_logs_stop = Button("Set task id", button_size="mini", plain=True)
+        # self._task_logs_stop = Button("Set task id", button_size="mini", plain=True)
 
-        self._task_id_input = InputNumber(value=task_id, size="small", controls=False)
-        self._task_id_change_btn = Button("OK", button_size="mini", plain=True)
-        self._task_id_input.hide()
-        self._task_id_change_btn.hide()
+        # self._task_id_input = InputNumber(value=task_id, size="small", controls=False)
+        # self._task_id_change_btn = Button("OK", button_size="mini", plain=True)
+        # self._task_id_input.hide()
+        # self._task_id_change_btn.hide()
 
-        self._task_id_change_controls = Flexbox(
-            widgets=[self._task_id_input, self._task_logs_stop, self._task_id_change_btn],
-        )
+        # self._task_id_change_controls = Flexbox(
+        #     widgets=[self._task_id_input, self._task_logs_stop, self._task_id_change_btn],
+        # )
 
-        @self._task_logs_stop.click
-        def stop_logs_ws():
-            self._task_logs_stop.loading = True
-            self._set_task_id(None)
-            self._task_logs_stop.loading = False
-            self._task_logs_stop.hide()
-            self._task_id_input.show()
-            self._task_id_change_btn.show()
+        # @self._task_logs_stop.click
+        # def stop_logs_ws():
+        #     self._task_logs_stop.loading = True
+        #     self._set_task_id(None)
+        #     self._task_logs_stop.loading = False
+        #     self._task_logs_stop.hide()
+        #     self._task_id_input.show()
+        #     self._task_id_change_btn.show()
 
-        @self._task_id_change_btn.click
-        def change_task_id():
-            self._task_id_change_btn.loading = True
-            new_task_id = int(self._task_id_input.get_value())
-            self._set_task_id(new_task_id)
-            self._task_id_change_btn.loading = False
-            self._task_id_change_btn.hide()
-            self._task_id_input.hide()
-            self._task_logs_stop.show()
+        # @self._task_id_change_btn.click
+        # def change_task_id():
+        #     self._task_id_change_btn.loading = True
+        #     new_task_id = int(self._task_id_input.get_value())
+        #     self._set_task_id(new_task_id)
+        #     self._task_id_change_btn.loading = False
+        #     self._task_id_change_btn.hide()
+        #     self._task_id_input.hide()
+        #     self._task_logs_stop.show()
 
         super().__init__(widget_id=widget_id, file_path=__file__)
 
@@ -55,13 +55,15 @@ class TaskLogs(Widget):
     def get_task_id(self) -> int:
         return DataJson()[self.widget_id]["taskId"]
 
-    def _set_task_id(self, task_id: Union[int, None]):
-        self._task_id = task_id
-        DataJson()[self.widget_id]["taskId"] = self._task_id
-        DataJson().send_changes()
+    # def _set_task_id(self, task_id: Union[int, None]):
+    #     self._task_id = task_id
+    #     DataJson()[self.widget_id]["taskId"] = self._task_id
+    #     DataJson().send_changes()
 
     def set_task_id(self, task_id: int):
         self._set_task_id(None)
         if type(task_id) != int:
             raise TypeError(f"task_id must be int, but {type(task_id)} was given")
-        self._set_task_id(task_id)
+        self._task_id = task_id
+        DataJson()[self.widget_id]["taskId"] = self._task_id
+        DataJson().send_changes()
