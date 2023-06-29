2307f62ae92bae03568cdd1e4bc9c3e959c8db5e
almaz <almazgimaev@gmail.com>
2023-06-29 17:49:17 +0300

    update task logs widget

--- supervisely/app/widgets/task_logs/task_logs.py
+++ supervisely/app/widgets/task_logs/task_logs.py
@@ -23,19 +23,11 @@ class TaskLogs(Widget):
     def get_json_data(self) -> Dict:
         return {
             "taskId": self._task_id,
-            "isDevelopment": self._is_development,
             "options": {"multiple": self._multiple, "filterable": self._filterable},
         }
 
     def get_json_state(self) -> Dict:
         return {}
-
-    def get_task_id(self) -> int:
-        return DataJson()[self.widget_id]["taskId"]
-
-    def set_task_id(self, value: int):
-        DataJson()[self.widget_id]["taskId"] = value
-        DataJson().send_changes()
 
     def get_task_id(self) -> int:
         return DataJson()[self.widget_id]["taskId"]
