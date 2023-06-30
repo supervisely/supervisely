54b3c44915114e7a4f3d0e5525b4cde174260799
TheoLisin <87002239+TheoLisin@users.noreply.github.com>
2023-06-30 14:34:36 +0300

    Lineplot widget - improve addition of datapoints with more argument types supported (tuple or dict)  (#587)
    * Added correct typing; change dict to tuples
    
    * change dict on tuple to add points
    
    * added comments, change data format in add_scalars
    
    * added comments
    
    * added clean_up function
    
    * rm time import
    
    * added backward compatibility

--- supervisely/app/widgets/grid_plot/grid_plot.py
+++ supervisely/app/widgets/grid_plot/grid_plot.py
@@ -1,4 +1,3 @@
-import time
 from typing import List, Dict
 from supervisely.app.widgets import Widget, Container, generate_id, LinePlot
 from supervisely.sly_logger import logger
@@ -14,7 +13,7 @@ class GridPlot(Widget):
         columns: int = 1,
         gap: int = 10,
         widget_id: str = None,
-    ):  
+    ):
         self._widgets = {}
         self._columns = columns
         self._gap = gap
@@ -23,7 +22,7 @@ class GridPlot(Widget):
             if isinstance(plot_data, dict):
                 # self._widgets[plot_data['title']] = LinePlot(title=plot_data['title'], series=plot_data.get('series', []), show_legend=plot_data.get('show_legend', True))
                 # передача параметров таким образом в конечном итоге приводит к ошибке JsonPatchConflict
-                self._widgets[plot_data['title']] = LinePlot(**plot_data)
+                self._widgets[plot_data["title"]] = LinePlot(**plot_data)
             else:
                 self._widgets[plot_data] = LinePlot(title=plot_data, series=[])
 
@@ -74,18 +73,46 @@ class GridPlot(Widget):
         return None
 
     def add_scalar(self, identifier: str, y, x):
-        plot_title, series_name = identifier.split('/')
+        """
+        Add scalar to series on plot. If no series with name,
+         defined in `identifier` exists,
+         one will be created automatically.
+
+        :param identifier: slash-separated plot and series name
+        :type identifier: str
+        :param y: y value
+        :type y: float | int
+        :param x: x value
+        :type x: float | int
+        """
+        plot_title, series_name = identifier.split("/")
         _, series = self._widgets[plot_title].get_series_by_name(series_name)
 
         if series is not None:
-            self._widgets[plot_title].add_to_series(name_or_id=series_name, data=[{"x": x, "y": y}])
+            self._widgets[plot_title].add_to_series(name_or_id=series_name, data=[(x, y)])
         else:
             self._widgets[plot_title].add_series(name=series_name, x=[x], y=[y])
-    
+
     def add_scalars(self, plot_title: str, new_values: dict, x):
+        """
+        Add scalars to several series on one plot at point `x`
+
+        :param plot_title: name of existing plot
+        :type plot_title: str
+        :param new_values: dictionary in the `{series_name: y_value, ...}` format
+        :type new_values: dict
+        :param x: value of `x`
+        :type x: float | int
+        """
         for series_name in new_values.keys():
             _, series = self._widgets[plot_title].get_series_by_name(series_name)
             if series is not None:
-                self._widgets[plot_title].add_to_series(name_or_id=series_name, data=[{"x": x, "y": new_values[series_name]}])
+                self._widgets[plot_title].add_to_series(
+                    name_or_id=series_name,
+                    # data=[{"x": x, "y": new_values[series_name]}],
+                    data=[(x, new_values[series_name])],
+                )
             else:
-                self._widgets[plot_title].add_series(name=series_name, x=[x], y=[new_values[series_name]])
+                self._widgets[plot_title].add_series(
+                    name=series_name, x=[x], y=[new_values[series_name]]
+                )
--- supervisely/app/widgets/line_plot/line_plot.py
+++ supervisely/app/widgets/line_plot/line_plot.py
@@ -1,13 +1,15 @@
 from supervisely.app.widgets import Widget
 from supervisely.app.content import StateJson, DataJson
-from typing import Union, List, Dict
+from typing import List, Tuple, Union
+
+NumT = Union[int, float]
 
 
 class LinePlot(Widget):
     def __init__(
         self,
         title: str,
-        series: list = [],
+        series: List[dict] = [],
         smoothing_weight: float = 0,
         group_key: str = None,
         show_legend: bool = True,
@@ -17,8 +19,34 @@ class LinePlot(Widget):
         widget_id=None,
         yaxis_autorescale: bool = True,  # issue in apex, need to refresh page
     ):
+        """
+        Create line plot.
+
+        :param title: plot title
+        :type title: str
+        :param series: List of dicts in format
+            `[{'name': series_name, 'data': [(x1, y1), ...]}]` or
+            `[{'name': series_name, 'data': [{'x': x1, 'y': y1}, ...]}]`
+        :type series: List[dict]
+        :param smoothing_weight: smoothing coeficient; float number from [0, 1]
+        :type smoothing_weight: float
+        :param group_key: _description_
+        :type group_key: str
+        :param show_legend: _description_
+        :type show_legend: bool
+        :param decimals_in_float: number of decimal places for y-axis
+        :type decimals_in_float: int
+        :param xaxis_decimals_in_float: number of decimal places for x-axis
+        :type xaxis_decimals_in_float: int
+        :param yaxis_interval: _description_
+        :type yaxis_interval: list
+        :param widget_id: _description_
+        :type widget_id: _type_
+        :param yaxis_autorescale: _description_
+        :type yaxis_autorescale: bool
+        """
         self._title = title
-        self._series = series
+        self._series = self._check_series(series)
         self._smoothing_weight = smoothing_weight
         self._group_key = group_key
         self._show_legend = show_legend
@@ -64,7 +92,8 @@ class LinePlot(Widget):
             f"Lists x and y have different lenght, {len(x)} != {len(y)}"
         )
 
-        data = [{"x": px, "y": py} for px, py in zip(x, y)]
+        # data = [{"x": px, "y": py} for px, py in zip(x, y)]
+        data = [(px, py) for px, py in zip(x, y)]
         series = {"name": name, "data": data}
         self._series.append(series)
 
@@ -83,18 +112,32 @@ class LinePlot(Widget):
             self.add_series(name, x, y, send_changes=False)
         DataJson().send_changes()
 
-    def add_to_series(self, name_or_id: str or int, data: Union[List[Dict], Dict]):
+    def add_to_series(
+        self,
+        name_or_id: Union[str, int],
+        data: Union[List[Union[tuple, dict]], Union[tuple, dict]],
+    ):
+        """
+        Add new points to series
+
+        :param name_or_id: series name
+        :type name_or_id: str | int
+        :param data: point or list of points to add; use one of the following formats
+            `[(x1, y1), ...]`, `[{'x': x1, 'y': y1}, ...]`, `(x1,y1)` or `{'x': x1, 'y': y1}`
+        :type data: Union[List[Union[tuple, dict]], Union[tuple, dict]]
+        """
         if isinstance(name_or_id, int):
             series_id = name_or_id
         else:
             series_id, _ = self.get_series_by_name(name_or_id)
 
         if isinstance(data, List):
-            # list of datapoints
-            self._series[series_id]["data"] += data
+            data_list = self._list_of_point_dicts_to_list_of_tuples(data)
         else:
             # single datapoint
-            self._series[series_id]["data"].append(data)
+            data_list = self._list_of_point_dicts_to_list_of_tuples([data])
+
+        self._series[series_id]["data"].extend(data_list)
         DataJson()[self.widget_id]["series"] = self._series
         DataJson().send_changes()
 
@@ -106,3 +149,29 @@ class LinePlot(Widget):
         )
         # assert series_id is not None, KeyError("Series with name: {name} doesn't exists.")
         return series_id, series_data
+
+    def clean_up(self):
+        self._series = []
+        self._ymin = 0
+        self._ymax = 10
+        DataJson()[self.widget_id]["series"] = self._series
+        DataJson()[self.widget_id]["ymin"] = self._ymin
+        DataJson()[self.widget_id]["ymax"] = self._ymax
+        DataJson().send_changes()
+
+    def _point_dict_to_tuple(self, point_dct: dict) -> Tuple[NumT, NumT]:
+        return (point_dct["x"], point_dct["y"])
+
+    def _list_of_point_dicts_to_list_of_tuples(
+        self, point_dcts: List[Union[dict, tuple]]
+    ) -> List[Tuple[NumT, NumT]]:
+        if len(point_dcts) == 0:
+            return []
+        if isinstance(point_dcts[0], tuple):
+            return point_dcts
+        return [self._point_dict_to_tuple(dct) for dct in point_dcts]
+
+    def _check_series(self, series: List[dict]) -> List[dict]:
+        for serie in series:
+            serie["data"] = self._list_of_point_dicts_to_list_of_tuples(serie["data"])
+        return series
