10ef64d5db08c671e1e7b694a987d481af3b81dd
Stan Soldatov <118521851+iwatkot@users.noreply.github.com>
2023-06-30 14:59:25 +0300

    New widget ReloadableArea (#590)

--- supervisely/app/fastapi/templating.py
+++ supervisely/app/fastapi/templating.py
@@ -12,7 +12,7 @@ from supervisely.app.widgets_context import JinjaWidgets
 js_bundle_version = "2.1.50"
 
 # https://github.com/supervisely-ecosystem/supervisely-app-frontend-js
-js_frontend_version = "0.0.42"
+js_frontend_version = "0.0.43"
 
 
 class Jinja2Templates(_fastapi_Jinja2Templates, metaclass=Singleton):
