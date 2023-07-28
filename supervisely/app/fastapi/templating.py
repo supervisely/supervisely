01f4138a8b66f19fe88a81b5024ece3261b9ab0d
Nikolai Petukhov <petukhov.niko@gmail.com>
2023-07-28 13:47:09 -0300

    update js_frontend_version

--- supervisely/app/fastapi/templating.py
+++ supervisely/app/fastapi/templating.py
@@ -12,7 +12,7 @@ from supervisely.app.widgets_context import JinjaWidgets
 js_bundle_version = "2.1.52"
 
 # https://github.com/supervisely-ecosystem/supervisely-app-frontend-js
-js_frontend_version = "0.0.47"
+js_frontend_version = "0.0.48"
 
 
 class Jinja2Templates(_fastapi_Jinja2Templates, metaclass=Singleton):
