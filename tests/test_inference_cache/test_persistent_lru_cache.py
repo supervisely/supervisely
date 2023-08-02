82a7ce56e6c67cc5efe4d3d267c8234c19e11fb6
Theo <theo.lisin@gmail.com>
2023-08-02 16:57:43 +0300

    utils

--- tests/test_inference_cache/test_persistent_lru_cache.py
+++ tests/test_inference_cache/test_persistent_lru_cache.py
@@ -4,14 +4,7 @@ import pytest
 from pathlib import Path
 
 from supervisely.nn.inference.cache import PersistentImageLRUCache
-
-
-def create_img():
-    return np.random.randint(0, 255, size=(320, 640, 3))
-
-
-def compare(img1: np.ndarray, img2: np.ndarray):
-    return np.allclose(img1, img2, rtol=1e-16)
+from utils import compare, create_img
 
 
 def test_save(tmp_path: Path):
