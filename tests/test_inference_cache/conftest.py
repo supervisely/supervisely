df8404acc565fd14b4eb8e83430a3387966e200c
Theo <theo.lisin@gmail.com>
2023-08-04 00:00:35 +0300

    add functions to api mock

--- tests/test_inference_cache/conftest.py
+++ tests/test_inference_cache/conftest.py
@@ -24,6 +24,8 @@ def api_mock():
     with mock.patch("supervisely.Api"):
         api = sly.Api()
         api.video.frame.download_nps.side_effect = get_n_frames
+        api.video.frame.download_np.side_effect = lambda vid, fid: create_img()
+        api.image.download_np.side_effect = lambda imid: create_img()
         yield api
 
 
