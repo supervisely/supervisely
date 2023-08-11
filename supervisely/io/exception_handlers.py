57e7ee968d3603fbf88b034f3e2e3c09af7c355c
Stan Soldatov <118521851+iwatkot@users.noreply.github.com>
2023-08-11 14:19:29 +0300

    Initial commit.

--- supervisely/io/exception_handlers.py
+++ supervisely/io/exception_handlers.py
@@ -1,13 +1,59 @@
+from typing import List
+
+from supervisely.sly_logger import logger
+from supervisely.app import show_dialog
+
 import traceback
 import re
 
+BASE_URL = "https://errors.supervisely.com/{code}"
+
 
 class HandleException:
-    def __init__(self, exception: Exception, headless: bool, **kwargs):
+    def __init__(
+        self, exception: Exception, stack: List[traceback.FrameSummary], headless: bool, **kwargs
+    ):
         self.exception = exception
+        self.stack = stack
+        self.headless = headless
         self.code = kwargs.get("code")
         self.title = kwargs.get("title")
         self.description = kwargs.get("description")
+
+        self.dev_logging()
+
+        if not self.headless:
+            self.gui_exception()
+        else:
+            self.headless_exception()
+
+    def dev_logging(self):
+        tracelog = " | ".join(traceback.format_list(self.stack))
+        logger.error(
+            "Detailed info about exception."
+            f"Exception type: {type(self.exception)}. "
+            f"Exception value: {self.exception}. "
+            f"Error code: {self.code}. "
+            f"Error title: {self.title}. "
+            f"Error description: {self.description}. "
+            f"Traceback: {tracelog}. "
+        )
+
+    def gui_exception(self):
+        url = BASE_URL.format(code=self.code)
+        description = (
+            f"{self.description}<br><br>"
+            f"Please, visit <a href='{url}'>SLY ERROR {self.code}</a> for more information and possible solutions."
+        )
+
+        show_dialog(
+            title=f"{self.title} (SLY ERROR {self.code})",
+            description=description,
+            status="error",
+        )
+
+    def headless_exception(self):
+        pass
 
 
 class ErrorHandler:
@@ -16,15 +62,16 @@ class ErrorHandler:
 
     class API:
         class TeamFilesFileNotFound(HandleException):
-            def __init__(self, exception: Exception, headless: bool):
-                self.exception = exception
-                self.headless = headless
+            def __init__(
+                self, exception: Exception, stack: List[traceback.FrameSummary], headless: bool
+            ):
                 self.code = 2001
                 self.title = "File on Team Files not found"
                 self.description = "The given path to the file on Team Files is incorrect."
 
                 super().__init__(
                     exception,
+                    stack,
                     headless,
                     code=self.code,
                     title=self.title,
@@ -33,40 +80,31 @@ class ErrorHandler:
 
 
 ERROR_PATTERNS = {
-    AttributeError: {r".*api\.file\.download.*": ErrorHandler.API.TeamFilesFileNotFound}
+    AttributeError: {r".*api\.file\.get_info_by_path.*": ErrorHandler.API.TeamFilesFileNotFound}
 }
 
 
 def handle_exception(exception: Exception, headless: bool = False):
+    # Extracting the stack trace.
+    stack = traceback.extract_stack()
+    # Adding the exception's last frame to the stack trace.
+    stack.append(traceback.extract_tb(exception.__traceback__)[-1])
+
     error_type = type(exception)
-    error_value = str(exception)
-    error_tb = exception.__traceback__
-    traces = traceback.extract_tb(error_tb)[::-1]
-
-    print("----------DEBUG SECTION PRINTING-----------")
-    print("error_type: ", error_type)
-    print("error_value: ", error_value)
-    print("--------------TRACEBACK--------------------")
-    print("traceback: ", error_tb)
-    print("--------------TRACES------------------------")
-    for trace in traces:
-        print(trace)
-    print("-------END OF DEBUG SECTION PRINTING-------")
-
     patterns = ERROR_PATTERNS.get(error_type)
-
     if not patterns:
         return
 
     handlers = []
 
-    for trace in traces:
+    # Looping through the stack trace from the bottom up.
+    for frame in stack[::-1]:
         for pattern, handler in patterns.items():
-            if re.match(pattern, trace.line):
+            if re.match(pattern, frame.line):
                 handlers.append(handler)
 
     if not handlers:
         return
 
     for handler in handlers:
-        handler(exception, headless)
+        handler(exception, stack, headless)
