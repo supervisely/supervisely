337d7a9529168f018eaab98eb9ea90f6ebf75580
Theo <theo.lisin@gmail.com>
2023-08-16 14:18:20 +0300

    change base_folder name

--- supervisely/nn/inference/cache.py
+++ supervisely/nn/inference/cache.py
@@ -117,7 +117,7 @@ class InferenceImageCache:
         maxsize: int,
         ttl: int,
         is_persistent: bool = True,
-        base_folder: str = "/tmp/smart_tool_cache",
+        base_folder: str = "/tmp/smart_cache",
     ) -> None:
         self._is_persistent = is_persistent
         self._maxsize = maxsize
