296c9ff3e2d91919f7c31d6148cd8efe9cd79c23
TheoLisin <theo.lisin@gmail.com>
2023-08-24 18:03:20 +0000

    docker pulling status

--- supervisely/io/docker_utils.py
+++ supervisely/io/docker_utils.py
@@ -1,6 +1,8 @@
 # coding: utf-8
+from __future__ import annotations
 from enum import Enum
 import json
+from typing import Optional
 from supervisely.task.progress import Progress
 
 
@@ -12,6 +14,29 @@ class PullPolicy(Enum):
     IF_AVAILABLE = "IfAvailable".lower()
     IF_NOT_PRESENT = "IfNotPresent".lower()
     NEVER = "Never".lower()
+
+
+class PullStatus(Enum):
+    START = "Pulling fs layer"
+    DOWNLOAD = "Downloading"
+    EXTRACT = "Extracting"
+    COMPLETE_LOAD = "Download complete"
+    COMPLETE_PULL = "Pull complete"
+    OTHER = "Other (unknown)"
+
+    def is_equal(self, status: str) -> bool:
+        return status == self.value
+
+    @classmethod
+    def from_str(cls, status: Optional[str]) -> PullStatus:
+        dct = {
+            "Pulling fs layer": PullStatus.START,
+            "Downloading": PullStatus.DOWNLOAD,
+            "Extracting": PullStatus.EXTRACT,
+            "Download complete": PullStatus.COMPLETE_LOAD,
+            "Pull complete": PullStatus.COMPLETE_PULL,
+        }
+        return dct.get(status, PullStatus.OTHER)
 
 
 def docker_pull_if_needed(docker_api, docker_image_name, policy, logger, progress=True):
@@ -79,25 +104,66 @@ def _docker_pull_progress(docker_api, docker_image_name, logger, raise_exception
     from docker.errors import DockerException
 
     try:
-        layers_total = {}
-        layers_current = {}
-        progress = Progress("Pulling dockerimage", 1, is_size=True, ext_logger=logger)
+        layers_total_load = {}
+        layers_current_load = {}
+        layers_total_extract = {}
+        layers_current_extract = {}
+        started = set()
+        loaded = set()
+        pulled = set()
+
+        progress_full = Progress("Preparing dockerimage", 1, ext_logger=logger)
+        progres_ext = Progress("Extracting layers", 1, is_size=True, ext_logger=logger)
+        progress_load = Progress("Downloading layers", 1, is_size=True, ext_logger=logger)
+
         for line in docker_api.api.pull(docker_image_name, stream=True, decode=True):
+            status = PullStatus.from_str(line.get("status", None))
             layer_id = line.get("id", None)
             progress_details = line.get("progressDetail", {})
-            if "total" in progress_details and "current" in progress_details:
-                layers_total[layer_id] = progress_details["total"]
-                layers_current[layer_id] = progress_details["current"]
-                total = sum(layers_total.values())
-                current = sum(layers_current.values())
-                if total > progress.total:
-                    progress.set(current, total)
-                    progress.report_progress()
-                elif (current - progress.current) / total > 0.01:
-                    progress.set(current, total)
-                    progress.report_progress()
+            need_report = True
 
-            # print(json.dumps(line, indent=4))
+            if status is PullStatus.START:
+                started.add(layer_id)
+                need_report = False
+            elif status is PullStatus.DOWNLOAD:
+                layers_total_load[layer_id] = progress_details["total"]
+                layers_current_load[layer_id] = progress_details["current"]
+                total_load = sum(layers_total_load.values())
+                current_load = sum(layers_current_load.values())
+                if total_load > progress_load.total:
+                    progress_load.set(current_load, total_load)
+                elif (current_load - progress_load.current) / total_load > 0.01:
+                    progress_load.set(current_load, total_load)
+                else:
+                    need_report = False
+            elif status is PullStatus.COMPLETE_LOAD:
+                loaded.add(layer_id)
+            elif status is PullStatus.EXTRACT:
+                layers_total_extract[layer_id] = progress_details["total"]
+                layers_current_extract[layer_id] = progress_details["current"]
+                total_ext = sum(layers_total_extract.values())
+                current_ext = sum(layers_current_extract.values())
+                if total_ext > progres_ext.total:
+                    progres_ext.set(current_ext, total_ext)
+                elif (current_ext - progres_ext.current) / total_ext > 0.01:
+                    progres_ext.set(current_ext, total_ext)
+                else:
+                    need_report = False
+            elif status is PullStatus.COMPLETE_PULL:
+                need_report = False
+                pulled.add(layer_id)
+            
+            if len(started) > len(pulled):
+                loading = len(loaded) < len(started)
+                if need_report:
+                    if loading:
+                        progress_load.report_progress()
+                    else:
+                        progres_ext.report_progress()
+            else:
+                progress_full.report_progress()
+
+        progress_full.iter_done()
         logger.info("Docker image has been pulled", extra={"image_name": docker_image_name})
     except DockerException as e:
         if raise_exception is True:
