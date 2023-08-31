3e0e3644244719cb5f8f1a3afd03904b9c170ebd
Nikolai Petukhov <petukhov.niko@gmail.com>
2023-08-30 22:39:52 -0300

    Add set_tag_meta method

--- supervisely/app/widgets/input_tag/input_tag.py
+++ supervisely/app/widgets/input_tag/input_tag.py
@@ -2,21 +2,22 @@ from typing import Dict, Union
 from supervisely.annotation.tag import Tag
 from supervisely.annotation.tag_meta import TagMeta, TagValueType
 from supervisely.app.widgets import Widget
-from supervisely.app.widgets import (
-    Switch,
-    Empty,
-    Input,
-    InputNumber,
-    RadioGroup,
-)
+from supervisely.app.widgets import Switch, Empty, Input, InputNumber, RadioGroup, OneOf, Select
 
 
 VALUE_TYPE_NAME = {
-    "none": "NONE",
-    "any_string": "TEXT",
-    "oneof_string": "ONE OF",
-    "any_number": "NUMBER"
+    str(TagValueType.NONE): "NONE",
+    str(TagValueType.ANY_STRING): "TEXT",
+    str(TagValueType.ONEOF_STRING): "ONE OF",
+    str(TagValueType.ANY_NUMBER): "NUMBER",
 }
+
+VALUE_TYPES = [
+    str(TagValueType.NONE),
+    str(TagValueType.ANY_NUMBER),
+    str(TagValueType.ANY_STRING),
+    str(TagValueType.ONEOF_STRING),
+]
 
 
 class InputTag(Widget):
@@ -26,27 +27,36 @@ class InputTag(Widget):
         max_width: int = 300,
         widget_id: int = None,
     ):
+        self._input_widgets = {}
+        self._init_input_components()
+        self._conditional_widget = Select(
+            items=[
+                Select.Item(value_type, content=self._input_widgets[value_type])
+                for value_type in VALUE_TYPES
+            ]
+        )
+        self._value_changed_callbacks = {}
+
         self._tag_meta = tag_meta
+        # if TagMeta ValueType is ONEOF_STRING, then we need to set items (possible values options) for RadioGroup
+        if self._tag_meta.value_type == str(TagValueType.ONEOF_STRING):
+            items = [RadioGroup.Item(pv, pv) for pv in self._tag_meta.possible_values]
+            self._input_widgets[str(TagValueType.ONEOF_STRING)].set_items(items)
+        self._conditional_widget.set_value(str(self._tag_meta.value_type))
+
         self._value_type_name = VALUE_TYPE_NAME[self._tag_meta.value_type]
         self._name = f"<b>{self._tag_meta.name}</b>"
         self._max_width = self._get_max_width(max_width)
         self._activation_widget = Switch()
-        self._input_widget = self._get_input_component()
+        self._input_widget = OneOf(self._conditional_widget)
 
         super().__init__(widget_id=widget_id, file_path=__file__)
 
-    def _get_input_component(self):
-        if self._tag_meta.value_type == str(TagValueType.NONE):
-            return Empty()
-        if self._tag_meta.value_type == str(TagValueType.ANY_NUMBER):
-            return InputNumber(debounce=500)
-        if self._tag_meta.value_type == str(TagValueType.ANY_STRING):
-            return Input()
-        if self._tag_meta.value_type == str(TagValueType.ONEOF_STRING):
-            items = [
-                RadioGroup.Item(pv, pv, Empty()) for pv in self._tag_meta.possible_values
-            ]
-            return RadioGroup(items=items)
+    def _init_input_components(self):
+        self._input_widgets[str(TagValueType.NONE)] = Empty()
+        self._input_widgets[str(TagValueType.ANY_NUMBER)] = InputNumber(debounce=500)
+        self._input_widgets[str(TagValueType.ANY_STRING)] = Input()
+        self._input_widgets[str(TagValueType.ONEOF_STRING)] = RadioGroup(items=[])
 
     def _get_max_width(self, value):
         if value < 150:
@@ -91,28 +101,31 @@ class InputTag(Widget):
         return Tag(self._tag_meta, tag_value)
 
     def _get_value(self):
-        if type(self._input_widget) is Empty:
+        input_widget = self._input_widgets[self._tag_meta.value_type]
+        if isinstance(input_widget, Empty):
             return None
         else:
-            return self._input_widget.get_value()
+            return input_widget.get_value()
 
     def _set_value(self, value):
         if not self.is_valid_value(value):
             raise ValueError(f'Tag value "{value}" is invalid')
-        if type(self._input_widget) is InputNumber:
-            self._input_widget.value = value
-        if type(self._input_widget) is Input:
-            self._input_widget.set_value(value)
-        if type(self._input_widget) is RadioGroup:
-            self._input_widget.set_value(value)
+        input_widget = self._input_widgets[self._tag_meta.value_type]
+        if isinstance(input_widget, InputNumber):
+            input_widget.value = value
+        if isinstance(input_widget, Input):
+            input_widget.set_value(value)
+        if isinstance(input_widget, RadioGroup):
+            input_widget.set_value(value)
 
     def _set_default_value(self):
-        if type(self._input_widget) is InputNumber:
-            self._input_widget.value = 0
-        if type(self._input_widget) is Input:
-            self._input_widget.set_value("")
-        if type(self._input_widget) is RadioGroup:
-            self._input_widget.set_value(None)
+        input_widget = self._input_widgets[self._tag_meta.value_type]
+        if isinstance(input_widget, InputNumber):
+            input_widget.value = 0
+        if isinstance(input_widget, Input):
+            input_widget.set_value("")
+        if isinstance(input_widget, RadioGroup):
+            input_widget.set_value(None)
 
     def get_json_data(self):
         return {
@@ -125,9 +138,30 @@ class InputTag(Widget):
         return None
 
     def value_changed(self, func):
-        if type(self._input_widget) is Empty:
-            return func
-        return self._input_widget.value_changed(func)
+        for value_type, input_widget in self._input_widgets.items():
+            if isinstance(input_widget, Empty):
+                self._value_changed_callbacks[value_type] = func
+            else:
+                self._value_changed_callbacks[value_type] = input_widget.value_changed(func)
+
+        def inner(*args, **kwargs):
+            return self._value_changed_callbacks[self._tag_meta.value_type](*args, **kwargs)
+
+        return inner
 
     def selection_changed(self, func):
         return self._activation_widget.value_changed(func)
+
+    def set_tag_meta(self, tag_meta: TagMeta):
+        self._tag_meta = tag_meta
+        self._value_type_name = VALUE_TYPE_NAME[self._tag_meta.value_type]
+        self._name = f"<b>{self._tag_meta.name}</b>"
+        # if TagMeta ValueType is ONEOF_STRING, then we need to set items (possible values options) for RadioGroup
+        if self._tag_meta.value_type == str(TagValueType.ONEOF_STRING):
+            items = [RadioGroup.Item(pv, pv) for pv in self._tag_meta.possible_values]
+            self._input_widgets[str(TagValueType.ONEOF_STRING)].set_items(items)
+
+        self._conditional_widget.set_value(str(self._tag_meta.value_type))
+        self._set_default_value()
+        self.deactivate()
+        self.update_data()
