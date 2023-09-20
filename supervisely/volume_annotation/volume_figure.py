5137a7091e9526697b7138fc8e3aeaee761da618
GoldenAnpu <seryios.sych@gmail.com>
2023-09-20 15:32:32 +0200

    replace type() by isinstance()

--- supervisely/volume_annotation/volume_figure.py
+++ supervisely/volume_annotation/volume_figure.py
@@ -93,7 +93,7 @@ class VolumeFigure(VideoFigure):
         created_at=None,
     ):
         # only Mask3D can be created without 'plane_name' and 'slice_index'
-        if type(geometry) not in [Mask3D, ClosedSurfaceMesh]:
+        if not isinstance(geometry, (Mask3D, ClosedSurfaceMesh)):
             if plane_name is None and slice_index is None:
                 raise TypeError(
                     "VolumeFigure.__init__() missing 2 required positional arguments: 'plane_name' and 'slice_index'"
