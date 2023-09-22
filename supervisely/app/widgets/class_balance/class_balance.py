2f1e2c8659cb09e2856259c2892f6fc33afb34cc
almaz <almazgimaev@gmail.com>
2023-09-22 11:33:47 +0300

    updated ClassBalance widget

--- supervisely/app/widgets/class_balance/class_balance.py
+++ supervisely/app/widgets/class_balance/class_balance.py
@@ -1,6 +1,7 @@
 from supervisely.app import DataJson, StateJson
 from supervisely.app.widgets import Widget
 from typing import List, Dict
+from copy import deepcopy
 
 
 class ClassBalance(Widget):
@@ -9,34 +10,35 @@ class ClassBalance(Widget):
 
     def __init__(
         self,
-        max_value: int = None,
         segments: List[Dict] = [],
         rows_data: List[Dict] = [],
-        slider_data: Dict = {},
-        max_height: int = 300,
+        slider_data: Dict[str, List] = {},
+        max_value: int = None,
+        max_height: int = 350,
+        rows_height: int = 100,
         selectable: bool = True,
         collapsable: bool = False,
         clickable_name: bool = False,
         clickable_segment: bool = False,
         widget_id: str = None,
     ):
-        self._max_value = max_value
         self._segments = segments
         self._rows_data = rows_data
         self._slider_data = slider_data
+        self._max_value = max_value
         self._max_height = f"{max_height}px"
+        self._rows_height = f"{rows_height}px"
         self._selectable = selectable
         self._collapsable = collapsable
         self._clickable_name = clickable_name
         self._clickable_segment = clickable_segment
         self._click_handled = False
 
-        if self._max_value is None and len(self._rows_data) != 0:
-            check_max_value = []
-            for curr_row in self._rows_data:
-                check_max_value.append(curr_row["total"])
-
-            self._max_value = max(check_max_value)
+        if self._max_value is None:
+            if len(self._rows_data) != 0:
+                self._max_value = max([curr_row["total"] for curr_row in self._rows_data])
+            else:
+                self._max_value = 0
 
         for curr_row in self._rows_data:
             if curr_row.get("segments") is None:
@@ -59,6 +61,7 @@ class ClassBalance(Widget):
                 "maxHeight": self._max_height,
             },
             "imageSliderData": self._slider_data,
+            "imageSliderOptions": {"selectable": False, "height": self._rows_height},
         }
 
     def get_json_state(self):
@@ -94,6 +97,8 @@ class ClassBalance(Widget):
         DataJson().send_changes()
 
     def set_max_height(self, value: int):
+        if type(value) != int:
+            raise TypeError("Max height must be an integer.")
         self._max_height = f"{value}px"
         DataJson()[self.widget_id]["options"]["maxHeight"] = self._max_height
         DataJson().send_changes()
@@ -102,7 +107,7 @@ class ClassBalance(Widget):
         self._max_height = DataJson()[self.widget_id]["options"]["maxHeight"]
         return int(self._max_height[:-2])
 
-    def add_segments(self, segments: List[Dict] = [], send_changes=True):
+    def add_segments(self, segments: List[Dict] = []):
         for curr_segment in segments:
             if curr_segment["name"] in [seg["name"] for seg in self._segments]:
                 raise ValueError("Segment name already exists.")
@@ -111,58 +116,55 @@ class ClassBalance(Widget):
             self._segments.append(curr_segment)
 
         self.update_data()
-        if send_changes:
-            DataJson().send_changes()
+        DataJson().send_changes()
 
     def get_segments(self):
         self._segments = DataJson()[self.widget_id]["content"]["segments"]
         return self._segments
 
-    def set_segments(self, segments: List[Dict] = [], send_changes=True):
+    def set_segments(self, segments: List[Dict] = []):
         self._segments = segments
         self.update_data()
-        if send_changes:
-            DataJson().send_changes()
+        DataJson().send_changes()
 
-    def add_rows_data(self, rows_data: List[Dict] = [], send_changes=True):
+    def add_rows_data(self, rows_data: List[Dict] = []):
         for curr_rows_data in rows_data:
             if curr_rows_data["name"] in [row_data["name"] for row_data in self._rows_data]:
                 raise ValueError("Row data with the same name already exists.")
             self._rows_data.append(curr_rows_data)
 
         self.update_data()
-        if send_changes:
-            DataJson().send_changes()
+        DataJson().send_changes()
 
     def get_rows_data(self):
         self._rows_data = DataJson()[self.widget_id]["content"]["rows"]
         return self._rows_data
 
-    def set_rows_data(self, rows_data: List[Dict] = [], send_changes=True):
+    def set_rows_data(self, rows_data: List[Dict] = []):
         self._rows_data = rows_data
         self.update_data()
-        if send_changes:
-            DataJson().send_changes()
+        DataJson().send_changes()
 
-    def add_slider_data(self, slider_data: Dict = {}, send_changes=True):
+    def add_slider_data(self, slider_data: Dict = {}):
         for slider_key, slider_value in slider_data.items():
             if slider_key not in [row_data["name"] for row_data in self._rows_data]:
                 raise ValueError("Row data with the same name does not exist.")
             self._slider_data[slider_key] = slider_value
 
         self.update_data()
-        if send_changes:
-            DataJson().send_changes()
+        DataJson().send_changes()
 
     def get_slider_data(self):
         self._slider_data = DataJson()[self.widget_id]["imageSliderData"]
         return self._slider_data
 
-    def set_slider_data(self, slider_data: Dict = {}, send_changes=True):
+    def set_slider_data(self, slider_data: Dict[str, List] = {}):
+        for slider_key in slider_data.keys():
+            if slider_key not in [row_data["name"] for row_data in self._rows_data]:
+                raise ValueError("Row data with the same name does not exist.")
         self._slider_data = slider_data
         self.update_data()
-        if send_changes:
-            DataJson().send_changes()
+        DataJson().send_changes()
 
     def get_selected_rows(self):
         return StateJson()[self.widget_id]["selectedRows"]
