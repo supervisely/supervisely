fdc7811fd11bdd03ad6f68268ed3f6503effeefb
almaz <almazgimaev@gmail.com>
2023-09-26 23:28:41 +0300

    update pagination

--- supervisely/app/widgets/pagination/pagination.py
+++ supervisely/app/widgets/pagination/pagination.py
@@ -2,6 +2,11 @@ from supervisely.app import DataJson, StateJson
 from supervisely.app.widgets import Widget
 from typing import List
 
+try:
+    from typing import Literal
+except ImportError:
+    from typing_extensions import Literal
+
 
 class Pagination(Widget):
     class Routes:
@@ -11,19 +16,21 @@ class Pagination(Widget):
     def __init__(
         self,
         total: int,
-        layout: str = "prev, pager, next, jumper, ->, total",
-        current_page: int = 1,
-        small: bool = False,
         page_size: int = 10,
-        page_sizes: List[int] = [10, 20, 30, 40, 50, 100],
+        current_page: int = 1,
+        layout: Literal[
+            "sizes", "prev", "pager", "next", "jumper", "->", "total", "slot"
+        ] = "prev, pager, next",
+        compact: bool = False,
+        page_size_options: List[int] = [10, 20, 30, 40, 50, 100],
         widget_id: str = None,
     ):
-        self._layout = layout
         self._total = total
-        self._current_page = current_page
-        self._small = small
         self._page_size = page_size
-        self._page_sizes = page_sizes
+        self._current_page = current_page
+        self._layout = layout
+        self._compact = compact
+        self._page_size_options = page_size_options
 
         self._current_change = False
         self._size_change = False
@@ -33,67 +40,62 @@ class Pagination(Widget):
         return {
             "layout": self._layout,
             "total": self._total,
-            "small": self._small,
-            "page_sizes": self._page_sizes,
+            "compact": self._compact,
+            "pageSizeOptions": self._page_size_options,
         }
 
     def get_json_state(self):
         return {
-            "current_page": self._current_page,
-            "page_size": self._page_size,
+            "currentPage": self._current_page,
+            "pageSize": self._page_size,
         }
 
     def get_current_page(self):
-        return StateJson()[self.widget_id]["current_page"]
+        return StateJson()[self.widget_id]["currentPage"]
 
-    def get_page_size(self):
-        return StateJson()[self.widget_id]["page_size"]
-
-    def set_page_size(self, value: int):
-        self._current_page = value
-        StateJson()[self.widget_id]["current_page"] = self._current_page
+    def set_current_page(self, page: int):
+        max_page = self.get_total() // self.get_page_size() + 1
+        if page > max_page:
+            page = max_page
+        if page < 1:
+            page = 1
+        if type(page) != int:
+            raise TypeError("Current page value must be int")
+        self._current_page = page
+        StateJson()[self.widget_id]["currentPage"] = self._current_page
         StateJson().send_changes()
-
-    def set_current_size(self, value: int):
-        self._page_size = value
-        StateJson()[self.widget_id]["page_size"] = self._page_size
-        StateJson().send_changes()
-
-    def get_layout(self):
-        return DataJson()[self.widget_id]["layout"]
-
-    def set_layout(self, value: str):
-        self._layout = value
-        DataJson()[self.widget_id]["layout"] = self._layout
-        DataJson().send_changes()
 
     def get_total(self):
         return DataJson()[self.widget_id]["total"]
 
     def set_total(self, value: int):
+        if type(value) != int:
+            raise TypeError("Total value must be int")
         self._total = value
         DataJson()[self.widget_id]["total"] = self._total
         DataJson().send_changes()
 
-    def unable_small_pagination(self):
-        self._small = True
-        DataJson()[self.widget_id]["small"] = self._small
+    def get_page_size(self):
+        return StateJson()[self.widget_id]["pageSize"]
+
+    def set_page_size(self, value: int):
+        if type(value) != int:
+            raise TypeError("Page size value must be int")
+        self._current_page = value
+        StateJson()[self.widget_id]["pageSize"] = self._current_page
+        StateJson().send_changes()
+
+    def get_page_size_options(self):
+        return DataJson()[self.widget_id]["pageSizeOptions"]
+
+    def set_page_size_options(self, value: List[int]):
+        if type(value) != list or not all(type(x) == int for x in value):
+            raise TypeError("Page size options value must be list of integers")
+        self._page_size_options = value
+        DataJson()[self.widget_id]["pageSizeOptions"] = self._page_size_options
         DataJson().send_changes()
 
-    def disable_small_pagination(self):
-        self._small = False
-        DataJson()[self.widget_id]["small"] = self._small
-        DataJson().send_changes()
-
-    def get_page_sizes(self):
-        return DataJson()[self.widget_id]["page_sizes"]
-
-    def set_page_sizes(self, value: List[int]):
-        self._page_sizes = value
-        DataJson()[self.widget_id]["page_sizes"] = self._page_sizes
-        DataJson().send_changes()
-
-    def current_change(self, func):
+    def page_changed(self, func):
         route_path = self.get_route_path(Pagination.Routes.CURRENT_CHANGE)
         server = self._sly_app.get_server()
         self._current_change = True
@@ -105,7 +107,7 @@ class Pagination(Widget):
 
         return _click
 
-    def size_change(self, func):
+    def page_size_changed(self, func):
         route_path = self.get_route_path(Pagination.Routes.SIZE_CHANGE)
         server = self._sly_app.get_server()
         self._size_change = True
