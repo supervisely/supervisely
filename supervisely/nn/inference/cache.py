9de2b58b2bb6e06497d59682d2f3e23e05c23ff6
Theo <theo.lisin@gmail.com>
2023-10-02 15:41:06 +0300

    fix recusion

--- supervisely/nn/inference/cache.py
+++ supervisely/nn/inference/cache.py
@@ -90,11 +90,11 @@ class PersistentImageTTLCache(TTLCache):
         silent_remove(filepath)
 
     def expire(self, time=None):
-        existing = set(self.keys())
+        existing = set(self._Cache__data.keys())
         sly.logger.debug(f"Existing keys: {existing}")
         super().expire(time)
-        sly.logger.debug(f"New keys: {self.keys()}")
-        deleted = existing.difference(self.keys())
+        sly.logger.debug(f"New keys: {self.self._Cache__data.keys()}")
+        deleted = existing.difference(self.self._Cache__data.keys())
         sly.logger.debug(f"Deleted keys: {deleted}")
         for key in deleted:
             self.__del_file(key)
