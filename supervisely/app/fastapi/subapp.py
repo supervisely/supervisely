a52d9a7492c2b80113f14f6b4670cce44bd97790
TheoLisin <theo.lisin@gmail.com>
2023-10-12 16:01:07 +0000

    hadle if task_id is not defined

--- supervisely/app/fastapi/subapp.py
+++ supervisely/app/fastapi/subapp.py
@@ -369,7 +369,10 @@ def set_autostart_flag_from_state(default: Optional[str] = None):
         return
 
     api = Api()
-    task_id = sly_env.task_id()
+    task_id = sly_env.task_id(raise_not_found=False)
+    if task_id is None:
+        logger.warn("Can't set `autostart` env: TASK_ID variable is not defined.")
+        return
     task_meta = api.task.get_info_by_id(task_id).get("meta", None)
     task_params = None
     task_state = None
