362c05c577e70ed45e53b21c25977173e17ca08a
Sergey <57998637+GoldenAnpu@users.noreply.github.com>
2023-10-16 13:09:37 +0200

    Widget tooltip (#720)
    * added 3d cuboid tracking support  (#704)
    
    * added cuboid3d_tracking base
    
    * added 3d tracking support
    
    * renamed files
    
    * mask3d functionality updates (#699)
    
    * add def append_classes
    
    * def append_objects
    
    * add def upload_sf_geometries;
    update def _append_bulk_mask3d with upload_sf_geometries;
    update def append_bulk to work with figures not json;
    
    * add def create_from_file;
    add def from_bytes;
    fix def from_json to add data astype(np.bool_);
    add def add_mask_2d;
    
    * add def add_objects
    
    * change def __init__ to skip plane_name, slice_index for Mask3D;
    add def from_mask3d;
    
    * add init to create with mask_3d figure
    
    * fix tag typehint
    
    * remove def from_mask3d
    
    * remove unused import
    
    * test for PR 699
    
    * fix pep and docstrings;
    fix return for mask_3d def from_json;
    
    * add AnyGeometry to def append_objects, add_objects
    
    * update typing
    
    * update _append_bulk_mask3d
    
    * update docstring
    
    * update add_mask_2d with Plane.names
    
    * add build.os to read-the-docs config, add python 3.11 to setup.py (#707)
    
    * add build.os to .readthedocs.yaml
    
    * Update .readthedocs.yaml
    
    * Update .readthedocs.yaml
    
    * add python 3.11
    
    * init
    
    * add tooltip attributes
    
    ---------
    
    Co-authored-by: Maxim Teselkin <91027877+MaxTeselkin@users.noreply.github.com>
    Co-authored-by: Pavel Bartsits <48913536+cxnt@users.noreply.github.com>

--- supervisely/app/widgets/__init__.py
+++ supervisely/app/widgets/__init__.py
@@ -101,3 +101,4 @@ from supervisely.app.widgets.pie_chart.pie_chart import PieChart
 from supervisely.app.widgets.nodes_flow.nodes_flow import NodesFlow
 from supervisely.app.widgets.dialog.dialog import Dialog
 from supervisely.app.widgets.draggable.draggable import Draggable
+from supervisely.app.widgets.tooltip.tooltip import Tooltip
