5f64a8a3a59bdf94aacc8361e9634b46bde5a941
cxnt <pavelbartsits@gmail.com>
2023-11-14 21:31:35 +0400

    add annotations

--- supervisely/app/widgets/compare_annotations/compare_annotations.py
+++ supervisely/app/widgets/compare_annotations/compare_annotations.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, List
 from supervisely import Annotation
 from supervisely.app.widgets import GridGallery
 
@@ -7,43 +7,37 @@ class CompareAnnotations(GridGallery):
     def __init__(
         self,
         columns_number: int,
-        annotations_opacity: float = 0.5,
-        show_opacity_slider: bool = True,
-        enable_zoom: bool = True,
-        resize_on_zoom: bool = True,
-        sync_views: bool = True,
+        default_opacity: float = 0.5,
         fill_rectangle: bool = True,
         border_width: int = 3,
-        show_preview: bool = False,
         view_height: Optional[int] = None,
         empty_message: str = "No image was provided",
         widget_id: str = None,
     ):
-        self._image_url = None
-        self._annotations = []
-        self._titles = []
+        self._image_url: str = None
+        self._annotations: List[Annotation] = []
+        self._titles: List[str] = []
         self._set_items(self._annotations)
 
         super().__init__(
             columns_number=columns_number,
-            annotations_opacity=annotations_opacity,
-            show_opacity_slider=show_opacity_slider,
-            enable_zoom=enable_zoom,
-            resize_on_zoom=resize_on_zoom,
-            sync_views=sync_views,
+            annotations_opacity=default_opacity,
+            show_opacity_slider=True,
+            enable_zoom=True,
+            sync_views=True,
             fill_rectangle=fill_rectangle,
             border_width=border_width,
-            show_preview=show_preview,
+            show_preview=False,
             view_height=view_height,
             empty_message=empty_message,
             widget_id=widget_id,
         )
 
     @property
-    def image_url(self):
+    def image_url(self) -> str:
         return self._image_url
 
-    def set_image_url(self, image_url: str):
+    def set_image_url(self, image_url: str) -> None:
         self._image_url = image_url
 
     def append(
@@ -54,15 +48,15 @@ class CompareAnnotations(GridGallery):
     ):
         super().append(self._image_url, annotation, title, column_index)
 
-    def is_empty(self):
+    def is_empty(self) -> bool:
         return self._image_url == None or self._image_url == ""
 
-    def clean_up(self):
+    def clean_up(self) -> None:
         super().clean_up()
         self._image_url = None
         self._annotations = []
         self._titles = []
 
-    def _set_items(self, annotations):
+    def _set_items(self, annotations: List[Annotation]) -> None:
         for ann in annotations:
             self.append(self._image_url, ann)
