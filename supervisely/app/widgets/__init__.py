4745ea53c2f7ccee0dfcf593b3249b99cdbdf34f
cxnt <pavelbartsits@gmail.com>
2023-11-14 17:24:36 +0400

    add ElementTagsList widget

--- supervisely/app/widgets/__init__.py
+++ supervisely/app/widgets/__init__.py
@@ -112,3 +112,4 @@ from supervisely.app.widgets.color_picker.color_picker import ColorPicker
 from supervisely.app.widgets.pagination.pagination import Pagination
 from supervisely.app.widgets.cascader.cascader import Cascader
 from supervisely.app.widgets.time_picker.time_picker import TimePicker
+from supervisely.app.widgets.element_tags_list.element_tags_list import ElementTagsList
