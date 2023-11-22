1daf438d889b94187e13260455dadc179855068e
almaz <almazgimaev@gmail.com>
2023-11-22 15:56:27 +0100

    handle error in GraphNodes.config_to_json(), link to the docs

--- supervisely/geometry/graph.py
+++ supervisely/geometry/graph.py
@@ -633,7 +633,15 @@ class GraphNodes(Geometry):
         :param config: dictionary(graph template) in json format
         :return: dictionary(graph template)
         """
-        return GraphNodes._transform_config_colors(config, hex2rgb)
+
+        try:
+            return GraphNodes._transform_config_colors(config, hex2rgb)
+        except Exception as e:
+            raise RuntimeError(
+                f"Failed to parse graph template from JSON format. "
+                "Check the example of graph template in JSON format: "
+                "https://developer.supervisely.com/getting-started/python-sdk-tutorials/images/keypoints#click-to-see-the-example-of-template-in-json-format"
+            )
 
     @staticmethod
     def config_to_json(config: Dict) -> Dict:
