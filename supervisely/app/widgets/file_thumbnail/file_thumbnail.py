83b036d7945f4104e0bf83572415ca02051c3aa6
iwatkot <iwatkot@gmail.com>
2024-01-02 17:13:40 +0300

    FileThumbnail.

--- supervisely/app/widgets/file_thumbnail/file_thumbnail.py
+++ supervisely/app/widgets/file_thumbnail/file_thumbnail.py
@@ -1,12 +1,20 @@
-import os
+from typing import Any, Dict, Optional
+
+from supervisely._utils import abs_url
 from supervisely.api.file_api import FileInfo
 from supervisely.app import DataJson
 from supervisely.app.widgets import Widget
-from supervisely._utils import abs_url
 
 
 class FileThumbnail(Widget):
-    def __init__(self, info: FileInfo = None, widget_id: str = None):
+    """FileThumbnail widget in Supervisely displays an icon, link, and path to the file in Team Files.
+
+    Read about it in `Developer Portal <https://developer.supervisely.com/app-development/widgets/thumbnails/filethumbnail`_
+        (including screenshots and examples).
+
+    """
+
+    def __init__(self, info: Optional[FileInfo] = None, widget_id: Optional[str] = None):
         self._id: int = None
         self._info: FileInfo = None
         self._description: str = None
@@ -15,13 +23,26 @@ class FileThumbnail(Widget):
 
         super().__init__(widget_id=widget_id, file_path=__file__)
 
-    def get_json_data(self):
+    def get_json_data(self) -> Dict[str, Any]:
+        """Returns dictionary with widget data, which defines the appearance and behavior of the widget.
+        Dictionary contains the following fields:
+            - id: id of the file
+            - name: name of the file
+            - description: path to the file
+            - url: url to the file
+            - icon: dictionary with following fields:
+                - className: class name of the icon
+                - color: color of the icon
+                - bgColor: background color of the icon
+
+        :return: dictionary with widget data
+        :rtype: Dict[str, Any]
+        """
         return {
             "id": self._id,
             "name": "File in Team files",
             "description": self._description,
             "url": self._url,
-            "description": self._description,
             "icon": {
                 "className": "zmdi zmdi-file-text",
                 "color": "#4977ff",
@@ -29,7 +50,8 @@ class FileThumbnail(Widget):
             },
         }
 
-    def get_json_state(self):
+    def get_json_state(self) -> None:
+        """FileThumbnail widget does not have state, the method returns None."""
         return None
 
     def _set_info(self, info: FileInfo = None):
@@ -44,7 +66,12 @@ class FileThumbnail(Widget):
         self._description = info.path
         self._url = abs_url(f"/files/{info.id}")
 
-    def set(self, info):
+    def set(self, info: FileInfo) -> None:
+        """Sets the file to display.
+
+        :param info: file info
+        :type info: FileInfo
+        """
         self._set_info(info)
         self.update_data()
         DataJson().send_changes()
