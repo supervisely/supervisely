7994d02be43a8ca10749821c71dcefe1be440eb4
Ars Vladimirov <grokhael@gmail.com>
2024-01-12 20:58:10 +0600

    rm docstring

--- supervisely/task/progress.py
+++ supervisely/task/progress.py
@@ -615,14 +615,13 @@ class tqdm_sly(tqdm, Progress):
         orig_tqdm: tqdm,
         **kwargs,
     ):
-        """
-        iterable=None, desc=None, total=None, leave=True, file=None,
-                 ncols=None, mininterval=0.1, maxinterval=10.0, miniters=None,
-                 ascii=None, disable=False, unit='it', unit_scale=False,
-                 dynamic_ncols=False, smoothing=0.3, bar_format=None, initial=0,
-                 position=None, postfix=None, unit_divisor=1000, write_bytes=False,
-                 lock_args=None, nrows=None, colour=None, delay=0, gui=False,
-        """
+        # iterable=None, desc=None, total=None, leave=True, file=None,
+        #          ncols=None, mininterval=0.1, maxinterval=10.0, miniters=None,
+        #          ascii=None, disable=False, unit='it', unit_scale=False,
+        #          dynamic_ncols=False, smoothing=0.3, bar_format=None, initial=0,
+        #          position=None, postfix=None, unit_divisor=1000, write_bytes=False,
+        #          lock_args=None, nrows=None, colour=None, delay=0, gui=False,
+
         vrs = vars(orig_tqdm).copy()
         sgn = inspect.signature(orig_tqdm.__init__)
 
