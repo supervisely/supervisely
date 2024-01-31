13c0992b327f0c6e8c33ca0c4d8e6a14a4d9429c
Nikolai Petukhov <petukhov.niko@gmail.com>
2024-01-31 00:01:57 -0300

    update urllib3 version requirement to be <2.0.0

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ INSTALL_REQUIRES = [
     "rich",
     "click",
     "imutils==0.5.4",
-    "urllib3>=1.26.15, <=2.1.0",
+    "urllib3>=1.26.15, <2.0.0",
     "cacheout==0.14.1",
     "jsonschema>=2.6.0,<=4.20.0",
     "pyjwt>=2.1.0,<3.0.0",
