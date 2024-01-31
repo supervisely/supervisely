d0a30f9e1be94eec50c67c1527324cc1eb4f1eb7
almaz <almazgimaev@gmail.com>
2024-01-31 11:50:35 +0100

    resolve dependencies conflict

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ INSTALL_REQUIRES = [
     "psutil>=5.9.0, <6.0.0",
     "jsonpatch>=1.32, <2.0",
     "MarkupSafe>=2.1.1, <3.0.0",
-    "arel>=0.2.0, <1.0.0",
+    "arel>=0.3.0, <1.0.0",
     "tqdm>=4.62.3, <5.0.0",
     "pandas>=1.1.3, <=2.1.4",
     "async_asgi_testclient",
