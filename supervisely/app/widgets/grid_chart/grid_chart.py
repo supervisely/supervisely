b0d3def1f33640a22bb97fc4f98fdef04aa28408
Ars Vladimirov <grokhael@gmail.com>
2024-02-07 20:03:29 +0600

    GridChart: add self.add_scalars()

--- supervisely/app/widgets/grid_chart/grid_chart.py
+++ supervisely/app/widgets/grid_chart/grid_chart.py
@@ -30,51 +30,52 @@ class GridChart(Widget):
         self._columns = columns
         self._gap = gap
 
-        for plot_data in data:
-            if isinstance(plot_data, dict):
-                self._widgets[plot_data["title"]] = LineChart(**plot_data)
-            else:
-                self._widgets[plot_data] = LineChart(title=plot_data, series=[])
+        if len(data) > 0:  # TODO: add the self.add_data([data:dict]) method
+            for plot_data in data:
+                if isinstance(plot_data, dict):
+                    self._widgets[plot_data["title"]] = LineChart(**plot_data)
+                else:
+                    self._widgets[plot_data] = LineChart(title=plot_data, series=[])
 
-        if self._columns < 1:
-            raise ValueError(f"columns ({self._columns}) < 1")
-        if self._columns > len(self._widgets):
-            logger.warn(
-                f"Number of columns ({self._columns}) > number of widgets ({len(self._widgets)}). Columns are set to {len(self._widgets)}"
-            )
-            self._columns = len(self._widgets)
-
-        self._content = None
-        if self._columns == 1:
-            self._content = Container(
-                direction="vertical",
-                widgets=self._widgets.values(),
-                gap=self._gap,
-                widget_id=generate_id(),
-            )
-        else:
-            rows = []
-            num_empty = len(self._widgets) % self._columns
-            for i in range(num_empty):
-                self._widgets[generate_id()] = Empty()
-            for batch in batched(list(self._widgets.values()), batch_size=self._columns):
-                rows.append(
-                    Container(
-                        direction="horizontal",
-                        widgets=batch,
-                        gap=self._gap,
-                        fractions=[1] * len(batch),
-                        widget_id=generate_id(),
-                        overflow=None,
-                        widgets_style="flex-direction: column",
-                    )
+            if self._columns < 1:
+                raise ValueError(f"columns ({self._columns}) < 1")
+            elif self._columns > len(self._widgets):
+                logger.warn(
+                    f"Number of columns ({self._columns}) > number of widgets ({len(self._widgets)}). Columns are set to {len(self._widgets)}"
                 )
-            self._content = Container(
-                direction="vertical",
-                widgets=rows,
-                gap=self._gap,
-                widget_id=generate_id(),
-            )
+                self._columns = len(self._widgets)
+
+            self._content = None
+            if self._columns == 1:
+                self._content = Container(
+                    direction="vertical",
+                    widgets=self._widgets.values(),
+                    gap=self._gap,
+                    widget_id=generate_id(),
+                )
+            else:
+                rows = []
+                num_empty = len(self._widgets) % self._columns
+                for i in range(num_empty):
+                    self._widgets[generate_id()] = Empty()
+                for batch in batched(list(self._widgets.values()), batch_size=self._columns):
+                    rows.append(
+                        Container(
+                            direction="horizontal",
+                            widgets=batch,
+                            gap=self._gap,
+                            fractions=[1] * len(batch),
+                            widget_id=generate_id(),
+                            overflow=None,
+                            widgets_style="flex-direction: column",
+                        )
+                    )
+                self._content = Container(
+                    direction="vertical",
+                    widgets=rows,
+                    gap=self._gap,
+                    widget_id=generate_id(),
+                )
 
         super().__init__(widget_id=widget_id, file_path=__file__)
 
@@ -84,9 +85,14 @@ class GridChart(Widget):
     def get_json_state(self):
         return None
 
-    def add_scalar(self, identifier: str, y: Union[float, int], x: Union[float, int]) -> None:
+    def add_scalar(
+        self,
+        identifier: str,
+        y: Union[float, int],
+        x: Union[float, int],
+    ) -> None:
         """
-        Add scalar to series on plot. If no series with name,
+        Add scalar to a single series on plot. If no series with name,
          defined in `identifier` exists, one will be created automatically.
 
         :param identifier: '/'-separated plot and series name
@@ -96,7 +102,7 @@ class GridChart(Widget):
         :param x: x value
         :type x: float | int
         """
-        plot_title, series_name = identifier.split("/")
+        plot_title, series_name = identifier.split("/")  # TODO: maybe Dict[str,str] is way better??
         w: LineChart = self._widgets[plot_title]
         series_id, series = w.get_series_by_name(series_name)
 
@@ -104,3 +110,30 @@ class GridChart(Widget):
             w.add_to_series(name_or_id=series_id, data=[(x, y)])
         else:
             w.add_series(name=series_name, x=[x], y=[y])
+
+    def add_scalars(
+        self,
+        plot_title: str,
+        new_values: Dict[str, Union[float, int]],
+        x: Union[float, int],
+    ):
+        """
+        Add scalars to several series on one plot at point `x`
+
+        :param plot_title: name of existing plot
+        :type plot_title: str
+        :param new_values: A dictionary in the `{'series_1': y1, 'series_2': y2, ...}` format.
+        :type new_values: Dict[str, (float | int)]]
+        :param x: value of `x`
+        :type x: float | int
+        """
+        w: LineChart = self._widgets[plot_title]
+        for s_name in new_values.keys():
+            series_id, series = w.get_series_by_name(s_name)
+            if series is not None:
+                w.add_to_series(
+                    name_or_id=series_id,
+                    data=w._wrap_xy(x, new_values[s_name]),
+                )
+            else:
+                w.add_series(name=s_name, x=[x], y=[new_values[s_name]])
