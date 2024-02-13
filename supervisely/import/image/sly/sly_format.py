7a0d8e93508b1b236e6e3d8e2b6e2240c51090e2
almaz <almazgimaev@gmail.com>
2024-02-13 18:32:48 +0100

    remove old files

--- supervisely/import/image/sly/sly_format.py
+++
@@ -1,9 +0,0 @@
-class SLYFormat:
-    def __init__(self):
-        pass
-
-    def to_supervisely(self, data, source_format, target_format):
-        raise NotImplementedError()
-
-    def from_supervisely(self, data, source_format, target_format):
-        raise NotImplementedError()
