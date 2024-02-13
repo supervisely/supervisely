0b1b9dcd972477ec687e5adfff66f1609537673a
almaz <almazgimaev@gmail.com>
2024-02-13 16:31:17 +0100

    init

---
+++ supervisely/import/image/coco/coco_converter.py
@@ -0,0 +1,12 @@
+
+class COCOFormat:
+    def __init__(self):
+        pass
+
+    def to_supervisely(self, data):
+        # Конвертировать из COCO в формат Supervisely
+        pass
+
+    def from_supervisely(self, data):
+        # Конвертировать из формата Supervisely в COCO
+        pass
---
+++ supervisely/import/image/yolo/yolo_converter.py
@@ -0,0 +1,11 @@
+class YOLOFormat:
+    def __init__(self):
+        pass
+
+    def to_supervisely(self, data):
+        # Конвертировать из YOLO в формат Supervisely
+        pass
+
+    def from_supervisely(self, data):
+        # Конвертировать из формата Supervisely в YOLO
+        pass
