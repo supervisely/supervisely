f79db68019cbe9904055daad3883886e2c66aefe
Sergey <57998637+GoldenAnpu@users.noreply.github.com>
2024-02-16 18:28:32 +0100

    Reove staticmethod, fix pylint warnings in pose_estimation.py and salient_object_segmentation.py (#880)

--- supervisely/nn/inference/salient_object_segmentation/salient_object_segmentation.py
+++ supervisely/nn/inference/salient_object_segmentation/salient_object_segmentation.py
@@ -29,8 +29,8 @@ class SalientObjectSegmentation(SemanticSegmentation):
             return None
         label = Label(geometry, obj_class)
         return [label]
-    
-    @staticmethod
+
+    # pylint: disable=no-self-argument
     def process_image_crop(func):
         """
         Decorator for processing annotation labels before and after inference.
@@ -105,6 +105,7 @@ class SalientObjectSegmentation(SemanticSegmentation):
 
         @functools.wraps(func)
         def wrapper_inference(*args, **kwargs):
+            # pylint: disable=not-callable
             settings = kwargs["settings"]
             rectangle_json = settings.get("rectangle")
 
@@ -122,7 +123,7 @@ class SalientObjectSegmentation(SemanticSegmentation):
                 elif "image_path" in kwargs.keys():
                     image_path = kwargs["image_path"]
                     image_np = sly_image.read(image_path)
-                rectangle = process_padded_bbox(image_np, rectangle) # pylint: disable=used-before-assignment
+                rectangle = process_padded_bbox(image_np, rectangle)  # pylint: disable=used-before-assignment  
 
             if "image_np" in kwargs.keys():
                 image_np = kwargs["image_np"]
