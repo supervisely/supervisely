f2adb2e83296b0b22b2dca1e0258d5fd7077e512
Sergey <57998637+GoldenAnpu@users.noreply.github.com>
2024-02-16 18:28:32 +0100

    Reove staticmethod, fix pylint warnings in pose_estimation.py and salient_object_segmentation.py (#880)

--- supervisely/nn/inference/pose_estimation/pose_estimation.py
+++ supervisely/nn/inference/pose_estimation/pose_estimation.py
@@ -89,7 +89,7 @@ class PoseEstimation(Inference):
             "Have to be implemented in child class If sliding_window_mode is 'advanced'."
         )
 
-    @staticmethod
+    # pylint: disable=no-self-argument
     def process_image_crop(func):
         """
         Decorator for processing annotation labels before and after inference.
@@ -107,6 +107,7 @@ class PoseEstimation(Inference):
 
         @functools.wraps(func)
         def wrapper_inference(*args, **kwargs):
+            # pylint: disable=not-callable
             settings = kwargs["settings"]
 
             if "detected_bboxes" in settings:
