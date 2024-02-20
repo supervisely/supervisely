d11ffd7a51db4ac8042a424710a7960d4cd32633
almaz <almazgimaev@gmail.com>
2024-02-20 13:21:09 +0100

    start sly video converter

--- supervisely/convert/video/sly/sly_video_converter.py
+++ supervisely/convert/video/sly/sly_video_converter.py
@@ -1,27 +1,113 @@
-from supervisely.convert.base_converter import AvailableVideoFormats, BaseConverter
+import imghdr
+import os
+
+# import supervisely.convert.image.sly.sly_image_helper as sly_image_helper
+from supervisely import VideoAnnotation, ProjectMeta, logger
+from supervisely.convert.base_converter import AvailableVideoConverters
+from supervisely.convert.video.video_converter import VideoConverter
+from supervisely.io.fs import JUNK_FILES, get_file_ext
+from supervisely.io.json import load_json_file
 
 
-class SLYVideoConverter(BaseConverter):
+class SLYVideoConverter(VideoConverter):
     def __init__(self, input_data):
-        super().__init__(input_data)
+        self._input_data = input_data
+        self._items = []
+        self._meta = None
+        self._key_id_map = None
 
     def __str__(self):
-        return AvailableVideoFormats.SLY
+        return AvailableVideoConverters.SLY
+
+    @property
+    def ann_ext(self):
+        return ".json"
+
+    def validate_ann_file(self, ann_path: str, meta: ProjectMeta):
+        try:
+            ann_json = load_json_file(ann_path)
+            if "annotation" in ann_json:
+                ann_json = ann_json["annotation"]
+            ann = VideoAnnotation.from_json(ann_json, meta)
+            return True
+        except:
+            return False
+
+    def validate_key_file(self, key_file_path: str):
+        try:
+            self._meta = ProjectMeta.from_json(load_json_file(key_file_path))
+            return True
+        except Exception:
+            return False
+
+    def validate_format(self):
+        detected_ann_cnt = 0
+        meta_path = None
+        images_list, ann_dict = [], {}
+        for root, _, files in os.walk(self._input_data):
+            for file in files:
+                full_path = os.path.join(root, file)
+                if file == "meta.json":
+                    is_valid = self.validate_key_file(full_path)
+                    if is_valid:
+                        continue
+
+                ext = get_file_ext(full_path)
+                if file in JUNK_FILES:  # add better check
+                    continue
+                elif ext in self.ann_ext:
+                    ann_dict[file] = full_path
+                elif imghdr.what(full_path) is None:
+                    # logger.info(f"Non-image file found: {full_path}")
+                    return False
+                else:
+                    images_list.append(full_path)
+
+        if self._meta is not None:
+            meta = self._meta
+        else:
+            meta = ProjectMeta()
+
+        # create Items
+        self._items = []
+        for image_path in images_list:
+            item = self.Item(image_path)
+            ann_name = f"{item.name}.json"
+            if ann_name in ann_dict:
+                ann_path = ann_dict[ann_name]
+                if self._meta is None:
+                    meta = self.generate_meta_from_annotation(ann_path, meta)
+                is_valid = self.validate_ann_file(ann_path, meta)
+                if is_valid:
+                    item.set_ann_data(ann_path)
+                    detected_ann_cnt += 1
+            self._items.append(item)
+        self._meta = meta
+        return detected_ann_cnt > 0
 
     def get_meta(self):
-        return super().get_meta()
+        return self._meta
+
+    def generate_meta_from_annotation(self, ann_path, meta):
+        # meta = sly_image_helper.get_meta_from_annotation(meta, ann_path)
+        return meta
 
     def get_items(self):
-        return super().get_items()
+        return self._items
 
-    def to_supervisely(self):
-        raise NotImplementedError()
+    def to_supervisely(self, item: VideoConverter.Item, meta: ProjectMeta = None) -> VideoAnnotation:
+        """Convert to Supervisely format."""
+        if meta is None:
+            meta = self._meta
 
-    def to_coco(self):
-        raise NotImplementedError()
+        if item.ann_data is None:
+            return item.create_empty_annotation()
 
-    def to_pascal_voc(self):
-        raise NotImplementedError()
-
-    def to_yolo(self):
-        raise NotImplementedError()
+        try:
+            ann_json = load_json_file(item.ann_data)
+            if "annotation" in ann_json:
+                ann_json = ann_json["annotation"]
+            return VideoAnnotation.from_json(ann_json, meta)
+        except Exception as e:
+            logger.warn(f"Failed to convert annotation: {repr(e)}")
+            return item.create_empty_annotation()
