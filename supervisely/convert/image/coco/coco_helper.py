ed645a96ca733b14a76e0a01dcabb502248767ae
almaz <almazgimaev@gmail.com>
2024-02-21 16:26:57 +0100

    implement skeleton support, add bindiing, fix captions

--- supervisely/convert/image/coco/coco_helper.py
+++ supervisely/convert/image/coco/coco_helper.py
@@ -1,3 +1,4 @@
+import uuid
 from copy import deepcopy
 from typing import List
 
@@ -9,7 +10,9 @@ from pycocotools.coco import COCO
 from supervisely import (
     Annotation,
     Bitmap,
+    GraphNodes,
     Label,
+    Node,
     ObjClass,
     PointLocation,
     Polygon,
@@ -21,6 +24,7 @@ from supervisely import (
     logger,
 )
 from supervisely.convert.image.image_converter import ImageConverter
+from supervisely.geometry.graph import KeypointsTemplate
 from supervisely.imaging.color import generate_rgb
 
 
@@ -32,6 +36,9 @@ def create_supervisely_annotation(
     imag_tags = []
     name_cat_id_map = coco_category_to_class_name(coco_categories)
     for object in item.ann_data:
+        caption = object.get("caption")
+        if caption is not None:
+            imag_tags.append(Tag(meta.get_tag_meta("caption"), caption))
         category_id = object.get("category_id")
         if category_id is None:
             continue
@@ -39,6 +46,7 @@ def create_supervisely_annotation(
         if obj_class_name is None:
             logger.warn(f"Category with id {category_id} not found in categories list")
             continue
+        key = None
         segm = object.get("segmentation")
         curr_labels = []
         if segm is not None and len(segm) > 0:
@@ -48,11 +56,42 @@ def create_supervisely_annotation(
                 polygons = convert_rle_mask_to_polygon(object)
                 for polygon in polygons:
                     figure = polygon
-                    label = Label(figure, obj_class_polygon)
+                    key = uuid.uuid4().hex
+                    label = Label(figure, obj_class_polygon, binding_key=key)
                     curr_labels.append(label)
             elif type(segm) is list and object["segmentation"]:
                 figures = convert_polygon_vertices(object, item.shape)
-                curr_labels.extend([Label(figure, obj_class_polygon) for figure in figures])
+                for figure in figures:
+                    key = uuid.uuid4().hex
+                    label = Label(figure, obj_class_polygon, binding_key=key)
+                    curr_labels.append(label)
+
+        keypoints = object.get("keypoints")
+        if keypoints is not None:
+            obj_class_keypoints = meta.get_obj_class(obj_class_name)
+            keypoints = list(get_coords(object["keypoints"]))
+            coco_categorie, keypoint_names = None, None
+            for cat in coco_categories:
+                if cat["id"] == category_id and cat["supercategory"] == obj_class_name:
+                    coco_categorie = cat
+                    break
+            if coco_categorie is not None:
+                keypoint_names = coco_categorie.get("keypoints")
+            if keypoint_names is not None:
+                nodes = []
+                for coords, keypoint_name in zip(keypoints, keypoint_names):
+                    col, row, visibility = coords
+                    if visibility in [0, 1]:
+                        continue  # skip invisible keypoints
+
+                    node = Node(label=keypoint_name, row=row, col=col)  # , disabled=v)
+                    nodes.append(node)
+                if len(nodes) != 0:
+                    key = uuid.uuid4().hex
+                    label = Label(
+                        GraphNodes(nodes), obj_class_keypoints, binding_key=key
+                    )
+                    curr_labels.append(label)
         labels.extend(curr_labels)
         bbox = object.get("bbox")
         if bbox is not None and len(bbox) == 4:
@@ -62,14 +101,17 @@ def create_supervisely_annotation(
             if len(curr_labels) > 1:
                 for label in curr_labels:
                     bbox = label.geometry.to_bbox()
-                    labels.append(Label(bbox, obj_class_rectangle))
+                    labels.append(
+                        Label(bbox, obj_class_rectangle, binding_key=label.binding_key)
+                    )
             else:
+                if len(curr_labels) == 1:
+                    key = curr_labels[0].binding_key
                 x, y, w, h = bbox
-                rectangle = Label(Rectangle(y, x, y + h, x + w), obj_class_rectangle)
+                rectangle = Label(
+                    Rectangle(y, x, y + h, x + w), obj_class_rectangle, binding_key=key
+                )
                 labels.append(rectangle)
-        caption = object.get("caption")
-        if caption is not None:
-            imag_tags.append(Tag(meta.get_tag_meta("caption"), caption))
     return Annotation(item.shape, labels=labels, img_tags=imag_tags)
 
 
@@ -100,7 +142,9 @@ def convert_polygon_vertices(coco_ann, image_size):
         return []
     exteriors = []
     for polygon in polygons:
-        polygon = [polygon[i * 2 : (i + 1) * 2] for i in range((len(polygon) + 2 - 1) // 2)]
+        polygon = [
+            polygon[i * 2 : (i + 1) * 2] for i in range((len(polygon) + 2 - 1) // 2)
+        ]
         exterior_points = [(width, height) for width, height in polygon]
         if len(exterior_points) == 0:
             continue
@@ -119,7 +163,8 @@ def convert_polygon_vertices(coco_ann, image_size):
             if idx == idy or idy in id2del:
                 continue
             points_inside = [
-                cv2.pointPolygonTest(contours[0], (x, y), False) > 0 for x, y in exterior2
+                cv2.pointPolygonTest(contours[0], (x, y), False) > 0
+                for x, y in exterior2
             ]
             if all(points_inside):
                 interiors[idx].append(deepcopy(exteriors[idy]))
@@ -135,21 +180,46 @@ def convert_polygon_vertices(coco_ann, image_size):
 
 
 def generate_meta_from_annotation(coco: COCO, meta: ProjectMeta = None):
-    obj_classes = []
-    tag_metas = []
     colors = []
-
     ann_types = get_ann_types(coco)
-    categories = coco.loadCats(ids=coco.getCatIds())
+    if len(coco.cats) == 0:
+        categories = []
+    else:
+        categories = coco.loadCats(ids=coco.getCatIds())
     if meta is None:
         meta = ProjectMeta()
     for category in categories:
         if category["name"] in [obj_class.name for obj_class in meta.obj_classes]:
             continue
+        obj_classes = []
         new_color = generate_rgb(colors)
         colors.append(new_color)
         if ann_types is not None:  # add skeleton support
-            if "segmentation" in ann_types:
+            if "keypoints" in ann_types and "keypoints" in category:
+                geometry_config = None
+                cat_num_kp = None
+                cat_labels = category.get("keypoints", None)
+                cat_edges = category.get("skeleton", None)
+                if cat_labels is None:
+                    cat_num_kp = []
+                    for img_id, img_info in coco.imgs.items():
+                        ann_p = coco.anns[img_id]
+                        for ann in ann_p:
+                            if ann["category_id"] == category["id"]:
+                                cat_num_kp.append(ann["num_keypoints"])
+                    cat_num_kp = max(cat_num_kp)
+
+                geometry_config = create_custom_geometry_config(
+                    num_keypoints=cat_num_kp, cat_labels=cat_labels, cat_edges=cat_edges
+                )
+                obj_class = ObjClass(
+                    name=category["name"],
+                    geometry_type=GraphNodes,
+                    color=new_color,
+                    geometry_config=geometry_config,
+                )
+                obj_classes.append(obj_class)
+            elif "segmentation" in ann_types:
                 obj_classes.append(ObjClass(category["name"], Polygon, new_color))
             if "bbox" in ann_types:
                 obj_classes.append(
@@ -159,12 +229,10 @@ def generate_meta_from_annotation(coco: COCO, meta: ProjectMeta = None):
             existing_classes = [obj_class.name for obj_class in meta.obj_classes]
             if obj_class.name not in existing_classes:
                 meta = meta.add_obj_class(obj_class)
-        if ann_types is not None and "caption" in ann_types:
-            tag_metas.append(TagMeta("caption", TagValueType.ANY_STRING))
-        for tag_meta in tag_metas:
-            existing_tags = [tag_meta.name for tag_meta in meta.tag_metas]
-            if tag_meta.name not in existing_tags:
-                meta = meta.add_tag_meta(tag_meta)
+    if ann_types is not None and "caption" in ann_types:
+        tag_meta = TagMeta("caption", TagValueType.ANY_STRING)
+        if meta.get_tag_meta(tag_meta.name) is None:
+            meta = meta.add_tag_meta(tag_meta)
     return meta
 
 
@@ -177,6 +245,8 @@ def get_ann_types(coco: COCO) -> List[str]:
         ann_types.append("segmentation")
     if any("caption" in coco.anns[ann_id] for ann_id in annotation_ids):
         ann_types.append("caption")
+    if any("keypoints" in coco.anns[ann_id] for ann_id in annotation_ids):
+        ann_types.append("keypoints")
     return ann_types
 
 
@@ -188,3 +258,32 @@ def add_tail(body: str, tail: str):
 
 def coco_category_to_class_name(coco_categories):
     return {category["id"]: category["name"] for category in coco_categories}
+
+
+def get_coords(keypoints):
+    for i in range(0, len(keypoints), 3):
+        yield keypoints[i : i + 3]
+
+
+def create_custom_geometry_config(num_keypoints=None, cat_labels=None, cat_edges=None):
+    template = KeypointsTemplate()
+
+    if cat_labels is None:
+        if num_keypoints is None:
+            raise ValueError(
+                "Number of keypoints can not be specified, please check your annotation (categories: num_keypoints)"
+            )
+        for p in list(range(num_keypoints)):
+            template.add_point(label=str(p), row=0, col=0)
+    else:
+        for label in cat_labels:
+            template.add_point(label=label, row=0, col=0)
+
+    if cat_edges is not None and cat_labels is not None:
+        for edge in cat_edges:
+            template.add_edge(src=cat_labels[edge[0] - 1], dst=cat_labels[edge[1] - 1])
+    else:
+        logger.warn(
+            "Edges can not be mapped without skeleton, please check your annotation"
+        )
+    return template
