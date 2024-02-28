04c8fc1dd0be214f271cb0e76e13688b2b52cd7e
almaz <almazgimaev@gmail.com>
2024-02-28 10:07:25 +0100

    fix meta merge func

--- supervisely/convert/base_converter.py
+++ supervisely/convert/base_converter.py
@@ -207,9 +207,10 @@ class BaseConverter:
             while meta1.obj_classes.get(new_name) is not None:
                 new_name = f"{new_cls.name}_{i}"
                 i += 1
-            logger.warn(f"Class {new_cls.name} renamed to {new_name}")
-            renamed_classes[new_cls.name] = new_name
-            new_cls = new_cls.clone(name=new_name)
+            if new_name != new_cls.name:
+                logger.warn(f"Class {new_cls.name} renamed to {new_name}")
+                renamed_classes[new_cls.name] = new_name
+                new_cls = new_cls.clone(name=new_name)
             new_obj_classes.append(new_cls)
 
         for existing_tag in meta1.tag_metas:
@@ -226,9 +227,10 @@ class BaseConverter:
             while meta1.tag_metas.get(new_name) is not None:
                 new_name = f"{new_tag.name}_{i}"
                 i += 1
-            logger.warn(f"Tag {new_tag.name} renamed to {new_name}")
-            renamed_tags[new_tag.name] = new_name
-            new_tag = new_tag.clone(name=new_name)
+            if new_name != new_tag.name:
+                logger.warn(f"Tag {new_tag.name} renamed to {new_name}")
+                renamed_tags[new_tag.name] = new_name
+                new_tag = new_tag.clone(name=new_name)
             new_tags.append(new_tag)
 
         new_meta = meta1.clone(obj_classes=new_obj_classes, tag_metas=new_tags)
