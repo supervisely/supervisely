42e508995f390f465c0ad34931d7eecaa58a113c
Nikolai Petukhov <petukhov.niko@gmail.com>
2024-02-28 14:30:25 -0300

    added methods to download projects using cache

--- supervisely/project/download.py
+++ supervisely/project/download.py
@@ -1,9 +1,14 @@
-from typing import Callable, List, Optional, Union
+import os
+from typing import Callable, List, Optional, Tuple, Union
 
 from tqdm import tqdm
 
 from supervisely import get_project_class
 from supervisely.api.api import Api
+from supervisely.api.dataset_api import DatasetInfo
+from supervisely.io.fs import copy_dir_recursively, dir_exists
+
+CACHE_DIR = "/apps_cache"
 
 
 def download(
@@ -150,3 +155,112 @@ def download(
         progress_cb=progress_cb,
         **kwargs,
     )
+
+
+def _get_cache_dir(project_id: int, dataset_name: str = None) -> str:
+    p = os.path.join(CACHE_DIR, str(project_id))
+    if dataset_name is not None:
+        os.path.join(p, dataset_name)
+    return p
+
+
+def is_cached(project_id, dataset_name: str = None) -> bool:
+    return dir_exists(_get_cache_dir(project_id, dataset_name))
+
+
+def _split_by_cache(project_id: int, dataset_names: List[str]) -> Tuple[List, List]:
+    if not is_cached(project_id):
+        return dataset_names, []
+    to_download = [ds_name for ds_name in dataset_names if not is_cached(project_id, ds_name)]
+    cached = [ds_name for ds_name in dataset_names if is_cached(project_id, ds_name)]
+    return to_download, cached
+
+
+def download_to_cache(
+    api: Api,
+    project_id: int,
+    dataset_infos: List[DatasetInfo] = None,
+    dataset_ids: List[int] = None,
+    log_progress: bool = True,
+    progress_cb=None,
+    **kwargs,
+) -> Tuple[List, List]:
+    """
+    Download datasets to cache.
+    If dataset_infos is not None, dataset_ids must be None and vice versa.
+    If both dataset_infos and dataset_ids are None, all datasets of the project will be downloaded.
+    :param api: supervisely.api.api.Api
+    :param project_id: int
+    :param dataset_infos: List[supervisely.api.dataset_api.DatasetInfo]
+    :param dataset_ids: List[int]
+    :param log_progress: bool
+    :param progress_cb: callable which will be called with number of items downloaded
+    :return: Tuple[List, List] where the first list contains names of downloaded datasets and the second list contains
+    names of cached datasets
+    """
+    cache_project_dir = _get_cache_dir(project_id)
+    if dataset_infos is not None and dataset_ids is not None:
+        raise ValueError("dataset_infos and dataset_ids cannot be specified at the same time")
+    if dataset_infos is None:
+        if dataset_ids is None:
+            dataset_infos = api.dataset.get_list(project_id)
+        else:
+            dataset_infos = [api.dataset.get_info_by_id(dataset_id) for dataset_id in dataset_ids]
+    name_to_info = {info.name: info for info in dataset_infos}
+    to_download, cached = _split_by_cache(project_id, [info.name for info in dataset_infos])
+    if progress_cb is not None:
+        cached_items_n = sum(name_to_info[ds_name].items_count for ds_name in cached)
+        progress_cb(cached_items_n)
+    dataset_ids = [name_to_info[name].id for name in to_download]
+    download(
+        api=api,
+        project_id=project_id,
+        dest_dir=cache_project_dir,
+        dataset_ids=dataset_ids,
+        log_progress=log_progress,
+        progress_cb=progress_cb,
+        **kwargs,
+    )
+    return to_download, cached
+
+
+def copy_from_cache(
+    project_id: int, dst_dir: str, dataset_name: str = None, progress_cb: Callable = None
+):
+    """
+    Copy project or dataset from cache to the specified directory.
+    If dataset_name is None, the whole project will be copied.
+    :param project_id: int
+    :param dst_dir: str
+    :param dataset_name: str
+    :param progress_cb: callable
+    :return: None
+    """
+    if not is_cached(project_id, dataset_name):
+        msg = f"Project {project_id} is not cached"
+        if dataset_name is not None:
+            msg = f"Dataset {dataset_name} of project {project_id} is not cached"
+        raise RuntimeError(msg)
+    cache_dir = _get_cache_dir(project_id, dataset_name)
+    copy_dir_recursively(cache_dir, dst_dir, progress_cb)
+
+
+def download_using_cache(
+    api: Api,
+    project_id: int,
+    dest_dir: str,
+    dataset_ids: Optional[List[int]] = None,
+    log_progress: Optional[bool] = False,
+    progress_cb: Optional[Union[tqdm, Callable]] = None,
+    **kwargs,
+) -> None:
+    """ """
+    download_to_cache(
+        api,
+        project_id,
+        dataset_ids=dataset_ids,
+        log_progress=log_progress,
+        progress_cb=progress_cb,
+        **kwargs,
+    )
+    copy_from_cache(project_id, dest_dir, progress_cb=progress_cb)
