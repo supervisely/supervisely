68c085758f407018d25c64b8a32ae4d59eb51e64
Ars Vladimirov <grokhael@gmail.com>
2024-03-01 13:42:23 +0000

    refactor tests

--- tests/generate_distinct_colors.py
+++ tests/generate_distinct_colors.py
@@ -7,18 +7,15 @@ import distinctipy
 import supervisely as sly
 
 data = {}
-num_cls = 100
-fname = f"colors_{num_cls}.json"
-for n in range():
+for n in range(500):
     print(n)
     colors = distinctipy.get_colors(n)
     rgb_colors = [distinctipy.get_rgb256(color) for color in colors]
     data[n] = rgb_colors
-sly.json.dump_json_file(data, fname)
+sly.json.dump_json_file(data, "colors.json")
 
-
-data = sly.json.load_json_file(fname)
-with gzip.open(fname + ".gz", "wt", encoding="UTF-8") as zipfile:
+data = sly.json.load_json_file("colors.json")
+with gzip.open("colors.json.gz", "wt", encoding="UTF-8") as zipfile:
     json.dump(data, zipfile)
-with gzip.open(fname + ".gz", "r") as fin:
+with gzip.open("colors.json.gz", "r") as fin:
     data = json.loads(fin.read().decode("utf-8"))
