b5bfbd8745696e8ef1d5aaf64eddc21f912d7e28
Ars Vladimirov <grokhael@gmail.com>
2024-03-27 19:55:50 +0500

    count area considering internal polygon

--- supervisely/geometry/validation.py
+++ supervisely/geometry/validation.py
@@ -1,13 +1,15 @@
 # coding: utf-8
+from shapely.geometry import Polygon as ShapelyPolygon
+
 from supervisely.geometry.constants import EXTERIOR, INTERIOR, POINTS
 
 
 def _is_2d_typed_coords_valid(coords, individual_coord_types):
-    """
-    """
+    """ """
     return isinstance(coords, (list, tuple)) and all(
-        len(point) == 2 and
-        all(isinstance(coord, individual_coord_types) for coord in point) for point in coords)
+        len(point) == 2 and all(isinstance(coord, individual_coord_types) for coord in point)
+        for point in coords
+    )
 
 
 def _is_2d_numeric_coords_valid(coords) -> bool:
@@ -44,19 +46,38 @@ def validate_geometry_points_fields(json_obj: dict) -> None:
     :return: None
     """
     if POINTS not in json_obj:
-        raise ValueError('Input data must contain {} field.'.format(POINTS))
+        raise ValueError("Input data must contain {} field.".format(POINTS))
 
     points_obj = json_obj[POINTS]
     if not isinstance(points_obj, dict):
         raise TypeError('Input data field "{}" must be dict object.'.format(POINTS))
 
     if EXTERIOR not in points_obj or INTERIOR not in points_obj:
-        raise ValueError('"{}" field must contain {} and {} fields.'.format(POINTS, EXTERIOR, INTERIOR))
+        raise ValueError(
+            '"{}" field must contain {} and {} fields.'.format(POINTS, EXTERIOR, INTERIOR)
+        )
 
-    if not _is_2d_numeric_coords_valid(points_obj[EXTERIOR]):
-        raise TypeError('{} field must be a list of 2 numbers lists.'.format(EXTERIOR))
+    exterior = points_obj[EXTERIOR]
+    if not _is_2d_numeric_coords_valid(exterior):
+        raise TypeError("{} field must be a list of 2 numbers lists.".format(EXTERIOR))
 
     interior = points_obj[INTERIOR]
     if not isinstance(interior, list) or not all(
-            _is_2d_numeric_coords_valid(interior_component) for interior_component in interior):
-        raise TypeError('{} field must be a list of lists of 2 numbers lists.'.format(INTERIOR))
+        _is_2d_numeric_coords_valid(interior_component) for interior_component in interior
+    ):
+        raise TypeError("{} field must be a list of lists of 2 numbers lists.".format(INTERIOR))
+
+    if len(interior) > 0:
+        exterior_polygon = ShapelyPolygon(exterior)
+        for interior_component in interior:
+            interior_polygon = ShapelyPolygon(interior_component)
+            if validate_polygon(exterior_polygon, interior_polygon) is False:
+                raise ValueError(
+                    f"The provided interior polygon points ({interior_component}) are interecting the external border: {exterior}"
+                )
+
+
+def validate_polygon(exterior_polygon, interior_polygon):
+    if interior_polygon.contains_properly(exterior_polygon):
+        return False
+    return True
