ced29293c6cda5a0cbc483a5338f1123d4b65cb3
almaz <almazgimaev@gmail.com>
2024-04-03 20:22:13 +0200

    add pascal converter

--- supervisely/convert/image/pascal_voc/pascal_voc_converter.py
+++ supervisely/convert/image/pascal_voc/pascal_voc_converter.py
@@ -1,17 +1,52 @@
-import xml.etree.ElementTree as ET
+import os
 from typing import List
 
-from supervisely import Annotation, ProjectMeta
+from supervisely import Annotation, ProjectMeta, logger
 from supervisely.convert.base_converter import AvailableImageConverters
 from supervisely.convert.image.image_converter import ImageConverter
+from supervisely.convert.image.pascal_voc import pascal_voc_helper
+from supervisely.imaging.image import SUPPORTED_IMG_EXTS
+from supervisely.io.fs import (
+    dir_exists,
+    dirs_filter,
+    file_exists,
+    get_file_name,
+    list_files_recursively,
+    remove_junk_from_dir,
+)
 
 
 class PascalVOCConverter(ImageConverter):
+    class Item(ImageConverter.Item):
+        def __init__(
+            self,
+            *args,
+            **kwargs,
+        ):
+            super().__init__(*args, **kwargs)
+            self._segm_path = None
+            self._inst_path = None
+
+        @property
+        def segm_path(self) -> str:
+            return self._segm_path
+
+        @property
+        def inst_path(self) -> str:
+            return self._inst_path
+
+        def set_segm_path(self, segm_path: str) -> None:
+            self._segm_path = segm_path
+
+        def set_inst_path(self, inst_path: str) -> None:
+            self._inst_path = inst_path
 
     def __init__(self, input_data: str):
         self._input_data: str = input_data
         self._items: List[ImageConverter.Item] = []
         self._meta: ProjectMeta = None
+        self.color2class_name = None
+        self.with_instances = False
 
     def __str__(self) -> str:
         return AvailableImageConverters.PASCAL_VOC
@@ -20,25 +55,80 @@ class PascalVOCConverter(ImageConverter):
     def ann_ext(self) -> str:
         return ".xml"
 
-    @property
-    def key_file_ext(self) -> str:
-        return None
-
     def validate_ann_file(self, ann_path: str, meta: ProjectMeta) -> bool:
-        tree = ET.parse(ann_path)
-        root = tree.getroot()
-        if root.tag == "annotation":
-            return True
-        return False
-
-    def to_supervisely(
-            self,
-            item: ImageConverter.Item,
-            meta: ProjectMeta = None,
-            renamed_classes: dict = None,
-            renamed_tags: dict = None,
-    ) -> Annotation:
-        raise NotImplementedError()
+        pass
 
     def validate_format(self) -> bool:
-        return False
+        detected_ann_cnt = 0
+
+        def check_function(dir_path):
+            if not dir_exists(os.path.join(dir_path, "ImageSets")):
+                return False
+            if not dir_exists(os.path.join(dir_path, "ImageSets", "Segmentation")):
+                return False
+            if not dir_exists(os.path.join(dir_path, "JPEGImages")):
+                return False
+            if not dir_exists(os.path.join(dir_path, "SegmentationClass")):
+                return False
+            return True
+
+        possible_pascal_voc_dir = [d for d in dirs_filter(self._input_data, check_function)]
+        if len(possible_pascal_voc_dir) == 0:
+            return False
+        if len(possible_pascal_voc_dir) > 1:
+            logger.warn("Multiple Pascal VOC directories not supported")
+            return False
+
+        possible_pascal_voc_dir = possible_pascal_voc_dir[0]
+        remove_junk_from_dir(possible_pascal_voc_dir)
+
+        colors_file = os.path.join(possible_pascal_voc_dir, "colors.txt")
+        obj_classes, color2class_name = pascal_voc_helper.read_colors(colors_file)
+        self.color2class_name = color2class_name
+        self.with_instances = dir_exists(
+            os.path.join(possible_pascal_voc_dir, "SegmentationObject")
+        )
+        self._meta = ProjectMeta(obj_classes=obj_classes)
+
+        images_list = list_files_recursively(
+            os.path.join(possible_pascal_voc_dir, "JPEGImages"),
+            valid_extensions=SUPPORTED_IMG_EXTS,
+        )
+
+        # create Items
+        self._items = []
+        for image_path in images_list:
+            item = self.Item(image_path)
+            item_name_noext = get_file_name(item.name)
+            segm_path = os.path.join(
+                possible_pascal_voc_dir, "SegmentationClass", item_name_noext + ".png"
+            )
+            if file_exists(segm_path):
+                item.set_segm_path(segm_path)
+                detected_ann_cnt += 1
+            if self.with_instances:
+                inst_path = os.path.join(
+                    possible_pascal_voc_dir, "SegmentationObject", item_name_noext + ".png"
+                )
+                if file_exists(inst_path):
+                    item.set_inst_path(inst_path)
+            self._items.append(item)
+        return detected_ann_cnt > 0
+
+    def to_supervisely(
+        self,
+        item: ImageConverter.Item,
+        meta: ProjectMeta = None,
+        renamed_classes: dict = None,
+        renamed_tags: dict = None,
+    ) -> Annotation:
+        if meta is None:
+            meta = self._meta
+
+        try:
+            ann = pascal_voc_helper.get_ann(item, self.color2class_name, renamed_classes)
+            return ann
+
+        except Exception as e:
+            logger.warn(f"Failed to convert annotation: {repr(e)}")
+            return item.create_empty_annotation()
---
+++ supervisely/convert/image/pascal_voc/pascal_voc_helper.py
@@ -0,0 +1,130 @@
+import os
+
+import numpy as np
+
+from supervisely import logger, Annotation, Label, ObjClass, ObjClassCollection
+from supervisely.geometry.bitmap import Bitmap
+from supervisely.imaging.image import read
+
+MASKS_EXTENSION = ".png"
+
+default_classes_colors = {
+    "neutral": (224, 224, 192),
+    "aeroplane": (128, 0, 0),
+    "bicycle": (0, 128, 0),
+    "bird": (128, 128, 0),
+    "boat": (0, 0, 128),
+    "bottle": (128, 0, 128),
+    "bus": (0, 128, 128),
+    "car": (128, 128, 128),
+    "cat": (64, 0, 0),
+    "chair": (192, 0, 0),
+    "cow": (64, 128, 0),
+    "diningtable": (192, 128, 0),
+    "dog": (64, 0, 128),
+    "horse": (192, 0, 128),
+    "motorbike": (64, 128, 128),
+    "person": (192, 128, 128),
+    "pottedplant": (0, 64, 0),
+    "sheep": (128, 64, 0),
+    "sofa": (0, 192, 0),
+    "train": (128, 192, 0),
+    "tvmonitor": (0, 64, 128),
+}
+
+
+# returns mapping: (r, g, b) color -> some (row, col) for each unique color except black
+def get_col2coord(img):
+    img = img.astype(np.int32)
+    h, w = img.shape[:2]
+    colhash = img[:, :, 0] * 256 * 256 + img[:, :, 1] * 256 + img[:, :, 2]
+    unq, unq_inv, unq_cnt = np.unique(colhash, return_inverse=True, return_counts=True)
+    indxs = np.split(np.argsort(unq_inv), np.cumsum(unq_cnt[:-1]))
+    col2indx = {unq[i]: indxs[i][0] for i in range(len(unq))}
+    return {
+        (col // (256**2), (col // 256) % 256, col % 256): (indx // w, indx % w)
+        for col, indx in col2indx.items()
+        if col != 0
+    }
+
+
+def read_colors(colors_file):
+    if os.path.isfile(colors_file):
+        logger.info("Will try to read segmentation colors from provided file.")
+        in_lines = filter(None, map(str.strip, open(colors_file, "r").readlines()))
+        in_splitted = (x.split() for x in in_lines)
+        # Format: {name: (R, G, B)}, values [0; 255]
+        cls2col = {}
+        for x in in_splitted:
+            if len(x) != 4:
+                raise ValueError("Invalid format of colors file.")
+            cls2col[x[0]] = (int(x[1]), int(x[2]), int(x[3]))
+    else:
+        logger.info("Will use default PascalVOC color mapping.")
+        cls2col = default_classes_colors
+
+    obj_classes_list = [
+        ObjClass(name=class_name, geometry_type=Bitmap, color=color)
+        for class_name, color in cls2col.items()
+    ]
+
+    logger.info(
+        f"Determined {len(cls2col)} class(es).",
+        extra={"classes": list(cls2col.keys())},
+    )
+
+    obj_classes = ObjClassCollection(obj_classes_list)
+    color2class_name = {v: k for k, v in cls2col.items()}
+    return obj_classes, color2class_name
+
+
+def get_ann(item, color2class_name, renamed_classes=None):
+    segm_path, inst_path = item.segm_path, item.inst_path
+    height, width = item.shape
+
+    ann = Annotation(img_size=(height, width))
+
+    segmentation_img = read(segm_path)
+
+    if inst_path is not None:
+        instance_img = read(inst_path)
+        colored_img = instance_img
+        instance_img16 = instance_img.astype(np.uint16)
+        col2coord = get_col2coord(instance_img16)
+        curr_col2cls = []
+        for col, coord in col2coord.items():
+            cls_name = color2class_name.get(tuple(segmentation_img[coord]))
+            if cls_name is not None:
+                if renamed_classes is not None and cls_name in renamed_classes:
+                    cls_name = renamed_classes[cls_name]
+            curr_col2cls.append((col, cls_name))
+        curr_col2cls = {
+            k: v for k, v in curr_col2cls if v is not None
+        }  # _instance_ color -> class name
+    else:
+        colored_img = segmentation_img
+        segmentation_img = segmentation_img.astype(np.uint16)
+        colors = list(get_col2coord(segmentation_img).keys())
+        curr_col2cls = {}
+        for color in colors:
+            cls_name = color2class_name.get(color)
+            if cls_name is not None:
+                if renamed_classes is not None and cls_name in renamed_classes:
+                    cls_name = renamed_classes[cls_name]
+            curr_col2cls[color] = cls_name
+
+    for color, class_name in curr_col2cls.items():
+        mask = np.all(colored_img == color, axis=2)  # exact match (3-channel img & rgb color)
+        bitmap = Bitmap(data=mask)
+        obj_class = ObjClass(name=class_name, geometry_type=Bitmap)
+
+        ann = ann.add_label(Label(bitmap, obj_class))
+        #  clear used pixels in mask to check missing colors, see below
+        colored_img[mask] = (0, 0, 0)
+
+    if np.sum(colored_img) > 0:
+        logger.warn(
+            f"Not all objects or classes are captured from source segmentation: {item.name}"
+        )
+
+    return ann
