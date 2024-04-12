96fc3521f8494992314fdae71bd7b6f0c4dc0e9b
almaz <almazgimaev@gmail.com>
2024-04-12 15:23:06 +0200

    Check codec, mimetype and convert video. Fix progress

--- supervisely/convert/video/video_converter.py
+++ supervisely/convert/video/video_converter.py
@@ -1,14 +1,19 @@
 import cv2
+import magic
+import subprocess
+import os
 
 from tqdm import tqdm
 from typing import List
 
-from supervisely import Api, batched, generate_free_name, KeyIdMap, logger, is_development, ProjectMeta, VideoAnnotation
+from supervisely import Api, batched, generate_free_name, KeyIdMap, logger, is_development, Progress, ProjectMeta, VideoAnnotation
 from supervisely.convert.base_converter import BaseConverter
-from supervisely.video.video import ALLOWED_VIDEO_EXTENSIONS
+from supervisely.video.video import ALLOWED_VIDEO_EXTENSIONS, get_info
+from supervisely.io.fs import get_file_ext, get_file_name
 
 class VideoConverter(BaseConverter):
     allowed_exts = ALLOWED_VIDEO_EXTENSIONS
+    base_video_extension = ".mp4"
 
     class Item(BaseConverter.BaseItem):
         def __init__(
@@ -95,17 +100,12 @@ class VideoConverter(BaseConverter):
             anns = []
             figures_cnt = 0
             for item in batch:
-                if item.name in existing_names:
-                    new_name = generate_free_name(
-                        existing_names, item.name, with_ext=True, extend_used_names=True
-                    )
-                    logger.warn(
-                        f"Video with name '{item.name}' already exists, renaming to '{new_name}'"
-                    )
-                    item_names.append(new_name)
-                else:
-                    item_names.append(item.name)
-                item_paths.append(item.path)
+                item_name, item_path = self.convert_to_mp4_if_needed(item.path)
+                item_name = generate_free_name(existing_names, item_name, with_ext=True, extend_used_names=True)
+                item.set_name(item_name)
+                item.set_path(item_path)
+                item_paths.append(item_path)
+                item_names.append(item_name)
 
                 ann = self.to_supervisely(item, meta, renamed_classes, renamed_tags)
                 figures_cnt += len(ann.figures)
@@ -115,8 +115,7 @@ class VideoConverter(BaseConverter):
                 dataset_id,
                 item_names,
                 item_paths,
-                progress_cb=progress_cb,
-                item_progress=True,
+                item_progress=progress_cb,
             )
             vid_ids = [vid_info.id for vid_info in vid_infos]
 
@@ -136,3 +135,78 @@ class VideoConverter(BaseConverter):
                 progress.close()
             ann_progress.close()
         logger.info(f"Dataset ID:{dataset_id} has been successfully uploaded.")
+
+
+    def convert_to_mp4_if_needed(self, video_path):
+        video_name = get_file_name(video_path)
+        # convert
+        convert_progress = Progress(message=f"Converting {video_name}", total_cnt=1)
+        output_video_name = f"{get_file_name(video_name)}{self.base_video_extension}"
+        output_video_path = os.path.splitext(video_path)[0] + "_h264" + self.base_video_extension
+
+        # check if video is already in mp4 format and mime type is `video/mp4`
+        if video_path.lower().endswith(".mp4"):
+            mime = magic.Magic(mime=True)
+            mime_type = mime.from_file(video_path)
+            if mime_type == "video/mp4":
+                logger.info(
+                    f'Video "{video_name}" is already in mp4 format, conversion is not required.'
+                )
+                return output_video_name, video_path
+
+        # read video meta_data
+        try:
+            vid_meta = get_info(video_path)
+            need_video_transc, need_audio_transc = self.check_codecs(vid_meta)
+        except:
+            need_video_transc, need_audio_transc = True, True
+
+        # convert videos
+        self.convert(
+            input_path=video_path,
+            output_path=output_video_path,
+            need_video_transc=need_video_transc,
+            need_audio_transc=need_audio_transc,
+        )
+        convert_progress.iter_done_report()
+
+        return output_video_name, output_video_path
+
+
+    def check_codecs(self, video_meta):
+        need_video_transc, need_audio_transc = False, False
+        for stream in video_meta["streams"]:
+            codec_type = stream["codecType"]
+            if codec_type not in ["video", "audio"]:
+                continue
+            codec_name = stream["codecName"]
+            if codec_type == "video" and codec_name != "h264":
+                logger.info(
+                    f"Video codec is not h264, transcoding is required: {codec_name}"
+                )
+                need_video_transc = True
+            elif codec_type == "audio" and codec_name != "aac":
+                logger.info(
+                    f"Audio codec is not aac, transcoding is required: {codec_name}"
+                )
+                need_audio_transc = True
+        return need_video_transc, need_audio_transc
+
+
+    def convert(self, input_path, output_path, need_video_transc, need_audio_transc):
+        video_codec = "libx264" if need_video_transc else "copy"
+        audio_codec = "aac" if need_audio_transc else "copy"
+        logger.info("Converting video...")
+        subprocess.call(
+            [
+                "ffmpeg",
+                "-y",
+                "-i",
+                f"{input_path}",
+                "-c:v",
+                f"{video_codec}",
+                "-c:a",
+                f"{audio_codec}",
+                f"{output_path}",
+            ]
+        )
