a23730026e4b28cafc98daf6b88363d07056b956
almaz <almazgimaev@gmail.com>
2024-04-17 22:10:43 +0200

    refactor

--- supervisely/convert/image/image_helper.py
+++ supervisely/convert/image/image_helper.py
@@ -1,5 +1,5 @@
 import mimetypes
-import os
+from pathlib import Path
 
 import magic
 from PIL import Image
@@ -17,33 +17,25 @@ EXT_TO_CONVERT = [".heic", ".avif"]
 
 
 def validate_image(path: str) -> tuple:
-    """
-    Validate image by mimetypes, ext, normalize exif and remove alpha channel.
-    """
+    """Validate image by mimetypes, ext, and remove alpha channel."""
+
     try:
         ext = get_file_ext(path)
         if ext == ".nrrd":
             return path
-        name = get_file_name_with_ext(path)
         if ext.lower() in EXT_TO_CONVERT:
             path = convert_to_jpg(path)
             return path
         if ext.lower() != ".mpo":
-            name = validate_mimetypes(name, path)
-            path = normalize_exif_and_remove_alpha_channel(name, path)
+            name = get_file_name_with_ext(path)
+            new_name = validate_mimetypes(name, path)
+            if new_name != name:
+                path = Path(path).with_name(new_name).as_posix()
+            img = read(path, remove_alpha_channel=True)  # rewite to remove alpha channel
+            write(path, img)
         return path
     except Exception as e:
-        logger.warning(f"Skip image {name}: {repr(e)}", extra={"file_path": path})
-
-
-def normalize_exif_and_remove_alpha_channel(name: str, path: str) -> tuple:
-    """Normalize exif and remove alpha channel."""
-    img = read(path)
-    if name != get_file_name_with_ext(path):
-        silent_remove(path)
-        path = os.path.join(os.path.dirname(path), name)
-    write(path, img)
-    return path
+        logger.warning(f"Skip image: {repr(e)}", extra={"file_path": path})
 
 
 def validate_mimetypes(name: str, path: str) -> list:
@@ -69,19 +61,15 @@ def validate_mimetypes(name: str, path: str) -> list:
 
 
 def convert_to_jpg(path) -> tuple:
-    """
-    Convert image to jpg.
-    """
+    """Convert image to jpg."""
+
     # * do not remove folllowing imports, it is used to register avif/heic formats
     import pillow_avif  # pylint: disable=import-error
-    from pillow_heif import register_heif_opener # pylint: disable=import-error
+    from pillow_heif import register_heif_opener  # pylint: disable=import-error
 
     register_heif_opener()
 
-    name = get_file_name(path)
-    new_name = f"{name}.jpeg"
-    dirname = os.path.dirname(path)
-    new_path = os.path.join(dirname, new_name)
+    new_path = Path(path).with_suffix(".jpeg")
     with Image.open(path) as image:
         image.convert("RGB").save(new_path)
     silent_remove(path)
