8d84cebf531c1a443c4298e7beafc3859e18af1c
cxnt <pavelbartsits@gmail.com>
2024-04-18 16:58:40 +0400

    * Add warning if frames mapping file not found for PCDe import. * Add support for uploading PCDe without rimgs. * Set log level to info for data without annotation format. * Upgrade JS bundle to "2.1.79". * Fix get_path method in FileStorageUpload widget

--- supervisely/convert/pointcloud_episodes/pointcloud_episodes_converter.py
+++ supervisely/convert/pointcloud_episodes/pointcloud_episodes_converter.py
@@ -1,7 +1,5 @@
 from typing import List, Optional
 
-from tqdm import tqdm
-
 from supervisely import (
     Api,
     PointcloudEpisodeAnnotation,
@@ -15,6 +13,7 @@ from supervisely.api.module_api import ApiField
 from supervisely.convert.base_converter import BaseConverter
 from supervisely.io.json import load_json_file
 from supervisely.pointcloud.pointcloud import ALLOWED_POINTCLOUD_EXTENSIONS
+from tqdm import tqdm
 
 
 class PointcloudEpisodeConverter(BaseConverter):
@@ -87,9 +86,13 @@ class PointcloudEpisodeConverter(BaseConverter):
     ):
         """Upload converted data to Supervisely"""
 
-        meta, renamed_classes, renamed_tags = self.merge_metas_with_conflicts(api, dataset_id)
+        meta, renamed_classes, renamed_tags = self.merge_metas_with_conflicts(
+            api, dataset_id
+        )
 
-        existing_names = set([pcde.name for pcde in api.pointcloud_episode.get_list(dataset_id)])
+        existing_names = set(
+            [pcde.name for pcde in api.pointcloud_episode.get_list(dataset_id)]
+        )
 
         if log_progress:
             progress, progress_cb = self.get_progress(
@@ -128,42 +131,55 @@ class PointcloudEpisodeConverter(BaseConverter):
                 item_metas.append({"frame": item._frame_number})
                 rimg_infos = []
                 camera_names = []
-                img_paths, rimg_ann_paths = list(zip(*item._related_images))
-                rimg_hashes = api.pointcloud_episode.upload_related_images(img_paths)
-                for img_ind, (img_hash, rimg_ann_path) in enumerate(
-                    zip(rimg_hashes, rimg_ann_paths)
-                ):
-                    meta_json = load_json_file(rimg_ann_path)
-                    try:
-                        if ApiField.META not in meta_json:
-                            raise ValueError("Related image meta not found in json file.")
-                        if ApiField.NAME not in meta_json:
-                            raise ValueError("Related image name not found in json file.")
-                        if "deviceId" not in meta_json[ApiField.META].keys():
-                            camera_names.append(f"CAM_{str(img_ind).zfill(2)}")
-                        else:
-                            camera_names.append(meta_json[ApiField.META]["deviceId"])
-                        rimg_infos.append(
-                            {
-                                ApiField.ENTITY_ID: pcd_id,
-                                ApiField.NAME: meta_json[ApiField.NAME],
-                                ApiField.HASH: img_hash,
-                                ApiField.META: meta_json[ApiField.META],
-                            }
-                        )
-                        api.pointcloud.add_related_images(rimg_infos, camera_names)
-                    except Exception as e:
-                        logger.warn(
-                            f"Failed to upload related image or add it to pointcloud episo: {repr(e)}"
-                        )
-                        continue
+                if len(item._related_images) > 0:
+                    img_paths, rimg_ann_paths = list(zip(*item._related_images))
+                    rimg_hashes = api.pointcloud_episode.upload_related_images(
+                        img_paths
+                    )
+                    for img_ind, (img_hash, rimg_ann_path) in enumerate(
+                        zip(rimg_hashes, rimg_ann_paths)
+                    ):
+                        meta_json = load_json_file(rimg_ann_path)
+                        try:
+                            if ApiField.META not in meta_json:
+                                raise ValueError(
+                                    "Related image meta not found in json file."
+                                )
+                            if ApiField.NAME not in meta_json:
+                                raise ValueError(
+                                    "Related image name not found in json file."
+                                )
+                            if "deviceId" not in meta_json[ApiField.META].keys():
+                                camera_names.append(f"CAM_{str(img_ind).zfill(2)}")
+                            else:
+                                camera_names.append(
+                                    meta_json[ApiField.META]["deviceId"]
+                                )
+                            rimg_infos.append(
+                                {
+                                    ApiField.ENTITY_ID: pcd_id,
+                                    ApiField.NAME: meta_json[ApiField.NAME],
+                                    ApiField.HASH: img_hash,
+                                    ApiField.META: meta_json[ApiField.META],
+                                }
+                            )
+                            api.pointcloud.add_related_images(rimg_infos, camera_names)
+                        except Exception as e:
+                            logger.warn(
+                                f"Failed to upload related image or add it to pointcloud episo: {repr(e)}"
+                            )
+                            continue
 
             if log_progress:
                 progress_cb(len(batch))
 
         if self.items_count > 0:
-            ann = self.to_supervisely(self._items[0], meta, renamed_classes, renamed_tags)
-            api.pointcloud_episode.annotation.append(dataset_id, ann, frame_to_pointcloud_ids)
+            ann = self.to_supervisely(
+                self._items[0], meta, renamed_classes, renamed_tags
+            )
+            api.pointcloud_episode.annotation.append(
+                dataset_id, ann, frame_to_pointcloud_ids
+            )
 
         if log_progress:
             if is_development():
