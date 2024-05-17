c7521fda9165adf1bda110fc7c0798038ccf6f9e
Sergey <57998637+GoldenAnpu@users.noreply.github.com>
2024-05-17 20:53:06 +0200

    Update def dcm_to_nrrd with writing nrrd header (#955)

--- supervisely/convert/volume/dicom/dicom_helper.py
+++ supervisely/convert/volume/dicom/dicom_helper.py
@@ -2,10 +2,11 @@ import os
 from typing import List
 
 import nrrd
+import numpy as np
 
-from supervisely.volume.volume import read_dicom_serie_volume_np
 from supervisely import logger
 from supervisely.io.fs import file_exists
+from supervisely.volume.volume import read_dicom_serie_volume_np
 
 
 def dcm_to_nrrd(id: str, paths: List[str]) -> str:
@@ -20,6 +21,19 @@ def dcm_to_nrrd(id: str, paths: List[str]) -> str:
     while file_exists(nrrd_path):
         i += 1
         nrrd_path = os.path.join(parent_dir, f"{id}_{i}.nrrd")
-    nrrd.write(nrrd_path, volume_np, volume_meta)
+
+    nrrd_header = {
+        "space": volume_meta.get("ACS", "RAS"),
+    }
+    if volume_meta.get("origin", None) is not None:
+        nrrd_header["space origin"] = volume_meta.get("origin")
+    directions = volume_meta.get("directions", None)
+    spacing = volume_meta.get("spacing", None)
+    if directions is not None and spacing is not None:
+        directions = np.array(directions).reshape(3, 3)
+        directions *= spacing
+        nrrd_header["space directions"] = directions
+
+    nrrd.write(nrrd_path, volume_np, nrrd_header)
 
     return nrrd_path, volume_meta
