b0d3209e904953f087e851ee5e21593a38b2b9dd
Nikolai Petukhov <petukhov.niko@gmail.com>
2024-05-31 10:35:00 -0300

    removed fast_reid from repo and add installation on import

--- supervisely/nn/tracker/bot_sort/__init__.py
+++ supervisely/nn/tracker/bot_sort/__init__.py
@@ -6,8 +6,14 @@ except ImportError:
     import sys
     from pathlib import Path
 
+    fast_reid_repo_url = "https://github.com/JDAI-CV/fast-reid.git"
     fast_reid_parent_path = Path(__file__).parent
     fast_reid_path = fast_reid_parent_path.joinpath("fast_reid")
+    if not fast_reid_path.exists():
+        import subprocess
+
+        subprocess.run(["git", "clone", fast_reid_repo_url, str(fast_reid_path.resolve())])
+
     sys.path.insert(0, str(fast_reid_parent_path.resolve()))
     sys.path.append(str(fast_reid_path.resolve()))
 
