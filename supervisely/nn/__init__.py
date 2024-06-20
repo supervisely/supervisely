9d7098036ec3b17b20052b0b66f7ce382b781523
Ars Vladimirov <grokhael@gmail.com>
2024-06-20 23:41:55 +0500

    rm checkpointsinfo

--- supervisely/nn/__init__.py
+++ supervisely/nn/__init__.py
@@ -1,7 +1,8 @@
 import supervisely.nn.artifacts as artifacts
 import supervisely.nn.inference as inference
 from supervisely.nn.artifacts.artifacts import BaseTrainArtifacts, TrainInfo
-from supervisely.nn.checkpoints.checkpoint import CheckpointInfo
+
+# from supervisely.nn.checkpoints.checkpoint import CheckpointInfo
 from supervisely.nn.prediction_dto import (
     Prediction,
     PredictionBBox,
