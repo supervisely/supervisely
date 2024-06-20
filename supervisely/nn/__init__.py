648997664bcb9fb20a4d4e03c5289932c6da58b1
Arsenii Vladimirov <78355358+grokhi@users.noreply.github.com>
2024-06-20 23:44:04 +0500

    Text widget: add support to clickable Sidebar (#996)
    * change run_async
    
    * fix sharing lock between threads
    
    * Merge branch 'run_async_fix' of https://github.com/supervisely/supervisely into run_async_fix
    
    * Merge branch 'master' into run_async_fix
    
    * Fix merge issues for content.py
    
    * Fix merge commit
    
    * Fix merge commit
    
    * Update widget template in separate thread
    
    * Add method to retrieve ecosystem module info by slug and version
    
    * Merge branch 'add-app-api-get-info-by-slug' into run_async_fix
    
    * remove get_info_by_slug method from app api
    
    * optimize index.js
    
    * Update index.html with new CSS stylesheet and JSON editor version
    
    * improve index page response time
    
    * read cached template from file on app start
    
    * Revert "read cached template from file on app start"
    
    This reverts commit bbba110b725a4476a526b5ce338df3b5f3cfb20b.
    
    * Upgrade js bundle and SlyApp, update dependencies and optimize index.html
    
    * Merge branch 'optimize-index' of https://github.com/supervisely/supervisely into optimize-index
    
    * Merge branch 'master' into optimize-index
    
    * Merge branch 'master' into optimize-index
    
    * Merge branch 'master' into optimize-index
    
    * Merge branch 'master' into optimize-index
    
    * Merge branch 'master' into optimize-index
    
    * Merge branch 'master' into optimize-index
    
    * update widget Text
    
    * Merge branch 'optimize-index' into model-benchmark-ars-2
    
    * Add id as widget id to all widgets
    
    * Merge branch 'model-benchmark-ars' into model-benchmark-ars-2
    
    * rm checkpointsinfo
    
    * Merge branch 'model-benchmark-ars' into model-benchmark-ars-2

--- supervisely/nn/__init__.py
+++ supervisely/nn/__init__.py
@@ -1,5 +1,4 @@
 import supervisely.nn.artifacts as artifacts
-import supervisely.nn.checkpoints as checkpoints
 import supervisely.nn.inference as inference
 from supervisely.nn.artifacts.artifacts import BaseTrainArtifacts, TrainInfo
 from supervisely.nn.prediction_dto import (
