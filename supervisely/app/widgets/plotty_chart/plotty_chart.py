d9ba73c615f3ec73fa27adf50e255b5534e59cc0
almaz <almazgimaev@gmail.com>
2024-06-22 20:06:37 +0200

    refactor widget and click callback

--- supervisely/app/widgets/plotty_chart/plotty_chart.py
+++ supervisely/app/widgets/plotty_chart/plotty_chart.py
@@ -1,9 +1,8 @@
 from __future__ import annotations
 
-from typing import Dict,  List, Literal, NamedTuple, Union
+from typing import Any, List, Literal, NamedTuple, Optional, Union
 from supervisely.app.widgets import Widget
 from supervisely.app.content import StateJson, DataJson
-from supervisely.sly_logger import logger
 import json
 
 
@@ -12,25 +11,68 @@ class PlottyChart(Widget):
         CLICK = "chart_clicked_cb"
 
     class ClickedDataPoint(NamedTuple):
-        """Class, representing clicked datapoint, which contains information about series, data index and data itself.
+        """
+        Class, representing clicked datapoint, which contains information about chart points.
         It will be returned after click event on datapoint in immutable namedtuple
-        with fields: x, y, value."""
+        """
 
-        x: int
-        y: int
-        value: Union[int, float, str]
+        type: Optional[str]
+        x: Optional[int]
+        y: Optional[int]
+        z: Optional[int]
+        i: Optional[int]
+        curveNumber: Optional[int]
+        pointIndex: Optional[int]
+        label: Optional[str]
+        color: Optional[str]
+        percent: Optional[float]
+        pointNumber: Optional[int]
+        pointNumbers: Optional[List[int]]
+        value: Optional[Union[int, float, str]]
+        v: Optional[Union[int, float, str]]
+        customdata: Optional[Any]
+        marker_size: Optional[float]
+        marker_color: Optional[float]
+        # data: Dict[str, Any] # too big to store in state?
 
+    @staticmethod
+    def datapoint_sequence():
+        return [
+            "type",
+            "x",
+            "y",
+            "z",
+            "i",
+            "curveNumber",
+            "pointIndex",
+            "label",
+            "color",
+            "percent",
+            "pointNumber",
+            "pointNumbers",
+            "value",
+            "v",
+            "customdata",
+            "marker_size",
+            "marker_color",
+            # "data"
+        ]
 
     def __init__(
         self,
-        content, # Union[go.Figure, px.Figure, dict],
+        figure=None,  # Union[plotly.graph_objects.Figure, dict, None],
         element_loading_text: str = "Loading...",
         widget_id: str = None,
     ):
-        import plotly.express as px
+        self._figure = self._validate_figure(figure if figure is not None else {})
+        self._click_handled = False
+        self._element_loading_text = element_loading_text
+        self._loading = False
+        super().__init__(widget_id=widget_id, file_path=__file__)
+
+    def _validate_figure(self, figure):
         import plotly.graph_objects as go
         from plotly.io import from_json
-        from plotly.utils import PlotlyJSONEncoder
         import numpy as np
 
         def _convert_arrays_to_lists(d):
@@ -47,27 +89,23 @@ class PlottyChart(Widget):
                             _convert_arrays_to_lists(item)
             return d
 
-        if isinstance(content, go.Figure):
-            content = content.to_dict()
-        elif isinstance(content, dict):
+        if isinstance(figure, go.Figure):
+            figure = figure.to_dict()
+        elif isinstance(figure, dict):
             # try to convert to Figure object to check if it is valid
-            fig = from_json(json.dumps(content, PlotlyJSONEncoder), skip_invalid=True)
-            content = fig.to_dict()
+            fig = from_json(json.dumps(figure), skip_invalid=True)
+            figure = fig.to_dict()
+        else:
+            raise ValueError(
+                f"figure must be Plotly Figure or dict, but {type(figure)} was given."
+            )
 
-        self._content = _convert_arrays_to_lists(content)
-
-        self._click_handled = False
-        self._element_loading_text = element_loading_text
-        self._loading = False
-
-        super().__init__(widget_id=widget_id, file_path=__file__)
+        return _convert_arrays_to_lists(figure)
 
     def get_json_data(self):
         return {
-            "content": self._content,
-            "options": {
-                "elementLoadingText": self._element_loading_text
-            }
+            "figure": self._figure,
+            "options": {"elementLoadingText": self._element_loading_text},
         }
 
     def get_json_state(self):
@@ -75,9 +113,6 @@ class PlottyChart(Widget):
             "clicked_value": None,
             "loading": self._loading,
         }
-
-    def get_clicked_value(self):
-        return StateJson()[self.widget_id]["clicked_value"]
 
     def click(self, func):
         route_path = self.get_route_path(PlottyChart.Routes.CLICK)
@@ -87,31 +122,46 @@ class PlottyChart(Widget):
 
         @server.post(route_path)
         def _click():
-            # res = self.get_clicked_datapoint()
-            res = self.get_clicked_value()
+            res = self.get_clicked_datapoint()
             if res is not None:
                 return func(res)
 
         return _click
 
-    # def get_clicked_datapoint(self) -> Union[PlottyChart.ClickedDataPoint, None]:
-    #     value = self.get_clicked_value()
-    #     res = PlottyChart.ClickedDataPoint(
-    #         value["x"], value["y"], value["value"]
-    #     )
-    #     return res
+    def get_clicked_datapoint(self) -> Union[PlottyChart.ClickedDataPoint, None]:
+        points = StateJson()[self.widget_id]["clicked_value"]
+        if not isinstance(points, list):
+            return None
+        res = []
+        for point_data in points:
+            data = point_data.pop("data", None)
+            point_data["type"] = data.get("type") if data is not None else None
+            point_data["label"] = data.get("name") if data is not None else None
+            fields = []
+            for field in self.datapoint_sequence():
+                if field.replace("_", ".") in point_data:
+                    fields.append(point_data.get(field.replace("_", ".")))
+                else:
+                    fields.append(point_data.get(field))
+            res.append(PlottyChart.ClickedDataPoint(*fields))
+        return res
 
-    def set_content(self, content):
-        self._content = content
-        DataJson()[self.widget_id]["content"] = self._content
+    def set_figure(self, figure):
+        self._figure = self._validate_figure(figure)
+        DataJson()[self.widget_id]["figure"] = self._figure
         DataJson().send_changes()
 
-    def get_content(self, return_type: Literal["dict", "figure"] = "figure"):
+    def get_figure(self, return_type: Literal["dict", "plottly_figure"] = "plottly_figure"):
         if return_type == "dict":
-            return self._content
-        elif return_type == "figure":
+            return self._figure
+        elif return_type == "plottly_figure":
             from plotly.io import from_json
             from plotly.utils import PlotlyJSONEncoder
-            return from_json(json.dumps(self._content, PlotlyJSONEncoder), skip_invalid=True)
+
+            figure = DataJson()[self.widget_id]["figure"]
+            figure = json.dumps(figure, cls=PlotlyJSONEncoder)
+            return from_json(figure, skip_invalid=True)
         else:
-            raise ValueError(f"return_type must be 'dict' or 'figure', but {return_type} was given.")
+            raise ValueError(
+                f"return_type must be 'dict' or 'figure', but {return_type} was given."
+            )
