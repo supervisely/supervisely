3725ccce1f7c2046423e997d9d3d1edef23b34b1
unfinity <poldersen2@gmail.com>
2024-07-29 18:24:48 +0000

    add inst-seg classes

---
+++ supervisely/nn/benchmark/evaluation/instance_segmentation/__init__.py
@@ -0,0 +1,2 @@
+from supervisely.nn.benchmark.evaluation.instance_segmentation.calculate_metrics import calculate_metrics
+# from supervisely.nn.benchmark.evaluation.object_detection.metric_provider import 
