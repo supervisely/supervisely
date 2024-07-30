492b871639f222ba9d91615cf00e3d3c7f8a7e66
Stan Soldatov <118521851+iwatkot@users.noreply.github.com>
2024-07-30 16:08:16 +0200

    Placeholders for selectors in SelectDatasetTree widget
    * Placeholders for selectors.
    
    * Placeholders for selectors.
    
    * Docsrings update.

--- supervisely/app/widgets/select_dataset_tree/select_dataset_tree.py
+++ supervisely/app/widgets/select_dataset_tree/select_dataset_tree.py
@@ -312,6 +312,7 @@ class SelectDatasetTree(Widget):
             always_open=always_open,
             append_to_body=self._append_to_body,
             width=193,
+            placeholder="Select dataset",
         )
         if self._dataset_id is not None:
             self._select_dataset.set_selected_by_id(self._dataset_id)
@@ -358,7 +359,7 @@ class SelectDatasetTree(Widget):
             self._project_id = project_id
 
         self._select_team = Select(
-            items=self._get_select_items(),
+            items=self._get_select_items(), placeholder="Select team", filterable=True
         )
         self._select_team.set_value(self._team_id)
         if not team_is_selectable:
@@ -366,6 +367,7 @@ class SelectDatasetTree(Widget):
 
         self._select_workspace = Select(
             items=self._get_select_items(team_id=self._team_id),
+            placeholder="Select workspace",
             filterable=True,
         )
         self._select_workspace.set_value(self._workspace_id)
@@ -374,6 +376,7 @@ class SelectDatasetTree(Widget):
 
         self._select_project = Select(
             items=self._get_select_items(workspace_id=self._workspace_id),
+            placeholder="Select project",
             filterable=True,
         )
         self._select_project.set_value(self._project_id)
