16689b33b230ea22839e4a440cc67cd82fda1304
Ars Vladimirov <grokhael@gmail.com>
2024-08-14 09:32:08 +0000

    add report thumbnail

--- supervisely/app/widgets/__init__.py
+++ supervisely/app/widgets/__init__.py
@@ -67,6 +67,7 @@ from supervisely.app.widgets.file_viewer.file_viewer import FileViewer
 from supervisely.app.widgets.switch.switch import Switch
 from supervisely.app.widgets.folder_thumbnail.folder_thumbnail import FolderThumbnail
 from supervisely.app.widgets.file_thumbnail.file_thumbnail import FileThumbnail
+from supervisely.app.widgets.report_thumbnail.report_thumbnail import ReportThumbnail
 from supervisely.app.widgets.model_info.model_info import ModelInfo
 from supervisely.app.widgets.match_tags_or_classes.match_tags_or_classes import (
     MatchTagMetas,
