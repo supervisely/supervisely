7da8967c90e710a233e7d4baa00cf22bb3cc6ed8
almaz <almazgimaev@gmail.com>
2024-08-21 12:28:14 +0200

    remove comments

--- supervisely/app/widgets/grid_gallery_v2/grid_gallery_v2.py
+++ supervisely/app/widgets/grid_gallery_v2/grid_gallery_v2.py
@@ -2,7 +2,7 @@ import copy
 import time
 import uuid
 from collections import defaultdict
-from typing import List, Optional, Union
+from typing import List, Union
 
 from supervisely.project.project_meta import ProjectMeta
 from supervisely.annotation.annotation import Annotation
@@ -64,44 +64,12 @@ class GridGalleryV2(Widget):
         self._enablePan = enable_panning
 
         self._filters_tags = default_tag_filters
-        # if filters_tags is not None:
-        #     if isinstance(filters_tags[0], str):
-        #         self._filters_tags = {tag_name: None for tag_name in filters_tags}
-
-        # self._show_opacity_header: bool = show_opacity_slider
-        # self._opacity: float = annotations_opacity
-        # self._enable_zoom: bool = enable_zoom
-        # self._sync_views: bool = sync_views
-        # self._resize_on_zoom: bool = resize_on_zoom
-        # self._show_preview: bool = show_preview
-        # self._views_bindings: list = []
-        # self._view_height: int = view_height
-        # self._empty_message: str = empty_message
-
         #############################
 
         self._filters = []
         self._object_bindings = []
 
         super().__init__(widget_id=widget_id, file_path=__file__)
-
-    # def _generate_project_meta(self):
-    #     objects_dict = dict()
-
-    #     for cell_data in self._data:
-    #         ann_info: AnnotationInfo = cell_data["annotation_info"]
-    #         project_meta = cell_data["project_meta"]
-    #         annotation = supervisely.Annotation.from_json(ann_info.annotation, project_meta)
-    #         for label in annotation.labels:
-    #             objects_dict[label.obj_class.name] = label.obj_class
-
-    #     objects_list = list(objects_dict.values())
-    #     objects_collection = (
-    #         supervisely.ObjClassCollection(objects_list) if len(objects_list) > 0 else None
-    #     )
-
-    #     self._project_meta = supervisely.ProjectMeta(obj_classes=objects_collection)
-    #     return self._project_meta.to_json()
 
     def _generate_project_meta(self):
         objects_dict = dict()
@@ -155,23 +123,6 @@ class GridGalleryV2(Widget):
             },
         }
 
-    # return {
-    #     "options": {
-    #         "showOpacityInHeader": self._show_opacity_header,
-    #         "opacity": self._opacity,
-    #         "enableZoom": self._enable_zoom,
-    #         "syncViews": self._sync_views,
-    #         "syncViewsBindings": self._views_bindings,
-    #         "resizeOnZoom": self._resize_on_zoom,
-    #         "fillRectangle": self._fill_rectangle,
-    #         "borderWidth": self._border_width,
-    #         "selectable": False,
-    #         "viewHeight": self._view_height,
-    #         "showPreview": self._show_preview,
-    #     },
-    #     "selectedImage": None,
-    #     "activeFigure": None,
-    # }
 
     def get_column_index(self, incoming_value):
         if incoming_value is not None and 0 > incoming_value > self.columns_number:
@@ -193,9 +144,6 @@ class GridGalleryV2(Widget):
         title: str = "",
         column_index: int = None,
         ignore_tags_filtering: Union[bool, List[str]] = False,
-        # zoom_to: int = None,
-        # zoom_factor: float = 1.2,
-        # title_url=None,
     ):
         column_index = self.get_column_index(incoming_value=column_index)
         cell_uuid = str(
@@ -208,29 +156,15 @@ class GridGalleryV2(Widget):
         if ignore_tags_filtering is True:
             pass
 
-        # {
-        #     "layoutDataKey": "ann_2",
-        #     "options": {
-        #     "skipObjectTagsFiltering": true
-        #     }
-        # }
         self._data.append(
             {
                 "image_url": image_url,
                 "annotation_info": annotation_info,
                 "project_meta": project_meta,
-                # "annotation": (
-                #     supervisely.Annotation((1, 1)) if annotation is None else annotation.clone()
-                # ),
                 "column_index": column_index,
-                # "title": (
-                #     title if title_url is None else title + ' <i class="zmdi zmdi-open-in-new"></i>'
-                # ),
                 "cell_uuid": cell_uuid,
                 "skipObjectTagsFiltering": ignore_tags_filtering,
-                # "zoom_to": zoom_to,
-                # "zoom_factor": zoom_factor,
-                # "title_url": title_url,
+
             }
         )
 
@@ -267,16 +201,7 @@ class GridGalleryV2(Widget):
             annotations[cell_data["cell_uuid"]] = {
                 "imageUrl": cell_data["image_url"],
                 "annotation": cell_data["annotation_info"]._asdict(),
-                # "title": cell_data["title"],
-                # "title_url": cell_data["title_url"],
             }
-            # if not cell_data["zoom_to"] is None:
-            #     zoom_params = {
-            #         "figureId": cell_data["zoom_to"],
-            #         "factor": cell_data["zoom_factor"],
-            #     }
-            #     annotations[cell_data["cell_uuid"]]["zoomToFigure"] = zoom_params
-
         self._annotations = copy.deepcopy(annotations)
         DataJson()[self.widget_id]["content"]["annotations"] = self._annotations
 
@@ -297,8 +222,6 @@ class GridGalleryV2(Widget):
             for item in dict_list:
                 if item["id"] not in seen_ids:
                     seen_ids.add(item["id"])
-                    # if item["annotationKey"] not in seen_anns:
-                    #     seen_anns.add(item["annotationKey"])
                     unique_dicts.append(item)
 
             return unique_dicts
@@ -340,8 +263,3 @@ class GridGalleryV2(Widget):
         self._loading = value
         DataJson()[self.widget_id]["loading"] = self._loading
         DataJson().send_changes()
-
-    # def sync_images(self, image_ids: List[str]):
-    #     self._views_bindings.append(image_ids)
-    #     StateJson()[self.widget_id]["options"]["syncViewsBindings"] = self._views_bindings
-    #     StateJson().send_changes()
