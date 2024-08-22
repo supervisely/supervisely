220c30d9830f04a1604fb48f38f1ed6a27925f04
cxnt <pavelbartsits@gmail.com>
2024-08-22 16:23:21 +0400

    update static path

--- supervisely/app/fastapi/offline.py
+++ supervisely/app/fastapi/offline.py
@@ -24,7 +24,10 @@ def get_static_paths_by_mounted_object(mount) -> list:
     static_paths = []
 
     if os.path.exists("static"):
-        static_paths.append(StaticPath(local_path=pathlib.Path("static"), url_path="./static"))
+        static_paths.append(
+            StaticPath(local_path=pathlib.Path("static"), url_path=pathlib.PosixPath("sly/static"))
+        )
+        # static_paths.append(StaticPath(local_path=pathlib.Path("static"), url_path="./static"))
 
     if hasattr(mount, "routes"):
         for current_route in mount.routes:
