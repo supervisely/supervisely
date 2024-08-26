c10ea9fa0ee1720699c839ee9ffef0bd0d072d38
cxnt <pavelbartsits@gmail.com>
2024-08-26 22:01:29 +0400

    update type hint

--- supervisely/app/widgets/custom_models_selector/custom_models_selector.py
+++ supervisely/app/widgets/custom_models_selector/custom_models_selector.py
@@ -209,15 +209,11 @@ class CustomModelsSelector(Widget):
             for checkpoint_info in self._checkpoints:
                 if isinstance(checkpoint_info, dict):
                     checkpoint_selector_items.append(
-                        Select.Item(
-                            value=checkpoint_info["path"], label=checkpoint_info["name"]
-                        )
+                        Select.Item(value=checkpoint_info["path"], label=checkpoint_info["name"])
                     )
                 elif isinstance(checkpoint_info, FileInfo):
                     checkpoint_selector_items.append(
-                        Select.Item(
-                            value=checkpoint_info.path, label=checkpoint_info.name
-                        )
+                        Select.Item(value=checkpoint_info.path, label=checkpoint_info.name)
                     )
 
             checkpoint_selector = Select(items=checkpoint_selector_items)
@@ -282,9 +278,7 @@ class CustomModelsSelector(Widget):
             )
 
             file_api = FileApi(self._api)
-            self._model_path_input = Input(
-                placeholder="Path to model file in Team Files"
-            )
+            self._model_path_input = Input(placeholder="Path to model file in Team Files")
 
             @self._model_path_input.value_changed
             def change_folder(value):
@@ -322,9 +316,7 @@ class CustomModelsSelector(Widget):
 
             self.custom_tab_widgets.hide()
 
-            self.show_custom_checkpoint_path_checkbox = Checkbox(
-                "Use custom checkpoint", False
-            )
+            self.show_custom_checkpoint_path_checkbox = Checkbox("Use custom checkpoint", False)
 
             @self.show_custom_checkpoint_path_checkbox.value_changed
             def show_custom_checkpoint_path_checkbox_changed(is_checked):
@@ -399,7 +391,7 @@ class CustomModelsSelector(Widget):
         self.disable_table()
         super().disable()
 
-    def _generate_table_rows(self, train_infos: List[TrainInfo]) -> List[Dict]:
+    def _generate_table_rows(self, train_infos: List[TrainInfo]) -> Dict[str, List[ModelRow]]:
         """Method to generate table rows from remote path to training app save directory"""
 
         def process_train_info(train_info):
@@ -441,8 +433,7 @@ class CustomModelsSelector(Widget):
         if "pose estimation" in task_types:
             sorted_tt.append("pose estimation")
         other_tasks = sorted(
-            set(task_types)
-            - set(["object detection", "instance segmentation", "pose estimation"])
+            set(task_types) - set(["object detection", "instance segmentation", "pose estimation"])
         )
         sorted_tt.extend(other_tasks)
         return sorted_tt
@@ -524,9 +515,7 @@ class CustomModelsSelector(Widget):
 
     def set_custom_checkpoint_task_type(self, task_type: str) -> None:
         if self.use_custom_checkpoint_path():
-            available_task_types = (
-                self.custom_checkpoint_task_type_selector.get_labels()
-            )
+            available_task_types = self.custom_checkpoint_task_type_selector.get_labels()
             if task_type not in available_task_types:
                 raise ValueError(f'"{task_type}" is not available task type')
             self.custom_checkpoint_task_type_selector.set_value(task_type)
