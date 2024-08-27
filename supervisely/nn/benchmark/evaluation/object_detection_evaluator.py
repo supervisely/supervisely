3de0ed1b4e9dad34509839f86898ca46a1463987
almaz <almazgimaev@gmail.com>
2024-08-27 23:37:14 +0200

    classes whitelist

--- supervisely/nn/benchmark/evaluation/object_detection_evaluator.py
+++ supervisely/nn/benchmark/evaluation/object_detection_evaluator.py
@@ -1,7 +1,8 @@
 import os
+
 from supervisely.io.json import dump_json_file
-from supervisely.nn.benchmark.evaluation import BaseEvaluator
 from supervisely.nn.benchmark.coco_utils import read_coco_datasets, sly2coco
+from supervisely.nn.benchmark.evaluation import BaseEvaluator
 from supervisely.nn.benchmark.evaluation.coco import calculate_metrics
 
 
@@ -11,11 +12,8 @@ class ObjectDetectionEvaluator(BaseEvaluator):
         self.cocoGt, self.cocoDt = read_coco_datasets(self.cocoGt_json, self.cocoDt_json)
         with self.pbar(message="Calculating metrics", total=10) as p:
             self.eval_data = calculate_metrics(
-                self.cocoGt,
-                self.cocoDt,
-                iouType="bbox",
-                progress_cb=p.update
-                )
+                self.cocoGt, self.cocoDt, iouType="bbox", progress_cb=p.update
+            )
         self._dump_eval_results()
 
     def _convert_to_coco(self):
@@ -27,14 +25,18 @@ class ObjectDetectionEvaluator(BaseEvaluator):
                 is_dt_dataset=False,
                 accepted_shapes=["rectangle"],
                 progress_cb=pbar.update,
+                classes_whitelist=self.classes_whitelist,
             )
             cocoDt_json = sly2coco(
                 self.dt_project_path,
                 is_dt_dataset=True,
                 accepted_shapes=["rectangle"],
                 progress_cb=pbar.update,
+                classes_whitelist=self.classes_whitelist,
             )
-        assert cocoDt_json["categories"] == cocoGt_json["categories"], "Classes in GT and Pred projects must be the same"
+        assert (
+            cocoDt_json["categories"] == cocoGt_json["categories"]
+        ), "Classes in GT and Pred projects must be the same"
         assert [x["id"] for x in cocoDt_json["images"]] == [x["id"] for x in cocoGt_json["images"]]
         return cocoGt_json, cocoDt_json
 
