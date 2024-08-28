3809c7fdb6a9a63547ee7169a47e2d7b9c357b77
Nikolai Petukhov <petukhov.niko@gmail.com>
2024-08-28 01:17:55 -0300

    support images from different dataset in get_info_by_id_batch in imageApi

--- supervisely/api/image_api.py
+++ supervisely/api/image_api.py
@@ -583,23 +583,30 @@ class ImageApi(RemoveableBulkModuleApi):
         results = []
         if len(ids) == 0:
             return results
-        dataset_id = self.get_info_by_id(ids[0], force_metadata_for_links=False).dataset_id
-        for batch in batched(ids):
-            filters = [{"field": ApiField.ID, "operator": "in", "value": batch}]
-            results.extend(
-                self.get_list_all_pages(
-                    "images.list",
-                    {
-                        ApiField.DATASET_ID: dataset_id,
-                        ApiField.FILTER: filters,
-                        ApiField.FORCE_METADATA_FOR_LINKS: force_metadata_for_links,
-                    },
+        infos_dict = {}
+        ids_set = set(ids)
+        while any(ids_set):
+            dataset_id = self.get_info_by_id(
+                ids_set.pop(), force_metadata_for_links=False
+            ).dataset_id
+            for batch in batched(ids):
+                filters = [{"field": ApiField.ID, "operator": "in", "value": batch}]
+                results.extend(
+                    self.get_list_all_pages(
+                        "images.list",
+                        {
+                            ApiField.DATASET_ID: dataset_id,
+                            ApiField.FILTER: filters,
+                            ApiField.FORCE_METADATA_FOR_LINKS: force_metadata_for_links,
+                        },
+                    )
                 )
-            )
-            if progress_cb is not None:
-                progress_cb(len(batch))
-        temp_map = {info.id: info for info in results}
-        ordered_results = [temp_map[id] for id in ids]
+                if progress_cb is not None:
+                    progress_cb(len(batch))
+            ids_set = ids_set - set([info.id for info in results])
+            infos_dict.update({info.id: info for info in results})
+
+        ordered_results = [infos_dict[id] for id in ids]
         return ordered_results
 
     def _download(self, id, is_stream=False):
