3a81b40f5502c3784ff4d5eb78fbd6aab51fd718
vorozhkog <vorozkhog@icloud.com>
2024-09-03 10:23:41 +0100

    change heritance

--- supervisely/app/widgets/select_cuda/select_cuda.py
+++ supervisely/app/widgets/select_cuda/select_cuda.py
@@ -4,11 +4,6 @@ from supervisely.app.widgets import Widget, ConditionalWidget
 from typing import List, Dict, Optional
 from supervisely.app.widgets.select.select import Select
 
-try:
-    from typing import Literal
-except ImportError:
-    from typing_extensions import Literal
-
 from supervisely.sly_logger import logger
 
 try:
@@ -17,19 +12,27 @@ except ImportError as ie:
     logger.warn("Unable to import Torch.", extra={"error message": str(ie)})
 
 
-class SelectCudaDevice(Select):
+class SelectCudaDevice(Widget):
+    class Routes:
+        VALUE_CHANGED = "value_changed"
+
     def __init__(
         self,
+        get_list_on_init: bool = True,  # подумать
         sort_by_free_ram: Optional[bool] = False,
         include_cpu_option: Optional[bool] = False,
-        filterable: Optional[bool] = False,
-        placeholder: Optional[str] = "Select CUDA device",
-        size: Optional[Literal["large", "small", "mini"]] = None,
-        multiple: Optional[bool] = False,
-        widget_id: Optional[str] = None,
-        width_percent: Optional[int] = None,
+        widget_id: str = None,
     ):
-        cuda_devices = self._get_gpu_infos(sort_by_free_ram)
+        self._select = Select([])
+        self._sort_by_free_ram = sort_by_free_ram
+        self._include_cpu_option = include_cpu_option
+        if get_list_on_init:
+            self.refresh()
+
+        super().__init__(widget_id=widget_id, file_path=__file__)
+
+    def refresh(self):
+        cuda_devices = self._get_gpu_infos(self._sort_by_free_ram)
         items = [
             Select.Item(
                 value=mem["device_idx"],
@@ -38,26 +41,16 @@ class SelectCudaDevice(Select):
             )
             for device, mem in cuda_devices.items()
         ]
-        if include_cpu_option:
+
+        if self._include_cpu_option:
             items.append(Select.Item(value="cpu", label="CPU"))
+        self._select.set(items)
 
-        super().__init__(
-            items=items,
-            filterable=filterable,
-            placeholder=placeholder,
-            multiple=multiple,
-            size=size,
-            widget_id=widget_id,
-            width_percent=width_percent,
-        )
-
-    @staticmethod
-    def _get_gpu_infos(sort_by_free_ram):
+    def _get_gpu_infos(self, sort_by_free_ram):
         cuda.init()
-        devices = None
+        devices = {}
         try:
             if cuda.is_available() is True:
-                devices = {}
                 for idx in range(cuda.device_count()):
                     device_name = cuda.get_device_name(idx)
                     device_idx = f"cuda:{idx}"
@@ -85,3 +78,27 @@ class SelectCudaDevice(Select):
         except Exception as e:
             logger.warning(repr(e))
         return devices
+
+    def get_json_data(self):
+        return self._select.get_json_data()
+
+    def get_json_state(self):
+        return self._select.get_json_state()
+
+    def value_changed(self, func):
+        route_path = self.get_route_path(SelectCudaDevice.Routes.VALUE_CHANGED)
+        server = self._sly_app.get_server()
+        self._changes_handled = True
+
+        @server.post(route_path)
+        async def _click():
+            res = self._select.get_value()
+            func(res)
+
+        return _click
+
+    def get_device(self):
+        return self._select.get_value()
+
+    def set_device(self, value):
+        return self._select.set_value(value)
