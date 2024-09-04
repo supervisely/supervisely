fabd273a804087d075cfc3937d3e8cd6200394be
almaz <almazgimaev@gmail.com>
2024-09-04 17:13:46 +0200

    update chart for batch infrerence speedtest

--- supervisely/nn/benchmark/visualization/inference_speed/speedtest_batch.py
+++ supervisely/nn/benchmark/visualization/inference_speed/speedtest_batch.py
@@ -23,13 +23,14 @@ class SpeedtestBatch(MetricVis):
         import plotly.graph_objects as go  # pylint: disable=import-error
         from plotly.subplots import make_subplots  # pylint: disable=import-error
 
-        ms_colors = iter(["#f0b8df", "#b8f1f7", "#e8e983", "#ffbe84", "#c7b0dd", "#82dd82"])
-        fps_colors = iter(["#e377c2", "#17becf", "#bcbd22", "#ff7f0e", "#9467bd", "#2ca02c"])
+        ms_color = "#e377c2"
+        fps_color = "#17becf"
 
         temp_res = {}
         for test in self._loader.speedtest["speedtest"]:
             batch_size = test["batch_size"]
 
+            std = test["benchmark_std"]["total"]
             ms = test["benchmark"]["total"]
             fps = round(1000 / test["benchmark"]["total"] * batch_size)
 
@@ -39,16 +40,54 @@ class SpeedtestBatch(MetricVis):
             ms_line[batch_size] = ms
             fps_line[batch_size] = fps
 
-        max_y = max(ms_line.values())
-        max_y_idx = max(ms_line, key=ms_line.get)
+            ms_lower, ms_upper = ms - std, ms + std
+            ms_lower_line = temp_res.setdefault("ms_lower", {})
+            ms_upper_line = temp_res.setdefault("ms_upper", {})
+            ms_lower_line[batch_size] = ms_lower
+            ms_upper_line[batch_size] = ms_upper
+
+            fps_lower = round(1000 / ms_lower * batch_size)
+            fps_upper = round(1000 / ms_upper * batch_size)
+            fps_lower_line = temp_res.setdefault("fps_lower", {})
+            fps_upper_line = temp_res.setdefault("fps_upper", {})
+            fps_lower_line[batch_size] = fps_lower
+            fps_upper_line[batch_size] = fps_upper
+
+        max_y_fps = max(fps_upper_line.values())
+        max_y_ms = max(ms_upper_line.values())
 
         fig = go.Figure()
+
+        # first add the range lines
+        fig.add_trace(
+            go.Scatter(
+                x=list(temp_res["ms_upper"].keys()) + list(temp_res["ms_lower"].keys())[::-1],
+                y=list(temp_res["ms_upper"].values()) + list(temp_res["ms_lower"].values())[::-1],
+                fill="toself",
+                fillcolor="rgba(240, 184, 223, 0.3)",
+                line_color="rgba(255,255,255,0)",
+                showlegend=False,
+                name="Infrence time (ms)",
+            )
+        )
+        fig.add_trace(
+            go.Scatter(
+                x=list(temp_res["fps_upper"].keys()) + list(temp_res["fps_lower"].keys())[::-1],
+                y=list(temp_res["fps_upper"].values()) + list(temp_res["fps_lower"].values())[::-1],
+                fill="toself",
+                yaxis="y2",
+                fillcolor="rgba(184, 241, 247, 0.3)",
+                line_color="rgba(255,255,255,0)",
+                showlegend=False,
+                name="FPS",
+            )
+        )
         fig.add_trace(
             go.Scatter(
                 x=list(temp_res["ms"].keys()),
                 y=list(temp_res["ms"].values()),
                 name="Infrence time (ms)",
-                line=dict(color=next(ms_colors)),
+                line=dict(color=ms_color),
             )
         )
         fig.add_trace(
@@ -57,22 +96,24 @@ class SpeedtestBatch(MetricVis):
                 y=list(temp_res["fps"].values()),
                 name="FPS",
                 yaxis="y2",
-                line=dict(color=next(fps_colors), dash="dot"),
+                line=dict(color=fps_color, dash="dot"),
             )
         )
         fig.update_layout(
             yaxis=dict(
                 title="Infrence time (ms)",
+                range=[0, max_y_ms * 1.1],
             ),
             yaxis2=dict(
-                title="FPS",
+                title="FPS (Images per second)",
                 overlaying="y",
                 side="right",
+                range=[0, max_y_fps * 1.1],
             ),
-            xaxis_title="Batch Size",
+            xaxis=dict(title="Batch Size", dtick=1),
             width=700,
             height=500,
-            legend=dict(x=max_y_idx, y=max_y * 0.7),
+            legend=dict(yanchor="top", y=0.99, xanchor="left", x=0.01),
         )
 
         return fig
