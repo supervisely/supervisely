c8c5cf84ae51e773a449f0027e430bf695898f2e
almaz <almazgimaev@gmail.com>
2024-09-05 18:52:06 +0200

    remove header

--- supervisely/nn/benchmark/visualization/text_templates/inference_speed_text.py
+++ supervisely/nn/benchmark/visualization/text_templates/inference_speed_text.py
@@ -18,7 +18,6 @@ This chart compares different runtimes and devices (CPU or GPU)."""
 # We additionally divide **predict** procedure into three stages: pre-process, inference, and post-process. Each bar in this chart consists of these three stages. For example, in the chart you can find how long the post-process phase lasts in a CPU device with an ONNXRuntime environment."""
 
 
-markdown_batch_inference = """## Batch Inference
-
+markdown_batch_inference = """
 This chart shows how the model's speed changes with different batch sizes . As the batch size increases, you can observe an increase in FPS (images per second). Each line represents one environment-device setting.
 """
