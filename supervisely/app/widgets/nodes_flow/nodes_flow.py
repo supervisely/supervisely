681c4b0be8ea4c147d1a283e08d2012d83aae36c
cxnt <pavelbartsits@gmail.com>
2024-09-09 17:09:25 +0400

    Fix clear nodes method in NodesFlow widget

--- supervisely/app/widgets/nodes_flow/nodes_flow.py
+++ supervisely/app/widgets/nodes_flow/nodes_flow.py
@@ -267,6 +267,10 @@ class NodesFlow(Widget):
         StateJson()[self.widget_id]["flow"]["nodes"] = []
         StateJson()[self.widget_id]["flow"]["edges"] = []
         StateJson()[self.widget_id]["flowState"] = {}
+
+        self._nodes = []
+        self.set_edges([])
+
         StateJson().send_changes()
 
     def set_edges(self, edges: List[dict]):
