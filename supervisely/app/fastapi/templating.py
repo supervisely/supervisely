522d4136cfa1c659a4125d1f958b7f9ba23891a2
cxnt <pavelbartsits@gmail.com>
2024-10-24 21:42:47 +0400

    Update js bundle version to v2.1.96

--- supervisely/app/fastapi/templating.py
+++ supervisely/app/fastapi/templating.py
@@ -12,7 +12,7 @@ from supervisely.app.singleton import Singleton
 from supervisely.app.widgets_context import JinjaWidgets
 
 # https://github.com/supervisely/js-bundle
-js_bundle_version = "2.1.95"
+js_bundle_version = "2.1.96"
 
 # https://github.com/supervisely-ecosystem/supervisely-app-frontend-js
 js_frontend_version = "v0.0.52"
