8b10044a7df8b5cb74fe6d7af9d628a6f7c941cf
almaz <almazgimaev@gmail.com>
2024-10-30 12:03:14 +0100

    temp remove init files

--- supervisely/nn/benchmark/evaluation/semantic_segmentation/image_diversity/__init__.py
+++
@@ -1,6 +0,0 @@
-from supervisely.nn.benchmark.evaluation.semantic_segmentation.image_diversity.clip_metrics import (
-    ClipMetrics,
-)
-from supervisely.nn.benchmark.evaluation.semantic_segmentation.image_diversity.inception_metrics import (
-    InceptionMetrics,
-)
--- supervisely/nn/benchmark/evaluation/semantic_segmentation/image_diversity/clip/__init__.py
+++
@@ -1,1 +0,0 @@
-from supervisely.nn.benchmark.evaluation.semantic_segmentation.image_diversity.clip import *
