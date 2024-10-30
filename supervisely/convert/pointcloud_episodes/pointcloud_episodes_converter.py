b5d52a6fe43aee1ce4e023e46876470a625946ee
vorozhkog <vorozkhog@icloud.com>
2024-10-30 11:25:44 +0000

    Revert "fix pcd episodes converter"
    This reverts commit 4a8e1e9a69fde9850624670f27856db34790a6a3.

--- supervisely/convert/pointcloud_episodes/pointcloud_episodes_converter.py
+++ supervisely/convert/pointcloud_episodes/pointcloud_episodes_converter.py
@@ -1,6 +1,5 @@
 from typing import Dict, Optional, Union
-import os
-from supervisely.io.fs import get_file_ext, get_file_name_with_ext
+
 from supervisely import (
     Api,
     PointcloudEpisodeAnnotation,
@@ -81,88 +80,6 @@ class PointcloudEpisodeConverter(BaseConverter):
     def validate_ann_file(ann_path, meta=None):
         return False
 
-    def detect_format(self) -> BaseConverter:
-        self._converter = self._detect_format()
-        return self._converter
-
-    def _detect_format(self):
-        found_formats = []
-        all_converters = self.__class__.__subclasses__()
-
-        progress, progress_cb = self.get_progress(1, "Detecting annotation format")
-        for converter in all_converters:
-            if converter.__name__ == "BaseConverter":
-                continue
-            converter = converter(
-                self._input_data,
-                self._labeling_interface,
-                self._upload_as_links,
-                self._remote_files_map,
-            )
-
-            if not converter.validate_labeling_interface():
-                continue
-
-            if self.upload_as_links and not converter.supports_links:
-                continue
-
-            if converter.validate_format():
-                logger.info(f"Detected format: {str(converter)}")
-                found_formats.append(converter)
-                if len(found_formats) > 1:
-                    raise RuntimeError(
-                        f"Multiple formats detected: {[str(f) for f in found_formats]}. "
-                        "Mixed formats are not supported yet."
-                    )
-
-        progress_cb(1)
-
-        if len(found_formats) == 0:
-            self._items, only_modality_items, unsupported_exts = (
-                self._collect_items_if_format_not_detected()
-            )
-
-            if self.items_count == 0:
-                if unsupported_exts:
-                    raise RuntimeError(
-                        f"Not found any {self.modality} to upload. "  # pylint: disable=no-member
-                        f"Unsupported file extensions detected: {unsupported_exts}. "
-                        f"Convert your data to one of the supported formats: {self.allowed_exts}"
-                    )
-                raise RuntimeError(
-                    "Please refer to the app overview and documentation for annotation formats, "
-                    "and ensure that your data contains valid information"
-                )
-            if not only_modality_items:
-                logger.warn(
-                    "Annotations not found. "  # pylint: disable=no-member
-                    f"Uploading {self.modality} without annotations. "
-                    "If you need assistance to upload data with annotations, please contact our support team."
-                )
-            return self
-
-        if len(found_formats) == 1:
-            return found_formats[0]
-
-    def _collect_items_if_format_not_detected(self):
-        only_modality_items = True
-        unsupported_exts = set()
-        items = []
-        for root, _, files in os.walk(self._input_data):
-            for file in files:
-                full_path = os.path.join(root, file)
-                ext = get_file_ext(full_path)
-                if ext.lower() in self.allowed_exts:  # pylint: disable=no-member
-                    items.append(
-                        self.Item(full_path, files.index(file))
-                    )  # pylint: disable=no-member
-                    continue
-                only_modality_items = False
-                if ext.lower() in self.unsupported_exts:
-                    unsupported_exts.add(ext)
-
-        return items, only_modality_items, unsupported_exts
-
     def upload_dataset(
         self,
         api: Api,
@@ -172,9 +89,13 @@ class PointcloudEpisodeConverter(BaseConverter):
     ):
         """Upload converted data to Supervisely"""
 
-        meta, renamed_classes, renamed_tags = self.merge_metas_with_conflicts(api, dataset_id)
+        meta, renamed_classes, renamed_tags = self.merge_metas_with_conflicts(
+            api, dataset_id
+        )
 
-        existing_names = set([pcde.name for pcde in api.pointcloud_episode.get_list(dataset_id)])
+        existing_names = set(
+            [pcde.name for pcde in api.pointcloud_episode.get_list(dataset_id)]
+        )
 
         if log_progress:
             progress, progress_cb = self.get_progress(
@@ -209,20 +130,28 @@ class PointcloudEpisodeConverter(BaseConverter):
                 camera_names = []
                 if len(item._related_images) > 0:
                     img_paths, rimg_ann_paths = list(zip(*item._related_images))
-                    rimg_hashes = api.pointcloud_episode.upload_related_images(img_paths)
+                    rimg_hashes = api.pointcloud_episode.upload_related_images(
+                        img_paths
+                    )
                     for img_ind, (img_hash, rimg_ann_path) in enumerate(
                         zip(rimg_hashes, rimg_ann_paths)
                     ):
                         meta_json = load_json_file(rimg_ann_path)
                         try:
                             if ApiField.META not in meta_json:
-                                raise ValueError("Related image meta not found in json file.")
+                                raise ValueError(
+                                    "Related image meta not found in json file."
+                                )
                             if ApiField.NAME not in meta_json:
-                                raise ValueError("Related image name not found in json file.")
+                                raise ValueError(
+                                    "Related image name not found in json file."
+                                )
                             if "deviceId" not in meta_json[ApiField.META].keys():
                                 camera_names.append(f"CAM_{str(img_ind).zfill(2)}")
                             else:
-                                camera_names.append(meta_json[ApiField.META]["deviceId"])
+                                camera_names.append(
+                                    meta_json[ApiField.META]["deviceId"]
+                                )
                             rimg_infos.append(
                                 {
                                     ApiField.ENTITY_ID: pcd_id,
@@ -242,8 +171,12 @@ class PointcloudEpisodeConverter(BaseConverter):
                 progress_cb(len(batch))
 
         if self.items_count > 0:
-            ann = self.to_supervisely(self._items[0], meta, renamed_classes, renamed_tags)
-            api.pointcloud_episode.annotation.append(dataset_id, ann, frame_to_pointcloud_ids)
+            ann = self.to_supervisely(
+                self._items[0], meta, renamed_classes, renamed_tags
+            )
+            api.pointcloud_episode.annotation.append(
+                dataset_id, ann, frame_to_pointcloud_ids
+            )
 
         if log_progress:
             if is_development():
