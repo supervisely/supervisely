efa24beec65b6cf12736e22d01728942e03b1595
almaz <almazgimaev@gmail.com>
2024-11-06 15:15:40 +0100

    download remote archives when importing as links

--- supervisely/convert/base_converter.py
+++ supervisely/convert/base_converter.py
@@ -10,7 +10,13 @@ from supervisely.annotation.annotation import Annotation
 from supervisely.annotation.tag_meta import TagValueType
 from supervisely.api.api import Api
 from supervisely.io.env import team_id
-from supervisely.io.fs import get_file_ext, get_file_name_with_ext, silent_remove
+from supervisely.io.fs import (
+    get_file_ext,
+    get_file_name_with_ext,
+    is_archive,
+    silent_remove,
+    unpack_archive,
+)
 from supervisely.project.project_meta import ProjectMeta
 from supervisely.project.project_settings import LabelingInterface
 from supervisely.sly_logger import logger
@@ -433,31 +439,58 @@ class BaseConverter:
         if not self.upload_as_links:
             return
 
-        files_to_download = {
+        ann_archives = {l: r for l, r in self._remote_files_map.items() if is_archive(l)}
+
+        anns_to_download = {
             l: r for l, r in self._remote_files_map.items() if get_file_ext(l) == self.ann_ext
         }
-        if not files_to_download:
+        if not anns_to_download and not ann_archives:
             return
 
         import asyncio
 
-        loop = asyncio.get_event_loop()
-        _, progress_cb = self.get_progress(
-            len(files_to_download),
-            "Downloading annotation files from remote storage",
-        )
+        for files_type, files in {
+            "annotations": anns_to_download,
+            "archives": ann_archives,
+        }.items():
+            if not files:
+                continue
 
-        for local_path in files_to_download.keys():
-            silent_remove(local_path)
+            is_archive_type = files_type == "archives"
 
-        logger.info("Downloading annotation files from remote storage...")
-        loop.run_until_complete(
-            self._api.storage.download_bulk_async(
-                team_id=self._team_id,
-                remote_paths=list(files_to_download.values()),
-                local_save_paths=list(files_to_download.keys()),
-                progress_cb=progress_cb,
-                progress_cb_type="number",
+            file_size = None
+            if is_archive_type:
+                logger.info(f"Remote archives detected.")
+                file_size = sum(
+                    self._api.storage.get_info_by_path(self._team_id, remote_path).sizeb
+                    for remote_path in files.values()
+                )
+
+            loop = asyncio.get_event_loop()
+            _, progress_cb = self.get_progress(
+                len(files) if not is_archive_type else file_size,
+                f"Downloading {files_type} from remote storage",
+                is_size=is_archive_type,
             )
-        )
-        logger.info("Annotation files downloaded successfully")
+
+            for local_path in files.keys():
+                silent_remove(local_path)
+
+            logger.info(f"Downloading {files_type} from remote storage...")
+            loop.run_until_complete(
+                self._api.storage.download_bulk_async(
+                    team_id=self._team_id,
+                    remote_paths=list(files.values()),
+                    local_save_paths=list(files.keys()),
+                    progress_cb=progress_cb,
+                    progress_cb_type="number" if not is_archive_type else "size",
+                )
+            )
+            logger.info("Possible annotations downloaded successfully.")
+
+            if is_archive_type:
+                for local_path in files.keys():
+                    target_dir = os.path.dirname(os.path.normpath(local_path))
+                    unpack_archive(local_path, target_dir)
+                    silent_remove(local_path)
+                    logger.info(f"Archive {local_path} unpacked successfully.")
