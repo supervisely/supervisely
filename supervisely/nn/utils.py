2073baff2ac501aa3faa0bbee839ad9988480139
cxnt <pavelbartsits@gmail.com>
2024-12-03 15:11:40 +0400

    Remove unused code

--- supervisely/nn/utils.py
+++ supervisely/nn/utils.py
@@ -55,39 +55,3 @@ class DeployInfo:
     model_precision: str
     hardware: str
     deploy_params: dict
-
-
-def convert_train_info2experiment_info(
-    train_infos: List[TrainInfo],
-) -> List[ExperimentInfo]:
-    # @TODO:
-    raise NotImplementedError
-    experiment_infos = []
-    for train_info in train_infos:
-        # app_name: str
-        # task_id: int
-        # artifacts_folder: str
-        # session_link: str
-        # task_type: str
-        # project_name: str
-        # checkpoints: List[FileInfo]
-        # config_path: str = None
-        info = {
-            "experiment_name": "unknown",
-            "framework_name": None,
-            "model_name": "unknown",
-            "task_type": train_info.task_type,
-            "project_id": None,
-            "task_id": train_info.task_id,
-            "model_files": {"config": train_info.config_path},
-            "checkpoints": None,
-            "best_checkpoint": None,
-            "app_state": None,
-            "model_meta": None,
-            "train_val_split": None,
-            "hyperparameters": None,
-            "artifacts_dir": train_info.artifacts_folder,
-            "datetime": None,
-            "evaluation_report_id": None,
-            "eval_metrics": {},
-        }
