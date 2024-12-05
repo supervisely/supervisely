6b1ae1df0754a5873be1a612603761cc2c9dd75a
cxnt <pavelbartsits@gmail.com>
2024-12-05 14:30:09 +0400

    Log export response in get_experiment_infos for better debugging

--- supervisely/nn/experiments.py
+++ supervisely/nn/experiments.py
@@ -97,6 +97,7 @@ def get_experiment_infos(api: Api, team_id: int, framework_name: str) -> List[Ex
             )
             response.raise_for_status()
             response_json = response.json()
+            logger.info(f"Response EXPORT: {response_json.get("export")}")
             required_fields = {field.name for field in fields(ExperimentInfo)}
             if not required_fields.issubset(response_json.keys()):
                 logger.debug(
