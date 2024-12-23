fix box rotation angles

supervisely/convert/pointcloud/lyft/lyft_helper.py
@@ -68,8 +68,10 @@ def extract_data_from_scene(lyft, scene):
         dims = np.array([b.wlh for b in boxes]).reshape(-1, 3)
         rots = np.array([b.orientation.yaw_pitch_roll[0] for b in boxes]).reshape(-1, 1)
 
-        names = np.array([b.name for b in boxes])
-        gt_boxes = np.concatenate([locs, dims, rots + np.pi / 2], axis=1)
+        rots = (rots + np.pi) % (2 * np.pi) - np.pi
+        gt_boxes = np.concatenate([locs, dims, rots], axis=1)
+
+        # gt_boxes = np.concatenate([locs, dims, rots + np.pi / 2], axis=1)
         data["lidar_path"] = str(lidar_path)
         data["ann_data"]["names"] = names
         data["ann_data"]["gt_boxes"] = gt_boxes