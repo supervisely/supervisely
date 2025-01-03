50acaa42e39c15bc768ea77a37b21e56fd65ada7
Nikolai Peukhov <petukhov.niko@gmail.com>
2025-01-03 03:25:39 -0300

    update release cli

--- supervisely/cli/release/release.py
+++ supervisely/cli/release/release.py
@@ -15,7 +15,8 @@ import requests
 from giturlparse import parse
 from requests_toolbelt import MultipartEncoder, MultipartEncoderMonitor
 from tqdm import tqdm
-from supervisely.io.fs import dir_exists, remove_dir, list_files_recursively
+
+from supervisely.io.fs import dir_exists, list_files_recursively, remove_dir
 
 
 class cd:
@@ -223,8 +224,10 @@ def archive_application(repo: git.Repo, config, slug):
             should_remove_dir = gui_folder_path
             # if gui folder is empty, need to render it
             with cd(str(working_dir_path), add_to_path=True):
-                exec(open("render.py", "r").read(), {"__name__": "__main__"})
-                file_paths.extend([Path(p).absolute() for p in list_files_recursively(str(gui_folder_path))])
+                exec(open("sly_sdk/render.py", "r").read(), {"__name__": "__main__"})
+                file_paths.extend(
+                    [Path(p).absolute() for p in list_files_recursively(str(gui_folder_path))]
+                )
         archive_path = archive_folder + "/archive.tar"
         write_mode = "w"
     else:
