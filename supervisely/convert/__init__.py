c28fa1bc600c46a8f244088f5a257e59dea492e1
almaz <almazgimaev@gmail.com>
2025-01-09 10:38:37 +0100

    Refactor initial imports of converters modules

--- supervisely/convert/__init__.py
+++ supervisely/convert/__init__.py
@@ -1,68 +1,13 @@
 # isort: skip_file
-from supervisely.convert.base_converter import (
-    AvailableImageConverters,
-    AvailablePointcloudConverters,
-    AvailableVideoConverters,
-    AvailableVolumeConverters,
-    BaseConverter,
-)
-from supervisely.convert.converter import ImportManager
 
-# Image
-from supervisely.convert.image.cityscapes.cityscapes_converter import (
-    CityscapesConverter,
-)
-from supervisely.convert.image.coco.coco_converter import COCOConverter
-from supervisely.convert.image.coco.coco_anntotation_converter import FastCOCOConverter
-from supervisely.convert.image.csv.csv_converter import CSVConverter
-from supervisely.convert.image.multispectral.multispectral_converter import (
-    MultiSpectralImageConverter,
-)
-from supervisely.convert.image.medical2d.medical2d_converter import Medical2DImageConverter
-from supervisely.convert.image.masks.images_with_masks_converter import ImagesWithMasksConverter
+# Project
+from supervisely.convert.image.coco.coco_helper import sly_project_to_coco as project_to_coco
+from supervisely.convert.image.yolo.yolo_helper import sly_project_to_yolo as project_to_yolo
 
-from supervisely.convert.image.pascal_voc.pascal_voc_converter import PascalVOCConverter
-from supervisely.convert.image.pdf.pdf_converter import PDFConverter
-from supervisely.convert.image.sly.sly_image_converter import SLYImageConverter
-from supervisely.convert.image.sly.fast_sly_image_converter import FastSlyImageConverter
-from supervisely.convert.image.yolo.yolo_converter import YOLOConverter
-from supervisely.convert.image.multi_view.multi_view import MultiViewImageConverter
-from supervisely.convert.image.label_me.label_me_converter import LabelmeConverter
-from supervisely.convert.image.label_studio.label_studio_converter import LabelStudioConverter
-from supervisely.convert.image.high_color.high_color_depth import HighColorDepthImageConverter
+# Dataset
+from supervisely.convert.image.coco.coco_helper import sly_ds_to_coco as dataset_to_coco
+from supervisely.convert.image.yolo.yolo_helper import sly_ds_to_yolo as dataset_to_yolo
 
-
-# Pointcloud
-from supervisely.convert.pointcloud.sly.sly_pointcloud_converter import SLYPointcloudConverter
-from supervisely.convert.pointcloud.las.las_converter import LasConverter
-from supervisely.convert.pointcloud.ply.ply_converter import PlyConverter
-from supervisely.convert.pointcloud.bag.bag_converter import BagConverter
-from supervisely.convert.pointcloud.lyft.lyft_converter import LyftConverter
-from supervisely.convert.pointcloud.nuscenes_conv.nuscenes_converter import NuscenesConverter
-
-# Pointcloud Episodes
-from supervisely.convert.pointcloud_episodes.sly.sly_pointcloud_episodes_converter import (
-    SLYPointcloudEpisodesConverter,
-)
-from supervisely.convert.pointcloud_episodes.bag.bag_converter import BagEpisodesConverter
-from supervisely.convert.pointcloud_episodes.lyft.lyft_converter import LyftEpisodesConverter
-from supervisely.convert.pointcloud_episodes.nuscenes_conv.nuscenes_converter import (
-    NuscenesEpisodesConverter,
-)
-
-# Video
-from supervisely.convert.video.mot.mot_converter import MOTConverter
-from supervisely.convert.video.sly.sly_video_converter import SLYVideoConverter
-
-# Volume
-from supervisely.convert.volume.sly.sly_volume_converter import SLYVolumeConverter
-from supervisely.convert.volume.dicom.dicom_converter import DICOMConverter
-
-# Export Converters
-from supervisely.convert.image.coco.coco_helper import sly_project_to_coco as to_coco
-from supervisely.convert.image.yolo.yolo_helper import sly_project_to_yolo as to_yolo
-
-__all__ = [
-    "to_coco",
-    "to_yolo",
-]
+# Image Annotations
+from supervisely.convert.image.coco.coco_helper import sly_ann_to_coco as annotation_to_coco
+from supervisely.convert.image.yolo.yolo_helper import sly_ann_to_yolo as annotation_to_yolo
---
+++ supervisely/convert/pointcloud/__init__.py
@@ -0,0 +1,7 @@
+# Pointcloud
+from supervisely.convert.pointcloud.sly.sly_pointcloud_converter import SLYPointcloudConverter
+from supervisely.convert.pointcloud.las.las_converter import LasConverter
+from supervisely.convert.pointcloud.ply.ply_converter import PlyConverter
+from supervisely.convert.pointcloud.bag.bag_converter import BagConverter
+from supervisely.convert.pointcloud.lyft.lyft_converter import LyftConverter
+from supervisely.convert.pointcloud.nuscenes_conv.nuscenes_converter import NuscenesConverter
