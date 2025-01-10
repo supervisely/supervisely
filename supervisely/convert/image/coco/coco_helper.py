f096ada144dc1a12946811573ab2f4ad2b0501cd
almaz <almazgimaev@gmail.com>
2025-01-10 18:07:51 +0100

    disable pylint

--- supervisely/convert/image/coco/coco_helper.py
+++ supervisely/convert/image/coco/coco_helper.py
@@ -712,8 +712,8 @@ def sly_ds_to_coco(
         image_info = ImageApi._convert_json_info(ImageApi(None), image_info_json)
 
         coco_ann["images"].append(image_coco(image_info, image_idx))
-        if with_captions is True and isinstance(coco_captions, dict):
-            coco_captions["images"].append(image_coco(image_info, image_idx))
+        if with_captions is True:
+            coco_captions["images"].append(image_coco(image_info, image_idx))  # pylint: disable=unsubscriptable-object
 
         ann = Annotation.load_json_file(ann_path, meta)
         if ann.img_size is None or ann.img_size == (0, 0) or ann.img_size == (None, None):
