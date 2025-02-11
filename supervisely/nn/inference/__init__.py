fcfb7b066928a894aac2123bab17b2563604354f
MaxTeselkin <tesmaxxxx@gmail.com>
2025-02-12 03:20:31 +0400

    fixed import issue

--- supervisely/nn/inference/__init__.py
+++ supervisely/nn/inference/__init__.py
@@ -34,12 +34,3 @@ from supervisely.nn.utils import (
     ModelPrecision,
 )
 from supervisely.nn.task_type import TaskType
-from supervisely.nn.auto_labeling.auto_labeling import (
-    get_supported_models,
-    get_supported_checkpoints,
-    deploy_foundation_model,
-    object_detection,
-    object_pointing,
-    instance_segmentation,
-    preview,
-)
