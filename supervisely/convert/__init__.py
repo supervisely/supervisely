c03b0b3bc6a0cca8d92ea70668bbe37c77bd2251
Almaz <79905215+almazgimaev@users.noreply.github.com>
2025-02-12 14:32:37 +0100

    Wrappers for conversion functions (from path/sly.Project/sly.Dataset to COCO/YOLO/PascalVOC formats). Added Support for COCO RLE masks. (#1238)

--- supervisely/convert/__init__.py
+++ supervisely/convert/__init__.py
@@ -3,14 +3,30 @@
 # Project
 from supervisely.convert.image.coco.coco_helper import sly_project_to_coco as project_to_coco
 from supervisely.convert.image.yolo.yolo_helper import sly_project_to_yolo as project_to_yolo
-from supervisely.convert.image.pascal_voc.pascal_voc_helper import sly_project_to_pascal_voc as project_to_pascal_voc
+from supervisely.convert.image.pascal_voc.pascal_voc_helper import (
+    sly_project_to_pascal_voc as project_to_pascal_voc,
+)
 
 # Dataset
 from supervisely.convert.image.coco.coco_helper import sly_ds_to_coco as dataset_to_coco
 from supervisely.convert.image.yolo.yolo_helper import sly_ds_to_yolo as dataset_to_yolo
-from supervisely.convert.image.pascal_voc.pascal_voc_helper import sly_ds_to_pascal_voc as dataset_to_pascal_voc
+from supervisely.convert.image.pascal_voc.pascal_voc_helper import (
+    sly_ds_to_pascal_voc as dataset_to_pascal_voc,
+)
 
 # Image Annotations
 from supervisely.convert.image.coco.coco_helper import sly_ann_to_coco as annotation_to_coco
 from supervisely.convert.image.yolo.yolo_helper import sly_ann_to_yolo as annotation_to_yolo
-from supervisely.convert.image.pascal_voc.pascal_voc_helper import sly_ann_to_pascal_voc as annotation_to_pascal_voc
+from supervisely.convert.image.pascal_voc.pascal_voc_helper import (
+    sly_ann_to_pascal_voc as annotation_to_pascal_voc,
+)
+
+
+# Supervisely Project/Dataset/Annotation to COCO
+from supervisely.convert.image.coco.coco_helper import to_coco
+
+# Supervisely Project/Dataset/Annotation to YOLO
+from supervisely.convert.image.yolo.yolo_helper import to_yolo
+
+# Supervisely Project/Dataset/Annotation to Pascal VOC
+from supervisely.convert.image.pascal_voc.pascal_voc_helper import to_pascal_voc
