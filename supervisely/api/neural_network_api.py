7b54d2f4767e6b547246c11529c1715dec9f6670
Nikolai Petukhov <petukhov.niko@gmail.com>
2025-02-16 23:48:39 -0300

    move deploy api to nn module, remove legacy methods

--- supervisely/api/neural_network_api.py
+++ supervisely/api/neural_network_api.py
@@ -1,202 +1,191 @@
 # coding: utf-8
 """download/upload/manipulate neural networks"""
 
-import os
-import tarfile
-from requests_toolbelt import MultipartEncoder, MultipartEncoderMonitor
-import numpy as np
-import json
+from pathlib import Path
+from typing import Any, Dict, List, Literal, Optional
 
-from supervisely.api.module_api import ApiField, CloneableModuleApi, RemoveableModuleApi
-from supervisely._utils import rand_str
-from supervisely.io.fs import ensure_base_path, silent_remove
-from supervisely.imaging import image as sly_image
-from supervisely.project.project_meta import ProjectMeta
+from supervisely.api.module_api import CloneableModuleApi, RemoveableModuleApi
+from supervisely.io import env
 
 
 class NeuralNetworkApi(CloneableModuleApi, RemoveableModuleApi):
-    """
-    """
-    @staticmethod
-    def info_sequence():
-        """
-        """
-        return [ApiField.ID,
-                ApiField.NAME,
-                ApiField.DESCRIPTION,
-                ApiField.CONFIG,
-                ApiField.HASH,
-                ApiField.ONLY_TRAIN,
-                ApiField.PLUGIN_ID,
-                ApiField.PLUGIN_VERSION,
-                ApiField.SIZE,
-                ApiField.WEIGHTS_LOCATION,
-                ApiField.README,
-                ApiField.TASK_ID,
-                ApiField.USER_ID,
-                ApiField.TEAM_ID,
-                ApiField.WORKSPACE_ID,
-                ApiField.CREATED_AT,
-                ApiField.UPDATED_AT]
+    """ """
 
-    @staticmethod
-    def info_tuple_name():
-        """
-        """
-        return 'ModelInfo'
+    def deploy_model_from_api(self, task_id, deploy_params):
+        self._api.task.send_request(
+            task_id, "deploy_from_api", data={"deploy_params": deploy_params}, raise_error=True
+        )
 
-    def get_list(self, workspace_id, filters=None):
-        """
-        """
-        return self.get_list_all_pages('models.list',  {ApiField.WORKSPACE_ID: workspace_id, ApiField.FILTER: filters or []})
+    def deploy(
+        self,
+        module_id: Optional[int] = None,
+        train_app_id: Optional[int] = None,
+        train_module_id: Optional[int] = None,
+        train_task_id: Optional[int] = None,
+        workspace_id: Optional[int] = None,
+        agent_id: Optional[int] = None,
+        description: Optional[str] = "application description",
+        params: Dict[str, Any] = None,
+        log_level: Optional[Literal["info", "debug", "warning", "error"]] = "info",
+        users_ids: Optional[List[int]] = None,
+        app_version: Optional[str] = "",
+        is_branch: Optional[bool] = False,
+        task_name: Optional[str] = "pythonSpawned",
+        restart_policy: Optional[Literal["never", "on_error"]] = "never",
+        proxy_keep_url: Optional[bool] = False,
+        redirect_requests: Optional[Dict[str, int]] = {},
+        limit_by_workspace: bool = False,
+        deploy_params: Dict[str, Any] = None,
+        timeout: int = 100,
+    ):
+        if train_task_id is not None:
+            if train_module_id or train_app_id:
+                raise ValueError(
+                    "train_module_id and train_app_id are not allowed with train_task_id"
+                )
+            task_info = self.get_info_by_id(train_task_id)
+            try:
+                data = task_info["meta"]["output"]["experiment"]["data"]
+            except KeyError:
+                raise ValueError("Task output does not contain experiment data")
 
-    def get_info_by_id(self, id):
-        """
-        """
-        return self._get_info_by_id(id, 'models.info')
+            if module_id is None:
+                train_module_id = task_info["meta"]["app"]["moduleId"]
+                module_id = self._get_serving_app_module_id(train_module_id)
 
-    def download(self, id):
-        """
-        """
-        response = self._api.post('models.download', {ApiField.ID: id}, stream=True)
-        return response
+            if workspace_id is None:
+                workspace_id = task_info["workspaceId"]
 
-    def download_to_tar(self, workspace_id, name, tar_path, progress_cb=None):
-        """
-        """
-        model = self.get_info_by_name(workspace_id, name)
-        response = self.download(model.id)
-        ensure_base_path(tar_path)
-        with open(tar_path, 'wb') as fd:
-            for chunk in response.iter_content(chunk_size=1024*1024):
-                fd.write(chunk)
-                if progress_cb is not None:
-                    read_mb = len(chunk) / 1024.0 / 1024.0
-                    progress_cb(read_mb)
+            experiment_name = data["experiment_name"]
 
-    def download_to_dir(self, workspace_id, name, directory, progress_cb=None):
-        """
-        """
-        model_tar = os.path.join(directory, rand_str(10) + '.tar')
-        self.download_to_tar(workspace_id, name, model_tar, progress_cb)
-        model_dir = os.path.join(directory, name)
-        with tarfile.open(model_tar) as archive:
-            archive.extractall(model_dir)
-        silent_remove(model_tar)
-        return model_dir
+            deploy_params_overwrite = deploy_params
+            if deploy_params_overwrite is None:
+                deploy_params_overwrite = {}
 
-    def generate_hash(self, task_id):
-        """"""
-        response = self._api.post('models.hash.create', {ApiField.TASK_ID: task_id})
-        return response.json()
+            if checkpoint_name is None:
+                checkpoint_name = data["best_checkpoint"]
 
-    def upload(self, hash, archive_path, progress_cb=None):
-        """
-        """
-        encoder = MultipartEncoder({'hash': hash,
-                                    'weights': (os.path.basename(archive_path), open(archive_path, 'rb'), 'application/x-tar')})
+            if task_name is None:
+                task_name = experiment_name + f" ({checkpoint_name})"
 
-        def callback(monitor_instance):
-            read_mb = monitor_instance.bytes_read / 1024.0 / 1024.0
-            if progress_cb is not None:
-                progress_cb(read_mb)
-        monitor = MultipartEncoderMonitor(encoder, callback)
-        self._api.post('models.upload', monitor)
+            if description is None:
+                description = f"""Serve from experiment
+                    Experiment name:   {experiment_name}
+                    Evaluation report: {data["evaluation_report_link"]}
+                """
+                while len(description) > 255:
+                    description = description.rsplit("\n", 1)[0]
 
-    def inference_remote_image(self, id, image_hash, ann=None, meta=None, mode=None):
-        """
-        """
-        data = {
-            "request_type": "inference",
-            "meta": meta or ProjectMeta().to_json(),
-            "annotation": ann or None,
-            "mode": mode or {},
-            "image_hash": image_hash
-        }
-        fake_img_data = sly_image.write_bytes(np.zeros([5, 5, 3]), '.jpg')
-        encoder = MultipartEncoder({'id': str(id).encode('utf-8'),
-                                    'data': json.dumps(data),
-                                    'image': ("img", fake_img_data, "")})
-        response = self._api.post('models.infer', MultipartEncoderMonitor(encoder))
-        return response.json()
+            deploy_params = {
+                "model_files": {
+                    "checkpoint": Path(
+                        data["artifacts_dir"], "checkpoints", checkpoint_name
+                    ).as_posix(),
+                    "config": Path(data["artifacts_dir"], data["model_files"]["config"]).as_posix(),
+                },
+                "model_source": "Custom models",
+                "model_info": data,
+                "device": "cuda",
+                "runtime": "PyTorch",
+            }
+            deploy_params = {**deploy_params, **deploy_params_overwrite}
+        elif train_module_id is not None:
+            if module_id is not None:
+                raise ValueError("module_id is not allowed with train_module_id")
+            module_id = self._get_serving_app_module_id(train_module_id)
+        elif train_app_id is not None:
+            if module_id is not None:
+                raise ValueError("module_id is not allowed with train_app_id")
+            train_module_id = self._api.app.get_info_by_id(train_app_id).module_id
+            module_id = self._get_serving_app_module_id(train_module_id)
 
-    def inference(self, id, img, ann=None, meta=None, mode=None, ext=None):
-        """
-        """
-        data = {
-            "request_type": "inference",
-            "meta": meta or ProjectMeta().to_json(),
-            "annotation": ann or None,
-            "mode": mode or {},
-        }
-        img_data = sly_image.write_bytes(img, ext or '.jpg')
-        encoder = MultipartEncoder({'id': str(id).encode('utf-8'),
-                                    'data': json.dumps(data),
-                                    'image': ("img", img_data, "")})
+        if workspace_id is None:
+            workspace_id = env.workspace_id()
+        return self._deploy_model_app(
+            module_id=module_id,
+            workspace_id=workspace_id,
+            agent_id=agent_id,
+            description=description,
+            params=params,
+            log_level=log_level,
+            users_ids=users_ids,
+            app_version=app_version,
+            is_branch=is_branch,
+            task_name=task_name,
+            restart_policy=restart_policy,
+            proxy_keep_url=proxy_keep_url,
+            redirect_requests=redirect_requests,
+            limit_by_workspace=limit_by_workspace,
+            deploy_params=deploy_params,
+            timeout=timeout,
+        )
 
-        response = self._api.post('models.infer', MultipartEncoderMonitor(encoder))
-        return response.json()
+    def _deploy_model_app(
+        self,
+        module_id: int,
+        workspace_id: int,
+        agent_id: Optional[int] = None,
+        description: Optional[str] = "application description",
+        params: Dict[str, Any] = None,
+        log_level: Optional[Literal["info", "debug", "warning", "error"]] = "info",
+        users_ids: Optional[List[int]] = None,
+        app_version: Optional[str] = "",
+        is_branch: Optional[bool] = False,
+        task_name: Optional[str] = "pythonSpawned",
+        restart_policy: Optional[Literal["never", "on_error"]] = "never",
+        proxy_keep_url: Optional[bool] = False,
+        redirect_requests: Optional[Dict[str, int]] = {},
+        limit_by_workspace: bool = False,
+        deploy_params: Dict[str, Any] = None,
+        timeout: int = 100,
+    ):
+        task_info = self._api.task.start(
+            agent_id=agent_id,
+            workspace_id=workspace_id,
+            module_id=module_id,
+            description=description,
+            params=params,
+            log_level=log_level,
+            users_ids=users_ids,
+            app_version=app_version,
+            is_branch=is_branch,
+            task_name=task_name,
+            restart_policy=restart_policy,
+            proxy_keep_url=proxy_keep_url,
+            redirect_requests=redirect_requests,
+            limit_by_workspace=limit_by_workspace,
+        )
 
-    def get_output_meta(self, id, input_meta=None, inference_mode=None):
-        """
-        """
-        data = {
-            "request_type": "get_out_meta",
-            "meta": input_meta or ProjectMeta().to_json(),
-            "mode": inference_mode or {}
-        }
-        encoder = MultipartEncoder({'id': str(id).encode('utf-8'),
-                                    'data': json.dumps(data)})
-        response = self._api.post('models.infer', MultipartEncoderMonitor(encoder))
-        response_json = response.json()
-        if 'out_meta' in response_json:
-            return response_json['out_meta']
-        if 'output_meta' in response_json:
-            return response_json['output_meta']
-        return response.json()
+        attempt_delay_sec = 10
+        attempts = (timeout + attempt_delay_sec) // attempt_delay_sec
+        ready = self._api.app.wait_until_ready_for_api_calls(
+            task_info["id"], attempts, attempt_delay_sec
+        )
+        if not ready:
+            raise TimeoutError(
+                f"Task {task_info['id']} is not ready for API calls after {timeout} seconds."
+            )
+        self.deploy_model_from_api(task_info["id"], deploy_params=deploy_params)
+        return task_info
 
-    def get_deploy_tasks(self, model_id):
-        """
-        """
-        response = self._api.post('models.info.deployed', {'id': model_id})
-        return [task[ApiField.ID] for task in response.json()]
+    def _get_serving_app_module_id(self, train_app_module_id: int):
+        train_module_info = self._api.app.get_ecosystem_module_info(train_app_module_id)
+        train_app_config = train_module_info.config
+        categories = train_app_config["categories"]
+        framework = None
+        for category in categories:
+            if category.lower().startswith("framework:"):
+                framework = category
+                break
+        if framework is None:
+            raise ValueError(
+                "Unable to define serving app. Framework is not specified in the train app"
+            )
 
-    def get_training_metrics(self, model_id):
-        """
-        """
-        response = self._get_response_by_id(id=model_id, method='tasks.train-metrics', id_field=ApiField.MODEL_ID)
-        return response.json() if (response is not None) else None
-
-    def _clone_api_method_name(self):
-        """
-        """
-        return 'models.clone'
-
-    def _remove_api_method_name(self):
-        """
-        """
-        return 'models.remove'
-
-    def create_from_checkpoint(self, task_id, checkpoint_id, model_name, change_name_if_conflict=True):
-        """
-        """
-        # FYI: checkpoint has these fields
-        # 'modelTitle': 'my_model_name_006',
-        # 'status': 'uploaded'
-
-        self._api.task._validate_checkpoints_support(task_id)
-        task_info = self._api.task.get_info_by_id(task_id)
-        workspace_id = task_info[ApiField.WORKSPACE_ID]
-        new_model_name = self.get_free_name(workspace_id, model_name)
-        if new_model_name != model_name and change_name_if_conflict is False:
-            raise KeyError("Model name={!r} already exists in workspace id={!r}".format(model_name, workspace_id))
-        resp = self._api.post("models.create-from-checkpoint", {ApiField.ID: checkpoint_id,
-                                                                ApiField.TASK_ID: task_id,
-                                                                ApiField.NAME: new_model_name})
-        process_task_id = resp.json()[ApiField.TASK_ID]
-        if process_task_id is not None:
-            self._api.task.wait(process_task_id, self._api.task.Status.FINISHED)
-        else:
-            # upload process skipped because checkpoint is already uploaded to server, just new model will be created
-            pass
-        return new_model_name
+        modules = self._api.app.get_list_ecosystem_modules(
+            categories=["serve", framework], categories_operation="and"
+        )
+        if len(modules) == 0:
+            raise ValueError(f"No serving apps found for framework {framework}")
+        module_id = modules[0]["id"]
+        return module_id
