ad5ad93ee696288a4861653024470e6db2bf433d
almaz <almazgimaev@gmail.com>
2025-02-17 17:22:24 +0100

    Reimplemetn summary metrics computation for COCOeval class to override maxDets param

--- supervisely/nn/benchmark/utils/detection/calculate_metrics.py
+++ supervisely/nn/benchmark/utils/detection/calculate_metrics.py
@@ -53,7 +53,93 @@ def calculate_metrics(
 
     cocoGt: COCO = cocoGt
 
+    def summarize(coco_eval_obj):
+        """
+        Compute and display summary metrics for evaluation results.
+        Note this functin can *only* be applied on the default parameter setting
+        """
+
+        def _summarize(ap=1, iouThr=None, areaRng="all", maxDets=100):
+            p = coco_eval_obj.params
+            iStr = " {:<18} {} @[ IoU={:<9} | area={:>6s} | maxDets={:>3d} ] = {:0.3f}"
+            titleStr = "Average Precision" if ap == 1 else "Average Recall"
+            typeStr = "(AP)" if ap == 1 else "(AR)"
+            iouStr = (
+                "{:0.2f}:{:0.2f}".format(p.iouThrs[0], p.iouThrs[-1])
+                if iouThr is None
+                else "{:0.2f}".format(iouThr)
+            )
+
+            aind = [i for i, aRng in enumerate(p.areaRngLbl) if aRng == areaRng]
+            mind = [i for i, mDet in enumerate(p.maxDets) if mDet == maxDets]
+            if ap == 1:
+                # dimension of precision: [TxRxKxAxM]
+                s = coco_eval_obj.eval["precision"]
+                # IoU
+                if iouThr is not None:
+                    t = np.where(iouThr == p.iouThrs)[0]
+                    s = s[t]
+                s = s[:, :, :, aind, mind]
+            else:
+                # dimension of recall: [TxKxAxM]
+                s = coco_eval_obj.eval["recall"]
+                if iouThr is not None:
+                    t = np.where(iouThr == p.iouThrs)[0]
+                    s = s[t]
+                s = s[:, :, aind, mind]
+            if len(s[s > -1]) == 0:
+                mean_s = -1
+            else:
+                mean_s = np.mean(s[s > -1])
+            print(iStr.format(titleStr, typeStr, iouStr, areaRng, maxDets, mean_s))
+            return mean_s
+
+        def _summarizeDets():
+            stats = np.zeros((12,))
+            stats[0] = _summarize(1, maxDets=coco_eval_obj.params.maxDets[2])
+            stats[1] = _summarize(1, iouThr=0.5, maxDets=coco_eval_obj.params.maxDets[2])
+            stats[2] = _summarize(1, iouThr=0.75, maxDets=coco_eval_obj.params.maxDets[2])
+            stats[3] = _summarize(1, areaRng="small", maxDets=coco_eval_obj.params.maxDets[2])
+            stats[4] = _summarize(1, areaRng="medium", maxDets=coco_eval_obj.params.maxDets[2])
+            stats[5] = _summarize(1, areaRng="large", maxDets=coco_eval_obj.params.maxDets[2])
+            stats[6] = _summarize(0, maxDets=coco_eval_obj.params.maxDets[0])
+            stats[7] = _summarize(0, maxDets=coco_eval_obj.params.maxDets[1])
+            stats[8] = _summarize(0, maxDets=coco_eval_obj.params.maxDets[2])
+            stats[9] = _summarize(0, areaRng="small", maxDets=coco_eval_obj.params.maxDets[2])
+            stats[10] = _summarize(0, areaRng="medium", maxDets=coco_eval_obj.params.maxDets[2])
+            stats[11] = _summarize(0, areaRng="large", maxDets=coco_eval_obj.params.maxDets[2])
+            return stats
+
+        # def _summarizeKps():
+        #     stats = np.zeros((10,))
+        #     stats[0] = _summarize(1, maxDets=20)
+        #     stats[1] = _summarize(1, maxDets=20, iouThr=0.5)
+        #     stats[2] = _summarize(1, maxDets=20, iouThr=0.75)
+        #     stats[3] = _summarize(1, maxDets=20, areaRng="medium")
+        #     stats[4] = _summarize(1, maxDets=20, areaRng="large")
+        #     stats[5] = _summarize(0, maxDets=20)
+        #     stats[6] = _summarize(0, maxDets=20, iouThr=0.5)
+        #     stats[7] = _summarize(0, maxDets=20, iouThr=0.75)
+        #     stats[8] = _summarize(0, maxDets=20, areaRng="medium")
+        #     stats[9] = _summarize(0, maxDets=20, areaRng="large")
+        #     return stats
+
+        if not coco_eval_obj.eval:
+            raise Exception("Please run accumulate() first")
+        iouType = coco_eval_obj.params.iouType
+        if iouType == "segm" or iouType == "bbox":
+            summarize = _summarizeDets
+        # elif iouType == "keypoints":
+        #     summarize = _summarizeKps
+        coco_eval_obj.stats = summarize()
+
+    COCOeval.summarize = summarize
+
+    evaluation_params = evaluation_params or {}
+    max_dets = evaluation_params.get("max_detection", 100)
+
     cocoEval = COCOeval(cocoGt, cocoDt, iouType=iouType)
+    cocoEval.params.maxDets[-1] = max_dets
     cocoEval.evaluate()
     progress_cb(1) if progress_cb is not None else None
     cocoEval.accumulate()
@@ -63,6 +149,7 @@ def calculate_metrics(
     # For classification metrics
     cocoEval_cls = COCOeval(cocoGt, cocoDt, iouType=iouType)
     cocoEval_cls.params.useCats = 0
+    cocoEval_cls.params.maxDets[-1] = max_dets
     cocoEval_cls.evaluate()
     progress_cb(1) if progress_cb is not None else None
     cocoEval_cls.accumulate()
@@ -70,7 +157,6 @@ def calculate_metrics(
     cocoEval_cls.summarize()
 
     iouThrs = cocoEval.params.iouThrs
-    evaluation_params = evaluation_params or {}
     iou_threshold = evaluation_params.get("iou_threshold", 0.5)
     iou_threshold_per_class = evaluation_params.get("iou_threshold_per_class")
     if iou_threshold_per_class is not None:
@@ -86,7 +172,7 @@ def calculate_metrics(
     if iou_threshold_per_class is not None or iou_threshold != 0.5:
         average_across_iou_thresholds = False
         evaluation_params["average_across_iou_thresholds"] = average_across_iou_thresholds
-    
+
     eval_img_dict = get_eval_img_dict(cocoEval)
     eval_img_dict_cls = get_eval_img_dict(cocoEval_cls)
     matches = get_matches(
