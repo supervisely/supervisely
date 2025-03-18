31162125cd21cc77fd2bede0d2d1e79fa4e5165f
GoldenAnpu <seryios.sych@gmail.com>
2025-03-18 20:57:08 +0100

    Refactor AutoRestartInfo to use deploy_params and update Inference class for autorestart handling

--- supervisely/nn/inference/inference.py
+++ supervisely/nn/inference/inference.py
@@ -93,83 +93,29 @@ except ImportError:
 
 @dataclass
 class AutoRestartInfo:
-    custom_model = False
-    device: str = None
-    custom_name: Optional[str] = None
-    custom_path: Optional[str] = None
-    pretrained_name: Optional[str] = None
-    pretrained_path: Optional[str] = None
+    deploy_params: dict
 
     class Fields:
         AUTO_RESTART_INFO = "autoRestartInfo"
-        IS_CUSTOM_MODEL = "isCustomModel"
-        DEVICE = "device"
-        CUSTOM_NAME = "customName"
-        CUSTOM_PATH = "customPath"
-        PRETRAINED_NAME = "pretrainedName"
-        PRETRAINED_PATH = "pretrainedPath"
-
-    def __pre_init__(
-        self,
-        custom_model: bool,
-        device: str,
-        custom_name: Optional[str],
-        custom_path: Optional[str],
-        pretrained_name: Optional[str],
-        pretrained_path: Optional[str],
-    ):
-        self.custom_model = custom_model
-        self.device = device
-        if custom_model and (custom_name is None or custom_path is None):
-            raise ValueError("Custom model name and path must be provided.")
-        self.custom_name = custom_name
-        self.custom_path = custom_path
-        if not custom_model and (pretrained_name is None or pretrained_path is None):
-            raise ValueError("Pretrained model name and path must be provided.")
-        elif custom_model and (pretrained_name is not None or pretrained_path is not None):
-            raise ValueError(
-                "You are using custom model, but provided pretrained model name and path for auto restart."
-            )
-        self.pretrained_name = pretrained_name
-        self.pretrained_path = pretrained_path
+        DEPLOY_PARAMS = "deployParams"
 
     def generate_fields(self) -> List[dict]:
         return [
             {
                 ApiField.FIELD: self.Fields.AUTO_RESTART_INFO,
-                ApiField.PAYLOAD: {
-                    self.Fields.IS_CUSTOM_MODEL: self.custom_model,
-                    self.Fields.DEVICE: self.device,
-                    self.Fields.CUSTOM_NAME: self.custom_name,
-                    self.Fields.CUSTOM_PATH: self.custom_path,
-                    self.Fields.PRETRAINED_NAME: self.pretrained_name,
-                    self.Fields.PRETRAINED_PATH: self.pretrained_path,
-                },
+                ApiField.PAYLOAD: {self.Fields.DEPLOY_PARAMS: self.deploy_params},
             }
         ]
 
     @classmethod
     def from_response(cls, data: dict):
-        if data.get(cls.Fields.AUTO_RESTART_INFO) is None:
+        autorestart_info = data.get(cls.Fields.AUTO_RESTART_INFO, None)
+        if autorestart_info is None:
             return None
-        return cls(
-            custom_model=data[AutoRestartInfo.Fields.IS_CUSTOM_MODEL],
-            device=data[AutoRestartInfo.Fields.DEVICE],
-            custom_name=data[AutoRestartInfo.Fields.CUSTOM_NAME],
-            custom_path=data[AutoRestartInfo.Fields.CUSTOM_PATH],
-            pretrained_name=data[AutoRestartInfo.Fields.PRETRAINED_NAME],
-            pretrained_path=data[AutoRestartInfo.Fields.PRETRAINED_PATH],
-        )
+        return cls(deploy_params=autorestart_info.get(cls.Fields.DEPLOY_PARAMS, None))
 
-    def is_changed(self, other: "AutoRestartInfo") -> bool:
-        return (
-            self.custom_model != other.custom_model
-            or self.device != other.device
-            or self.custom_name != other.custom_name
-            or self.custom_path != other.custom_path
-            or self.pretrained_name != other.pretrained_name
-            or self.pretrained_path != other.pretrained_path
-        )
+    def is_changed(self, deploy_params: dict) -> bool:
+        return self.deploy_params != deploy_params
 
 
 class Inference:
@@ -208,6 +154,7 @@ class Inference:
                 model_dir = os.path.join(get_data_dir(), "models")
         sly_fs.mkdir(model_dir)
 
+        self.autorestart = None
         self.device: str = None
         self.runtime: str = None
         self.model_precision: str = None
@@ -335,7 +282,6 @@ class Inference:
             base_folder=sly_env.smart_cache_container_dir(),
             log_progress=True,
         )
-        self.autorestart = None
 
     def get_batch_size(self):
         if self.max_batch_size is not None:
@@ -409,6 +355,9 @@ class Inference:
                 self._user_layout_card.unlock()
 
     def set_params_to_gui(self, deploy_params: dict) -> None:
+        """
+        Set params for load_model method to GUI.
+        """
         if isinstance(self.gui, GUI.ServingGUI):
             self._user_layout_card.hide()
             self._api_request_model_info.set_text(json.dumps(deploy_params), "json")
@@ -450,8 +399,12 @@ class Inference:
             restore_default_button=False,
             auto_format=True,
         )
+        if self.autorestart:
+            title = "Model was deployed during auto restart with the following settings"
+        else:
+            title = "Model was deployed from API request with the following settings"
         self._api_request_model_layout = Card(
-            title="Model was deployed from API request with the following settings",
+            title=title,
             content=self._api_request_model_info,
         )
         self._api_request_model_layout.hide()
@@ -759,6 +712,16 @@ class Inference:
         self.load_model(**deploy_params)
         self._model_served = True
         self._deploy_params = deploy_params
+        try:
+            if self.autorestart is None:
+                logger.debug("Autorestart info is not set. Creating new one.")
+                self.autorestart = AutoRestartInfo(self._deploy_params)
+                self.api.task.set_fields(self._task_id, self.autorestart.generate_fields())
+            elif self.autorestart.is_changed(self._deploy_params):
+                logger.debug("Autorestart info is changed. Updating.")
+                self.autorestart.deploy_params.update(self._deploy_params)
+        except Exception as e:
+            logger.warning(f"Failed to update autorestart info: {repr(e)}")
         if self.gui is not None:
             self.update_gui(self._model_served)
             self.gui.show_deployed_model_info(self)
@@ -2502,6 +2465,25 @@ class Inference:
             future.add_done_callback(end_callback)
         logger.debug("Scheduled task.", extra={"inference_request_uuid": inference_request_uuid})
 
+    def _deploy_on_autorestart(self):
+        try:
+            deploy_params = self.autorestart.deploy_params
+            if isinstance(self.gui, GUI.ServingGUITemplate):
+                model_files = self._download_model_files(deploy_params)
+                deploy_params["model_files"] = model_files
+                self._load_model_headless(**deploy_params)
+            elif isinstance(self.gui, GUI.ServingGUI):
+                self._load_model(deploy_params)
+
+            self.set_params_to_gui(deploy_params)
+            # update to set correct device
+            device = deploy_params.get("device", "cpu")
+            self.gui.set_deployed(device)
+            return {"result": "model was successfully deployed"}
+        except Exception as e:
+            self.gui._success_label.hide()
+            raise e
+
     def serve(self):
         if not self._use_gui and not self._is_local_deploy:
             Progress("Deploying model ...", 1)
@@ -2536,18 +2518,6 @@ class Inference:
                 self._inference_by_local_deploy_args()
                 exit(0)
 
-        if self._task_id is not None:
-            logger.debug("Checking autorestart info...")
-            response = self._api.task.get_field(
-                self._task_id, AutoRestartInfo.Fields.AUTO_RESTART_INFO
-            )
-            self.autorestart = AutoRestartInfo.from_response(response)
-            if self.autorestart is not None:
-                logger.debug("Autorestart info:", extra=asdict(self.autorestart))
-                # TODO adjust params for deployment
-            else:
-                logger.debug("Autorestart info is not set.")
-
         if isinstance(self.gui, GUI.InferenceGUI):
             self._app = Application(layout=self.get_ui())
         elif isinstance(self.gui, GUI.ServingGUI):
@@ -2556,6 +2526,21 @@ class Inference:
         #     self._app = Application(layout=self.get_ui())
         else:
             self._app = Application(layout=self.get_ui())
+
+        try:
+            if self._task_id is not None:
+                logger.debug("Checking autorestart info...")
+                response = self.api.task.get_fields(
+                    self._task_id, [AutoRestartInfo.Fields.AUTO_RESTART_INFO]
+                )
+                self.autorestart = AutoRestartInfo.from_response(response)
+                if self.autorestart is not None:
+                    logger.debug("Autorestart info:", extra=self.autorestart.deploy_params)
+                    self._deploy_on_autorestart()
+                else:
+                    logger.debug("Autorestart info is not set.")
+        except Exception:
+            logger.error("Autorestart failed.", exc_info=True)
 
         server = self._app.get_server()
         self._app.set_ready_check_function(self.is_model_deployed)
@@ -2581,14 +2566,6 @@ class Inference:
             Progress("Model deployed", 1).iter_done_report()
         else:
             autostart_func()
-
-        if self.autorestart is None:
-            logger.debug("Autorestart info is not set. Creating new one.")
-            self.autorestart = AutoRestartInfo()  # TODO fullfill with values
-            self._api.task.set_fields(self._task_id, self.autorestart.generate_fields())
-        elif self.autorestart.is_changed():
-            logger.debug("Autorestart info is changed.")
-            # TODO handle changes
 
         self.cache.add_cache_endpoint(server)
         self.cache.add_cache_files_endpoint(server)
