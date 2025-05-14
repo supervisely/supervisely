f2104e6da23aaf48bd79cc2f83a791058339e671
Nikolai Petukhov <petukhov.niko@gmail.com>
2025-05-14 20:16:41 -0300

    temp

--- setup.py
+++ setup.py
@@ -49,6 +49,7 @@ def get_release_commit(tag: str):
 
 
 def get_version():
+    return "6.73.357+deploy.api"
     version = os.getenv("RELEASE_VERSION", None)
     if version is not None:
         return version
