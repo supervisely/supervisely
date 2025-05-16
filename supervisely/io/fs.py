5eb1d94d0f0755c5af53ccbd1a6fcacfe1a29647
Nikolai Petukhov <petukhov.niko@gmail.com>
2025-05-16 10:40:39 -0300

    copy_dir_recursively fix

--- supervisely/io/fs.py
+++ supervisely/io/fs.py
@@ -1153,8 +1153,7 @@ def download(
 def copy_dir_recursively(
     src_dir: str, dst_dir: str, progress_cb: Optional[Union[tqdm, Callable]] = None
 ) -> List[str]:
-    mkdir(dst_dir)
-    files = list_files_recursively(src_dir)
+    files = list_dir_recursively(src_dir, include_subdirs=True)
     for src_file_path in files:
         dst_file_path = os.path.normpath(src_file_path.replace(src_dir, dst_dir))
         ensure_base_path(dst_file_path)
