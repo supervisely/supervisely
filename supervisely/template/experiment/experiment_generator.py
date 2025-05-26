afeb99e91a401302e5c9b632a325c33511dce7ba
cxnt <pavelbartsits@gmail.com>
2025-05-26 22:38:01 +0400

    refactor ExperimentGenerator and update experiment template: add support for best and optimized checkpoints, integrate sample prediction gallery, and improve Markdown formatting

--- supervisely/template/experiment/experiment_generator.py
+++ supervisely/template/experiment/experiment_generator.py
@@ -16,6 +16,7 @@ from supervisely.project import ProjectMeta
 from supervisely.template.base_generator import BaseGenerator
 
 
+# @TODO: Partly supports unreleased apps
 class ExperimentGenerator(BaseGenerator):
 
     def __init__(
@@ -59,7 +60,7 @@ class ExperimentGenerator(BaseGenerator):
 
     def state(self) -> dict:
         return {}
-    
+
     def context(self) -> dict:
         exp_name = self.info["experiment_name"]
         model_name = self.info["model_name"]
@@ -73,7 +74,7 @@ class ExperimentGenerator(BaseGenerator):
         project_train_size = self.info["train_size"]
         project_val_size = self.info["val_size"]
         model_classes = [cls.name for cls in self.model_meta.obj_classes]
-        
+
         date = self._get_date()
         metrics = self._generate_metrics_table()
         checkpoints = self._generate_checkpoints_table()
@@ -82,9 +83,16 @@ class ExperimentGenerator(BaseGenerator):
         experiment_dir = os.path.basename(artifacts_dir)
         docker_image = self._get_docker_image()
         repo_info = self._get_repository_info()
-        has_onnx, has_tensorrt, optimized_checkpoint = self._get_optimized_checkpoint()
+
+        best_checkpoint = self._get_best_checkpoint()
+        onnx_checkpoint, trt_checkpoint = self._get_optimized_checkpoints()
+
+        sample_pred_gallery = self.get_sample_predictions_gallery()
 
         context = {
+            "env": {
+                "server_address": self.api.server_address,
+            },
             "experiment": {
                 "name": exp_name,
                 "model_name": model_name,
@@ -106,7 +114,11 @@ class ExperimentGenerator(BaseGenerator):
             "links": {
                 "training_session": {
                     "id": self.info.get("task_id"),
-                    "url": f"{self.api.server_address}/apps/sessions/{self.info.get('task_id')}" if self.info.get("task_id") else None,
+                    "url": (
+                        f"{self.api.server_address}/apps/sessions/{self.info.get('task_id')}"
+                        if self.info.get("task_id")
+                        else None
+                    ),
                 },
                 "evaluation_report": {
                     "id": self.info.get("evaluation_report_id"),
@@ -114,11 +126,19 @@ class ExperimentGenerator(BaseGenerator):
                 },
                 "tensorboard_logs": {
                     "path": self.info.get("logs", {}).get("link", None),
-                    "url": f"{self.api.server_address}/files/?path={self.info.get('logs', {}).get('link')}" if self.info.get("logs", {}).get("link") else None,
+                    "url": (
+                        f"{self.api.server_address}/files/?path={self.info.get('logs', {}).get('link')}"
+                        if self.info.get("logs", {}).get("link")
+                        else None
+                    ),
                 },
                 "team_files": {
                     "path": artifacts_dir,
-                    "url": f"{self.api.server_address}/files/?path={self.artifacts_dir}" if self.artifacts_dir else None,
+                    "url": (
+                        f"{self.api.server_address}/files/?path={self.artifacts_dir}"
+                        if self.artifacts_dir
+                        else None
+                    ),
                 },
                 "checkpoint_dir_url": f"{self.api.server_address}/files/?path={self.artifacts_dir}",
             },
@@ -127,8 +147,9 @@ class ExperimentGenerator(BaseGenerator):
                 "metrics_table": metrics,
                 "hyperparameters": hyperparameters,
                 "experiment_dir": experiment_dir,
-                "best_checkpoint": self.info["best_checkpoint"],
-                "optimized_checkpoint": optimized_checkpoint,
+                "best_checkpoint": best_checkpoint,
+                "onnx_checkpoint": onnx_checkpoint,
+                "trt_checkpoint": trt_checkpoint,
             },
             "code": {
                 "docker": {
@@ -141,9 +162,8 @@ class ExperimentGenerator(BaseGenerator):
                     "serving_class": self.serving_class.__name__ if self.serving_class else None,
                 },
             },
-            "buttons": {
-                "has_onnx": has_onnx,
-                "has_tensorrt": has_tensorrt,
+            "widgets": {
+                "sample_pred_gallery": sample_pred_gallery,
             },
         }
 
@@ -151,7 +171,7 @@ class ExperimentGenerator(BaseGenerator):
 
     def _generate_metrics_table(self) -> str:
         """Generate HTML table with evaluation metrics.
-        
+
         :returns: HTML string with metrics table
         :rtype: str
         """
@@ -174,7 +194,7 @@ class ExperimentGenerator(BaseGenerator):
 
     def _generate_checkpoints_table(self) -> str:
         """Generate HTML table with checkpoint information.
-        
+
         :returns: HTML string with checkpoints table
         :rtype: str
         """
@@ -185,15 +205,18 @@ class ExperimentGenerator(BaseGenerator):
         checkpoints = pytorch_checkpoints.copy()
         export = self.info.get("export", {})
         if export:
-            for runtime_type in [RuntimeType.ONNXRUNTIME, RuntimeType.TENSORRT]:
-                if checkpoint := export.get(runtime_type):
-                    checkpoints.append(checkpoint)
+            onnx_checkpoint = export.get(RuntimeType.ONNXRUNTIME)
+            trt_checkpoint = export.get(RuntimeType.TENSORRT)
+            if onnx_checkpoint is not None:
+                checkpoints.append(onnx_checkpoint)
+            if trt_checkpoint is not None:
+                checkpoints.append(trt_checkpoint)
 
         checkpoint_paths = [os.path.join(self.artifacts_dir, ckpt) for ckpt in checkpoints]
         checkpoint_infos = [
             self.api.file.get_info_by_path(self.team_id, path) for path in checkpoint_paths
         ]
-        
+
         checkpoint_sizes = [f"{info.sizeb / 1024 / 1024:.2f} MB" for info in checkpoint_infos]
         checkpoint_dl_links = [
             f"<a href='{info.full_storage_url}' download='{sly_fs.get_file_name_with_ext(info.path)}'>Download</a>"
@@ -214,7 +237,7 @@ class ExperimentGenerator(BaseGenerator):
 
     def _generate_hyperparameters_yaml(self) -> str:
         """Return hyperparameters as YAML string.
-        
+
         :returns: YAML string with hyperparameters
         :rtype: str
         """
@@ -227,7 +250,7 @@ class ExperimentGenerator(BaseGenerator):
 
     def _get_app_info(self):
         """Get app information from task.
-        
+
         :returns: App info object or None if not found
         :rtype: Optional[Any]
         """
@@ -238,7 +261,7 @@ class ExperimentGenerator(BaseGenerator):
 
     def _get_date(self) -> str:
         """Format experiment date.
-        
+
         :returns: Formatted date string
         :rtype: str
         """
@@ -252,24 +275,69 @@ class ExperimentGenerator(BaseGenerator):
                 pass
         return date
 
-    def _get_optimized_checkpoint(self) -> Tuple[bool, bool, Optional[str]]:
+    def _get_best_checkpoint(self) -> dict:
+        """Get best checkpoint filename.
+
+        :returns: Best checkpoint info
+        :rtype: dict
+        """
+        best_checkpoint_path = os.path.join(
+            self.artifacts_dir, "checkpoints", self.info["best_checkpoint"]
+        )
+        best_checkpoint = {
+            "name": self.info["best_checkpoint"],
+            "path": best_checkpoint_path,
+            "url": self.api.file.get_info_by_path(
+                self.team_id, best_checkpoint_path
+            ).full_storage_url,
+        }
+        return best_checkpoint
+
+    def _get_optimized_checkpoints(self) -> Tuple[dict, dict]:
         """Get optimized checkpoint filename (ONNX or TensorRT).
-        
-        :returns: Checkpoint filename or None if not available
-        :rtype: Optional[str]
+
+        :returns: Checkpoint info or None if not available
+        :rtype: Optional[dict]
         """
         export = self.info.get("export", {})
-        has_onnx, has_tensorrt, optimized_checkpoint = False, False, None
-        if export:
-            if optimized_checkpoint := export.get(RuntimeType.TENSORRT):
-                has_tensorrt = True
-            if optimized_checkpoint := export.get(RuntimeType.ONNXRUNTIME):
-                has_onnx = True
-        return has_onnx, has_tensorrt, optimized_checkpoint
+
+        onnx_checkpoint = export.get(RuntimeType.ONNXRUNTIME)
+        onnx_checkpoint = {
+            "name": None,
+            "path": None,
+            "url": None,
+        }
+        if onnx_checkpoint is not None:
+            onnx_checkpoint_path = os.path.join(
+                self.artifacts_dir, export.get(RuntimeType.ONNXRUNTIME)
+            )
+            onnx_checkpoint = {
+                "name": os.path.basename(export.get(RuntimeType.ONNXRUNTIME)),
+                "path": onnx_checkpoint_path,
+                "url": self.api.file.get_info_by_path(
+                    self.team_id, onnx_checkpoint_path
+                ).full_storage_url,
+            }
+        trt_checkpoint = export.get(RuntimeType.TENSORRT)
+        trt_checkpoint = {
+            "name": None,
+            "path": None,
+            "url": None,
+        }
+        if trt_checkpoint is not None:
+            trt_checkpoint_path = os.path.join(self.artifacts_dir, export.get(RuntimeType.TENSORRT))
+            trt_checkpoint = {
+                "name": os.path.basename(export.get(RuntimeType.TENSORRT)),
+                "path": trt_checkpoint_path,
+                "url": self.api.file.get_info_by_path(
+                    self.team_id, trt_checkpoint_path
+                ).full_storage_url,
+            }
+        return onnx_checkpoint, trt_checkpoint
 
     def _get_docker_image(self) -> str:
         """Get Docker image for model.
-        
+
         :returns: Docker image name
         :rtype: str
         """
@@ -280,7 +348,7 @@ class ExperimentGenerator(BaseGenerator):
 
     def _get_repository_info(self) -> Dict[str, str]:
         """Get repository information.
-        
+
         :returns: Dictionary with repo URL and name
         :rtype: Dict[str, str]
         """
@@ -294,31 +362,72 @@ class ExperimentGenerator(BaseGenerator):
         repo_name = framework_name.replace(" ", "-")
         repo_link = f"https://github.com/supervisely-ecosystem/{repo_name}"
         return {"url": repo_link, "name": repo_name}
-    
+
     # @TODO: method not used (might be helpful for unreleased apps)
     def _find_app_config(self):
         """
         Find app config.json in project structure.
-        
+
         :returns: Config dictionary or None if not found
         :rtype: Optional[Dict[str, Any]]
         """
         try:
             current_dir = Path(os.path.abspath(os.path.dirname(__file__)))
             root_dir = current_dir
-            
+
             while root_dir.parent != root_dir:
                 config_path = root_dir / "supervisely_integration" / "train" / "config.json"
                 if config_path.exists():
                     break
                 root_dir = root_dir.parent
-                
+
             config_path = root_dir / "supervisely_integration" / "train" / "config.json"
             if config_path.exists():
                 return sly_json.load_json_file(config_path)
-                
+
         except Exception as e:
             logger.warning(f"Failed to load config.json: {e}")
             return None
+
+    def get_sample_predictions_gallery(self):
+        benchmark_file_info = self.api.file.get_info_by_id(self.info["evaluation_report_id"])
+        evaluation_report_path = os.path.dirname(benchmark_file_info.path)
+        if os.path.basename(evaluation_report_path) != "visualizations":
+            logger.debug(
+                f"Visualizations directory is not found in the report directory: '{evaluation_report_path}'"
+            )
+            return None
+
+        evaluation_report_data_path = os.path.join(evaluation_report_path, "data")
+
+        seek_file = "explore_predictions_gallery_widget"
+        remote_gallery_widget_json = None
+        for file in self.api.file.list(
+            self.team_id, evaluation_report_data_path, False, "fileinfo"
+        ):
+            if file.name.startswith(seek_file) and file.name.endswith(".json"):
+                remote_gallery_widget_json = file.path
+                break
+
+        if remote_gallery_widget_json is None:
+            logger.debug(
+                f"Gallery widget is not found in the report directory: '{evaluation_report_path}'"
+            )
+            return None
+
+        save_path = os.path.join(
+            self.output_dir, "data", "explore_predictions_gallery_widget_expmt.json"
+        )
+        self.api.file.download(self.team_id, remote_gallery_widget_json, save_path)
+
+        widget_html = """
+<sly-iw-gallery ref="gallery_widget_expmt" iw-widget-id="gallery_widget_expmt" :options="{'isModalWindow': false}"
+        :actions="{
+        'init': {
+            'dataSource': '/data/explore_predictions_gallery_widget_expmt.json',
+        },
         
-# @TODO: Partly supports unreleased apps
+    }" :command="command" :data="data">        
+    </sly-iw-gallery>
+    """
+        return widget_html
