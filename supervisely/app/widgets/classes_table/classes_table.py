a59ab9dfa1e411d2f27b5d96992f6002b6b22cb0
Almaz <79905215+almazgimaev@users.noreply.github.com>
2025-06-10 15:48:02 +0200

    small changes

--- supervisely/app/widgets/classes_table/classes_table.py
+++ supervisely/app/widgets/classes_table/classes_table.py
@@ -412,8 +412,8 @@ class ClassesTable(Widget):
         :rtype: List[str]
         """
         classes = []
+        checkboxes = StateJson()[self.widget_id]["checkboxes"]
         for i, line in enumerate(self._table_data):
-            checkboxes = StateJson()[self.widget_id]["checkboxes"]
             if len(checkboxes) == 0:
                 checkboxes = [False] * len(self._table_data)
             if i >= len(checkboxes):
