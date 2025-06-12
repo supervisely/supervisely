34562aafe4157af9578ca945c6949ec4d21d2611
Nikolai Petukhov <petukhov.niko@gmail.com>
2025-06-12 00:02:22 -0300

    Fix exception type in version retrieval for accuracy

--- supervisely/__init__.py
+++ supervisely/__init__.py
@@ -5,7 +5,7 @@ import os
 
 try:
     __version__ = pkg_resources.require("supervisely")[0].version
-except Exception as e:
+except TypeError as e:
     __version__ = "development"
 
 from supervisely.sly_logger import (
