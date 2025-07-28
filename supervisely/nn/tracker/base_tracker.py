65ee2cc54df46643a6774cb2b9b621c0048c3532
max-unfinity <poldersen2@gmail.com>
2025-07-28 16:46:31 +0000

    add _create_video_annotation

--- supervisely/nn/tracker/base_tracker.py
+++ supervisely/nn/tracker/base_tracker.py
@@ -23,3 +23,39 @@ class BaseTracker:
             video_annotation.add(annotation)
         
         return video_annotation
+    
+    def _create_video_annotation(
+        frame_to_annotation: dict,
+        tracking_results: list,
+        frame_shape: tuple,
+        cat2obj: dict,
+    ):
+        img_h, img_w = frame_shape
+        video_objects = {}  # track_id -> VideoObject
+        frames = []
+        for (i, ann), tracks in zip(frame_to_annotation.items(), tracking_results):
+            frame_figures = []
+            for track in tracks:
+                # crop bbox to image size
+                dims = np.array([img_w, img_h, img_w, img_h]) - 1
+                track[:4] = np.clip(track[:4], 0, dims)
+                x1, y1, x2, y2, track_id, conf, cat = track[:7]
+                cat = int(cat)
+                track_id = int(track_id)
+                rect = sly.Rectangle(y1, x1, y2, x2)
+                video_object = video_objects.get(track_id)
+                if video_object is None:
+                    obj_cls = cat2obj[cat]
+                    video_object = sly.VideoObject(obj_cls)
+                    video_objects[track_id] = video_object
+                frame_figures.append(sly.VideoFigure(video_object, rect, i))
+            frames.append(sly.Frame(i, frame_figures))
+
+        objects = list(video_objects.values())
+        video_ann = sly.VideoAnnotation(
+            img_size=frame_shape,
+            frames_count=len(frame_to_annotation),
+            objects=sly.VideoObjectCollection(objects),
+            frames=sly.FrameCollection(frames),
+        )
+        return video_ann
