4f7bf9757db045ed9a76134f1ed456c4e3083458
unfinity <poldersen2@gmail.com>
2025-08-21 16:20:32 +0500

    pseudo code for base methods

---
+++ supervisely/nn/tracker/utils.py
@@ -0,0 +1,8 @@
+from typing import List, Optional
+from supervisely.nn.model.prediction import Prediction
+
+
+def predictions_to_video_annotation(
+    predictions: List[Prediction],
+):
+    pass
---
+++ supervisely/nn/tracker/visualize.py
@@ -0,0 +1,5 @@
+def visualize(predictions, output_path, ...):
+    pass
+
+def visualize_video_annotation(video_annotation, output_path, ...):
+    pass
