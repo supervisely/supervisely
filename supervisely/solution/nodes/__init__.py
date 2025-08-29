8c93324ce7f44c5515945a29e6efd012d8d4c461
almaz <almazgimaev@gmail.com>
2025-08-29 19:09:17 +0200

    feat: add EmailNotificationNode with GUI and history tracking for email notifications

--- supervisely/solution/nodes/__init__.py
+++ supervisely/solution/nodes/__init__.py
@@ -2,6 +2,8 @@ from supervisely.solution.nodes.all_experiments.node import AllExperimentsNode
 from supervisely.solution.nodes.auto_import.node import AutoImportNode
 from supervisely.solution.nodes.cloud_import.node import CloudImportNode
 from supervisely.solution.nodes.compare_models.node import CompareModelsNode
+from supervisely.solution.nodes.data_versioning.node import DataVersioningNode
+from supervisely.solution.nodes.email_notification.node import EmailNotificationNode
 from supervisely.solution.nodes.evaluation.node import EvaluationNode
 from supervisely.solution.nodes.evaluation_report.node import EvaluationReportNode
 from supervisely.solution.nodes.input_project.node import InputProjectNode
@@ -11,14 +13,13 @@ from supervisely.solution.nodes.labeling_performance.node import (
 from supervisely.solution.nodes.labeling_project.node import LabelingProjectNode
 from supervisely.solution.nodes.labeling_queue.node import LabelingQueueNode
 from supervisely.solution.nodes.move_labeled.node import MoveLabeledNode
+from supervisely.solution.nodes.pretrained_models.node import PretrainedModelsNode
 from supervisely.solution.nodes.qa_stats.node import QAStatsNode
 from supervisely.solution.nodes.smart_sampling.ai_index import AiIndexNode
 from supervisely.solution.nodes.smart_sampling.clip_service import OpenAIClipServiceNode
 from supervisely.solution.nodes.smart_sampling.node import SmartSamplingNode
 from supervisely.solution.nodes.train_val_split.node import TrainValSplitNode
 from supervisely.solution.nodes.training_project.node import TrainingProjectNode
-from supervisely.solution.nodes.data_versioning.node import DataVersioningNode
-from supervisely.solution.nodes.pretrained_models.node import PretrainedModelsNode
 
 # from supervisely.solution.components.video_samling import VideoSampling
 
@@ -39,6 +40,7 @@ __all__ = [
     "OpenAIClipServiceNode",
     "DataVersioningNode",
     "PretrainedModelsNode",
+    "EmailNotificationNode",
     # "VideoSampling",
     "AllExperimentsNode",
     "EvaluationNode",
