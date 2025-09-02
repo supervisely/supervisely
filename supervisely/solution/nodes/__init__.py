cc831535511fa06562b340be11a82284e2bd6ae7
cxnt <pavelbartsits@gmail.com>
2025-09-02 19:39:58 +0400

    remove TrainingMetricsNode and its associated files from the solution nodes, streamlining the training workflow components

--- supervisely/solution/nodes/__init__.py
+++ supervisely/solution/nodes/__init__.py
@@ -26,7 +26,6 @@ from supervisely.solution.nodes.train_val_split.node import TrainValSplitNode
 from supervisely.solution.nodes.training_project.node import TrainingProjectNode
 from supervisely.solution.nodes.training_experiment.node import TrainingExperimentNode
 from supervisely.solution.nodes.training_artifacts.node import TrainingArtifactsNode
-from supervisely.solution.nodes.training_metrics.node import TrainingMetricsNode
 from supervisely.solution.nodes.training_evaluation.node import TrainingEvaluationReportNode
 
 # from supervisely.solution.components.video_samling import VideoSampling
@@ -56,7 +55,6 @@ __all__ = [
     "PretrainedModelsNode",
     "TrainingExperimentNode",
     "TrainingArtifactsNode",
-    "TrainingMetricsNode",
     "TrainingEvaluationReportNode",
     # Prediction
     "DeployPretrainedModelNode",
--- supervisely/solution/nodes/training_metrics/__init__.py
+++
@@ -1,1 +0,0 @@
-from .node import TrainingMetricsNode
