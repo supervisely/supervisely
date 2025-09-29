404d09d6e0b87bd64cd94d14024ec05c897fbbc3
Nikolai Petukhov <petukhov.niko@gmail.com>
2025-09-29 11:17:39 -0300

    Remove hardcoded version string from get_version function

--- setup.py
+++ setup.py
@@ -49,7 +49,6 @@ def get_release_commit(tag: str):
 
 
 def get_version():
-    return "v6.73.438+predict.app.fixes.niko"
     version = os.getenv("RELEASE_VERSION", None)
     if version is not None:
         return version
