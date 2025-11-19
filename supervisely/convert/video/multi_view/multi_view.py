0032398a80b9b352a2a7af0952dd8778316527c6
cxnt <pavelbartsits@gmail.com>
2025-11-19 20:29:24 +0400

    Add multiview lableing interface support and converter for videos

---
+++ supervisely/convert/video/multi_view/multi_view.py
@@ -0,0 +1,130 @@
+import os
+from typing import List
+
+import supervisely.convert.video.sly.sly_video_helper as sly_video_helper
+from supervisely import ProjectMeta, VideoAnnotation, logger
+from supervisely.convert.base_converter import AvailableVideoConverters
+from supervisely.convert.video.video_converter import VideoConverter
+from supervisely.io.fs import JUNK_FILES, get_file_ext
+from supervisely.io.json import load_json_file
+from supervisely.project.project_settings import LabelingInterface
+from supervisely.video.video import validate_ext as validate_video_ext
+
+
+class MultiViewVideoConverter(VideoConverter):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._supports_links = True
+
+    def __str__(self) -> str:
+        return AvailableVideoConverters.MULTI_VIEW
+
+    @property
+    def ann_ext(self) -> str:
+        return ".json"
+
+    @property
+    def key_file_ext(self) -> str:
+        return ".json"
+
+    def validate_labeling_interface(self) -> bool:
+        return self._labeling_interface == LabelingInterface.MULTIVIEW
+
+    def generate_meta_from_annotation(self, ann_path: str, meta: ProjectMeta) -> ProjectMeta:
+        meta = sly_video_helper.get_meta_from_annotation(ann_path, meta)
+        return meta
+
+    def validate_ann_file(self, ann_path: str, meta: ProjectMeta) -> bool:
+        try:
+            ann_json = load_json_file(ann_path)
+            if "annotation" in ann_json:
+                ann_json = ann_json["annotation"]
+            VideoAnnotation.from_json(ann_json, meta)
+            return True
+        except Exception:
+            return False
+
+    def validate_key_file(self, key_file_path: str) -> bool:
+        try:
+            self._meta = ProjectMeta.from_json(load_json_file(key_file_path))
+            return True
+        except Exception:
+            return False
+
+    def validate_format(self) -> bool:
+        if self.upload_as_links and self._supports_links:
+            self._download_remote_ann_files()
+        detected_ann_cnt = 0
+        videos_list, ann_dict = [], {}
+        for root, _, files in os.walk(self._input_data):
+            # Skip optional metadata directories entirely
+            normalized_root = os.path.normpath(root)
+            if "metadata" in normalized_root.split(os.sep):
+                continue
+            for file in files:
+                full_path = os.path.join(root, file)
+                if file == "key_id_map.json":
+                    continue
+                if file == "meta.json":
+                    is_valid = self.validate_key_file(full_path)
+                    if is_valid:
+                        continue
+
+                ext = get_file_ext(full_path)
+                if file in JUNK_FILES:
+                    continue
+                elif ext in self.ann_ext:
+                    ann_dict[file] = full_path
+                else:
+                    try:
+                        validate_video_ext(ext)
+                        videos_list.append(full_path)
+                    except Exception:
+                        continue
+
+        if self._meta is not None:
+            meta = self._meta
+        else:
+            meta = ProjectMeta()
+
+        self._items = []
+        for image_path in videos_list:
+            item = self.Item(image_path)
+            ann_name = f"{item.name}.json"
+            if ann_name in ann_dict:
+                ann_path = ann_dict[ann_name]
+                if self._meta is None:
+                    meta = self.generate_meta_from_annotation(ann_path, meta)
+                is_valid = self.validate_ann_file(ann_path, meta)
+                if is_valid:
+                    item.ann_data = ann_path
+                    detected_ann_cnt += 1
+            self._items.append(item)
+        self._meta = meta
+        return len(self._items) > 0
+
+    def to_supervisely(
+        self,
+        item: VideoConverter.Item,
+        meta: ProjectMeta = None,
+        renamed_classes: dict = None,
+        renamed_tags: dict = None,
+    ) -> VideoAnnotation:
+        if meta is None:
+            meta = self._meta
+
+        if item.ann_data is None:
+            if self._upload_as_links:
+                return None
+            return item.create_empty_annotation()
+
+        try:
+            ann_json = load_json_file(item.ann_data)
+            if "annotation" in ann_json:
+                ann_json = ann_json["annotation"]
+            if renamed_classes or renamed_tags:
+                ann_json = sly_video_helper.rename_in_json(ann_json, renamed_classes, renamed_tags)
+            return VideoAnnotation.from_json(ann_json, meta)
+        except Exception as e:
+            logger.warning(f"Failed to convert annotation: {repr(e)}")
+            return item.create_empty_annotation()
