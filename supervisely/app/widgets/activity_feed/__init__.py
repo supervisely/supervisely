b22355d80cb7232586058aec550c4f28c8e284b1
iwatkot <iwatkot@gmail.com>
2025-12-08 10:45:17 +0100

    Cleaning up.

--- supervisely/app/widgets/activity_feed/__init__.py
+++ supervisely/app/widgets/activity_feed/__init__.py
@@ -1,3 +1,1 @@
-from supervisely.app.widgets.activity_feed.activity_feed import ActivityFeed
 
-__all__ = ["ActivityFeed"]
--- supervisely/app/widgets/modal/__init__.py
+++ supervisely/app/widgets/modal/__init__.py
@@ -1,3 +1,1 @@
-from supervisely.app.widgets.modal.modal import Modal
 
-__all__ = ["Modal"]
