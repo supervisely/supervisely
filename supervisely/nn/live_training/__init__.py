60fdcb15bd91624724a82dfcfe05ceb0005522f9
Олег <bragin1703@gmail.com>
2026-01-07 13:57:06 +0000

    fix checkpoint utils init

--- supervisely/nn/live_training/__init__.py
+++ supervisely/nn/live_training/__init__.py
@@ -1,22 +1,18 @@
 from .loss_plateau_detector import LossPlateauDetector
 from .request_queue import RequestQueue, RequestType
-from .checkpoint_utils import (
-    resolve_checkpoint,
-    download_checkpoint_from_team_files,
-)
 from .artifacts_uploader import OnlineTrainingArtifactsUploader
 from .live_training import LiveTraining
 from .incremental_dataset import IncrementalDataset
 from .dynamic_sampler import DynamicSampler
+from .checkpoint_utils import restore_dataset_from_checkpoint
 
 __all__ = [
     'LossPlateauDetector',
     'RequestQueue',
     'RequestType',
-    'resolve_checkpoint',
-    'download_checkpoint_from_team_files',
     'OnlineTrainingArtifactsUploader',
     'LiveTraining',
     'IncrementalDataset',
-    'DynamicSampler'
+    'DynamicSampler',
+    'restore_dataset_from_checkpoint'
 ]
