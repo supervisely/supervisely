25eaeb733efe446a63228c4a5ceff0e5f6d7ea0d
Олег <bragin1703@gmail.com>
2026-01-19 16:03:45 +0000

    rm wait for starts from checkpoint mode

--- supervisely/nn/live_training/live_training.py
+++ supervisely/nn/live_training/live_training.py
@@ -158,7 +158,7 @@ class LiveTraining:
         self.load_state(state)
         image_ids = state.get('image_ids', [])
 
-        self._wait_for_start()
+        # self._wait_for_start()
         if image_ids:
             self._restore_dataset(image_ids)
         else:
