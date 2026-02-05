315dee9dab0dd9971c98911a7c37a472e94f3923
Nikolai Petukhov <petukhov.niko@gmail.com>
2026-02-05 03:52:51 -0300

    Update the logic in _get_sample_data

--- supervisely/nn/benchmark/comparison/detection_visualization/vis_metrics/explore_predictions.py
+++ supervisely/nn/benchmark/comparison/detection_visualization/vis_metrics/explore_predictions.py
@@ -1,9 +1,13 @@
+from ast import Dict
 from typing import List, Tuple
 
 from supervisely.annotation.annotation import Annotation
+from supervisely.api.api import Api
+from supervisely.api.dataset_api import DatasetInfo
 from supervisely.api.image_api import ImageInfo
 from supervisely.api.module_api import ApiField
 from supervisely.nn.benchmark.base_visualizer import BaseVisMetrics
+from supervisely.nn.benchmark.visualization.evaluation_result import EvalResult
 from supervisely.nn.benchmark.visualization.widgets import GalleryWidget, MarkdownWidget
 from supervisely.project.project_meta import ProjectMeta
 
@@ -43,40 +47,69 @@ class ExplorePredictions(BaseVisMetrics):
         annotations = []
         metas = [self.eval_results[0].gt_project_meta]
         skip_tags_filtering = []
-        api = self.eval_results[0].api
+        api: Api = self.eval_results[0].api
         min_conf = float("inf")
-        names = None
-        ds_name = None
+        sample_images: List[ImageInfo] = [] # from 1st dt project
+        sample_annotations: List[Annotation] = []
+        sample_infos_by_dataset: Dict[str, List[ImageInfo]] = {}
         for idx, eval_res in enumerate(self.eval_results):
-            if idx == 0:
-                dataset_info = eval_res.gt_dataset_infos[0]
-                image_infos = api.image.get_list(dataset_info.id, limit=5, force_metadata_for_links=False)
-                ds_name = dataset_info.name
-                images_ids = [image_info.id for image_info in image_infos]
-                names = [image_info.name for image_info in image_infos]
-                images.append(image_infos)
-                anns = api.annotation.download_batch(dataset_info.id, images_ids, force_metadata_for_links=False)
-                annotations.append(anns)
-                skip_tags_filtering.append(True)
             metas.append(eval_res.pred_project_meta)
-            assert ds_name is not None, "Failed to get GT dataset name for gallery"
-
-            dataset_info = api.dataset.get_info_by_name(eval_res.pred_project_id, ds_name)
-
-            assert names is not None, "Failed to get GT image names for gallery"
-            image_infos = eval_res.api.image.get_list(
-                dataset_info.id,
-                filters=[
-                    {ApiField.FIELD: ApiField.NAME, ApiField.OPERATOR: "in", ApiField.VALUE: names}
-                ],
-                force_metadata_for_links=False,
-            )
-            images_ids = [image_info.id for image_info in image_infos]
-            images.append(image_infos)
-            anns = eval_res.api.annotation.download_batch(dataset_info.id, images_ids, force_metadata_for_links=False)
-            annotations.append(anns)
-            skip_tags_filtering.append(False)
             min_conf = min(min_conf, eval_res.mp.conf_threshold)
+            this_eval_res_images = []
+            this_eval_res_annotations = []
+            if idx == 0:
+                for dataset_info in eval_res.dt_dataset_infos:    
+                    batch_images = api.image.get_list(dataset_info.id, limit=5, force_metadata_for_links=False)
+                    if len(batch_images) == 0:
+                        continue
+
+                    gt_dataset_info = api.dataset.get_info_by_name(eval_res.gt_project_id, name=dataset_info.name)
+                    assert gt_dataset_info is not None, f"Dataset {dataset_info.name} not found in GT project"
+                    
+                    gt_batch_images = api.image.get_list(gt_dataset_info.id, filters=[{ApiField.FIELD: ApiField.NAME, ApiField.OPERATOR: "in", ApiField.VALUE: [info.name for info in batch_images]}], force_metadata_for_links=False)
+                    assert len(gt_batch_images) == len(batch_images) is not None, "Failed to get GT images for gallery"
+                    
+                    sample_images.extend(gt_batch_images)
+                    gt_batch_annotations = api.annotation.download_batch(gt_dataset_info.id, [info.id for info in gt_batch_images], force_metadata_for_links=False)
+                    sample_annotations.extend(gt_batch_annotations)
+                    batch_anns = api.annotation.download_batch(dataset_info.id, [info.id for info in batch_images], force_metadata_for_links=False)
+                    this_eval_res_images.extend(batch_images)
+                    this_eval_res_annotations.extend(batch_anns)
+
+                    if len(sample_images) >= 5:
+                        sample_infos_by_dataset[dataset_info.name] = gt_batch_images[:-(len(sample_images)-5)]
+                        sample_images = sample_images[:5]
+                        sample_annotations = sample_annotations[:5]
+                        this_eval_res_images = this_eval_res_images[:5]
+                        this_eval_res_annotations = this_eval_res_annotations[:5]
+                        # gt
+                        images.append(sample_images)
+                        annotations.append(sample_annotations)
+                        skip_tags_filtering.append(True)
+                        break
+                    else:
+                        sample_infos_by_dataset[dataset_info.name] = gt_batch_images
+
+                assert len(sample_images) > 0, "No images found in the DT project"
+            else:
+                for dataset_name, gt_image_infos in sample_infos_by_dataset:
+                    dataset_info = api.dataset.get_info_by_name(eval_res.pred_project_id, dataset_name)
+                    assert dataset_info is not None, f"Dataset {dataset_name} not found in DT project"
+                    image_infos = eval_res.api.image.get_list(
+                        dataset_info.id,
+                        filters=[
+                            {ApiField.FIELD: ApiField.NAME, ApiField.OPERATOR: "in", ApiField.VALUE: [info.name for info in gt_image_infos]}
+                        ],
+                        force_metadata_for_links=False,
+                    )
+                    anns = eval_res.api.annotation.download_batch(dataset_info.id, [info.id for info in image_infos], force_metadata_for_links=False)
+                    this_eval_res_images.extend(image_infos)
+                    this_eval_res_annotations.extend(anns)
+                assert len(this_eval_res_images) == len(sample_images), "Failed to get DT images"
+
+            images.append(this_eval_res_images)
+            annotations.append(this_eval_res_annotations)
+            skip_tags_filtering.append(False)
 
         images = list(i for x in zip(*images) for i in x)
         annotations = list(i for x in zip(*annotations) for i in x)
