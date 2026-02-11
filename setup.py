9432cd54281b8c5b0b3523494941c7912f17f5c3
vorozhkog <vorozhkog@icloud.com>
2026-02-11 15:43:24 +0000

    temp: return version for tests

--- setup.py
+++ setup.py
@@ -49,6 +49,7 @@ def get_release_commit(tag: str):
 
 
 def get_version():
+    return "v6.73.530"
     version = os.getenv("RELEASE_VERSION", None)
     if version is not None:
         return version
