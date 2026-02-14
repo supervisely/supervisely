bd8e785f07b1fc013a9c6a42b2b56d7c68197bed
vorozhkog <vorozhkog@icloud.com>
2026-02-14 05:54:26 +0000

    Regenerated worker_api_pb2.py and worker_api_pb2_grpc.py with grpcio-tools; change protobuf constraints

--- setup.py
+++ setup.py
@@ -311,7 +311,7 @@ setup(
             "numpy>=1.19, <2.0.0",
         ],
         "agent": [
-            "protobuf>=3.19.5, <=3.20.3",
+            "protobuf>=6.33.5,<7.0.0",
         ],
         "versioning": [
             "pyarrow>=17.0.0, <=22.0.0",
