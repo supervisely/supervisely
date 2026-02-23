68e3c99392772da2cf6dfc0c15b90bbbc80d523f
cxnt <pavelbartsits@gmail.com>
2026-02-23 14:39:15 +0400

    remove debug version

--- setup.py
+++ setup.py
@@ -49,7 +49,6 @@ def get_release_commit(tag: str):
 
 
 def get_version():
-    return "0.0.0+readthedocs"
     version = os.getenv("RELEASE_VERSION", None)
     if version is not None:
         return version
