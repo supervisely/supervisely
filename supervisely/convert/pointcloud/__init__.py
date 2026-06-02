f26c5cea6404699113c31de76610f98f4f8e7174
Pavel Bartsits <48913536+cxnt@users.noreply.github.com>
2026-06-02 11:02:11 +0400

    Add converter for PCD with semantic labels (#1736)
    * Add coverter for PCD with semantic labels (ASPRS Standard Point Classes)
    
    * Fix label index handling in PCD reading functions to support missing counts and adjust label column indexing
    
    * Refactor label index handling in PCD reading functions to support dynamic label field names
    
    * Enhance PCDSemanticLabelsConverter to correctly identify labeled items and handle empty annotations
    
    * Disable support for links in PCDSemanticLabelsConverter

--- supervisely/convert/pointcloud/__init__.py
+++ supervisely/convert/pointcloud/__init__.py
@@ -2,6 +2,9 @@
 from supervisely.convert.pointcloud.sly.sly_pointcloud_converter import SLYPointcloudConverter
 from supervisely.convert.pointcloud.las.las_converter import LasConverter
 from supervisely.convert.pointcloud.ply.ply_converter import PlyConverter
+from supervisely.convert.pointcloud.pcd_semantic_labels.pcd_semantic_labels_converter import (
+    PCDSemanticLabelsConverter,
+)
 from supervisely.convert.pointcloud.bag.bag_converter import BagConverter
 from supervisely.convert.pointcloud.lyft.lyft_converter import LyftConverter
 from supervisely.convert.pointcloud.nuscenes_conv.nuscenes_converter import NuscenesConverter
