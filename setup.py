644b035ad939b19e75db3a90127df00afb2c922d
Oleg1703 <bragin1703@gmail.com>
2026-09-15 14:21:39 +0000

    Revert "Hardcode package version for testing on dev"
    This reverts commit 02de5e7a639684da8ff689583df2c10260347873.

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ def get_version():
     return version
 
 
-version = "6.99.999"  # hardcoded for testing fix-live-training-stop-upload-progress, was: get_version()
+version = get_version()
 
 
 INSTALL_REQUIRES = [
